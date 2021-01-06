1c033867e10925716dda3bcae43a9482b849ddfa
Ullrich Praetz <ullrich@praetz.de>
2021-01-06 16:10:15 +0100

    introduced Buddy & Hobby in example

--- Json.Tests/Common/Examples/ParseJsonExample.cs
+++ Json.Tests/Common/Examples/ParseJsonExample.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Friflo.Json.Burst;
 using NUnit.Framework;
 
@@ -9,36 +8,45 @@ namespace Friflo.Json.Tests.Common.Examples
 {
     public class ParseJsonExample
     {
+        static readonly string jsonString = @"
+{
+    ""firstName"":  ""John"",
+    ""age"":        24,
+    ""hobbies"":    [
+        {""name"":  ""Gaming"" },
+        {""name"":  ""STAR WARS""}
+    ]
+}";
+        
         [Test]
         public void ParseJson() {
-            string jsonString = @"
-{
-    ""name"":       ""John"",
-    ""age"":        24,
-    ""hobbies"":    [""Gaming"", ""STAR WARS""]
-}";
-            String  name = "";
-            int     age = 0;
-            var     hobbies = new List<string>();
+            Buddy buddy = new Buddy();
             
             JsonParser p = new JsonParser();
             Bytes json = new Bytes(jsonString); 
             p.InitParser(json);
-            p.NextEvent(); // JsonEvent.ObjectStart
+            p.NextEvent(); // expect JsonEvent.ObjectStart
 
-            ReadObject readRoot = new ReadObject();
-            while (readRoot.NextEvent(ref p)) {
-                if      (readRoot.UseStr(ref p, "name")) {
-                    name = p.value.ToString();
+            var readRoot = new ReadObject();
+            while (readRoot.NextEvent(ref p)) {                     // descend to root object 
+                if      (readRoot.UseStr(ref p, "firstName")) {
+                    buddy.name = p.value.ToString();
                 }
                 else if (readRoot.UseNum(ref p, "age")) {
-                    age = p.ValueAsInt(out _);
+                    buddy.age = p.ValueAsInt(out _);
                 }
-                else if (readRoot.UseArr(ref p, "hobbies")) { // descend to array node
-                    ReadArray readHobbies = new ReadArray();
-                    while (readHobbies.NextEvent(ref p)) {
-                        if (readHobbies.UseStr(ref p)) {
-                            hobbies.Add(p.value.ToString());
+                else if (readRoot.UseArr(ref p, "hobbies")) {       // descend to hobbies array
+                    var readHobbies = new ReadArray();
+                    while (readHobbies.NextEvent(ref p)) {          // iterate array elements
+                        if (readHobbies.UseObj(ref p)) {        
+                            var hobby = new Hobby();
+                            var readHobby = new ReadObject();
+                            while (readHobby.NextEvent(ref p)) {    // descend to hobby object 
+                                if (readHobby.UseStr(ref p, "name")) {
+                                    hobby.name = p.value.ToString();
+                                }
+                            }
+                            buddy.hobbies.Add(hobby);
                         }
                     }
                 }
@@ -46,10 +54,23 @@ namespace Friflo.Json.Tests.Common.Examples
             AreEqual(JsonEvent.EOF, p.NextEvent());
             if (p.error.ErrSet)
                 Fail(p.error.msg.ToString());
-            AreEqual("John",        name);
-            AreEqual(24,            age);
-            AreEqual("Gaming",      hobbies[0]);
-            AreEqual("STAR WARS",   hobbies[1]);
+            AreEqual("John",        buddy.name);
+            AreEqual(24,            buddy.age);
+            AreEqual("Gaming",      buddy.hobbies[0].name);
+            AreEqual("STAR WARS",   buddy.hobbies[1].name);
         }
+    }
+
+    public class Buddy
+    {
+        public          string       name;
+        public          int          age;
+        public readonly List<Hobby>  hobbies = new List<Hobby>();
+    }
+    
+    public struct Hobby
+    {
+        public string   name;
+        public int      passion;
     }
 }
