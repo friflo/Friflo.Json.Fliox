267e96651ae36a9e45331cb61f90c91e2b32106d
Ullrich Praetz <ullrich@praetz.de>
2021-01-06 16:37:56 +0100

    add Read functions example

--- Json.Tests/Common/Examples/ParseJsonExample.cs
+++ Json.Tests/Common/Examples/ParseJsonExample.cs
@@ -17,7 +17,16 @@ namespace Friflo.Json.Tests.Common.Examples
         {""name"":  ""STAR WARS""}
     ]
 }";
-        
+        public class Buddy {
+            public          string       firstName;
+            public          int          age;
+            public readonly List<Hobby>  hobbies = new List<Hobby>();
+        }
+    
+        public struct Hobby {
+            public string   name;
+        }
+
         [Test]
         public void ParseJson() {
             Buddy buddy = new Buddy();
@@ -26,22 +35,74 @@ namespace Friflo.Json.Tests.Common.Examples
             Bytes json = new Bytes(jsonString); 
             p.InitParser(json);
             p.NextEvent(); // expect JsonEvent.ObjectStart
-
+            
+            ReadBuddy(ref p, ref buddy);
+            
+            AreEqual(JsonEvent.EOF, p.NextEvent());
+            if (p.error.ErrSet)
+                Fail(p.error.msg.ToString());
+            AreEqual("John",        buddy.firstName);
+            AreEqual(24,            buddy.age);
+            AreEqual("Gaming",      buddy.hobbies[0].name);
+            AreEqual("STAR WARS",   buddy.hobbies[1].name);
+        }
+        
+        public static void ReadBuddy(ref JsonParser p, ref Buddy buddy) {
             var readRoot = new ReadObject();
-            while (readRoot.NextEvent(ref p)) {                     // descend to root object 
+            while (readRoot.NextEvent(ref p)) {                     // descend to root object & iterate key/values
                 if      (readRoot.UseStr(ref p, "firstName")) {
-                    buddy.name = p.value.ToString();
+                    buddy.firstName = p.value.ToString();
                 }
                 else if (readRoot.UseNum(ref p, "age")) {
                     buddy.age = p.ValueAsInt(out _);
                 }
-                else if (readRoot.UseArr(ref p, "hobbies")) {       // descend to hobbies array
+                else if (readRoot.UseArr(ref p, "hobbies")) {       
                     var readHobbies = new ReadArray();
-                    while (readHobbies.NextEvent(ref p)) {          // iterate array elements
+                    while (readHobbies.NextEvent(ref p)) {          // descend to hobbies array & iterate elements
+                        if (readHobbies.UseObj(ref p)) {        
+                            var hobby = new Hobby();
+                            ReadHobby(ref p, ref hobby);
+                            buddy.hobbies.Add(hobby);
+                        }
+                    }
+                }
+            }
+        }
+        
+        public static void ReadHobby(ref JsonParser p, ref Hobby hobby) {
+            var readHobby = new ReadObject();
+            while (readHobby.NextEvent(ref p)) {    // descend to hobby object & iterate key/values
+                if (readHobby.UseStr(ref p, "name")) {
+                    hobby.name = p.value.ToString();
+                }
+            }
+        }
+
+        // ------------- Demonstrating an anti pattern, which is not recommended -------------
+        [Test]
+        public void ParseJsonMonolith() {
+            Buddy buddy = new Buddy();
+            
+            JsonParser p = new JsonParser();
+            Bytes json = new Bytes(jsonString); 
+            p.InitParser(json);
+            p.NextEvent(); // expect JsonEvent.ObjectStart
+
+            var readRoot = new ReadObject();
+            while (readRoot.NextEvent(ref p)) {                     // descend to root object & iterate key/values
+                if      (readRoot.UseStr(ref p, "firstName")) {
+                    buddy.firstName = p.value.ToString();
+                }
+                else if (readRoot.UseNum(ref p, "age")) {
+                    buddy.age = p.ValueAsInt(out _);
+                }
+                else if (readRoot.UseArr(ref p, "hobbies")) {       
+                    var readHobbies = new ReadArray();
+                    while (readHobbies.NextEvent(ref p)) {          // descend to hobbies array & iterate elements
                         if (readHobbies.UseObj(ref p)) {        
                             var hobby = new Hobby();
                             var readHobby = new ReadObject();
-                            while (readHobby.NextEvent(ref p)) {    // descend to hobby object 
+                            while (readHobby.NextEvent(ref p)) {    // descend to hobby object & iterate key/values
                                 if (readHobby.UseStr(ref p, "name")) {
                                     hobby.name = p.value.ToString();
                                 }
@@ -54,23 +115,10 @@ namespace Friflo.Json.Tests.Common.Examples
             AreEqual(JsonEvent.EOF, p.NextEvent());
             if (p.error.ErrSet)
                 Fail(p.error.msg.ToString());
-            AreEqual("John",        buddy.name);
+            AreEqual("John",        buddy.firstName);
             AreEqual(24,            buddy.age);
             AreEqual("Gaming",      buddy.hobbies[0].name);
             AreEqual("STAR WARS",   buddy.hobbies[1].name);
         }
-    }
-
-    public class Buddy
-    {
-        public          string       name;
-        public          int          age;
-        public readonly List<Hobby>  hobbies = new List<Hobby>();
-    }
-    
-    public struct Hobby
-    {
-        public string   name;
-        public int      passion;
     }
 }
