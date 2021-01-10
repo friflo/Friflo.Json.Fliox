5794ff2cf017453b72353a17142463c3fcd3c5b6
Ullrich Praetz <ullrich@praetz.de>
2021-01-10 09:07:55 +0100

    JsonParser - check application error in UseElement() & UseMemberArr() methods

--- Json/Burst/JsonParserExt.cs
+++ Json/Burst/JsonParserExt.cs
@@ -14,192 +14,127 @@ namespace Friflo.Json.Burst
     public partial struct JsonParser
     {
         // ----------- object member checks -----------
-        [Conditional("DEBUG")]
-        private void AssertObject() {
+        private bool UseObject(JsonEvent expect, ref Str32 name) {
             int level = stateLevel;
             if (lastEvent == JsonEvent.ObjectStart || lastEvent == JsonEvent.ArrayStart)
                 level--;
             State curState = state[level];
-            if (curState == State.ExpectMember || curState == State.ExpectMemberFirst)
-                return;
-            throw new InvalidOperationException("Must call UseMember...() method only within an object");
+            if (curState == State.ExpectMember) {
+                if (lastEvent != expect || !key.IsEqual32(name))
+                    return false;
+                usedMember[level] = true;
+                return true;
+            }
+            return SetApplicationError("Must call UseMember...() method only within an object");
         }
         
         public bool UseMemberObj(ref Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ObjectStart || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel - 1] = true;
-            return true;
+            return UseObject(JsonEvent.ObjectStart, ref name);
         }
         
 #if JSON_BURST
         [Obsolete("Performance degradation by string copy > to avoid use the (ref FixedString32) version", false)]
 #endif
         public bool UseMemberObj(Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ObjectStart || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel - 1] = true;
-            return true;
+            return UseObject(JsonEvent.ObjectStart, ref name);
         }
+        
         // ---
         public bool UseMemberArr(ref Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ArrayStart || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel - 1] = true;
-            return true;
+            return UseObject(JsonEvent.ArrayStart, ref name);
         }
         
 #if JSON_BURST
         [Obsolete("Performance degradation by string copy > to avoid use the (ref FixedString32) version", false)]
 #endif
         public bool UseMemberArr(Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ArrayStart || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel - 1] = true;
-            return true;
+            return UseObject(JsonEvent.ArrayStart, ref name);
         }
         
         // ---
         public bool UseMemberNum(ref Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ValueNumber || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseObject(JsonEvent.ValueNumber, ref name);
         }
         
 #if JSON_BURST
         [Obsolete("Performance degradation by string copy > to avoid use the (ref FixedString32) version", false)]
 #endif
         public bool UseMemberNum(Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ValueNumber || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseObject(JsonEvent.ValueNumber, ref name);
         }
         
         // ---
         public bool UseMemberStr(ref Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ValueString || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseObject(JsonEvent.ValueString, ref name);
         }
 #if JSON_BURST
         [Obsolete("Performance degradation by string copy > to avoid use the (ref FixedString32) version", false)]
 #endif
         public bool UseMemberStr(Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ValueString || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseObject(JsonEvent.ValueString, ref name);
         }
         
         // ---
         public bool UseMemberBln(ref Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ValueBool || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseObject(JsonEvent.ValueBool, ref name);
         }
         
 #if JSON_BURST
         [Obsolete("Performance degradation by string copy > to avoid use the (ref FixedString32) version", false)]
 #endif
         public bool UseMemberBln(Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ValueBool || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseObject(JsonEvent.ValueBool, ref name);
         }
         
         // ---
         public bool UseMemberNul(ref Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ValueNull || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseObject(JsonEvent.ValueNull, ref name);
         }
 
 #if JSON_BURST
         [Obsolete("Performance degradation by string copy > to avoid use the (ref FixedString32) version", false)]
 #endif
         public bool UseMemberNul(Str32 name) {
-            AssertObject();
-            if (lastEvent != JsonEvent.ValueNull || !key.IsEqual32(name))
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseObject(JsonEvent.ValueNull, ref name);
         }
         
         // ----------- array element checks -----------
-        [Conditional("DEBUG")]
-        private void AssertArray() {
+        private bool UseElement(JsonEvent expect) {
             int level = stateLevel;
             if (lastEvent == JsonEvent.ObjectStart || lastEvent == JsonEvent.ArrayStart)
                 level--;
             State curState = state[level];
-            if (curState == State.ExpectElement || curState == State.ExpectElementFirst)
-                return;
-            throw new InvalidOperationException("Must call UseElement...() method on within an array");
+            if (curState == State.ExpectElement) {
+                if (lastEvent != expect)
+                    return false;
+                usedMember[level] = true;
+                return true;
+            }
+            return SetApplicationError("Must call UseElement...() method on within an array");
         }
         
         public bool UseElementObj() {
-            AssertArray();
-            if (lastEvent != JsonEvent.ObjectStart)
-                return false;
-            usedMember[stateLevel - 1] = true;
-            return true;
+            return UseElement(JsonEvent.ObjectStart);
         }
         
         public bool UseElementArr() {
-            AssertArray();
-            if (lastEvent != JsonEvent.ArrayStart)
-                return false;
-            usedMember[stateLevel - 1] = true;
-            return true;
+            return UseElement(JsonEvent.ArrayStart);
         }
         
         public bool UseElementNum() {
-            AssertArray();
-            if (lastEvent != JsonEvent.ValueNumber)
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseElement(JsonEvent.ValueNumber);
         }
         
         public bool UseElementStr() {
-            AssertArray();
-            if (lastEvent != JsonEvent.ValueString)
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseElement(JsonEvent.ValueString);
         }
         
         public bool UseElementBln() {
-            AssertArray();
-            if (lastEvent != JsonEvent.ValueBool)
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseElement(JsonEvent.ValueBool);
         }
         
         public bool UseElementNul() {
-            AssertArray();
-            if (lastEvent != JsonEvent.ValueNull)
-                return false;
-            usedMember[stateLevel] = true;
-            return true;
+            return UseElement(JsonEvent.ValueNull);
         }
         
         // ------------------------------------------------------------------------------------------------
