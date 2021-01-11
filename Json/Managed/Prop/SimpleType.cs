f5510233400f5d2b3567b7017a9af3d7b0b9044e
Ullrich Praetz <ullrich@praetz.de>
2021-01-11 18:06:17 +0100

    removed rubbish

--- Json/Managed/Prop/SimpleType.cs
+++ Json/Managed/Prop/SimpleType.cs
@@ -30,15 +30,15 @@ namespace Friflo.Json.Managed.Prop
 
         public static Id ? IdFromType (Type type)
         {
-            if      (type == typeof( String     ))  return Id.String;
-            else if (type == typeof( long       ))  return Id.Long;
-            else if (type == typeof( int        ))  return Id.Integer;
-            else if (type == typeof( short      ))  return Id.Short;
-            else if (type == typeof( byte       ))  return Id.Byte;
-            else if (type == typeof( bool       ))  return Id.Bool;
-            else if (type == typeof( double     ))  return Id.Double;
-            else if (type == typeof( float      ))  return Id.Float;
-            else if (Reflect.IsAssignableFrom (typeof(Object), type))   return Id.Object;
+            if (type == typeof( String     ))  return Id.String;
+            if (type == typeof( long       ))  return Id.Long;
+            if (type == typeof( int        ))  return Id.Integer;
+            if (type == typeof( short      ))  return Id.Short;
+            if (type == typeof( byte       ))  return Id.Byte;
+            if (type == typeof( bool       ))  return Id.Bool;
+            if (type == typeof( double     ))  return Id.Double;
+            if (type == typeof( float      ))  return Id.Float;
+            if (Reflect.IsAssignableFrom (typeof(Object), type))   return Id.Object;
             return null;
         }
     
@@ -59,106 +59,5 @@ namespace Friflo.Json.Managed.Prop
                 throw new FrifloException("unsupported simple type: " + type. FullName + " of method " + method. Name);
             return id .Value;
         }
-        
-        public static object ObjectFromDouble(Id? id, double value, out bool success) {
-            if (id == null) {
-                success = false;
-                return null;
-            }
-            switch (id)
-            {
-                case Id. String:
-                    success = false;
-                    return null;
-                case Id. Long:
-                    success = true;
-                    return (long) value;
-                case Id. Integer:
-                    success = true;
-                    return (int) value;
-                case Id. Short:
-                    success = true;
-                    return (short) value;
-                case Id. Byte:
-                    success = true;
-                    return (byte) value;
-                case Id. Bool:
-                    success = false;
-                    return null;
-                case Id. Double:
-                    success = true;
-                    return value;
-                case Id. Float:
-                    success = true;
-                    return (float) value;
-                case Id. Object:
-                    success = false;
-                    return null;
-                default:
-                    success = false;
-                    return null;
-            }
-        }
-        /*
-        public static object ObjectFromLong(Id? id, long value, out bool success) {
-            if (id == null) {
-                success = false;
-                return null;
-            }
-            switch (id)
-            {
-                case Id. String:
-                    success = false;
-                    return null;
-                case Id. Long:
-                    success = true;
-                    return value;
-                case Id. Integer:
-                    success = true;
-                    return (int) value;
-                case Id. Short:
-                    success = true;
-                    return (short) value;
-                case Id. Byte:
-                    success = true;
-                    return (byte) value;
-                case Id. Bool:
-                    success = false;
-                    return null;
-                case Id. Double:
-                    success = true;
-                    return (double)value;
-                case Id. Float:
-                    success = true;
-                    return (float) value;
-                case Id. Object:
-                    success = false;
-                    return null;
-                default:
-                    success = false;
-                    return null;
-            }
-        }  */
-        
-/*      public static bool IsAssignable (SimpleType.ID typeID)
-        {
-            switch (typeID)
-            {
-            case SimpleType.ID. String:
-            case SimpleType.ID. Long:
-            case SimpleType.ID. Integer:
-            case SimpleType.ID. Short:
-            case SimpleType.ID. Byte:
-            case SimpleType.ID. Bool:
-            case SimpleType.ID. Double:
-            case SimpleType.ID. Float:
-            case SimpleType.ID. Object:
-                return true;
-            default:
-                return false;
-            }   
-        }
-        */
-
     }
 }
