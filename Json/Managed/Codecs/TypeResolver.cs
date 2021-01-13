2f80b9b0b8075710d080ba2b7e884fcd076f48d9
Ullrich Praetz <ullrich@praetz.de>
2021-01-13 10:36:31 +0100

    add copyright headers

--- Json/Managed/Codecs/TypeResolver.cs
+++ Json/Managed/Codecs/TypeResolver.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+using System;
 using Friflo.Json.Managed.Prop;
 
 namespace Friflo.Json.Managed.Codecs
