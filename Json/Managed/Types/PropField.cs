b28a15476f8b2e6b1ed3b9b9ffe8d105739eeb50
Ullrich Praetz <ullrich@praetz.de>
2021-01-16 19:38:06 +0100

    PropField: use GetValueDirect() / SetValueDirect()

--- Json/Managed/Types/PropField.cs
+++ Json/Managed/Types/PropField.cs
@@ -1,6 +1,5 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
-
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -8,7 +7,6 @@ using Friflo.Json.Burst;
 
 namespace Friflo.Json.Managed.Types
 {
-
     public class PropField : IDisposable
     {
         internal readonly   String          name;
@@ -58,22 +56,40 @@ namespace Friflo.Json.Managed.Types
         {
             field.SetValue (prop, val);
         }
+
+        private static readonly bool useDirect = true;
         
         public void SetField (object obj, ref Slot val)
         {
             if (field != null) {
-                switch (val.Cat) {
-                    case SlotType.Object:   field.SetValue (obj, val.Obj);    return;
-                    //
-                    case SlotType.Double:   field.SetValue (obj, val.Dbl);    return;
-                    case SlotType.Float:    field.SetValue (obj, val.Flt);    return;
-                    //
-                    case SlotType.Long:     field.SetValue (obj, val.Lng);    return;
-                    case SlotType.Int:      field.SetValue (obj, val.Int);    return;
-                    case SlotType.Short:    field.SetValue (obj, val.Short);  return;
-                    case SlotType.Byte:     field.SetValue (obj, val.Byte);   return;
-                    //
-                    case SlotType.Bool:     field.SetValue (obj, val.Bool);   return;
+                if (useDirect) {
+                    switch (val.Cat) {
+                        case SlotType.Object:   field.SetValue      (obj,            val.Obj);    return;
+                        //
+                        case SlotType.Double:   field.SetValueDirect(__makeref(obj), val.Dbl);    return;
+                        case SlotType.Float:    field.SetValueDirect(__makeref(obj), val.Flt);    return;
+                        //
+                        case SlotType.Long:     field.SetValueDirect(__makeref(obj), val.Lng);    return;
+                        case SlotType.Int:      field.SetValueDirect(__makeref(obj), val.Int);    return;
+                        case SlotType.Short:    field.SetValueDirect(__makeref(obj), val.Short);  return;
+                        case SlotType.Byte:     field.SetValueDirect(__makeref(obj), val.Byte);   return;
+                        //
+                        case SlotType.Bool:     field.SetValueDirect(__makeref(obj), val.Bool);   return;
+                    }
+                } else {
+                    switch (val.Cat) {
+                        case SlotType.Object:   field.SetValue (obj, val.Obj);    return;
+                        //
+                        case SlotType.Double:   field.SetValue (obj, val.Dbl);    return;
+                        case SlotType.Float:    field.SetValue (obj, val.Flt);    return;
+                        //
+                        case SlotType.Long:     field.SetValue (obj, val.Lng);    return;
+                        case SlotType.Int:      field.SetValue (obj, val.Int);    return;
+                        case SlotType.Short:    field.SetValue (obj, val.Short);  return;
+                        case SlotType.Byte:     field.SetValue (obj, val.Byte);   return;
+                        //
+                        case SlotType.Bool:     field.SetValue (obj, val.Bool);   return;
+                    }
                 }
             } else {
                 switch (val.Cat) {
@@ -93,35 +109,51 @@ namespace Friflo.Json.Managed.Types
         }
         
         // ReSharper disable PossibleNullReferenceException
-        public void GetField (object prop, ref Slot val)
+        public void GetField (object obj, ref Slot val)
         {
             if (field != null) {
-                switch (slotType) {
-                    case SlotType.Object:   val.Obj     =           field.GetValue   (prop);    return;
-                    //
-                    case SlotType.Double:   val.Dbl     = (double)  field.GetValue   (prop);    return;
-                    case SlotType.Float:    val.Flt     = (float)   field.GetValue   (prop);    return;
-                    //
-                    case SlotType.Long:     val.Lng     = (long)    field.GetValue   (prop);    return;
-                    case SlotType.Int:      val.Int     = (int)     field.GetValue   (prop);    return;
-                    case SlotType.Short:    val.Short   = (short)   field.GetValue   (prop);    return;
-                    case SlotType.Byte:     val.Byte    = (byte)    field.GetValue   (prop);    return;
-                    //
-                    case SlotType.Bool:     val.Bool    = (bool)    field.GetValue   (prop);    return;
+                if (useDirect) {
+                    switch (slotType) {
+                        case SlotType.Object:   val.Obj     =           field.GetValueDirect (__makeref(obj)); return;
+                     
+                        case SlotType.Double:   val.Dbl     = (double)  field.GetValueDirect (__makeref(obj)); return;
+                        case SlotType.Float:    val.Flt     = (float)   field.GetValueDirect (__makeref(obj)); return;
+                     
+                        case SlotType.Long:     val.Lng     = (long)    field.GetValueDirect (__makeref(obj)); return;
+                        case SlotType.Int:      val.Int     = (int)     field.GetValueDirect (__makeref(obj)); return;
+                        case SlotType.Short:    val.Short   = (short)   field.GetValueDirect (__makeref(obj)); return;
+                        case SlotType.Byte:     val.Byte    = (byte)    field.GetValueDirect (__makeref(obj)); return;
+                     
+                        case SlotType.Bool:     val.Bool    = (bool)    field.GetValueDirect (__makeref(obj)); return;
+                    }
+                } else {
+                    switch (slotType) {
+                        case SlotType.Object:   val.Obj     =           field.GetValue   (obj); return;
+                       
+                        case SlotType.Double:   val.Dbl     = (double)  field.GetValue   (obj); return;
+                        case SlotType.Float:    val.Flt     = (float)   field.GetValue   (obj); return;
+                       
+                        case SlotType.Long:     val.Lng     = (long)    field.GetValue   (obj); return;
+                        case SlotType.Int:      val.Int     = (int)     field.GetValue   (obj); return;
+                        case SlotType.Short:    val.Short   = (short)   field.GetValue   (obj); return;
+                        case SlotType.Byte:     val.Byte    = (byte)    field.GetValue   (obj); return;
+                       
+                        case SlotType.Bool:     val.Bool    = (bool)    field.GetValue   (obj); return;
+                    }
                 }
             } else {
                 switch (slotType) {
-                    case SlotType.Object:   val.Obj     =           setter.GetValue   (prop);    return;
+                    case SlotType.Object:   val.Obj     =           setter.GetValue   (obj);    return;
                     //
-                    case SlotType.Double:   val.Dbl     = (double)  setter.GetValue   (prop);    return;
-                    case SlotType.Float:    val.Flt     = (float)   setter.GetValue   (prop);    return;
+                    case SlotType.Double:   val.Dbl     = (double)  setter.GetValue   (obj);    return;
+                    case SlotType.Float:    val.Flt     = (float)   setter.GetValue   (obj);    return;
                     //
-                    case SlotType.Long:     val.Lng     = (long)    setter.GetValue   (prop);    return;
-                    case SlotType.Int:      val.Int     = (int)     setter.GetValue   (prop);    return;
-                    case SlotType.Short:    val.Short   = (short)   setter.GetValue   (prop);    return;
-                    case SlotType.Byte:     val.Byte    = (byte)    setter.GetValue   (prop);    return;
+                    case SlotType.Long:     val.Lng     = (long)    setter.GetValue   (obj);    return;
+                    case SlotType.Int:      val.Int     = (int)     setter.GetValue   (obj);    return;
+                    case SlotType.Short:    val.Short   = (short)   setter.GetValue   (obj);    return;
+                    case SlotType.Byte:     val.Byte    = (byte)    setter.GetValue   (obj);    return;
                     //
-                    case SlotType.Bool:     val.Bool    = (bool)    setter.GetValue   (prop);    return;
+                    case SlotType.Bool:     val.Bool    = (bool)    setter.GetValue   (obj);    return;
                 }
             }
         }
