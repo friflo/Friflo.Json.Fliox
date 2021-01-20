795f0181b65886133b8e0d331c26e6ba201c6f5f
Ullrich Praetz <ullrich@praetz.de>
2021-01-20 11:16:03 +0100

    cleanup

--- Json.Tests/Common/UnitTest/Mapper/TestApi.cs
+++ Json.Tests/Common/UnitTest/Mapper/TestApi.cs
@@ -13,25 +13,25 @@ namespace Friflo.Json.Tests.Common.UnitTest.Mapper
         public void Run() {
             // Ensure existence of basic API methods
             using (TypeStore typeStore = new TypeStore(new DebugTypeResolver()))
-            using (JsonReader enc = new JsonReader(typeStore))
+            using (JsonReader read = new JsonReader(typeStore))
             using (JsonWriter write = new JsonWriter(typeStore))
             
             using (var num1 = new Bytes("1"))
             {
                 // --- Read ---
                 // 1. a
-                AreEqual(1,     enc.Read<int>(num1));               // generic
+                AreEqual(1,     read.Read<int>(num1));               // generic
                 
                 // 1. b
-                AreEqual(true,  enc.Read(num1, out int result));    // generic
+                IsTrue(         read.Read(num1, out int result));    // generic
                 AreEqual(1, result);
 
                 // 2.
-                AreEqual(1,     enc.Read(num1, typeof(int)));       // non generic
+                AreEqual(1,     read.Read(num1, typeof(int)));       // non generic
                 
                 // 3.
                 var varResult = new Var();
-                IsTrue(enc.Read<int>(num1, ref varResult));         // generic
+                IsTrue(read.Read<int>(num1, ref varResult));         // generic
                 AreEqual(1, varResult.Int);
 
 
