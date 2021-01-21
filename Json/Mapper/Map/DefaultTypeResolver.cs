6860fa9bb04c4f7b210486ce54aac24ac2edfccb
Ullrich Praetz <ullrich@praetz.de>
2021-01-21 05:31:33 +0100

    TypeResolver - support enumeration of mappers

--- Json/Mapper/Map/DefaultTypeResolver.cs
+++ Json/Mapper/Map/DefaultTypeResolver.cs
@@ -2,10 +2,12 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using Friflo.Json.Mapper.Map.Arr;
 using Friflo.Json.Mapper.Map.Obj;
 using Friflo.Json.Mapper.Map.Val;
 using Friflo.Json.Mapper.Types;
+// ReSharper disable InlineOutVariableDeclaration
 
 namespace Friflo.Json.Mapper.Map
 {
@@ -37,77 +39,107 @@ namespace Friflo.Json.Mapper.Map
     
     public class DebugTypeResolver : ITypeResolver {
         
-        public StubType CreateStubType (Type type) {
-            // find a codec manually to simplify debugging 
-            StubType result;
+        // find a codec manually to simplify debugging
+        private StubType QueryStubType (Type type, Query q) {
             
             // Specific types on top
-            if (Match(BigIntMapper.     Interface,          type, out result)) return result;
-            if (Match(DateTimeMapper.   Interface,          type, out result)) return result;
+            if (Match(BigIntMapper.     Interface,          type, ref q)) return q.hit;
+            if (Match(DateTimeMapper.   Interface,          type, ref q)) return q.hit;
             
             //
-            if (Match(StringMapper.     Interface,          type, out result)) return result;
-            if (Match(DoubleMapper.     Interface,          type, out result)) return result;
-            if (Match(FloatMapper.      Interface,          type, out result)) return result;
-            if (Match(LongMapper.       Interface,          type, out result)) return result;
-            if (Match(IntMapper.        Interface,          type, out result)) return result;
-            if (Match(ShortMapper.      Interface,          type, out result)) return result;
-            if (Match(ByteMapper.       Interface,          type, out result)) return result;
-            if (Match(BoolMapper.       Interface,          type, out result)) return result;
+            if (Match(StringMapper.     Interface,          type, ref q)) return q.hit;
+            if (Match(DoubleMapper.     Interface,          type, ref q)) return q.hit;
+            if (Match(FloatMapper.      Interface,          type, ref q)) return q.hit;
+            if (Match(LongMapper.       Interface,          type, ref q)) return q.hit;
+            if (Match(IntMapper.        Interface,          type, ref q)) return q.hit;
+            if (Match(ShortMapper.      Interface,          type, ref q)) return q.hit;
+            if (Match(ByteMapper.       Interface,          type, ref q)) return q.hit;
+            if (Match(BoolMapper.       Interface,          type, ref q)) return q.hit;
             // --- List
-            if (Match(PrimitiveList.    DoubleInterface,    type, out result)) return result;
-            if (Match(PrimitiveList.    FloatInterface,     type, out result)) return result;
-            if (Match(PrimitiveList.    LongInterface,      type, out result)) return result;
-            if (Match(PrimitiveList.    IntInterface,       type, out result)) return result;
-            if (Match(PrimitiveList.    ShortInterface,     type, out result)) return result;
-            if (Match(PrimitiveList.    ByteInterface,      type, out result)) return result;
-            if (Match(PrimitiveList.    BoolInterface,      type, out result)) return result;
+            if (Match(PrimitiveList.    DoubleInterface,    type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    FloatInterface,     type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    LongInterface,      type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    IntInterface,       type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    ShortInterface,     type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    ByteInterface,      type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    BoolInterface,      type, ref q)) return q.hit;
             
-            if (Match(PrimitiveList.    DoubleNulInterface, type, out result)) return result;
-            if (Match(PrimitiveList.    FloatNulInterface,  type, out result)) return result;
-            if (Match(PrimitiveList.    LongNulInterface,   type, out result)) return result;
-            if (Match(PrimitiveList.    IntNulInterface,    type, out result)) return result;
-            if (Match(PrimitiveList.    ShortNulInterface,  type, out result)) return result;
-            if (Match(PrimitiveList.    ByteNulInterface,   type, out result)) return result;
-            if (Match(PrimitiveList.    BoolNulInterface,   type, out result)) return result;
+            if (Match(PrimitiveList.    DoubleNulInterface, type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    FloatNulInterface,  type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    LongNulInterface,   type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    IntNulInterface,    type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    ShortNulInterface,  type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    ByteNulInterface,   type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    BoolNulInterface,   type, ref q)) return q.hit;
             // --- array            
-            if (Match(PrimitiveArray.   DoubleInterface,    type, out result)) return result;
-            if (Match(PrimitiveArray.   FloatInterface,     type, out result)) return result;
-            if (Match(PrimitiveArray.   LongInterface,      type, out result)) return result;
-            if (Match(PrimitiveArray.   IntInterface,       type, out result)) return result;
-            if (Match(PrimitiveArray.   ShortInterface,     type, out result)) return result;
-            if (Match(PrimitiveArray.   ByteInterface,      type, out result)) return result;
-            if (Match(PrimitiveArray.   BoolInterface,      type, out result)) return result;
+            if (Match(PrimitiveArray.   DoubleInterface,    type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   FloatInterface,     type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   LongInterface,      type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   IntInterface,       type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   ShortInterface,     type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   ByteInterface,      type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   BoolInterface,      type, ref q)) return q.hit;
 
-            if (Match(PrimitiveArray.   DoubleNulInterface, type, out result)) return result;
-            if (Match(PrimitiveArray.   FloatNulInterface,  type, out result)) return result;
-            if (Match(PrimitiveArray.   LongNulInterface,   type, out result)) return result;
-            if (Match(PrimitiveArray.   IntNulInterface,    type, out result)) return result;
-            if (Match(PrimitiveArray.   ShortNulInterface,  type, out result)) return result;
-            if (Match(PrimitiveArray.   ByteNulInterface,   type, out result)) return result;
-            if (Match(PrimitiveArray.   BoolNulInterface,   type, out result)) return result;
+            if (Match(PrimitiveArray.   DoubleNulInterface, type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   FloatNulInterface,  type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   LongNulInterface,   type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   IntNulInterface,    type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   ShortNulInterface,  type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   ByteNulInterface,   type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   BoolNulInterface,   type, ref q)) return q.hit;
             
-            if (Match(PrimitiveArray.   StringInterface ,   type, out result)) return result;
+            if (Match(PrimitiveArray.   StringInterface ,   type, ref q)) return q.hit;
 
             //
             // The order of codecs bellow need to be irrelevant to ensure same behavior independent
             // when adding various codecs to a custom resolver.
-            if (Match(ArrayMapper.      Interface,          type, out result)) return result;
+            if (Match(ArrayMapper.      Interface,          type, ref q)) return q.hit;
             //
             //
-            if (Match(EnumMapper.       Interface,          type, out result)) return result;
-            if (Match(ListMapper.       Interface,          type, out result)) return result;
-            if (Match(DictionaryMapper. Interface,          type, out result)) return result;
-            if (Match(ClassMapper.      Interface,          type, out result)) return result;
+            if (Match(EnumMapper.       Interface,          type, ref q)) return q.hit;
+            if (Match(ListMapper.       Interface,          type, ref q)) return q.hit;
+            if (Match(DictionaryMapper. Interface,          type, ref q)) return q.hit;
+            if (Match(ClassMapper.      Interface,          type, ref q)) return q.hit;
 
             return null;
         }
 
-        private bool Match(IJsonMapper mapper, Type type, out StubType result) {
-            result = mapper.CreateStubType(type);
-            return result != null;
+        private bool Match(IJsonMapper mapper, Type type, ref Query query) {
+            query.hit = mapper.CreateStubType(type);
+            if (query.mode == Mode.Search)
+                return query.hit != null;
+            
+            query.mappers.Add(mapper);
+            query.hit = null;
+            return false;
+        }
+        
+        enum Mode {
+            Search,
+            Enumerate
         }
 
+        class Query {
+            public readonly Mode                mode;
+            public          List<IJsonMapper>   mappers;
+            public          StubType            hit;
+
+            public Query(Mode mode) {
+                this.mode = mode;
+            }
+        }
+        
+        public List<IJsonMapper>  GetMappers() {
+            var query = new Query(Mode.Enumerate) {
+                mappers = new List<IJsonMapper>()
+            };
+            return query.mappers;
+        }
+
+        public StubType CreateStubType(Type type) {
+            var query = new Query(Mode.Search);
+            return QueryStubType(type, query);
+        }
     }
 }
 
