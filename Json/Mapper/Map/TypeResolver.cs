051a32a4bc0050b187fbcaaed934a59f9ccb324f
Ullrich Praetz <ullrich@praetz.de>
2021-01-21 08:13:17 +0100

    removed redundant ref

--- Json/Mapper/Map/TypeResolver.cs
+++ Json/Mapper/Map/TypeResolver.cs
@@ -37,65 +37,65 @@ namespace Friflo.Json.Mapper.Map
             if (MatchMappers(specificTypeMappers, type, q))               return q.hit;
             
             // Specific types on top
-            if (Match(BigIntMapper.     Interface,          type, ref q)) return q.hit;
-            if (Match(DateTimeMapper.   Interface,          type, ref q)) return q.hit;
+            if (Match(BigIntMapper.     Interface,          type, q)) return q.hit;
+            if (Match(DateTimeMapper.   Interface,          type, q)) return q.hit;
             
             //
-            if (Match(StringMapper.     Interface,          type, ref q)) return q.hit;
-            if (Match(DoubleMapper.     Interface,          type, ref q)) return q.hit;
-            if (Match(FloatMapper.      Interface,          type, ref q)) return q.hit;
-            if (Match(LongMapper.       Interface,          type, ref q)) return q.hit;
-            if (Match(IntMapper.        Interface,          type, ref q)) return q.hit;
-            if (Match(ShortMapper.      Interface,          type, ref q)) return q.hit;
-            if (Match(ByteMapper.       Interface,          type, ref q)) return q.hit;
-            if (Match(BoolMapper.       Interface,          type, ref q)) return q.hit;
+            if (Match(StringMapper.     Interface,          type, q)) return q.hit;
+            if (Match(DoubleMapper.     Interface,          type, q)) return q.hit;
+            if (Match(FloatMapper.      Interface,          type, q)) return q.hit;
+            if (Match(LongMapper.       Interface,          type, q)) return q.hit;
+            if (Match(IntMapper.        Interface,          type, q)) return q.hit;
+            if (Match(ShortMapper.      Interface,          type, q)) return q.hit;
+            if (Match(ByteMapper.       Interface,          type, q)) return q.hit;
+            if (Match(BoolMapper.       Interface,          type, q)) return q.hit;
             // --- List
-            if (Match(PrimitiveList.    DoubleInterface,    type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    FloatInterface,     type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    LongInterface,      type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    IntInterface,       type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    ShortInterface,     type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    ByteInterface,      type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    BoolInterface,      type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    DoubleInterface,    type, q)) return q.hit;
+            if (Match(PrimitiveList.    FloatInterface,     type, q)) return q.hit;
+            if (Match(PrimitiveList.    LongInterface,      type, q)) return q.hit;
+            if (Match(PrimitiveList.    IntInterface,       type, q)) return q.hit;
+            if (Match(PrimitiveList.    ShortInterface,     type, q)) return q.hit;
+            if (Match(PrimitiveList.    ByteInterface,      type, q)) return q.hit;
+            if (Match(PrimitiveList.    BoolInterface,      type, q)) return q.hit;
             
-            if (Match(PrimitiveList.    DoubleNulInterface, type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    FloatNulInterface,  type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    LongNulInterface,   type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    IntNulInterface,    type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    ShortNulInterface,  type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    ByteNulInterface,   type, ref q)) return q.hit;
-            if (Match(PrimitiveList.    BoolNulInterface,   type, ref q)) return q.hit;
+            if (Match(PrimitiveList.    DoubleNulInterface, type, q)) return q.hit;
+            if (Match(PrimitiveList.    FloatNulInterface,  type, q)) return q.hit;
+            if (Match(PrimitiveList.    LongNulInterface,   type, q)) return q.hit;
+            if (Match(PrimitiveList.    IntNulInterface,    type, q)) return q.hit;
+            if (Match(PrimitiveList.    ShortNulInterface,  type, q)) return q.hit;
+            if (Match(PrimitiveList.    ByteNulInterface,   type, q)) return q.hit;
+            if (Match(PrimitiveList.    BoolNulInterface,   type, q)) return q.hit;
             // --- array            
-            if (Match(PrimitiveArray.   DoubleInterface,    type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   FloatInterface,     type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   LongInterface,      type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   IntInterface,       type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   ShortInterface,     type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   ByteInterface,      type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   BoolInterface,      type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   DoubleInterface,    type, q)) return q.hit;
+            if (Match(PrimitiveArray.   FloatInterface,     type, q)) return q.hit;
+            if (Match(PrimitiveArray.   LongInterface,      type, q)) return q.hit;
+            if (Match(PrimitiveArray.   IntInterface,       type, q)) return q.hit;
+            if (Match(PrimitiveArray.   ShortInterface,     type, q)) return q.hit;
+            if (Match(PrimitiveArray.   ByteInterface,      type, q)) return q.hit;
+            if (Match(PrimitiveArray.   BoolInterface,      type, q)) return q.hit;
 
-            if (Match(PrimitiveArray.   DoubleNulInterface, type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   FloatNulInterface,  type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   LongNulInterface,   type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   IntNulInterface,    type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   ShortNulInterface,  type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   ByteNulInterface,   type, ref q)) return q.hit;
-            if (Match(PrimitiveArray.   BoolNulInterface,   type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   DoubleNulInterface, type, q)) return q.hit;
+            if (Match(PrimitiveArray.   FloatNulInterface,  type, q)) return q.hit;
+            if (Match(PrimitiveArray.   LongNulInterface,   type, q)) return q.hit;
+            if (Match(PrimitiveArray.   IntNulInterface,    type, q)) return q.hit;
+            if (Match(PrimitiveArray.   ShortNulInterface,  type, q)) return q.hit;
+            if (Match(PrimitiveArray.   ByteNulInterface,   type, q)) return q.hit;
+            if (Match(PrimitiveArray.   BoolNulInterface,   type, q)) return q.hit;
             
-            if (Match(PrimitiveArray.   StringInterface ,   type, ref q)) return q.hit;
+            if (Match(PrimitiveArray.   StringInterface ,   type, q)) return q.hit;
 
             
             if (MatchMappers(genericTypeMappers, type, q))               return q.hit;
             //
             // The order of codecs bellow need to be irrelevant to ensure same behavior independent
             // when adding various codecs to a custom resolver.
-            if (Match(ArrayMapper.      Interface,          type, ref q)) return q.hit;
+            if (Match(ArrayMapper.      Interface,          type, q)) return q.hit;
             //
             //
-            if (Match(EnumMapper.       Interface,          type, ref q)) return q.hit;
-            if (Match(ListMapper.       Interface,          type, ref q)) return q.hit;
-            if (Match(DictionaryMapper. Interface,          type, ref q)) return q.hit;
-            if (Match(ClassMapper.      Interface,          type, ref q)) return q.hit;
+            if (Match(EnumMapper.       Interface,          type, q)) return q.hit;
+            if (Match(ListMapper.       Interface,          type, q)) return q.hit;
+            if (Match(DictionaryMapper. Interface,          type, q)) return q.hit;
+            if (Match(ClassMapper.      Interface,          type, q)) return q.hit;
 
             return null;
         }
@@ -112,13 +112,13 @@ namespace Friflo.Json.Mapper.Map
 
         private static bool MatchMappers(List<IJsonMapper> mappers, Type type, Query query) {
             for (int i = 0; i < mappers.Count; i++) {
-                if (Match(mappers[i], type, ref query))
+                if (Match(mappers[i], type, query))
                     return true;
             }
             return false;
         }
 
-        private static bool Match(IJsonMapper mapper, Type type, ref Query query) {
+        private static bool Match(IJsonMapper mapper, Type type, Query query) {
             if (query.mode == Mode.Search) {
                 query.hit = mapper.CreateStubType(type);
                 return query.hit != null;
