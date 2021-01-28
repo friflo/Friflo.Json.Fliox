74a25061f12606c985291a1a93dc370887a4ce8f
Ullrich Praetz <ullrich@praetz.de>
2021-01-28 15:31:28 +0100

    enhance readability

--- Json/Mapper/Map/Obj/Class/IL/ILCodeGen.cs
+++ Json/Mapper/Map/Obj/Class/IL/ILCodeGen.cs
@@ -7,7 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using Friflo.Json.Mapper.Map.Obj.Class.Reflect;
-
+using Exp = System.Linq.Expressions.Expression;
+    
 namespace Friflo.Json.Mapper.Map.Obj.Class.IL
 {
     public static class ILCodeGen
@@ -19,10 +20,10 @@ namespace Friflo.Json.Mapper.Map.Obj.Class.IL
         // [Optimizing reflection in C# via dynamic code generation | by Sergio Pedri | Medium]
         // https://medium.com/@SergioPedri/optimizing-reflection-with-dynamic-code-generation-6e15cef4b1a2
         internal static Expression<Action<long[], object>> LoadInstanceExpression (PropertyFields propFields, Type type) {
-            var dst         = Expression.Parameter(typeof(long[]), "dst");      // parameter: long[] dst
-            var src         = Expression.Parameter(typeof(object), "src");      // parameter: object src;
+            var dst         = Exp.Parameter(typeof(long[]), "dst");      // parameter: long[] dst
+            var src         = Exp.Parameter(typeof(object), "src");      // parameter: object src;
             
-            var srcTyped    = Expression.Convert(src, type);                    // <Type> srcTyped = (<Type>)src;
+            var srcTyped    = Exp.Convert(src, type);                    // <Type> srcTyped = (<Type>)src;
             
             var doubleToInt64Bits = typeof(BitConverter).GetMethod(nameof(BitConverter.DoubleToInt64Bits));
             var singleToInt32Bits = typeof(BitConverter).GetMethod(nameof(BitConverter.SingleToInt32Bits));
@@ -34,41 +35,41 @@ namespace Friflo.Json.Mapper.Map.Obj.Class.IL
                 if (!field.isValueType || !field.fieldTypeNative.IsPrimitive)
                     continue;
                 
-                var memberVal   = Expression.PropertyOrField(srcTyped, field.name); // memberVal = srcTyped.<field.name>;
+                var memberVal   = Exp.PropertyOrField(srcTyped, field.name); // memberVal = srcTyped.<field.name>;
                 
                 Expression longVal;
                 if (fieldType == typeof(long) || fieldType == typeof(int) ||fieldType == typeof(short) || fieldType == typeof(byte)) {
-                    longVal     = Expression.Convert(memberVal, typeof(long));      // longVal   = (long)memberVal;
+                    longVal     = Exp.Convert(memberVal, typeof(long));      // longVal   = (long)memberVal;
                 } else if (fieldType == typeof(bool)) {
-                    longVal     = Expression.Condition(memberVal, Expression.Constant(1L), Expression.Constant(0L)); // longVal   = memberVal ? 1 : 0;
+                    longVal     = Exp.Condition(memberVal, Exp.Constant(1L), Exp.Constant(0L)); // longVal   = memberVal ? 1 : 0;
                 } else if (fieldType == typeof(double)) {
                     // ReSharper disable once AssignNullToNotNullAttribute
-                    longVal     = Expression.Call(doubleToInt64Bits, memberVal);
+                    longVal     = Exp.Call(doubleToInt64Bits, memberVal);
                 } else if (fieldType == typeof(float)) {
                     // ReSharper disable once AssignNullToNotNullAttribute
-                    var intVal  = Expression.Call(singleToInt32Bits, memberVal);
-                    longVal     = Expression.Convert(intVal, typeof(long));
+                    var intVal  = Exp.Call(singleToInt32Bits, memberVal);
+                    longVal     = Exp.Convert(intVal, typeof(long));
                 }
                 else
                     throw new InvalidOperationException("Unexpected primitive type: " + fieldType);
 
-                var arrayIndex  = Expression.Constant(n, typeof(int));              // int arrayIndex = <field index>;
-                var dstElement  = Expression.ArrayAccess(dst, arrayIndex);          // ref long[] dstElement = ref dst[arrayIndex];
+                var arrayIndex  = Exp.Constant(n, typeof(int));              // int arrayIndex = <field index>;
+                var dstElement  = Exp.ArrayAccess(dst, arrayIndex);          // ref long[] dstElement = ref dst[arrayIndex];
 
-                var dstAssign   = Expression.Assign(dstElement, longVal);           // dstElement = longVal;
+                var dstAssign   = Exp.Assign(dstElement, longVal);           // dstElement = longVal;
                 assignmentList.Add(dstAssign);
             }
-            var assignmentsBlock = Expression.Block(assignmentList);
+            var assignmentsBlock= Exp.Block(assignmentList);
             
-            var lambda = Expression.Lambda<Action<long[], object>> (assignmentsBlock, dst, src);
+            var lambda = Exp.Lambda<Action<long[], object>> (assignmentsBlock, dst, src);
             return lambda;
         }
 
         internal static Expression<Action<object, long[]>> StoreInstanceExpression (PropertyFields propFields, Type type) {
-            var dst         = Expression.Parameter(typeof(object), "dst");      // parameter: long[] dst
-            var src         = Expression.Parameter(typeof(long[]), "src");      // parameter: object src;
+            var dst         = Exp.Parameter(typeof(object), "dst");      // parameter: long[] dst
+            var src         = Exp.Parameter(typeof(long[]), "src");      // parameter: object src;
             
-            var dstTyped    = Expression.Convert(dst, type);                    // <Type> dstTyped = (<Type>)dst;
+            var dstTyped    = Exp.Convert(dst, type);                    // <Type> dstTyped = (<Type>)dst;
             
             var int64BitsToDouble = typeof(BitConverter).GetMethod(nameof(BitConverter.Int64BitsToDouble));
             var int32BitsToSingle = typeof(BitConverter).GetMethod(nameof(BitConverter.Int32BitsToSingle));
@@ -80,34 +81,34 @@ namespace Friflo.Json.Mapper.Map.Obj.Class.IL
                 if (!field.isValueType || !field.fieldTypeNative.IsPrimitive)
                     continue;
                 
-                var arrayIndex  = Expression.Constant(n, typeof(int));                  // int arrayIndex = <field index>;
-                var srcElement  = Expression.ArrayAccess(src, arrayIndex);              // ref long[] srcElement = ref src[arrayIndex];
+                var arrayIndex  = Exp.Constant(n, typeof(int));                  // int arrayIndex = <field index>;
+                var srcElement  = Exp.ArrayAccess(src, arrayIndex);              // ref long[] srcElement = ref src[arrayIndex];
                 Expression srcTyped;
                 if (fieldType == typeof(long) || fieldType == typeof(int) ||fieldType == typeof(short) || fieldType == typeof(byte)) {
-                    srcTyped    = Expression.Convert(srcElement, fieldType);// srcTyped  = (<Field Type>)srcElement;
+                    srcTyped    = Exp.Convert(srcElement, fieldType);// srcTyped  = (<Field Type>)srcElement;
                 } else if (fieldType == typeof(bool)) {
-                    var not0    = Expression.NotEqual(srcElement, Expression.Constant(0L));
-                    srcTyped    = Expression.Condition(not0, Expression.Constant(true), Expression.Constant(false)); // srcTyped   = srcElement != 0;
+                    var not0    = Exp.NotEqual(srcElement, Exp.Constant(0L));
+                    srcTyped    = Exp.Condition(not0, Exp.Constant(true), Exp.Constant(false)); // srcTyped   = srcElement != 0;
                 } else if (fieldType == typeof(double)) {
                     // ReSharper disable once AssignNullToNotNullAttribute
-                    srcTyped    = Expression.Call(int64BitsToDouble, srcElement);
+                    srcTyped    = Exp.Call(int64BitsToDouble, srcElement);
                 } else if (fieldType == typeof(float)) {
-                    var srcInt  = Expression.Convert(srcElement, typeof(int));
+                    var srcInt  = Exp.Convert(srcElement, typeof(int));
                     // ReSharper disable once AssignNullToNotNullAttribute
-                    srcTyped    = Expression.Call(int32BitsToSingle, srcInt);
+                    srcTyped    = Exp.Call(int32BitsToSingle, srcInt);
                 }
                 else
                     throw new InvalidOperationException("Unexpected primitive type: " + fieldType);
                 
                  
-                var dstMember   = Expression.PropertyOrField(dstTyped, field.name);     // ref dstMember = ref dstTyped.<field.name>;
+                var dstMember   = Exp.PropertyOrField(dstTyped, field.name);     // ref dstMember = ref dstTyped.<field.name>;
 
-                var dstAssign   = Expression.Assign(dstMember, srcTyped);               // dstMember = srcTyped;
+                var dstAssign   = Exp.Assign(dstMember, srcTyped);               // dstMember = srcTyped;
                 assignmentList.Add(dstAssign);
             }
-            var assignmentsBlock = Expression.Block(assignmentList);
+            var assignmentsBlock= Exp.Block(assignmentList);
             
-            var lambda = Expression.Lambda<Action<object, long[]>> (assignmentsBlock, dst, src);
+            var lambda = Exp.Lambda<Action<object, long[]>> (assignmentsBlock, dst, src);
             return lambda;
         }
     }
