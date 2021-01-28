56435a2db72b3c7d9d00d7832994ac202da9ba62
Ullrich Praetz <ullrich@praetz.de>
2021-01-28 16:17:33 +0100

    simplify: removed redundant ClassLayout > int[] fieldPos - used for double indexing

--- Json/Mapper/Map/Obj/Class/IL/ClassPayload.cs
+++ Json/Mapper/Map/Obj/Class/IL/ClassPayload.cs
@@ -41,28 +41,28 @@ namespace Friflo.Json.Mapper.Map.Obj.Class.IL
         }
         
 #if !UNITY_5_3_OR_NEWER
-        public void     StoreDbl    (int idx,           double value) {  data.array[layout.fieldPos[idx]] = BitConverter.DoubleToInt64Bits(value); }
+        public void     StoreDbl    (int idx,           double value) {  data.array[idx] = BitConverter.DoubleToInt64Bits(value); }
         public double   LoadDbl     (int idx) {
-            return BitConverter.Int64BitsToDouble(                       data.array[layout.fieldPos[idx]]); }
+            return BitConverter.Int64BitsToDouble(                       data.array[idx]); }
         
-        public void     StoreFlt    (int idx,           float value) {   data.array[layout.fieldPos[idx]] = BitConverter.SingleToInt32Bits(value); }
+        public void     StoreFlt    (int idx,           float value) {   data.array[idx] = BitConverter.SingleToInt32Bits(value); }
         public float    LoadFlt     (int idx) {
-            return BitConverter.Int32BitsToSingle(                  (int)data.array[layout.fieldPos[idx]]); }
+            return BitConverter.Int32BitsToSingle(                  (int)data.array[idx]); }
 
-        public void     StoreLong   (int idx,            long value)   { data.array[layout.fieldPos[idx]] = value; }
-        public long     LoadLong    (int idx)                   { return data.array[layout.fieldPos[idx]]; }
+        public void     StoreLong   (int idx,            long value)   { data.array[idx] = value; }
+        public long     LoadLong    (int idx)                   { return data.array[idx]; }
         
-        public void     StoreInt    (int idx,            int value)    { data.array[layout.fieldPos[idx]] = value; }
-        public int      LoadInt     (int idx)  { return (int)            data.array[layout.fieldPos[idx]]; }
+        public void     StoreInt    (int idx,            int value)    { data.array[idx] = value; }
+        public int      LoadInt     (int idx)  { return (int)            data.array[idx]; }
         
-        public void     StoreShort  (int idx,            short value)  { data.array[layout.fieldPos[idx]] = value; }
-        public short    LoadShort   (int idx)  { return (short)          data.array[layout.fieldPos[idx]]; }
+        public void     StoreShort  (int idx,            short value)  { data.array[idx] = value; }
+        public short    LoadShort   (int idx)  { return (short)          data.array[idx]; }
         
-        public void     StoreByte   (int idx,            byte value)   { data.array[layout.fieldPos[idx]] = value; }
-        public byte     LoadByte    (int idx)  { return (byte)           data.array[layout.fieldPos[idx]]; }
+        public void     StoreByte   (int idx,            byte value)   { data.array[idx] = value; }
+        public byte     LoadByte    (int idx)  { return (byte)           data.array[idx]; }
         
-        public void     StoreBool   (int idx,            bool value)   { data.array[layout.fieldPos[idx]] = value ? 1 : 0; }
-        public bool     LoadBool    (int idx)  { return                  data.array[layout.fieldPos[idx]] != 0; }
+        public void     StoreBool   (int idx,            bool value)   { data.array[idx] = value ? 1 : 0; }
+        public bool     LoadBool    (int idx)  { return                  data.array[idx] != 0; }
 #endif
 
     }
@@ -70,19 +70,10 @@ namespace Friflo.Json.Mapper.Map.Obj.Class.IL
     public readonly struct ClassLayout
     {
         internal readonly int   size;
-        internal readonly int[] fieldPos;
-
 
         internal ClassLayout(Type type, PropertyFields  propFields, ResolverConfig config) {
             var fields = propFields.fields;
-            int count = 0;
-            int[] tempPos = new int[fields.Length]; 
-            for (int n = 0; n < fields.Length; n++) {
-                tempPos[n] = n; // fake pos;
-                count ++;
-            }
-            size       = count;
-            fieldPos   = tempPos;
+            size       = fields.Length;
             
             // create load/store instance expression
 
