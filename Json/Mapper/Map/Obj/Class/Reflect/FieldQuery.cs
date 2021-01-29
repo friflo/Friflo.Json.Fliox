23fdfa4e00293b676af537e2154c5cc2f9ec6b33
Ullrich Praetz <ullrich@praetz.de>
2021-01-29 05:13:06 +0100

    simplify: removed inheritance, renamed Property -> FieldQuery

---
+++ Json/Mapper/Map/Obj/Class/Reflect/FieldQuery.cs
@@ -0,0 +1,86 @@
+// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Friflo.Json.Mapper.Utils;
+
+namespace Friflo.Json.Mapper.Map.Obj.Class.Reflect
+{
+    public interface IProperties
+    {
+        // void     SetProperties (Property prop) ; 
+    }
+    
+    public class  FieldQuery
+    {
+        internal readonly           List<PropField>     fieldList = new List <PropField>();
+
+        private static readonly     Type[] Types = new Type [] { typeof( FieldQuery ) };
+
+
+        private void Set(Type type, String name)
+        {
+            CreatePropField (type, name, name);
+        }
+
+        public void Set(Type type, String name, String field)
+        {
+            CreatePropField (type, name, field);
+        }
+        
+        private void CreatePropField (Type type, String name, String fieldName) {
+            // getter have higher priority than fields with the same fieldName. Same behavior as other serialization libs
+            PropertyInfo getter = ReflectUtils.GetPropertyGet(type, fieldName );
+            if (getter != null)
+            {
+                PropertyInfo setter = ReflectUtils.GetPropertySet(type, fieldName );
+                PropField pf = new PropField(name, getter.PropertyType, null, getter, setter, fieldList.Count);
+                fieldList. Add (pf);
+                return;
+            }
+            // create property from field
+            FieldInfo field = ReflectUtils.GetField(type, fieldName );
+            if (field != null) {
+                PropField pf = new PropField(name, field.FieldType,     field, null, null, fieldList.Count);
+                fieldList. Add (pf);
+                return;
+            }
+            throw new FrifloException ("Field '" + name + "' ('" + fieldName + "') not found in type " + type);
+        }
+
+
+        private static MethodInfo GetPropertiesDeclaration (Type type)
+        {
+            return ReflectUtils.GetMethodEx(type, "SetProperties", Types);
+        }
+
+        internal void SetProperties (Type type)
+        {
+            try
+            {
+                MethodInfo method = GetPropertiesDeclaration(type);
+                if (method != null)
+                {
+                    Object[] args = new Object[] { this };
+                    ReflectUtils.Invoke (method, null, args);
+                }
+                else
+                {
+                    PropertyInfo[] properties = ReflectUtils.GetProperties(type);
+                    for (int n = 0; n < properties. Length; n++)
+                        Set(type, properties[n]. Name);
+
+                    FieldInfo[] field = ReflectUtils.GetFields(type);
+                    for (int n = 0; n < field. Length; n++)
+                        Set(type, field[n]. Name);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new FrifloException("SetProperties() failed for type: " + type, e);
+            }
+        }
+    }
+}
--- Json/Mapper/Map/Obj/Class/Reflect/PropertyFields.cs
+++ Json/Mapper/Map/Obj/Class/Reflect/PropertyFields.cs
@@ -2,8 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
-using System.Reflection;
 using Friflo.Json.Mapper.Utils;
 
 namespace Friflo.Json.Mapper.Map.Obj.Class.Reflect
@@ -12,14 +10,12 @@ namespace Friflo.Json.Mapper.Map.Obj.Class.Reflect
 #if !UNITY_5_3_OR_NEWER
     [CLSCompliant(true)]
 #endif
-    public sealed class PropertyFields : Property, IDisposable
+    public sealed class PropertyFields : IDisposable
     {
         public      readonly    PropField []        fields;
         public      readonly    PropField []        fieldsSerializable;
         public      readonly    int                 num;
         
-        private     readonly    Type                type;
-        private     readonly    List<PropField>     fieldList = new List <PropField>();
         
         // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
         private     readonly    String              typeName;
@@ -29,11 +25,11 @@ namespace Friflo.Json.Mapper.Map.Obj.Class.Reflect
 
         public PropertyFields (Type type)
         {
-            this.type           = type;
             this.typeName       = type. ToString();
-            try
-            {
-                SetProperties(type);
+            try {
+                var query = new FieldQuery();
+                query.SetProperties(type);
+                var fieldList = query.fieldList;
                 num = fieldList. Count;
                 fields = new PropField [num];
                 for (int n = 0; n < num; n++)
@@ -54,37 +50,6 @@ namespace Friflo.Json.Mapper.Map.Obj.Class.Reflect
         public void Dispose() {
             for (int i = 0; i < fields.Length; i++)
                 fields[i].Dispose();
-        }
-
-        private void CreatePropField (String name, String fieldName)
-        {
-            // getter have higher priority than fields with the same fieldName. Same behavior as other serialization libs
-            PropertyInfo getter = ReflectUtils.GetPropertyGet(type, fieldName );
-            if (getter != null)
-            {
-                PropertyInfo setter = ReflectUtils.GetPropertySet(type, fieldName );
-                PropField pf = new PropField(name, getter.PropertyType, null, getter, setter, fieldList.Count);
-                fieldList. Add (pf);
-                return;
-            }
-            // create property from field
-            FieldInfo field = ReflectUtils.GetField(type, fieldName );
-            if (field != null) {
-                PropField pf = new PropField(name, field.FieldType,     field, null, null, fieldList.Count);
-                fieldList. Add (pf);
-                return;
-            }
-            throw new FrifloException ("Field '" + name + "' ('" + fieldName + "') not found in type " + type);
-        }
-
-        protected override void Set(String name)
-        {
-            CreatePropField (name, name);
-        }
-
-        public override void Set(String name, String field)
-        {
-            CreatePropField (name, field);
         }
     }
 }
