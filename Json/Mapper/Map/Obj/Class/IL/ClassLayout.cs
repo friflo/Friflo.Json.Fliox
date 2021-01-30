e81b6c0e8c16d247e8116486c428bcdf94732bf0
Ullrich Praetz <ullrich@praetz.de>
2021-01-30 06:26:08 +0100

    separate ClassLayout

---
+++ Json/Mapper/Map/Obj/Class/IL/ClassLayout.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using Friflo.Json.Mapper.Map.Obj.Class.Reflect;
+
+#if !UNITY_5_3_OR_NEWER
+
+namespace Friflo.Json.Mapper.Map.Obj.Class.IL
+{
+    public abstract class ClassLayout {
+
+        internal readonly int   primCount;
+        internal readonly int   objCount;
+        
+        internal ClassLayout(PropertyFields  propFields) {
+            primCount       = propFields.primCount;
+            objCount        = propFields.objCount;
+        }
+
+        public abstract void LoadObjectToMirror  (long[] dstPrim, object[] dstObj, object src);
+        public abstract void StoreMirrorToPayload(object dst, long[] srcPrim, object[] srcObj);
+    }
+
+    public class ClassLayout<T> : ClassLayout
+    {
+        internal ClassLayout(PropertyFields propFields) : base(propFields) {
+            loadObjectToPayload = null;
+            storePayloadToObject = null;
+        }
+
+        internal void InitClassLayout(Type type, PropertyFields propFields, ResolverConfig config) {
+
+            // create load/store instance expression
+            Action<long[], object[], T> load = null;
+            Action<T, long[], object[]> store = null;
+            if (config.useIL) {
+                var loadLambda = ILCodeGen.LoadInstanceExpression<T>(propFields);
+                load = loadLambda.Compile();
+
+                var storeLambda = ILCodeGen.StoreInstanceExpression<T>(propFields);
+                store = storeLambda.Compile();
+            }
+
+            loadObjectToPayload = load;
+            storePayloadToObject = store;
+        }
+
+        public override void LoadObjectToMirror(long[] dstPrim, object[] dstObj, object src) {
+            loadObjectToPayload(dstPrim, dstObj, (T) src);
+        }
+
+        public override void StoreMirrorToPayload(object dst, long[] srcPrim, object[] srcObj) {
+            storePayloadToObject((T) dst, srcPrim, srcObj);
+        }
+
+        internal Action<long[], object[], T> loadObjectToPayload;
+        internal Action<T, long[], object[]> storePayloadToObject;
+
+    
+        // Unity dummies
+        // internal        ClassLayout(PropertyFields propFields) : base(propFields) { }
+        // internal void   InitClassLayout (Type type, PropertyFields propFields, ResolverConfig config) { }
+    }
+}
+
+#endif
--- Json/Mapper/Map/Obj/Class/IL/ClassMirror.cs
+++ Json/Mapper/Map/Obj/Class/IL/ClassMirror.cs
@@ -4,7 +4,6 @@
 using System;
 using Friflo.Json.Burst;
 using Friflo.Json.Burst.Utils;
-using Friflo.Json.Mapper.Map.Obj.Class.Reflect;
 
 
 namespace Friflo.Json.Mapper.Map.Obj.Class.IL
@@ -73,63 +72,6 @@ namespace Friflo.Json.Mapper.Map.Obj.Class.IL
 
         public void     StoreObj    (int idx,            object value) { objects.array[idx] = value; }
         public object   LoadObj     (int idx)  { return                  objects.array[idx]; }
-    }
-    
-    public abstract class ClassLayout {
-
-        internal readonly int   primCount;
-        internal readonly int   objCount;
-        
-        internal ClassLayout(PropertyFields  propFields) {
-            primCount       = propFields.primCount;
-            objCount        = propFields.objCount;
-        }
-
-        public abstract void LoadObjectToMirror  (long[] dstPrim, object[] dstObj, object src);
-        public abstract void StoreMirrorToPayload(object dst, long[] srcPrim, object[] srcObj);
-    }
-    
-    
-
-    public class ClassLayout<T> : ClassLayout
-    {
-        internal ClassLayout(PropertyFields propFields) : base(propFields) {
-            loadObjectToPayload = null;
-            storePayloadToObject = null;
-        }
-
-        internal void InitClassLayout(Type type, PropertyFields propFields, ResolverConfig config) {
-
-            // create load/store instance expression
-            Action<long[], object[], T> load = null;
-            Action<T, long[], object[]> store = null;
-            if (config.useIL) {
-                var loadLambda = ILCodeGen.LoadInstanceExpression<T>(propFields);
-                load = loadLambda.Compile();
-
-                var storeLambda = ILCodeGen.StoreInstanceExpression<T>(propFields);
-                store = storeLambda.Compile();
-            }
-
-            loadObjectToPayload = load;
-            storePayloadToObject = store;
-        }
-
-        public override void LoadObjectToMirror(long[] dstPrim, object[] dstObj, object src) {
-            loadObjectToPayload(dstPrim, dstObj, (T) src);
-        }
-
-        public override void StoreMirrorToPayload(object dst, long[] srcPrim, object[] srcObj) {
-            storePayloadToObject((T) dst, srcPrim, srcObj);
-        }
-
-        internal Action<long[], object[], T> loadObjectToPayload;
-        internal Action<T, long[], object[]> storePayloadToObject;
-
-    
-        // Unity dummies
-        // internal        ClassLayout(PropertyFields propFields) : base(propFields) { }
-        // internal void   InitClassLayout (Type type, PropertyFields propFields, ResolverConfig config) { }
     }
 #else    
     public class ClassMirror {}
