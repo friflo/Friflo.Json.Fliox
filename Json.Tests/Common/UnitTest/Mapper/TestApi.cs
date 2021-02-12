8f0694df65a9fa166a73a24fcc7e0d8ce8dcf97b
Ullrich Praetz <ullrich@praetz.de>
2021-02-12 16:51:35 +0100

    cleanup

--- Json.Tests/Common/UnitTest/Mapper/TestApi.cs
+++ Json.Tests/Common/UnitTest/Mapper/TestApi.cs
@@ -59,121 +59,6 @@ namespace Friflo.Json.Tests.Common.UnitTest.Mapper
             }
         }
         
-       
-        // ------------------------------ Assert Bytes / string / Stream ------------------------------
-        private void AssertReaderBytes(IJsonReader read) {
-            using (var num1 = new Bytes("1"))
-            using (var arr1 = new Bytes("[1]"))
-            {
-                // --- Read ---
-                AreEqual(1, read.Read<int>(num1));                      // generic
-                
-                AreEqual(1, read.ReadObject(num1, typeof(int)));        // non generic
-                
-                
-                // --- ReadTo ---
-                int[] reuse  = new int[1];
-                int[] expect = { 1 };
-                int[] result = read.ReadTo(arr1, reuse);                // generic
-                AreEqual(expect, result);   
-                IsTrue(reuse == result); // same reference - size did not change
-                
-                object resultObj = read.ReadToObject(arr1, reuse);      // non generic
-                AreEqual(expect, resultObj);
-                IsTrue(reuse == resultObj); // same reference - size did not change
-            }
-        }
-        
-        private void AssertWriterBytes(IJsonWriter write) {
-            using (var dst  = new TestBytes())
-            {
-                // --- Write ---
-                write.Write(1, ref dst.bytes);                          // generic
-                AreEqual("1", dst.bytes.ToString());
-                
-                write.WriteObject(1, ref dst.bytes);                    // non generic 
-                AreEqual("1", dst.bytes.ToString());
-            }
-        }
-
-        private void AssertReaderStream(IJsonReader read) {
-            // --- Read ---
-            AreEqual(1, read.Read<int>(StreamFromString("1")));                     // generic
-            
-            AreEqual(1, read.ReadObject(StreamFromString("1"), typeof(int)));       // non generic
-            
-          
-            // --- ReadTo ---
-            int[] reuse  = new int[1];
-            int[] expect = { 1 };
-            int[] result = read.ReadTo(StreamFromString("[1]"), reuse);             // generic
-            AreEqual(expect, result);   
-            IsTrue(reuse == result); // same reference - size did not change
-            
-            object resultObj = read.ReadToObject(StreamFromString("[1]"), reuse);   // non generic
-            AreEqual(expect, resultObj);
-            IsTrue(reuse == resultObj); // same reference - size did not change
-        }
-        
-        private void AssertWriterStream(IJsonWriter write) {
-#if !JSON_BURST   // todo
-            // --- Write ---
-            Stream stream = new MemoryStream();
-            write.Write(1, stream);                                                 // generic
-            AreEqual("1", StringFromStream(stream));
-
-            stream.Position = 0;
-            write.WriteObject(1, stream);                                           // non generic 
-            AreEqual("1", StringFromStream(stream));
-#endif
-        }
-
-        private void AssertReaderString(IJsonReader read) {
-            // --- Read ---
-            AreEqual(1, read.Read<int>("1"));                       // generic
-            
-            AreEqual(1, read.ReadObject("1", typeof(int)));         // non generic
-            
-           
-            // --- ReadTo ---
-            int[] reuse  = new int[1];
-            int[] expect = { 1 };
-            int[] result = read.ReadTo("[1]", reuse);               // generic
-            AreEqual(expect, result);   
-            IsTrue(reuse == result); // same reference - size did not change
-            
-            object resultObj = read.ReadToObject("[1]", reuse);     // non generic
-            AreEqual(expect, resultObj);
-            IsTrue(reuse == resultObj); // same reference - size did not change
-        }
-        
-        private void AssertWriterString(IJsonWriter write) {
-            // --- Write ---
-            var json1 = write.Write(1);                             // generic
-            AreEqual("1", json1);
-            
-            var json2 = write.WriteObject(1);                       // non generic 
-            AreEqual("1", json2);
-        }
-        
-        private static Stream StreamFromString(string s)
-        {
-            var stream = new MemoryStream();
-            var writer = new StreamWriter(stream);
-            writer.Write(s);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
-        
-        private static string StringFromStream(Stream stream)
-        {
-            stream.Position = 0;
-            StreamReader reader = new StreamReader(stream);
-            string str = reader.ReadToEnd();
-            return str;
-        }
-        
         [Test]
         public void ReaderException() {
             using (TypeStore typeStore = new TypeStore())
@@ -197,6 +82,122 @@ namespace Friflo.Json.Tests.Common.UnitTest.Mapper
                 AreEqual("JsonParser/JSON error: unexpected character while reading value. Found: i path: '(root)' at position: 1", read.Error.msg.ToString());
                 AreEqual(1, read.Error.Pos);
             }
+        }
+        
+       
+        // ------------------------------ Assert Bytes / string / Stream ------------------------------
+        private static void AssertReaderBytes(IJsonReader read) {
+            using (var num1 = new Bytes("1"))
+            using (var arr1 = new Bytes("[1]"))
+            {
+                // --- Read ---
+                AreEqual(1, read.Read<int>(num1));                      // generic
+                
+                AreEqual(1, read.ReadObject(num1, typeof(int)));        // non generic
+                
+                
+                // --- ReadTo ---
+                int[] reuse  = new int[1];
+                int[] expect = { 1 };
+                int[] result = read.ReadTo(arr1, reuse);                // generic
+                AreEqual(expect, result);   
+                IsTrue(reuse == result); // same reference - size did not change
+                
+                object resultObj = read.ReadToObject(arr1, reuse);      // non generic
+                AreEqual(expect, resultObj);
+                IsTrue(reuse == resultObj); // same reference - size did not change
+            }
+        }
+        
+        private static void AssertWriterBytes(IJsonWriter write) {
+            using (var dst  = new TestBytes())
+            {
+                // --- Write ---
+                write.Write(1, ref dst.bytes);                          // generic
+                AreEqual("1", dst.bytes.ToString());
+                
+                write.WriteObject(1, ref dst.bytes);                    // non generic 
+                AreEqual("1", dst.bytes.ToString());
+            }
+        }
+
+        private static void AssertReaderStream(IJsonReader read) {
+            // --- Read ---
+            AreEqual(1, read.Read<int>(StreamFromString("1")));                     // generic
+            
+            AreEqual(1, read.ReadObject(StreamFromString("1"), typeof(int)));       // non generic
+            
+          
+            // --- ReadTo ---
+            int[] reuse  = new int[1];
+            int[] expect = { 1 };
+            int[] result = read.ReadTo(StreamFromString("[1]"), reuse);             // generic
+            AreEqual(expect, result);   
+            IsTrue(reuse == result); // same reference - size did not change
+            
+            object resultObj = read.ReadToObject(StreamFromString("[1]"), reuse);   // non generic
+            AreEqual(expect, resultObj);
+            IsTrue(reuse == resultObj); // same reference - size did not change
+        }
+        
+        private static void AssertWriterStream(IJsonWriter write) {
+#if !JSON_BURST   // todo
+            // --- Write ---
+            Stream stream = new MemoryStream();
+            write.Write(1, stream);                                                 // generic
+            AreEqual("1", StringFromStream(stream));
+
+            stream.Position = 0;
+            write.WriteObject(1, stream);                                           // non generic 
+            AreEqual("1", StringFromStream(stream));
+#endif
+        }
+
+        private static void AssertReaderString(IJsonReader read) {
+            // --- Read ---
+            AreEqual(1, read.Read<int>("1"));                       // generic
+            
+            AreEqual(1, read.ReadObject("1", typeof(int)));         // non generic
+            
+           
+            // --- ReadTo ---
+            int[] reuse  = new int[1];
+            int[] expect = { 1 };
+            int[] result = read.ReadTo("[1]", reuse);               // generic
+            AreEqual(expect, result);   
+            IsTrue(reuse == result); // same reference - size did not change
+            
+            object resultObj = read.ReadToObject("[1]", reuse);     // non generic
+            AreEqual(expect, resultObj);
+            IsTrue(reuse == resultObj); // same reference - size did not change
+        }
+        
+        private static void AssertWriterString(IJsonWriter write) {
+            // --- Write ---
+            var json1 = write.Write(1);                             // generic
+            AreEqual("1", json1);
+            
+            var json2 = write.WriteObject(1);                       // non generic 
+            AreEqual("1", json2);
+        }
+        
+        // ----------------------------------- utils -----------------------------------
+        private static Stream StreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+        
+        private static string StringFromStream(Stream stream)
+        {
+            stream.Position = 0;
+            StreamReader reader = new StreamReader(stream);
+            string str = reader.ReadToEnd();
+            return str;
         }
     }
 }
