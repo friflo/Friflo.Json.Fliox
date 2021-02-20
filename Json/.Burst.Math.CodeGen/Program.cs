2d0f18e6bb7c3514f7225ce8a0c992379bbaf882
Ullrich Praetz <ullrich@praetz.de>
2021-02-20 06:35:23 +0100

    Math.CodeGen: Add RenderType() & RenderTypeDim1()

--- Json/.Burst.Math.CodeGen/Program.cs
+++ Json/.Burst.Math.CodeGen/Program.cs
@@ -21,11 +21,58 @@ namespace Friflo.Json.Burst.Math.CodeGen
             }
         }
         
-        private static void RenderType(StringBuilder sb, string name) {
-            
-            sb.Append("// Copyright (c) Ullrich Praetz. All rights reserved.\n// See LICENSE file in the project root for full license information.");
-            
-            
+        private static void RenderType(StringBuilder sb, string name)
+        {
+            var header = $@"// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+using Unity.Mathematics;
+
+#if JSON_BURST
+    using Str32 = Unity.Collections.FixedString32;
+#else
+    using Str32 = System.String;
+#endif
+
+// ReSharper disable InconsistentNaming
+namespace Friflo.Json.Burst.Math
+{{
+    public static partial class Json
+    {{";
+            sb.Append(header);
+
+            RenderTypeDim1(sb, name, 2);
+            RenderTypeDim1(sb, name, 3);
+            RenderTypeDim1(sb, name, 4);
+
+            var footer = $@"    }}
+}}
+";
+            sb.Append(footer);
+        }
+
+        private static void RenderTypeDim1(StringBuilder sb, string name, int size) {
+        
+        var str = $@"
+        public static bool UseMemberFloatX{size}(this ref JObj i, ref JsonParser p, in Str32 key, ref float{size} value) {{
+            if (i.UseMemberArr(ref p, in key, out JArr arr)) {{
+                ReadFloat{size}(ref arr, ref p, ref value);
+                return true;
+            }}
+            return false;
+        }}
+
+        private static void ReadFloat{size}(ref JArr i, ref JsonParser p, ref float{size} value) {{
+            int index = 0;
+            while (i.NextArrayElement(ref p)) {{
+                if (i.UseElementNum(ref p)) {{
+                    if (index < {size})
+                        value[index++] = p.ValueAsFloat(out bool _);
+                }} else 
+                    p.ErrorMsg(""Json.Burst.Math"", ""expect JSON number"");
+            }}
+        }}
+";
+        sb.Append(str);
         }
     }
 }
--- Json/Burst.Math/float.gen.cs
+++ Json/Burst.Math/float.gen.cs
@@ -1,2 +1,73 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
+using Unity.Mathematics;
+
+#if JSON_BURST
+    using Str32 = Unity.Collections.FixedString32;
+#else
+    using Str32 = System.String;
+#endif
+
+// ReSharper disable InconsistentNaming
+namespace Friflo.Json.Burst.Math
+{
+    public static partial class Json
+    {
+        public static bool UseMemberFloatX2(this ref JObj i, ref JsonParser p, in Str32 key, ref float2 value) {
+            if (i.UseMemberArr(ref p, in key, out JArr arr)) {
+                ReadFloat2(ref arr, ref p, ref value);
+                return true;
+            }
+            return false;
+        }
+
+        private static void ReadFloat2(ref JArr i, ref JsonParser p, ref float2 value) {
+            int index = 0;
+            while (i.NextArrayElement(ref p)) {
+                if (i.UseElementNum(ref p)) {
+                    if (index < 2)
+                        value[index++] = p.ValueAsFloat(out bool _);
+                } else 
+                    p.ErrorMsg("Json.Burst.Math", "expect JSON number");
+            }
+        }
+
+        public static bool UseMemberFloatX3(this ref JObj i, ref JsonParser p, in Str32 key, ref float3 value) {
+            if (i.UseMemberArr(ref p, in key, out JArr arr)) {
+                ReadFloat3(ref arr, ref p, ref value);
+                return true;
+            }
+            return false;
+        }
+
+        private static void ReadFloat3(ref JArr i, ref JsonParser p, ref float3 value) {
+            int index = 0;
+            while (i.NextArrayElement(ref p)) {
+                if (i.UseElementNum(ref p)) {
+                    if (index < 3)
+                        value[index++] = p.ValueAsFloat(out bool _);
+                } else 
+                    p.ErrorMsg("Json.Burst.Math", "expect JSON number");
+            }
+        }
+
+        public static bool UseMemberFloatX4(this ref JObj i, ref JsonParser p, in Str32 key, ref float4 value) {
+            if (i.UseMemberArr(ref p, in key, out JArr arr)) {
+                ReadFloat4(ref arr, ref p, ref value);
+                return true;
+            }
+            return false;
+        }
+
+        private static void ReadFloat4(ref JArr i, ref JsonParser p, ref float4 value) {
+            int index = 0;
+            while (i.NextArrayElement(ref p)) {
+                if (i.UseElementNum(ref p)) {
+                    if (index < 4)
+                        value[index++] = p.ValueAsFloat(out bool _);
+                } else 
+                    p.ErrorMsg("Json.Burst.Math", "expect JSON number");
+            }
+        }
+    }
+}
