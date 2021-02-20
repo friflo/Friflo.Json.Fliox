b2fe96b2c74f965087bb8a12b75d56e069a2a351
Ullrich Praetz <ullrich@praetz.de>
2021-02-20 05:49:15 +0100

    Prepare Math.CodeGen

---
+++ Json/Burst.Math.CodeGen/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Friflo.Json.Burst.Math.CodeGen
+{
+    class Program
+    {
+        private static void Main(string[] args) {
+            var sb = new StringBuilder();
+
+            string type = "float";
+            
+            RenderType(sb, type);
+            
+            string baseDir = Directory.GetCurrentDirectory() + "/../../../../Burst.Math/";
+            baseDir = Path.GetFullPath(baseDir);
+            string path = baseDir + type + ".gen.cs";
+            using (StreamWriter fileStream = new StreamWriter(path)) {
+                fileStream.Write(sb);
+            }
+        }
+        
+        private static void RenderType(StringBuilder sb, string name) {
+            
+            sb.Append("// Copyright (c) Ullrich Praetz. All rights reserved.\n// See LICENSE file in the project root for full license information.");
+            
+            
+        }
+    }
+}
