e50494003afdb83c9e5a1eca71bd07a95ab0eb8b
Ullrich Praetz <ullrich@praetz.de>
2021-02-23 09:56:13 +0100

    add unit test: TestFactoryCollection()

--- Json.Tests/Common/UnitTest/Mapper/TestMapper.cs
+++ Json.Tests/Common/UnitTest/Mapper/TestMapper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using Friflo.Json.Burst;
 using Friflo.Json.Mapper;
@@ -165,18 +167,18 @@ namespace Friflo.Json.Tests.Common.UnitTest.Mapper
         }
 
         // ------ interface support
-        [JsonType (InstanceFactory = typeof(TestFactory))]
-        interface ITest {
+        [JsonType (InstanceFactory = typeof(BookFactory))]
+        interface IBook {
         }
 
-        class TestImpl : ITest {
+        class Book : IBook {
             public int int32;
         }
 
-        class TestFactory : InstanceFactory<ITest>
+        class BookFactory : InstanceFactory<IBook>
         {
-            public override ITest CreateInstance(string name) {
-                return new TestImpl();
+            public override IBook CreateInstance(string name) {
+                return new Book();
             }
         }
         
@@ -190,8 +192,8 @@ namespace Friflo.Json.Tests.Common.UnitTest.Mapper
             using (var writer = new JsonWriter(typeStore))
             {
                 // typeStore.AddInstanceFactory(new TestFactory());
-                var result = reader.Read<ITest>(json);
-                AreEqual(123, ((TestImpl)result).int32);
+                var result = reader.Read<IBook>(json);
+                AreEqual(123, ((Book)result).int32);
                 
                 var jsonResult = writer.Write(result);
                 AreEqual(json, jsonResult);
@@ -236,5 +238,44 @@ namespace Friflo.Json.Tests.Common.UnitTest.Mapper
             }
         }
         
+        // ------ factory instances within collection
+        
+        class FactoryCollection
+        {
+            public List<IBook>      iTest   = new List<IBook>();
+            public List<IAnimal>    animals = new List<IAnimal>();
+        }
+        
+        [Test]  public void  TestFactoryCollectionReflect()   { TestFactoryCollection(TypeAccess.Reflection); }
+        [Test]  public void  TestFactoryCollectionIL()        { TestFactoryCollection(TypeAccess.IL); }
+        
+        private void TestFactoryCollection(TypeAccess typeAccess) {
+            var json = @"
+{
+    ""iTest"": [
+        {
+            ""int32"":123
+        }
+    ],
+    ""animals"": [
+        {
+            ""animalType"":""Lion"",
+            ""int32"":123
+        }
+    ]
+}";
+            string expect = string.Concat(json.Where(c => !char.IsWhiteSpace(c)));
+            
+            using (var typeStore = new TypeStore(null, new StoreConfig(typeAccess)))
+            using (var reader = new JsonReader(typeStore, JsonReader.NoThrow))
+            using (var writer = new JsonWriter(typeStore))
+            {
+                // typeStore.AddInstanceFactory(new TestFactory());
+                var result = reader.Read<FactoryCollection>(json);
+                
+                var jsonResult = writer.Write(result);
+                AreEqual(expect, jsonResult);
+            }
+        }
     }
 }
