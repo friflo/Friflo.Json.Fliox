5d42af4c6c2f7b5bdb3c22fda35a9d6c85b0fddc
Ullrich Praetz <ullrich@praetz.de>
2021-02-24 07:41:36 +0100

    add example TestPolymorphClass

---
+++ Json.Tests/Common/Examples/Mapper/PolymorphClass.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Friflo.Json.Mapper;
+using NUnit.Framework;
+
+namespace Friflo.Json.Tests.Common.Examples.Mapper
+{
+    public class TestPolymorphClass
+    {
+        [Discriminator("vehicleType")]
+        [Polymorph(typeof(Car),     Discriminant = "car")]
+        [Polymorph(typeof(Bike),    Discriminant = "bike")]
+        class Vehicle {
+        }
+        
+        class Car : Vehicle {
+            public int  seatCount;
+        }
+        
+        class Bike : Vehicle {
+            public bool hasLuggageRack;
+        }
+
+        
+        [Test]
+        public void Run() {
+            string json = @"
+ [
+    {
+        ""vehicleType"":    ""car"",
+        ""seatCount"":      4
+    },
+    {
+        ""vehicleType"":    ""bike"",
+        ""hasLuggageRack"": true
+    }
+]";
+            using (var m = new JsonMapper()) {
+                var vehicles = m.Read<Vehicle[]>(json);
+
+                var result = m.Write(vehicles);
+                string expect = string.Concat(json.Where(c => !char.IsWhiteSpace(c)));
+
+                Assert.AreEqual(expect, result);
+            }
+        }
+    }
+}
