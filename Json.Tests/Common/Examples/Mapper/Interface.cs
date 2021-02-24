a7a21f469bb4deba3c7f62042654c706c405215e
Ullrich Praetz <ullrich@praetz.de>
2021-02-24 07:54:59 +0100

    add example TestInterface

---
+++ Json.Tests/Common/Examples/Mapper/Interface.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Friflo.Json.Mapper;
+using NUnit.Framework;
+
+namespace Friflo.Json.Tests.Common.Examples.Mapper
+{
+    public class TestInterface
+    {
+        [Instance(typeof(Employee))]
+        interface IPerson {
+        }
+        
+        class Employee : IPerson {
+            public int employeeId;
+        }
+
+        [Test]
+        public void Run() {
+            string json = @"
+            {
+                ""employeeId"":   123
+            }";
+            using (var m = new JsonMapper()) {
+                var person = m.Read<IPerson>(json);
+
+                var result = m.Write(person);
+                string expect = string.Concat(json.Where(c => !char.IsWhiteSpace(c)));
+
+                Assert.AreEqual(expect, result);
+            }
+        }
+    }
+}
