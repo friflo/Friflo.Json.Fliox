057cf55fca09330b3a1673cfce2b2d4b5e51c3a7
Ullrich Praetz <ullrich@praetz.de>
2021-02-24 08:51:00 +0100

    Moved Attributes to separate file

---
+++ Json/Mapper/Attributes.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+using System;
+
+namespace Friflo.Json.Mapper
+{
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
+    public sealed class JsonTypeAttribute : Attribute {
+        public Type     TypeMapper    { get; set; }
+    }
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
+    public sealed class DiscriminatorAttribute : Attribute {
+        public DiscriminatorAttribute (string discriminator) {}
+    }
+    
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class, AllowMultiple = true)]
+    public sealed class PolymorphAttribute : Attribute {
+        public string     Discriminant    { get; set; }
+        public PolymorphAttribute (Type instance) {}
+    }
+    
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
+    public sealed class InstanceAttribute : Attribute {
+        public InstanceAttribute (Type instance) {}
+    }
+}
--- Json/Mapper/TypeStore.cs
+++ Json/Mapper/TypeStore.cs
@@ -130,25 +130,4 @@ namespace Friflo.Json.Mapper
             return null;
         }
     }
-
-    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
-    public sealed class JsonTypeAttribute : Attribute {
-        public Type     TypeMapper    { get; set; }
-    }
-    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
-    public sealed class DiscriminatorAttribute : Attribute {
-        public DiscriminatorAttribute (string discriminator) {}
-    }
-    
-    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class, AllowMultiple = true)]
-    public sealed class PolymorphAttribute : Attribute {
-        public string     Discriminant    { get; set; }
-        public PolymorphAttribute (Type instance) {}
-    }
-    
-    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
-    public sealed class InstanceAttribute : Attribute {
-        public InstanceAttribute (Type instance) {}
-    }
-
 }
