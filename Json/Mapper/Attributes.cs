94d341f5fa162d6020fbb8ace47b03aa5b538c14
Ullrich Praetz <ullrich@praetz.de>
2021-02-27 11:44:26 +0100

    cleanup

--- Json/Mapper/Attributes.cs
+++ Json/Mapper/Attributes.cs
@@ -4,50 +4,37 @@ using System;
 
 namespace Friflo.Json.Mapper
 {
+#if !UNITY_5_3_OR_NEWER
+    [CLSCompliant(true)]
+#endif
     public static class Fri {
-    #if !UNITY_5_3_OR_NEWER
-        [CLSCompliant(true)]
-    #endif
+
         [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
         public sealed class TypeMapperAttribute : Attribute {
             public TypeMapperAttribute (Type  typeMapper) {}
         }
         
-        
-    #if !UNITY_5_3_OR_NEWER
-        [CLSCompliant(true)]
-    #endif
         [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
         public sealed class DiscriminatorAttribute : Attribute {
             public DiscriminatorAttribute (string discriminator) {}
         }
-    #if !UNITY_5_3_OR_NEWER
-        [CLSCompliant(true)]
-    #endif
+
         [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class, AllowMultiple = true)]
         public sealed class PolymorphAttribute : Attribute {
             public string     Discriminant    { get; set; }
             public PolymorphAttribute (Type instance) {}
         }
-    #if !UNITY_5_3_OR_NEWER
-        [CLSCompliant(true)]
-    #endif
+
         [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
         public sealed class InstanceAttribute : Attribute {
             public InstanceAttribute (Type instance) {}
         }
         
-    #if !UNITY_5_3_OR_NEWER
-        [CLSCompliant(true)]
-    #endif
         [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
         public sealed class PropertyAttribute : Attribute {
             public string     Name    { get; set; }
         }
         
-    #if !UNITY_5_3_OR_NEWER
-        [CLSCompliant(true)]
-    #endif
         [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
         public sealed class IgnoreAttribute : Attribute {
         }
