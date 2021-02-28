a7fdc87646fca1c4d765020cad1d04f284d48c3b
Ullrich Praetz <ullrich@praetz.de>
2021-02-28 09:54:35 +0100

    PoC: either id or entity is set

--- Json.Tests/Common/UnitTest/Misc/RelationPoC.cs
+++ Json.Tests/Common/UnitTest/Misc/RelationPoC.cs
@@ -25,15 +25,22 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc
 
     public class Ref<T> where T : Entity
     {
+        // either id or entity is set. Never both
         public string   id;
         public T        entity;
         
         public static implicit operator Ref<T>(T entity) {
             var reference = new Ref<T>();
             reference.entity    = entity;
-            reference.id        = entity.id;
             return reference;
         }
+        
+        public static implicit operator Ref<T>(string id) {
+            var reference = new Ref<T>();
+            reference.id    = id;
+            return reference;
+        }
+
     }
     
     // ------------------------------ models ------------------------------
@@ -68,12 +75,18 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc
             var customer    = db.CreateEntity<Customer>("customer-1");
             customer.lastName   = "Smith";
 
-            var article     = db.CreateEntity<Article>("article-1");
-            article.name        = "Camera";
+            var camera     = db.CreateEntity<Article>("article-1");
+            camera.name        = "Camera";
+
+            var item1 = new OrderItem();
+            item1.article = camera;     // assign reference
+            item1.amount = 1;
+            order.items.Add(item1);
             
-            var item = new OrderItem();
-            item.article = article;     // assign reference
-            order.items.Add(item);
+            var item2 = new OrderItem();
+            item2.article = "article-2";     // assign reference
+            item2.amount = 1;
+            order.items.Add(item2);
 
             order.customer = customer;  // assign reference
         }
