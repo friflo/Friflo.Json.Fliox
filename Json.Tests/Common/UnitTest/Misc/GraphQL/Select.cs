669d23a20fc81bdabf26c77a8d63d1290828fd01
Ullrich Praetz <ullrich@praetz.de>
2021-03-03 09:02:51 +0100

    Add DebugLinqQuery()

--- Json.Tests/Common/UnitTest/Misc/GraphQL/Select.cs
+++ Json.Tests/Common/UnitTest/Misc/GraphQL/Select.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Friflo.Json.Mapper;
@@ -33,7 +34,55 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc.GraphQL
                 Console.WriteLine(json);
             }
         }
-        
+
+        class GqlEnumerable<T> : IEnumerable<T>
+        {
+            private readonly IEnumerable<T> enumerable;
+            
+            public GqlEnumerable(IEnumerable<T> enumerable) {
+                this.enumerable = enumerable;
+            }
+            
+            public IEnumerator<T> GetEnumerator() {
+                return enumerable.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() {
+                return GetEnumerator();
+            }
+        }
+
+        private static bool WhereOrderEqual(Order order, string test) {
+            return order.id == test;
+        }
+
+        private static Order SelectOrder(Order order) {
+            return order;
+        }
+
+        [Test]
+        public void DebugLinqQuery() {
+            var order1 = TestRelationPoC.CreateOrder();
+            var orders = new List<Order> { order1 };
+            
+            IQueryable<Order> queryable = orders.AsQueryable(); // for illustration only: Create queryable explicit from orders
+
+            var gqlOrders = new GqlEnumerable<Order>(queryable); // <=> new GqlEnumerable<Order>(orders)
+            // var gqlOrders = new GqlEnumerable<Order>(orders);
+
+            var orderQuery =
+                from order in gqlOrders
+                where WhereOrderEqual(order, "order-1")  // where  order.id == "order-1"
+                select SelectOrder(order);               // select order;
+
+            int n = 0;
+            foreach (var order in orderQuery) {
+                n++;
+                IsTrue(order1 == order);
+            }
+            AreEqual(1, n);
+        }
+
         [Test]
         public void TestSelectSameInstance() {
             var order1 = TestRelationPoC.CreateOrder();
