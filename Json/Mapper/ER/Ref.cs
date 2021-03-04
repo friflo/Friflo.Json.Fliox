73b26315ea8cbb50695c07ea9a0071490e790916
Ullrich Praetz <ullrich@praetz.de>
2021-03-04 14:37:21 +0100

    add missing copyright header

--- Json/Mapper/ER/Ref.cs
+++ Json/Mapper/ER/Ref.cs
@@ -1,4 +1,7 @@
-﻿namespace Friflo.Json.Mapper.ER
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace Friflo.Json.Mapper.ER
 {
     // Change to attribute
     public class Entity {
