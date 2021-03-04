33ef03b71173f8edfdeaf41c3d0793fe2ac315c0
Ullrich Praetz <ullrich@praetz.de>
2021-03-04 15:48:54 +0100

    Add FileContainer<>

--- Json/Mapper/ER/FileContainer.cs
+++ Json/Mapper/ER/FileContainer.cs
@@ -1,31 +1,30 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Friflo.Json.Mapper.ER
 {
-    public class MemoryContainer<T> : EntityContainer<T> where T : Entity
+    public class FileContainer<T> : EntityContainer<T> where T : Entity
     {
-        private readonly Dictionary<string, T>  map                 = new Dictionary<string, T>();
+        private readonly string folder;
         
-        public MemoryContainer() { }
-
-        public override int Count => map.Count;
+        public FileContainer(string folder) {
+            this.folder = folder;
+        }
+        
+        public override int Count => throw new NotImplementedException();
 
 #pragma warning disable 1998 // This async method lacks 'await' operators and will run synchronously. Consider using the 'await' operator to await non-blocking API calls, or 'await TaskEx.Run(...)' to do CPU-bound work on a background thread
         public override async Task AddEntities(IEnumerable<T> entities) {
-            foreach (var entity in entities) {
-                map.Add(entity.id, entity);
-            }
+            throw new NotImplementedException();
         }
 
         public override async Task<IEnumerable<T>> GetEntities(IEnumerable<string> ids) {
-            var result = new List<T>();
-            foreach (var id in ids) {
-                result.Add(map[id]);
-            }
-            return result;
+            throw new NotImplementedException();
+            // var result = new List<T>();
+            // return result;
         }
 #pragma warning restore 1998
     }
