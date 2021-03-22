5d6d4fa9e4767d39f1945c99b1d774b9c6a24ef6
Ullrich Praetz <ullrich@praetz.de>
2021-03-22 14:46:16 +0100

    cleanup

--- Json/Mapper/Diff/DiffNode.cs
+++ Json/Mapper/Diff/DiffNode.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+using System.Collections.Generic;
 using System.Text;
 using Friflo.Json.Mapper.Map;
 
