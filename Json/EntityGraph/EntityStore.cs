ec0b71ffd0d5043e6567231f2a59452734bbc244
Ullrich Praetz <ullrich@praetz.de>
2021-03-26 05:17:34 +0100

    separate SynRequest

--- Json/EntityGraph/EntityStore.cs
+++ Json/EntityGraph/EntityStore.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Friflo.Json.EntityGraph.Database;
 using Friflo.Json.EntityGraph.Map;
@@ -105,7 +104,7 @@ namespace Friflo.Json.EntityGraph
     // ----------------------------------------- CRUD -----------------------------------------
     public class Read<T>
     {
-        internal readonly   string id;
+        private  readonly   string id;
         internal            T      result;
         internal            bool   synced;
 
@@ -164,63 +163,4 @@ namespace Friflo.Json.EntityGraph
             this.entity = entity;
         }
     }
-
-
-    // ----------------------------------- StoreSyncRequest -----------------------------------
-    public class SyncRequest
-    {
-        public List<DatabaseCommand> commands;
-
-        public void Execute(EntityDatabase database) {
-            foreach (var command in commands) {
-                command.Execute(database);
-            }
-        }
-    }
-
-    [Fri.Discriminator("command")]
-    [Fri.Polymorph(typeof(CreateEntities),  Discriminant = "create")]
-    [Fri.Polymorph(typeof(ReadEntities),    Discriminant = "read")]
-    public abstract class DatabaseCommand
-    {
-        public abstract void        Execute(EntityDatabase database);
-        public abstract CommandType CommandType { get; }
-    }
-    
-    public class CreateEntities : DatabaseCommand
-    {
-        public  string              containerName;
-        public  List<KeyValue>      entities;
-
-        public override CommandType CommandType => CommandType.Create;
-
-        public override void Execute(EntityDatabase database) {
-            var container = database.GetContainer(containerName);
-            container.CreateEntities(entities);   
-        }
-    }
-    
-    public class ReadEntities : DatabaseCommand
-    {
-        public  string              containerName;
-        public  List<string>        ids;
-        
-        [Fri.Ignore]
-        public  List<KeyValue>      entitiesResult;
-
-        
-        public override CommandType CommandType => CommandType.Read;
-        
-        public override void Execute(EntityDatabase database) {
-            var container = database.GetContainer(containerName);
-            entitiesResult = container.ReadEntities(ids).ToList(); 
-        }
-    }
-
-    public enum CommandType
-    {
-        Read,
-        Create
-    }
-
 }
---
+++ Json/EntityGraph/SyncRequest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Friflo.Json.EntityGraph.Database;
+using Friflo.Json.Mapper;
+
+namespace Friflo.Json.EntityGraph
+{
+    public class SyncRequest
+    {
+        public List<DatabaseCommand> commands;
+
+        public void Execute(EntityDatabase database) {
+            foreach (var command in commands) {
+                command.Execute(database);
+            }
+        }
+    }
+
+    [Fri.Discriminator("command")]
+    [Fri.Polymorph(typeof(CreateEntities),  Discriminant = "create")]
+    [Fri.Polymorph(typeof(ReadEntities),    Discriminant = "read")]
+    public abstract class DatabaseCommand
+    {
+        public abstract void        Execute(EntityDatabase database);
+        public abstract CommandType CommandType { get; }
+    }
+    
+    public class CreateEntities : DatabaseCommand
+    {
+        public  string              containerName;
+        public  List<KeyValue>      entities;
+
+        public override CommandType CommandType => CommandType.Create;
+
+        public override void Execute(EntityDatabase database) {
+            var container = database.GetContainer(containerName);
+            container.CreateEntities(entities);   
+        }
+    }
+    
+    public class ReadEntities : DatabaseCommand
+    {
+        public  string              containerName;
+        public  List<string>        ids;
+        
+        [Fri.Ignore]
+        public  List<KeyValue>      entitiesResult;
+
+        
+        public override CommandType CommandType => CommandType.Read;
+        
+        public override void Execute(EntityDatabase database) {
+            var container = database.GetContainer(containerName);
+            entitiesResult = container.ReadEntities(ids).ToList(); 
+        }
+    }
+
+    public enum CommandType
+    {
+        Read,
+        Create
+    }
+}
