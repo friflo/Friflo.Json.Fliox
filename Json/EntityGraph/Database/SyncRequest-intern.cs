df71157386a63b1ed08aafed3c17850d1feeffb1
Ullrich Praetz <ullrich@praetz.de>
2021-04-06 14:24:24 +0200

    separate protocol methods and made them internal

---
+++ Json/EntityGraph/Database/SyncRequest-intern.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using Friflo.Json.Burst; // UnityExtension.TryAdd()
+
+namespace Friflo.Json.EntityGraph.Database
+{
+    // ------------------------------ SyncRequest / SyncResponse ------------------------------
+    public partial class SyncResponse
+    {
+        internal ContainerEntities GetContainerResult(string container) {
+            if (containerResults.TryGetValue(container, out ContainerEntities result))
+                return result;
+            result = new ContainerEntities {
+                container = container,
+                entities = new Dictionary<string,EntityValue>()
+            };
+            containerResults.Add(container, result);
+            return result;
+        }
+    }
+    
+    // ------ ContainerEntities
+    public partial class ContainerEntities
+    {
+        internal void AddEntities(Dictionary<string, EntityValue> add) {
+            foreach (var entity in add) {
+                entities.TryAdd(entity.Key, entity.Value);
+            }
+        }
+    }
+    
+    // ------ CreateEntities
+    public partial class CreateEntities
+    {
+        internal override CommandType   CommandType => CommandType.Create;
+        public   override string        ToString() => "container: " + container;
+        
+        internal override CommandResult Execute(EntityDatabase database, SyncResponse response) {
+            var entityContainer = database.GetContainer(container);
+            // may call patcher.Copy() always to ensure a valid JSON value
+            if (entityContainer.Pretty) {
+                var patcher = entityContainer.SyncContext.jsonPatcher;
+                foreach (var entity in entities) {
+                    entity.Value.value.json = patcher.Copy(entity.Value.value.json, true);
+                }
+            }
+            entityContainer.CreateEntities(entities);
+            return new CreateEntitiesResult();
+        }
+    }
+    
+    // ------ ReadEntities
+    public partial class ReadEntities
+    {
+        internal override CommandType   CommandType => CommandType.Read;
+        public   override string        ToString() => "container: " + container;
+        
+        internal override CommandResult Execute(EntityDatabase database, SyncResponse response) {
+            var entityContainer = database.GetContainer(container);
+            var entities = entityContainer.ReadEntities(ids);
+            var containerResult = response.GetContainerResult(container);
+            containerResult.AddEntities(entities);
+            var dependencyResults = entityContainer.ReadDependencies(dependencies, entities, response);
+            var result = new ReadEntitiesResult {
+                dependencies = dependencyResults
+            };
+            return result;
+        }
+    }
+    
+    public partial class ReadEntitiesResult
+    {
+        internal override CommandType CommandType => CommandType.Read;
+    }
+    
+    // ------ PatchEntities
+    public partial class PatchEntities
+    {
+        internal override CommandType   CommandType => CommandType.Patch;
+        public   override string        ToString() => "container: " + container;
+        
+        internal override CommandResult Execute(EntityDatabase database, SyncResponse response) {
+            var entityContainer = database.GetContainer(container);
+            entityContainer.PatchEntities(entityPatches);
+            return new PatchEntitiesResult(); 
+        }
+    }
+}
--- Json/EntityGraph/Database/SyncRequest.cs
+++ Json/EntityGraph/Database/SyncRequest.cs
@@ -2,7 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
-using Friflo.Json.Burst; // UnityExtension.TryAdd()
 using Friflo.Json.Mapper;
 using Friflo.Json.Mapper.Graph;
 
@@ -11,49 +10,31 @@ namespace Friflo.Json.EntityGraph.Database
     // ------------------------------ SyncRequest / SyncResponse ------------------------------
     public class SyncRequest
     {
-        public List<DatabaseCommand> commands;
+        public  List<DatabaseCommand>                   commands;
     }
     
-    public class SyncResponse
+    public partial class SyncResponse
     {
         public  List<CommandResult>                     results;
         public  Dictionary<string, ContainerEntities>   containerResults;
-
-        public ContainerEntities GetContainerResult(string container) {
-            if (containerResults.TryGetValue(container, out ContainerEntities result))
-                return result;
-            result = new ContainerEntities {
-                container = container,
-                entities = new Dictionary<string,EntityValue>()
-            };
-            containerResults.Add(container, result);
-            return result;
-        }
     }
     
     // ------ ContainerEntities
-    public class ContainerEntities
+    public partial class ContainerEntities
     {
         public  string                          container; // only for debugging
         public  Dictionary<string, EntityValue> entities;
-
-        public void AddEntities(Dictionary<string, EntityValue> add) {
-            foreach (var entity in add) {
-                entities.TryAdd(entity.Key, entity.Value);
-            }
-        }
     }
     
     // ------------------------------ DatabaseCommand ------------------------------
-    
     [Fri.Discriminator("command")]
     [Fri.Polymorph(typeof(CreateEntities),          Discriminant = "create")]
     [Fri.Polymorph(typeof(ReadEntities),            Discriminant = "read")]
     [Fri.Polymorph(typeof(PatchEntities),           Discriminant = "patch")]
     public abstract class DatabaseCommand
     {
-        public abstract CommandResult   Execute(EntityDatabase database, SyncResponse response);
-        public abstract CommandType     CommandType { get; }
+        internal abstract CommandResult   Execute(EntityDatabase database, SyncResponse response);
+        internal abstract CommandType     CommandType { get; }
     }
     
     // ------------------------------ CommandResult ------------------------------
@@ -63,7 +44,7 @@ namespace Friflo.Json.EntityGraph.Database
     [Fri.Polymorph(typeof(PatchEntitiesResult),     Discriminant = "patch")]
     public abstract class CommandResult
     {
-        public abstract CommandType CommandType { get; }
+        internal abstract CommandType CommandType { get; }
     }
     
     public enum CommandType
@@ -74,62 +55,29 @@ namespace Friflo.Json.EntityGraph.Database
     }
     
     // ------ CreateEntities
-    public class CreateEntities : DatabaseCommand
+    public partial class CreateEntities : DatabaseCommand
     {
         public  string                          container;
         public  Dictionary<string, EntityValue> entities;
-
-        public override CommandType CommandType => CommandType.Create;
-        public override string      ToString() => "container: " + container;
-
-        public override CommandResult Execute(EntityDatabase database, SyncResponse response) {
-            var entityContainer = database.GetContainer(container);
-            // may call patcher.Copy() always to ensure a valid JSON value
-            if (entityContainer.Pretty) {
-                var patcher = entityContainer.SyncContext.jsonPatcher;
-                foreach (var entity in entities) {
-                    entity.Value.value.json = patcher.Copy(entity.Value.value.json, true);
-                }
-            }
-            entityContainer.CreateEntities(entities);
-            return new CreateEntitiesResult();
-        }
     }
     
     public class CreateEntitiesResult : CommandResult
     {
-        public override CommandType CommandType => CommandType.Create;
+        internal override CommandType CommandType => CommandType.Create;
     }
 
     // ------ ReadEntities
-    public class ReadEntities : DatabaseCommand
+    public partial class ReadEntities : DatabaseCommand
     {
-        public  string                  container;
-        public  List<string>            ids;
-        public  List<ReadDependency>    dependencies;
-        
-        public override CommandType CommandType => CommandType.Read;
-        public override string      ToString() => "container: " + container;
-        
-        public override CommandResult Execute(EntityDatabase database, SyncResponse response) {
-            var entityContainer = database.GetContainer(container);
-            var entities = entityContainer.ReadEntities(ids);
-            var containerResult = response.GetContainerResult(container);
-            containerResult.AddEntities(entities);
-            var dependencyResults = entityContainer.ReadDependencies(dependencies, entities, response);
-            var result = new ReadEntitiesResult {
-                dependencies    = dependencyResults
-            };
-            return result; 
-        }
+        public  string                      container;
+        public  List<string>                ids;
+        public  List<ReadDependency>        dependencies;
     }
     
     /// The data of requested entities are added to <see cref="ContainerEntities.entities"/> 
-    public class ReadEntitiesResult : CommandResult
+    public partial class ReadEntitiesResult : CommandResult
     {
         public  List<ReadDependencyResult>  dependencies;
-
-        public override CommandType CommandType => CommandType.Read;
     }
     
     // ------ ReadDependency
@@ -137,41 +85,32 @@ namespace Friflo.Json.EntityGraph.Database
     {
         /// Path to a <see cref="Ref{T}"/> field referencing an <see cref="Entity"/>.
         /// These dependent entities are also loaded via the next <see cref="EntityStore.Sync"/> request.
-        public  string          refPath; // e.g. ".items[*].article"
-        public  string          container;
-        public  List<string>    ids;
+        public  string              refPath; // e.g. ".items[*].article"
+        public  string              container;
+        public  List<string>        ids;
     }
     
     public class ReadDependencyResult
     {
-        public  string          container;
-        public  List<string>    ids;
+        public  string              container;
+        public  List<string>        ids;
     }
     
     // ------ PatchEntities
-    public class PatchEntities : DatabaseCommand
+    public partial class PatchEntities : DatabaseCommand
     {
         public  string              container;
         public  List<EntityPatch>   entityPatches;
-        
-        public override CommandType CommandType => CommandType.Patch;
-        public override string      ToString() => "container: " + container;
-        
-        public override CommandResult Execute(EntityDatabase database, SyncResponse response) {
-            var entityContainer = database.GetContainer(container);
-            entityContainer.PatchEntities(entityPatches);
-            return new PatchEntitiesResult(); 
-        }
     }
 
     public class EntityPatch
     {
-        public string       id;
-        public List<Patch>  patches;
+        public string               id;
+        public List<Patch>          patches;
     }
 
     public class PatchEntitiesResult : CommandResult
     {
-        public override CommandType CommandType => CommandType.Patch;
+        internal override CommandType CommandType => CommandType.Patch;
     }
 }
