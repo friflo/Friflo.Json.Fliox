839ca1b771c66e11b751a2ae46a17a292f05faf0
Ullrich Praetz <ullrich@praetz.de>
2021-04-09 06:46:36 +0200

    separate logical & compare operators in two files

--- Json/EntityGraph/Filter/GraphOp-Compare.cs
+++ Json/EntityGraph/Filter/GraphOp-Compare.cs
@@ -6,13 +6,11 @@ using Friflo.Json.Mapper.Graph;
 
 namespace Friflo.Json.EntityGraph.Filter
 {
-    public abstract class BoolOp : GraphOp { }
-
     // -------------------------------------- comparison operators --------------------------------------
     public abstract class BinaryBoolOp : BoolOp
     {
-        public GraphOp      left;
-        public GraphOp      right;
+        protected GraphOp      left;
+        protected GraphOp      right;
 
         protected BinaryBoolOp(GraphOp left, GraphOp right) {
             this.left = left;
@@ -122,121 +120,4 @@ namespace Friflo.Json.EntityGraph.Filter
             return result;
         }
     }
-
-    
-    // -------------------------------------- logical operators --------------------------------------
-    // --- unary logical operators
-    public abstract class UnaryBoolOp : BoolOp
-    {
-        public BoolOp       operand;     // e.g.   i => i.amount < 1
-
-        public UnaryBoolOp(BoolOp operand) { this.operand = operand; }
-        
-        internal override void Init(GraphOpContext cx) {
-            operand.Init(cx);
-        }
-    }
-    
-    public class Not : UnaryBoolOp
-    {
-        public override     string      ToString() => $"!({operand})";
-        
-        public Not(BoolOp operand) : base(operand) { }
-        
-        internal override List<SelectorValue> Eval() {
-            var eval = operand.Eval();
-            foreach (var result in eval) {
-                if (result.CompareTo(True) == 0)
-                    return SingleFalse;
-            }
-            return SingleTrue;
-        }
-    }
-    
-    public class Any : UnaryBoolOp
-    {
-        public override     string      ToString() => $"Any({operand})";
-        
-        public Any(BoolOp operand) : base(operand) { }
-        
-        internal override List<SelectorValue> Eval() {
-            var eval = operand.Eval();
-            foreach (var result in eval) {
-                if (result.CompareTo(True) == 0)
-                    return SingleTrue;
-            }
-            return SingleFalse;
-        }
-    }
-    
-    public class All : UnaryBoolOp
-    {
-        public override     string      ToString() => $"All({operand})";
-        
-        public All(BoolOp operand) : base(operand) { }
-        
-        internal override List<SelectorValue> Eval() {
-            var eval = operand.Eval();
-            foreach (var result in eval) {
-                if (result.CompareTo(True) != 0)
-                    return SingleFalse;
-            }
-            return SingleTrue;
-        }
-    }
-    
-    // --- (n-ary) group logical operators
-    public abstract class GroupBoolOp : BoolOp
-    {
-        public List<BoolOp>       operands;
-
-        public GroupBoolOp(List<BoolOp> operands) { this.operands = operands; }
-        
-        internal override void Init(GraphOpContext cx) {
-            foreach (var operand in operands) {
-                operand.Init(cx);
-            }
-        }
-    }
-    
-    public class And : GroupBoolOp
-    {
-        public override     string      ToString() => string.Join(" && ", operands);
-        
-        public And(List<BoolOp> operands) : base(operands) { }
-        
-        internal override List<SelectorValue> Eval() {
-            var results = new List<SelectorValue>();
-            foreach (var operand in operands) {
-                var eval = operand.Eval();
-                results.AddRange(eval);
-            }
-            foreach (var result in results) {
-                if (result.CompareTo(True) != 0)
-                    return SingleFalse;
-            }
-            return SingleTrue;
-        }
-    }
-    
-    public class Or : GroupBoolOp
-    {
-        public override     string      ToString() => string.Join(" && ", operands);
-        
-        public Or(List<BoolOp> operands) : base(operands) { }
-        
-        internal override List<SelectorValue> Eval() {
-            var results = new List<SelectorValue>();
-            foreach (var operand in operands) {
-                var eval = operand.Eval();
-                results.AddRange(eval);
-            }
-            foreach (var result in results) {
-                if (result.CompareTo(True) == 0)
-                    return SingleTrue;
-            }
-            return SingleFalse;
-        }
-    }
-    
 }
---
+++ Json/EntityGraph/Filter/GraphOp-Logical.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using Friflo.Json.Mapper.Graph;
+
+namespace Friflo.Json.EntityGraph.Filter
+{
+    public abstract class BoolOp : GraphOp { }
+    
+    // -------------------------------------- logical operators --------------------------------------
+    // --- unary logical operators
+    public abstract class UnaryBoolOp : BoolOp
+    {
+        protected BoolOp       operand;     // e.g.   i => i.amount < 1
+
+        protected UnaryBoolOp(BoolOp operand) { this.operand = operand; }
+        
+        internal override void Init(GraphOpContext cx) {
+            operand.Init(cx);
+        }
+    }
+    
+    public class Not : UnaryBoolOp
+    {
+        public override     string      ToString() => $"!({operand})";
+        
+        public Not(BoolOp operand) : base(operand) { }
+        
+        internal override List<SelectorValue> Eval() {
+            var eval = operand.Eval();
+            foreach (var result in eval) {
+                if (result.CompareTo(True) == 0)
+                    return SingleFalse;
+            }
+            return SingleTrue;
+        }
+    }
+    
+    public class Any : UnaryBoolOp
+    {
+        public override     string      ToString() => $"Any({operand})";
+        
+        public Any(BoolOp operand) : base(operand) { }
+        
+        internal override List<SelectorValue> Eval() {
+            var eval = operand.Eval();
+            foreach (var result in eval) {
+                if (result.CompareTo(True) == 0)
+                    return SingleTrue;
+            }
+            return SingleFalse;
+        }
+    }
+    
+    public class All : UnaryBoolOp
+    {
+        public override     string      ToString() => $"All({operand})";
+        
+        public All(BoolOp operand) : base(operand) { }
+        
+        internal override List<SelectorValue> Eval() {
+            var eval = operand.Eval();
+            foreach (var result in eval) {
+                if (result.CompareTo(True) != 0)
+                    return SingleFalse;
+            }
+            return SingleTrue;
+        }
+    }
+    
+    // --- (n-ary) group logical operators
+    public abstract class GroupBoolOp : BoolOp
+    {
+        protected List<BoolOp>       operands;
+
+        protected GroupBoolOp(List<BoolOp> operands) { this.operands = operands; }
+        
+        internal override void Init(GraphOpContext cx) {
+            foreach (var operand in operands) {
+                operand.Init(cx);
+            }
+        }
+    }
+    
+    public class And : GroupBoolOp
+    {
+        public override     string      ToString() => string.Join(" && ", operands);
+        
+        public And(List<BoolOp> operands) : base(operands) { }
+        
+        internal override List<SelectorValue> Eval() {
+            var results = new List<SelectorValue>();
+            foreach (var operand in operands) {
+                var eval = operand.Eval();
+                results.AddRange(eval);
+            }
+            foreach (var result in results) {
+                if (result.CompareTo(True) != 0)
+                    return SingleFalse;
+            }
+            return SingleTrue;
+        }
+    }
+    
+    public class Or : GroupBoolOp
+    {
+        public override     string      ToString() => string.Join(" && ", operands);
+        
+        public Or(List<BoolOp> operands) : base(operands) { }
+        
+        internal override List<SelectorValue> Eval() {
+            var results = new List<SelectorValue>();
+            foreach (var operand in operands) {
+                var eval = operand.Eval();
+                results.AddRange(eval);
+            }
+            foreach (var result in results) {
+                if (result.CompareTo(True) == 0)
+                    return SingleTrue;
+            }
+            return SingleFalse;
+        }
+    }
+    
+}
