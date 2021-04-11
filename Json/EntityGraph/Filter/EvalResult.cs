a66cce0e490ff04c4b42be8105eec8bdbf15949f
Ullrich Praetz <ullrich@praetz.de>
2021-04-11 15:23:04 +0200

    moved EvalResult to new file

---
+++ Json/EntityGraph/Filter/EvalResult.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+using System.Collections.Generic;
+using Friflo.Json.Mapper.Graph;
+
+namespace Friflo.Json.EntityGraph.Filter
+{
+    internal readonly struct EvalResult
+    {
+        internal readonly  List<Scalar> values;
+
+        internal EvalResult (Scalar singleValue) {
+            values = new List<Scalar> { singleValue };
+        }
+        
+        internal EvalResult (List<Scalar> values) {
+            this.values = values;
+        }
+
+        internal int Count => values.Count;
+
+        internal void Clear() {
+            values.Clear();
+        }
+        
+        internal void Add(Scalar value) {
+            values.Add(value);
+        }
+        
+        internal void SetSingle(Scalar value) {
+            values[0] = value;
+        }
+        
+    }
+}
--- Json/EntityGraph/Filter/Operator.cs
+++ Json/EntityGraph/Filter/Operator.cs
@@ -31,35 +31,6 @@ namespace Friflo.Json.EntityGraph.Filter
         }
     }
 
-    internal readonly struct EvalResult
-    {
-        internal readonly  List<Scalar> values;
-
-        internal EvalResult (Scalar singleValue) {
-            values = new List<Scalar> { singleValue };
-        }
-        
-        internal EvalResult (List<Scalar> values) {
-            this.values = values;
-        }
-
-        internal int Count => values.Count;
-
-        internal void Clear() {
-            values.Clear();
-        }
-        
-        internal void Add(Scalar value) {
-            values.Add(value);
-        }
-        
-        internal void SetSingle(Scalar value) {
-            values[0] = value;
-        }
-        
-    }
-    
-    
     internal class GraphOpContext
     {
         internal readonly Dictionary<string, Field> selectors = new Dictionary<string, Field>();
