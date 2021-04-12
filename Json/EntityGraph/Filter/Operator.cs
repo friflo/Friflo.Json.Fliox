6fb27c610ca67f76b3d0ba783048101cff1ee49b
Ullrich Praetz <ullrich@praetz.de>
2021-04-12 14:00:04 +0200

    fix Unity compiler error

--- Json/EntityGraph/Filter/Operator.cs
+++ Json/EntityGraph/Filter/Operator.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq.Expressions;
+using Friflo.Json.Burst; // UnityExtension.TryAdd()
 using Friflo.Json.Mapper.Patch;
 
 namespace Friflo.Json.EntityGraph.Filter
