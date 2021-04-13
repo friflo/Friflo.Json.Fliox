a1490a0cecda7a2c3cdf235b51416f6c254abb1c
Ullrich Praetz <ullrich@praetz.de>
2021-04-13 09:14:04 +0200

    removed leading dot from field path (selector)

--- Json.Tests/Common/UnitTest/EntityGraph/TestStore.cs
+++ Json.Tests/Common/UnitTest/EntityGraph/TestStore.cs
@@ -139,12 +139,12 @@ namespace Friflo.Json.Tests.Common.UnitTest.EntityGraph
             var read6 = store.orders.ReadWhere(o => o.items.Any(i => i.article.Entity.name == "Smartphone"));
 
             
-            Dependency<Customer>     customer   = order1.DependencyByPath<Customer>(".customer");
-            Dependency<Customer>     customer2  = order1.DependencyByPath<Customer>(".customer");
+            Dependency<Customer>     customer   = order1.DependencyByPath<Customer>("customer");
+            Dependency<Customer>     customer2  = order1.DependencyByPath<Customer>("customer");
             AreSame(customer, customer2);
             Dependency<Customer>     customer3  = order1.Dependency(o => o.customer);
             AreSame(customer, customer3);
-            AreEqual("'order-1' .customer", customer.ToString());
+            AreEqual("'order-1' customer", customer.ToString());
             
             Exception e;
             e = Throws<PeerNotSyncedException>(() => { var _ = customer.Id; });
@@ -163,18 +163,18 @@ namespace Friflo.Json.Tests.Common.UnitTest.EntityGraph
             AreEqual("Smith",       customer.Result.lastName);
 
             // schedule dependency an already synced Read operation
-            e = Throws<InvalidOperationException>(() => { order1.DependencyByPath<Article>(".customer"); });
+            e = Throws<InvalidOperationException>(() => { order1.DependencyByPath<Article>("customer"); });
             AreEqual("Read already synced. Type: Order, id: order-1", e.Message);
-            e = Throws<InvalidOperationException>(() => { order1.DependenciesByPath<Article>(".items[*].article"); });
+            e = Throws<InvalidOperationException>(() => { order1.DependenciesByPath<Article>("items[*].article"); });
             AreEqual("Read already synced. Type: Order, id: order-1", e.Message);
 
             order1 =    store.orders.Read("order-1");
-            Dependencies<Article>    articleDeps    = order1.DependenciesByPath<Article>(".items[*].article");
-            Dependencies<Article>    articleDeps2   = order1.DependenciesByPath<Article>(".items[*].article");
+            Dependencies<Article>    articleDeps    = order1.DependenciesByPath<Article>("items[*].article");
+            Dependencies<Article>    articleDeps2   = order1.DependenciesByPath<Article>("items[*].article");
             AreSame(articleDeps, articleDeps2);
             Dependencies<Article>    articleDeps3   = order1.Dependencies(o => o.items.Select(a => a.article));
             AreSame(articleDeps, articleDeps3);
-            AreEqual("'order-1' .items[*].article", articleDeps.ToString());
+            AreEqual("'order-1' items[*].article", articleDeps.ToString());
             
             e = Throws<PeerNotSyncedException>(() => { var _ = articleDeps[0].Id; });
             AreEqual("Dependencies not synced. Dependencies<Article>", e.Message);
--- Json/EntityGraph/MemberSelector.cs
+++ Json/EntityGraph/MemberSelector.cs
@@ -15,17 +15,18 @@ namespace Friflo.Json.EntityGraph
             if (selector is LambdaExpression lambda) {
                 var body = lambda.Body;
                 var sb = new StringBuilder();
-                TraceExpression(body, sb, ref isArraySelector);
+                TraceExpression(body, sb, ref isArraySelector, true);
                 return sb.ToString();
             }
             throw new NotSupportedException($"selector not supported: {selector}");
         }
 
-        private static void TraceExpression(Expression expression, StringBuilder sb, ref bool isArraySelector) {
+        private static void TraceExpression(Expression expression, StringBuilder sb, ref bool isArraySelector, bool isRoot) {
             switch (expression) {
                 case MemberExpression member:
                     MemberInfo memberInfo = member.Member;
-                    sb.Append('.');
+                    if (!isRoot)
+                        sb.Append('.');
                     sb.Append(memberInfo.Name);
                     if (typeof(IEnumerable).IsAssignableFrom(expression.Type)) {
                         sb.Append("[*]");
@@ -36,12 +37,12 @@ namespace Friflo.Json.EntityGraph
                     var args = methodCall.Arguments;
                     for (int n = 0; n < args.Count; n++) {
                         var arg = args[n];
-                        TraceExpression(arg, sb, ref isArraySelector);
+                        TraceExpression(arg, sb, ref isArraySelector, true);
                     }
                     return;
                 case LambdaExpression lambda: 
                     var body = lambda.Body;
-                    TraceExpression(body, sb, ref isArraySelector);
+                    TraceExpression(body, sb, ref isArraySelector, false);
                     return;
                 default:
                     throw new NotSupportedException($"Body not supported: {expression}");
