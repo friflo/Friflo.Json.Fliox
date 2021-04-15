7c9f874280b1ec986cccfe00a018f28dcac9206b
Ullrich Praetz <ullrich@praetz.de>
2021-04-15 06:24:04 +0200

    rename BinaryQuantifyOp fields

--- Json/Flow/Graph/Query/Operator-Quantify.cs
+++ Json/Flow/Graph/Query/Operator-Quantify.cs
@@ -5,32 +5,32 @@ namespace Friflo.Json.Flow.Graph.Query
 {
     public abstract class BinaryQuantifyOp : BoolOp
     {
-        protected       Field       group;
-        protected       BoolOp      operand;     // e.g.   i => i.amount < 1
+        protected       Field       field;
+        protected       BoolOp      predicate;     // e.g.   i => i.amount < 1
 
-        protected BinaryQuantifyOp(Field group, BoolOp operand) {
-            this.group      = group;
-            this.operand    = operand;
+        protected BinaryQuantifyOp(Field field, BoolOp predicate) {
+            this.field      = field;
+            this.predicate  = predicate;
         }
         
         internal override void Init(OperatorContext cx) {
             cx.ValidateReuse(this); // results are reused
-            group.Init(cx);
-            operand.Init(cx);
+            field.Init(cx);
+            predicate.Init(cx);
         }
     }
     
     public class Any : BinaryQuantifyOp
     {
-        public override     string      ToString() => $"Any({group}, {operand})";
+        public override     string      ToString() => $"Any({field}, {predicate})";
 
-        public Any(Field group, BoolOp operand) : base(group, operand) { }
+        public Any(Field field, BoolOp predicate) : base(field, predicate) { }
         
         internal override EvalResult Eval(EvalCx cx) {
-            var groupEval = group.Eval(cx);
+            var groupEval = field.Eval(cx);
             for (int groupIndex = 0; groupIndex < groupEval.Count; groupIndex++) {
                 var groupCx = new EvalCx(groupIndex);
-                var eval = operand.Eval(groupCx);
+                var eval = predicate.Eval(groupCx);
                 foreach (var val in eval.values) {
                     if (val.CompareTo(True) == 0)
                         return SingleTrue;
@@ -42,15 +42,15 @@ namespace Friflo.Json.Flow.Graph.Query
     
     public class All : BinaryQuantifyOp
     {
-        public override     string      ToString() => $"All({group}, {operand})";
+        public override     string      ToString() => $"All({field}, {predicate})";
         
-        public All(Field group, BoolOp operand) : base(group, operand) { }
+        public All(Field field, BoolOp predicate) : base(field, predicate) { }
         
         internal override EvalResult Eval(EvalCx cx) {
-            var groupEval = group.Eval(cx);
+            var groupEval = field.Eval(cx);
             for (int groupIndex = 0; groupIndex < groupEval.Count; groupIndex++) {
                 var groupCx = new EvalCx(groupIndex);
-                var eval = operand.Eval(groupCx);
+                var eval = predicate.Eval(groupCx);
                 foreach (var val in eval.values) {
                     if (val.CompareTo(True) != 0)
                         return SingleFalse;
