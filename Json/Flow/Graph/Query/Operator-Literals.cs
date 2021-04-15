c604ae42ed0801f519bf4f863cee1ef049a049c4
Ullrich Praetz <ullrich@praetz.de>
2021-04-15 11:50:25 +0200

    separate literal operators to new file

---
+++ Json/Flow/Graph/Query/Operator-Literals.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Globalization;
+using Friflo.Json.Flow.Graph.Select;
+
+// ReSharper disable ConvertToAutoProperty
+namespace Friflo.Json.Flow.Graph.Query
+{
+    // --- literals
+    public abstract class Literal : Operator {
+        // is set always to the same value in Eval() so it can be reused
+        internal  readonly  EvalResult          evalResult = new EvalResult(new List<Scalar> {new Scalar()});
+        
+        internal override void Init(OperatorContext cx) { }
+    }
+        
+    public class StringLiteral : Literal
+    {
+        public              string      value;
+        
+        public override     string      ToString() => $"'{value}'";
+
+        public StringLiteral(string value) { this.value = value; }
+
+        internal override EvalResult Eval(EvalCx cx) {
+            evalResult.SetSingle(new Scalar(value));
+            return evalResult;
+        }
+    }
+    
+    public class DoubleLiteral : Literal
+    {
+        private             double      value;
+
+        public override     string      ToString() => value.ToString(CultureInfo.InvariantCulture);
+
+        public DoubleLiteral(double value) { this.value = value; }
+
+        internal override EvalResult Eval(EvalCx cx) {
+            evalResult.SetSingle(new Scalar(value));
+            return evalResult;
+        }
+    }
+    
+    public class LongLiteral : Literal
+    {
+        private             long      value;
+
+        public override     string      ToString() => value.ToString();
+
+        public LongLiteral(long value) { this.value = value; }
+
+        internal override EvalResult Eval(EvalCx cx) {
+            evalResult.SetSingle(new Scalar(value));
+            return evalResult;
+        }
+    }
+    
+    public class BoolLiteral : Literal
+    {
+        public bool         value;
+        
+        public override     string      ToString() => value ? "true" : "false";
+
+        public BoolLiteral(bool value) {
+            this.value = value;
+        }
+
+        internal override EvalResult Eval(EvalCx cx) {
+            evalResult.SetSingle(value ? True : False);
+            return evalResult;
+        }
+    }
+
+    public class NullLiteral : Literal
+    {
+        public override     string      ToString() => "null";
+
+
+        internal override EvalResult Eval(EvalCx cx) {
+            evalResult.SetSingle(Null);
+            return evalResult;
+        }
+    }
+}
--- Json/Flow/Graph/Query/Operator.cs
+++ Json/Flow/Graph/Query/Operator.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq.Expressions;
 using Friflo.Json.Burst; // UnityExtension.TryAdd()
 using Friflo.Json.Flow.Graph.Select;
@@ -94,83 +93,6 @@ namespace Friflo.Json.Flow.Graph.Query
                 endIndex = evalResult.values.Count;
             }
             evalResult.SetRange(startIndex, endIndex);
-            return evalResult;
-        }
-    }
-
-    // --- primitive operators ---
-    public abstract class Literal : Operator {
-        // is set always to the same value in Eval() so it can be reused
-        internal  readonly  EvalResult          evalResult = new EvalResult(new List<Scalar> {new Scalar()});
-        
-        internal override void Init(OperatorContext cx) { }
-    }
-        
-    public class StringLiteral : Literal
-    {
-        public              string      value;
-        
-        public override     string      ToString() => $"'{value}'";
-
-        public StringLiteral(string value) { this.value = value; }
-
-        internal override EvalResult Eval(EvalCx cx) {
-            evalResult.SetSingle(new Scalar(value));
-            return evalResult;
-        }
-    }
-    
-    public class DoubleLiteral : Literal
-    {
-        private             double      value;
-
-        public override     string      ToString() => value.ToString(CultureInfo.InvariantCulture);
-
-        public DoubleLiteral(double value) { this.value = value; }
-
-        internal override EvalResult Eval(EvalCx cx) {
-            evalResult.SetSingle(new Scalar(value));
-            return evalResult;
-        }
-    }
-    
-    public class LongLiteral : Literal
-    {
-        private             long      value;
-
-        public override     string      ToString() => value.ToString();
-
-        public LongLiteral(long value) { this.value = value; }
-
-        internal override EvalResult Eval(EvalCx cx) {
-            evalResult.SetSingle(new Scalar(value));
-            return evalResult;
-        }
-    }
-    
-    public class BoolLiteral : Literal
-    {
-        public bool         value;
-        
-        public override     string      ToString() => value ? "true" : "false";
-
-        public BoolLiteral(bool value) {
-            this.value = value;
-        }
-
-        internal override EvalResult Eval(EvalCx cx) {
-            evalResult.SetSingle(value ? True : False);
-            return evalResult;
-        }
-    }
-
-    public class NullLiteral : Literal
-    {
-        public override     string      ToString() => "null";
-
-
-        internal override EvalResult Eval(EvalCx cx) {
-            evalResult.SetSingle(Null);
             return evalResult;
         }
     }
