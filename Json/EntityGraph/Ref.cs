baa21e70600c40907b653d7273e321e1aac8bde7
Ullrich Praetz <ullrich@praetz.de>
2021-04-22 07:19:43 +0200

    add runtime assertion: Entity id must not be changed.

--- Json/EntityGraph/Ref.cs
+++ Json/EntityGraph/Ref.cs
@@ -2,13 +2,30 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
+using System.Diagnostics;
 using Friflo.Json.EntityGraph.Internal;
+// ReSharper disable InconsistentNaming
 
 namespace Friflo.Json.EntityGraph
 {
     // Change to attribute
-    public class Entity {
-        public              string  id;
+    public class Entity
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private string _id;
+        
+        public string id {
+            get => _id;
+            set {
+                if (_id == value)
+                    return;
+                if (_id == null) {
+                    _id = value;
+                    return;
+                }
+                throw new InvalidOperationException($"Entity id must not be changed. Type: {GetType()}, id: {_id}, used: {value}");
+            }
+        }
 
         public override     string  ToString() => id ?? "null";
     }
