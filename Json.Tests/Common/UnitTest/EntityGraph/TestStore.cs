661eb8d21164facb9e47f7e431fd8e6bd5cc782f
Ullrich Praetz <ullrich@praetz.de>
2021-04-23 11:12:11 +0200

    separate test assertion AssertEntityIdentity()

--- Json.Tests/Common/UnitTest/EntityGraph/TestStore.cs
+++ Json.Tests/Common/UnitTest/EntityGraph/TestStore.cs
@@ -94,6 +94,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.EntityGraph
 
         private static async Task TestStores(PocStore createStore, PocStore useStore) {
             await WriteRead(createStore);
+            await AssertReading(createStore);
             await AssertEmptyStore(useStore);
         }
 
@@ -114,14 +115,14 @@ namespace Friflo.Json.Tests.Common.UnitTest.EntityGraph
         } 
 
 
-        private static async Task WriteRead(PocStore store) {
+        private static async Task WriteRead(PocStore createStore) {
             // --- cache empty
-            var order = store.orders.Read("order-1");
-            await store.Sync();
-            // var xxx = order.Result.customer.Entity;
-            using (ObjectMapper mapper = new ObjectMapper(store.TypeStore)) {
+            var order = createStore.orders.Read("order-1");
+            await createStore.Sync();
+            
+            using (ObjectMapper mapper = new ObjectMapper(createStore.TypeStore)) {
                 WriteRead(order.Result, mapper);
-                await AssertStore(order.Result, store);
+                await AssertEntityIdentity(createStore);
             }
         }
         
@@ -169,37 +170,36 @@ namespace Friflo.Json.Tests.Common.UnitTest.EntityGraph
 
         private static bool lab = false;
 
-        private static async Task AssertStore(Order order, PocStore store) {
-            var orders      = store.orders;
-            var articles    = store.articles;
-            var customers   = store.customers;
+        private static async Task AssertReading(PocStore store) {
+            var orders = store.orders;
+            var articles = store.articles;
 
-            ReadTask<Order> order1 =    orders.Read("order-1");
+            ReadTask<Order> order1 = orders.Read("order-1");
             AreEqual("order-1", order1.ToString());
-            var allArticles     = articles.QueryAll();
-            var allArticles2    = articles.QueryByFilter(Operation.FilterTrue);
-            var hasOrderCamera  = orders.Query(o => o.items.Any(i => i.name == "Camera"));
-            var read1           = orders.Query(o => o.customer.id == "customer-1");
-            var read2           = orders.Query(o => o.customer.Entity.lastName == "Smith");
-            var read3           = orders.Query(o => o.items.Count(i => i.amount < 1) > 0);
-            var read4           = orders.Query(o => o.items.Any(i => i.amount < 1));
-            var read5           = orders.Query(o => o.items.All(i => i.amount < 1));
-            var read6           = orders.Query(o => o.items.Any(i => i.article.Entity.name == "Smartphone"));
+            var allArticles = articles.QueryAll();
+            var allArticles2 = articles.QueryByFilter(Operation.FilterTrue);
+            var hasOrderCamera = orders.Query(o => o.items.Any(i => i.name == "Camera"));
+            var read1 = orders.Query(o => o.customer.id == "customer-1");
+            var read2 = orders.Query(o => o.customer.Entity.lastName == "Smith");
+            var read3 = orders.Query(o => o.items.Count(i => i.amount < 1) > 0);
+            var read4 = orders.Query(o => o.items.Any(i => i.amount < 1));
+            var read5 = orders.Query(o => o.items.All(i => i.amount < 1));
+            var read6 = orders.Query(o => o.items.Any(i => i.article.Entity.name == "Smartphone"));
 
-            
-            ReadRefTask<Customer>     customer   = order1.ReadRefByPath<Customer>(".customer");
-            ReadRefTask<Customer>     customer2  = order1.ReadRefByPath<Customer>(".customer");
+
+            ReadRefTask<Customer> customer = order1.ReadRefByPath<Customer>(".customer");
+            ReadRefTask<Customer> customer2 = order1.ReadRefByPath<Customer>(".customer");
             AreSame(customer, customer2);
-            ReadRefTask<Customer>     customer3  = order1.ReadRef(o => o.customer);
+            ReadRefTask<Customer> customer3 = order1.ReadRef(o => o.customer);
             AreSame(customer, customer3);
             AreEqual("Order['order-1'] .customer", customer.ToString());
-            
+
             Exception e;
             e = Throws<PeerNotSyncedException>(() => { var _ = customer.Id; });
             AreEqual("ReadRefTask.Id requires Sync(). Order['order-1'] .customer", e.Message);
             e = Throws<PeerNotSyncedException>(() => { var _ = customer.Result; });
             AreEqual("ReadRefTask.Result requires Sync(). Order['order-1'] .customer", e.Message);
-            
+
             e = Throws<PeerNotSyncedException>(() => { var _ = hasOrderCamera.Result; });
             AreEqual("QueryTask.Result requires Sync(). Entity: Order filter: .items.Any(i => i.name == 'Camera')", e.Message);
             e = Throws<PeerNotSyncedException>(() => { var _ = hasOrderCamera[0]; });
@@ -207,18 +207,18 @@ namespace Friflo.Json.Tests.Common.UnitTest.EntityGraph
 
             // lab - test ReadRef expressions
             if (lab) {
-                ReadRefsTask<Article> articles2 =   order1.ReadRefsOfType<Article>();
-                ReadRefsTask<Entity> allDeps =      order1.ReadAllRefs();
+                ReadRefsTask<Article> articles2 = order1.ReadRefsOfType<Article>();
+                ReadRefsTask<Entity> allDeps = order1.ReadAllRefs();
             }
 
-            await store.Sync();  // -------- Sync --------
+            await store.Sync(); // -------- Sync --------
 
-            AreEqual(4,             allArticles.Result.Count);
-            AreEqual(1,             hasOrderCamera.Result.Count);
-            AreEqual("order-1",     hasOrderCamera[0].id);
+            AreEqual(4, allArticles.Result.Count);
+            AreEqual(1, hasOrderCamera.Result.Count);
+            AreEqual("order-1", hasOrderCamera[0].id);
 
-            AreEqual("customer-1",  customer.Id);
-            AreEqual("Smith",       customer.Result.lastName);
+            AreEqual("customer-1", customer.Id);
+            AreEqual("Smith", customer.Result.lastName);
 
             // schedule ReadRefs on an already synced Read operation
             e = Throws<InvalidOperationException>(() => { order1.ReadRefByPath<Article>("customer"); });
@@ -226,27 +226,40 @@ namespace Friflo.Json.Tests.Common.UnitTest.EntityGraph
             e = Throws<InvalidOperationException>(() => { order1.ReadRefsByPath<Article>("items[*].article"); });
             AreEqual("Used ReadTask is already synced. ReadTask<Order>, id: order-1", e.Message);
 
-            order1 =    orders.Read("order-1");
-            ReadRefsTask<Article>    articleDeps    = order1.ReadRefsByPath<Article>(".items[*].article");
-            ReadRefsTask<Article>    articleDeps2   = order1.ReadRefsByPath<Article>(".items[*].article");
+            order1 = orders.Read("order-1");
+            ReadRefsTask<Article> articleDeps = order1.ReadRefsByPath<Article>(".items[*].article");
+            ReadRefsTask<Article> articleDeps2 = order1.ReadRefsByPath<Article>(".items[*].article");
             AreSame(articleDeps, articleDeps2);
-            ReadRefsTask<Article>    articleDeps3   = order1.ReadRefs(o => o.items.Select(a => a.article));
+            ReadRefsTask<Article> articleDeps3 = order1.ReadRefs(o => o.items.Select(a => a.article));
             AreSame(articleDeps, articleDeps3);
             AreEqual("Order['order-1'] .items[*].article", articleDeps.ToString());
-            
+
             e = Throws<PeerNotSyncedException>(() => { var _ = articleDeps[0]; });
             AreEqual("ReadRefsTask[] requires Sync(). Order['order-1'] .items[*].article", e.Message);
             e = Throws<PeerNotSyncedException>(() => { var _ = articleDeps.Results; });
             AreEqual("ReadRefsTask.Results requires Sync(). Order['order-1'] .items[*].article", e.Message);
 
             await store.Sync(); // -------- Sync --------
+
+            AreEqual("article-1", articleDeps[0].Id);
+            AreEqual("Changed name", articleDeps[0].Result.name);
+            AreEqual("article-2", articleDeps[1].Id);
+            AreEqual("Smartphone", articleDeps[1].Result.name);
+            AreEqual("article-1", articleDeps[2].Id);
+            AreEqual("Changed name", articleDeps[2].Result.name);
+        }
+        
+        private static async Task AssertEntityIdentity(PocStore store) {
+            var orderTask = store.orders.Read("order-1");
+            await store.Sync();
             
-            AreEqual("article-1",       articleDeps[0].Id);
-            AreEqual("Changed name",    articleDeps[0].Result.name);
-            AreEqual("article-2",       articleDeps[1].Id);
-            AreEqual("Smartphone",      articleDeps[1].Result.name);
-            AreEqual("article-1",       articleDeps[2].Id);
-            AreEqual("Changed name",    articleDeps[2].Result.name);
+            var order = orderTask.Result;
+            
+            var articles    = store.articles;
+            var customers   = store.customers;
+            var orders      = store.orders;
+            
+            ReadTask<Order> order1 = orders.Read("order-1");
             
             var article1            =  articles.Read("article-1");
             var article1Redundant   =  articles.Read("article-1");
