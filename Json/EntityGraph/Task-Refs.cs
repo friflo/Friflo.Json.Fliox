6928c70644c3e8ff7d0131b2e55726321d79fa4a
Ullrich Praetz <ullrich@praetz.de>
2021-04-26 09:00:51 +0200

    separate RefsTask<> to new file

---
+++ Json/EntityGraph/Task-Refs.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Friflo.Json.EntityGraph.Internal;
+
+namespace Friflo.Json.EntityGraph
+{
+    internal interface ISetTask
+    {
+        string  Label { get; }
+    }
+    
+    public abstract class RefsTask<T> : ISetTask where T : Entity
+    {
+        internal            bool                                synced;
+        /// key: <see cref="ISubRefsTask.Selector"/>
+        internal readonly   Dictionary<string, ISubRefsTask>    subRefs;
+        
+        public   abstract   string                              Label { get; }
+
+        internal RefsTask() {
+            this.subRefs    = new Dictionary<string, ISubRefsTask>();
+        }
+        
+        private   Exception AlreadySyncedError() {
+            return new InvalidOperationException($"Used task is already synced. {Label}");
+        }
+        
+        protected Exception RequiresSyncError(string message) {
+            return new TaskNotSyncedException($"{message} {Label}");
+        }
+        
+        private SubRefsTask<TValue> SubRefsByExpression<TValue>(Expression selector) where TValue : Entity {
+            string path = MemberSelector.PathFromExpression(selector, out _);
+            return SubRefsByPath<TValue>(path);
+        }
+        
+        private SubRefsTask<TValue> SubRefsByPath<TValue>(string selector) where TValue : Entity {
+            if (synced)
+                throw AlreadySyncedError();
+            if (subRefs.TryGetValue(selector, out ISubRefsTask subRefsTask))
+                return (SubRefsTask<TValue>)subRefsTask;
+            var newQueryRefs = new SubRefsTask<TValue>(this, selector, typeof(TValue).Name);
+            subRefs.Add(selector, newQueryRefs);
+            return newQueryRefs;
+        }
+        
+        public SubRefsTask<TValue> SubRef<TValue>(Expression<Func<T, Ref<TValue>>> selector) where TValue : Entity {
+            if (synced)
+                throw AlreadySyncedError();
+            return SubRefsByExpression<TValue>(selector);
+        }
+        
+        public SubRefsTask<TValue> SubRefs<TValue>(Expression<Func<T, IEnumerable<Ref<TValue>>>> selector) where TValue : Entity {
+            if (synced)
+                throw AlreadySyncedError();
+            return SubRefsByExpression<TValue>(selector);
+        }
+    }
+}
--- Json/EntityGraph/Task-SubRefs.cs
+++ Json/EntityGraph/Task-SubRefs.cs
@@ -1,66 +1,11 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
-using Friflo.Json.EntityGraph.Internal;
+
 
 namespace Friflo.Json.EntityGraph
 {
-    internal interface ISetTask
-    {
-        string  Label { get; }
-    }
-    
-    public abstract class RefsBase<T> : ISetTask where T : Entity
-    {
-        internal            bool                                synced;
-        /// key: <see cref="ISubRefsTask.Selector"/>
-        internal readonly   Dictionary<string, ISubRefsTask>    subRefs;
-        
-        public   abstract   string                              Label { get; }
-
-        internal RefsBase() {
-            this.subRefs    = new Dictionary<string, ISubRefsTask>();
-        }
-        
-        private   Exception AlreadySyncedError() {
-            return new InvalidOperationException($"Used task is already synced. {Label}");
-        }
-        
-        protected Exception RequiresSyncError(string message) {
-            return new TaskNotSyncedException($"{message} {Label}");
-        }
-        
-        private SubRefsTask<TValue> SubRefsByExpression<TValue>(Expression selector) where TValue : Entity {
-            string path = MemberSelector.PathFromExpression(selector, out _);
-            return SubRefsByPath<TValue>(path);
-        }
-        
-        private SubRefsTask<TValue> SubRefsByPath<TValue>(string selector) where TValue : Entity {
-            if (synced)
-                throw AlreadySyncedError();
-            if (subRefs.TryGetValue(selector, out ISubRefsTask subRefsTask))
-                return (SubRefsTask<TValue>)subRefsTask;
-            var newQueryRefs = new SubRefsTask<TValue>(this, selector, typeof(TValue).Name);
-            subRefs.Add(selector, newQueryRefs);
-            return newQueryRefs;
-        }
-        
-        public SubRefsTask<TValue> SubRef<TValue>(Expression<Func<T, Ref<TValue>>> selector) where TValue : Entity {
-            if (synced)
-                throw AlreadySyncedError();
-            return SubRefsByExpression<TValue>(selector);
-        }
-        
-        public SubRefsTask<TValue> SubRefs<TValue>(Expression<Func<T, IEnumerable<Ref<TValue>>>> selector) where TValue : Entity {
-            if (synced)
-                throw AlreadySyncedError();
-            return SubRefsByExpression<TValue>(selector);
-        }
-    }
-
     // ----------------------------------------- QueryRefsTask -----------------------------------------
     public interface ISubRefsTask
     {
@@ -69,7 +14,7 @@ namespace Friflo.Json.EntityGraph
         Dictionary<string, ISubRefsTask>    SubRefs     { get; }
     }
 
-    public class SubRefsTask<T> : RefsBase<T>, ISubRefsTask where T : Entity
+    public class SubRefsTask<T> : RefsTask<T>, ISubRefsTask where T : Entity
     {
         private   readonly  ISetTask                            parent;
         internal  readonly  Dictionary<string, T>               results = new Dictionary<string, T>();
