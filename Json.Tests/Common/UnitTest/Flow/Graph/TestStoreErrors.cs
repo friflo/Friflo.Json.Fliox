98ee8edc4414827ab76880b15f2860f12c70e50c
Ullrich Praetz <ullrich@praetz.de>
2021-05-04 10:46:44 +0200

    prepare TestStoreErrors

---
+++ Json.Tests/Common/UnitTest/Flow/Graph/TestStoreErrors.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using Friflo.Json.Flow.Database;
+using Friflo.Json.Flow.Graph;
+using Friflo.Json.Flow.Transform;
+using Friflo.Json.Tests.Common.Utils;
+using Friflo.Json.Tests.Unity.Utils;
+using UnityEngine.TestTools;
+using static NUnit.Framework.Assert;
+
+#if UNITY_5_3_OR_NEWER
+    using UnitTest.Dummy;
+#else
+    using NUnit.Framework;
+#endif
+
+namespace Friflo.Json.Tests.Common.UnitTest.Flow.Graph
+{
+    public class TestStoreErrors : LeakTestsFixture
+    {
+        [UnityTest] public IEnumerator FileEmptyCoroutine() { yield return RunAsync.Await(FileEmpty()); }
+        [Test]      public async Task  FileEmptyAsync() { await FileEmpty(); }
+        
+        private async Task FileEmpty() {
+            using (var fileDatabase = new FileDatabase(CommonUtils.GetBasePath() + "assets/dbErrors"))
+            using (var useStore     = new PocStore(fileDatabase)) {
+                await TestStoresErrors(useStore);
+            }
+        }
+
+        private static async Task TestStoresErrors(PocStore useStore) {
+            await AssertQueryTask(useStore);
+        }
+        
+        private static async Task AssertQueryTask(PocStore store) {
+            var orders = store.orders;
+            var articles = store.articles;
+
+            var readOrders  = orders.Read();
+            var order1      = readOrders.Find("order-1");
+            AreEqual("ReadId<Order> id: order-1", order1.ToString());
+            var allArticles             =  articles.QueryAll();
+            var allArticles2            = articles.QueryByFilter(Operation.FilterTrue);
+            var producersTask           = allArticles.ReadRefs(a => a.producer);
+            var hasOrderCamera          = orders.Query(o => o.items.Any(i => i.name == "Camera"));
+            var ordersWithCustomer1     = orders.Query(o => o.customer.id == "customer-1");
+            var read3                   = orders.Query(o => o.items.Count(i => i.amount < 1) > 0);
+            var ordersAnyAmountLower2   = orders.Query(o => o.items.Any(i => i.amount < 2));
+            var ordersAllAmountGreater0 = orders.Query(o => o.items.All(i => i.amount > 0));
+
+            ReadRefTask<Customer> customer  = readOrders.ReadRefByPath<Customer>(".customer");
+            ReadRefTask<Customer> customer2 = readOrders.ReadRefByPath<Customer>(".customer");
+            AreSame(customer, customer2);
+            ReadRefTask<Customer> customer3 = readOrders.ReadRef(o => o.customer);
+            AreSame(customer, customer3);
+            AreEqual("ReadTask<Order> #ids: 1 > .customer", customer.ToString());
+
+            Exception e;
+            e = Throws<TaskNotSyncedException>(() => { var _ = customer.Id; });
+            AreEqual("ReadRefTask.Id requires Sync(). ReadTask<Order> #ids: 1 > .customer", e.Message);
+            e = Throws<TaskNotSyncedException>(() => { var _ = customer.Result; });
+            AreEqual("ReadRefTask.Result requires Sync(). ReadTask<Order> #ids: 1 > .customer", e.Message);
+
+            e = Throws<TaskNotSyncedException>(() => { var _ = hasOrderCamera.Results; });
+            AreEqual("QueryTask.Result requires Sync(). QueryTask<Order> filter: .items.Any(i => i.name == 'Camera')", e.Message);
+            e = Throws<TaskNotSyncedException>(() => { var _ = hasOrderCamera["arbitrary"]; });
+            AreEqual("QueryTask[] requires Sync(). QueryTask<Order> filter: .items.Any(i => i.name == 'Camera')", e.Message);
+
+            var producerEmployees = producersTask.ReadArrayRefs(p => p.employeeList);
+            AreEqual("QueryTask<Article> filter: true > .producer > .employees[*]", producerEmployees.ToString());
+
+
+            await store.Sync(); // -------- Sync --------
+            AreEqual(1,                 ordersWithCustomer1.Results.Count);
+            NotNull(ordersWithCustomer1["order-1"]);
+            
+            AreEqual(1,                 ordersAnyAmountLower2.Results.Count);
+            NotNull(ordersAnyAmountLower2["order-1"]);
+            
+            AreEqual(1,                 ordersAllAmountGreater0.Results.Count);
+            NotNull(ordersAllAmountGreater0["order-1"]);
+
+            AreEqual(4,                 allArticles.Results.Count);
+            AreEqual("Galaxy S10",      allArticles.Results["article-galaxy"].name);
+            AreEqual("iPad Pro",        allArticles.Results["article-ipad"].name);
+            
+            AreEqual(1,                 hasOrderCamera.Results.Count);
+            AreEqual(3,                 hasOrderCamera["order-1"].items.Count);
+    
+            AreEqual("customer-1",      customer.Id);
+            AreEqual("Smith Ltd.",      customer.Result.name);
+                
+            AreEqual(3,                 producersTask.Results.Count);
+            AreEqual("Samsung",         producersTask["producer-samsung"].name);
+            AreEqual("Canon",           producersTask["producer-canon"].name);
+            AreEqual("Apple",           producersTask["producer-apple"].name);
+                
+            AreEqual(1,                 producerEmployees.Results.Count);
+            AreEqual("Steve",           producerEmployees["apple-0001"].firstName);
+        }
+    }
+}
