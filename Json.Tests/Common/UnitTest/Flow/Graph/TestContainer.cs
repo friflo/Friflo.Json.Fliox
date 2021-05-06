57e8d5d2bb9b2e5c6bf157a0e842dda3089ba851
Ullrich Praetz <ullrich@praetz.de>
2021-05-06 17:24:26 +0200

    fixed Unity compiler error

--- Json.Tests/Common/UnitTest/Flow/Graph/TestContainer.cs
+++ Json.Tests/Common/UnitTest/Flow/Graph/TestContainer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Friflo.Json.Burst;  // UnityExtension.TryAdd(), ToHashSet()
 using Friflo.Json.Flow.Database;
 using Friflo.Json.Flow.Sync;
 
