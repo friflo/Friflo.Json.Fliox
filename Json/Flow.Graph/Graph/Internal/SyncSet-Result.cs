5f30a05fb84086aa56e13e32a12afe96be16e41e
Ullrich Praetz <ullrich@praetz.de>
2021-05-10 09:56:25 +0200

    moved SyncSet ...Result() methods to separate file

---
+++ Json/Flow.Graph/Graph/Internal/SyncSet-Result.cs
@@ -0,0 +1,230 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Friflo.Json.Flow.Sync;
+using Friflo.Json.Flow.Graph.Internal.Map;
+using Friflo.Json.Flow.Transform;
+
+namespace Friflo.Json.Flow.Graph.Internal
+{
+    internal abstract class SyncSet
+    {
+        internal    Dictionary<string, EntityError> createErrors = new Dictionary<string, EntityError>();
+        internal    Dictionary<string, EntityError> updateErrors = new Dictionary<string, EntityError>();
+        internal    Dictionary<string, EntityError> patchErrors  = new Dictionary<string, EntityError>();
+        internal    Dictionary<string, EntityError> deleteErrors = new Dictionary<string, EntityError>();
+
+        internal  abstract  void    AddTasks                (List<DatabaseTask> tasks);
+        
+        internal  abstract  void    CreateEntitiesResult    (CreateEntities     task, TaskResult result);
+        internal  abstract  void    UpdateEntitiesResult    (UpdateEntities     task, TaskResult result);
+        internal  abstract  void    ReadEntitiesListResult  (ReadEntitiesList   task, TaskResult result, ContainerEntities readEntities);
+        internal  abstract  void    QueryEntitiesResult     (QueryEntities      task, TaskResult result, ContainerEntities queryEntities);
+        internal  abstract  void    PatchEntitiesResult     (PatchEntities      task, TaskResult result);
+        internal  abstract  void    DeleteEntitiesResult    (DeleteEntities     task, TaskResult result);
+    }
+
+    internal partial class SyncSet<T>
+    {
+        internal override void CreateEntitiesResult(CreateEntities task, TaskResult result) {
+            CreateUpdateEntitiesResult(task.entities, result, createTasks, createErrors);
+        }
+
+        internal override void UpdateEntitiesResult(UpdateEntities task, TaskResult result) {
+            CreateUpdateEntitiesResult(task.entities, result, updateTasks, updateErrors);
+        }
+
+        private void CreateUpdateEntitiesResult(
+            Dictionary<string, EntityValue> entities,
+            TaskResult                      result,
+            List<WriteTask>                 writeTasks,
+            Dictionary<string, EntityError> writeErrors)
+        {
+            if (result is TaskError taskError) {
+                foreach (var writeTask in writeTasks) {
+                    writeTask.state.SetError(new TaskErrorInfo(taskError));
+                }
+                return;
+            }
+            foreach (var entry in entities) {
+                var id = entry.Key;
+                if (writeErrors != null && writeErrors.TryGetValue(id, out EntityError error)) {
+                    continue;
+                }
+                var peer = set.GetPeerById(id);
+                peer.created = false;
+                peer.updated = false;
+                peer.SetPatchSource(set.intern.jsonMapper.Read<T>(entry.Value.Json));
+            }
+            foreach (var writeTask in writeTasks) {
+                var entityErrorInfo = new TaskErrorInfo();
+                idsBuf.Clear();
+                writeTask.GetIds(idsBuf);
+                foreach (var id in idsBuf) {
+                    var value = entities[id];
+                    if (value.Error != null)
+                        entityErrorInfo.AddEntityError(value.Error);
+                }
+                if (entityErrorInfo.HasErrors) {
+                    writeTask.state.SetError(entityErrorInfo);
+                    continue;
+                }
+                writeTask.state.Synced = true;
+            }
+        }
+
+        internal override void ReadEntitiesListResult(ReadEntitiesList taskList, TaskResult result, ContainerEntities readEntities) {
+            if (result is TaskError taskError) {
+                foreach (var read in reads) {
+                    read.state.SetError(new TaskErrorInfo(taskError));
+                }
+                return;
+            }
+            var readListResult = (ReadEntitiesListResult) result;
+            var expect = reads.Count;
+            var actual = taskList.reads.Count;
+            if (expect != actual) {
+                throw new InvalidOperationException($"Expect reads.Count == result.reads.Count. expect: {expect}, actual: {actual}");
+            }
+
+            for (int i = 0; i < taskList.reads.Count; i++) {
+                var task = taskList.reads[i];
+                var read = reads[i];
+                var readResult = readListResult.reads[i];
+                ReadEntitiesResult(task, readResult, read, readEntities);
+            }
+        }
+
+        private void ReadEntitiesResult(ReadEntities task, ReadEntitiesResult result, ReadTask<T> read, ContainerEntities readEntities) {
+            // remove all requested peers from EntitySet which are not present in database
+            foreach (var id in task.ids) {
+                var value = readEntities.entities[id];
+                if (value.Error != null) {
+                    continue;
+                }
+                var json = value.Json;  // in case of RemoteClient json is "null"
+                var isNull = json == null || json == "null";
+                if (isNull)
+                    set.DeletePeer(id);
+            }
+
+            var entityErrorInfo = new TaskErrorInfo();
+            var readIds = read.idMap.Keys.ToList();
+            foreach (var id in readIds) {
+                var value = readEntities.entities[id];
+                if (value.Error != null) {
+                    entityErrorInfo.AddEntityError(value.Error);
+                    continue;
+                }
+                var json = value.Json;  // in case of RemoteClient json is "null"
+                if (json == null || json == "null") {
+                    read.idMap[id] = null;
+                } else {
+                    var peer = set.GetPeerById(id);
+                    read.idMap[id] = peer.entity;
+                }
+            }
+            // A ReadTask is set to error if at least one of its JSON results has an error.
+            if (entityErrorInfo.HasErrors) {
+                read.state.SetError(entityErrorInfo);
+                return;
+            }
+            read.state.Synced = true;
+            AddReferencesResult(task.references, result.references, read.refsTask.subRefs);
+        }
+        
+        internal override void QueryEntitiesResult(QueryEntities task, TaskResult result, ContainerEntities queryEntities) {
+            var filterLinq = task.filterLinq;
+            var query = queries[filterLinq];
+            if (result is TaskError taskError) {
+                query.state.SetError(new TaskErrorInfo(taskError));
+                return;
+            }
+            var queryResult = (QueryEntitiesResult)result;
+            var entityErrorInfo = new TaskErrorInfo();
+            var entities = query.entities = new Dictionary<string, T>(queryResult.ids.Count);
+            foreach (var id in queryResult.ids) {
+                var value = queryEntities.entities[id];
+                if (value.Error != null) {
+                    entityErrorInfo.AddEntityError(value.Error);
+                    continue;
+                }
+                var peer = set.GetPeerById(id);
+                entities.Add(id, peer.entity);
+            }
+            if (entityErrorInfo.HasErrors) {
+                query.state.SetError(entityErrorInfo);
+                return;
+            }
+            AddReferencesResult(task.references, queryResult.references, query.refsTask.subRefs);
+            query.state.Synced = true;
+        }
+
+        private void AddReferencesResult(List<References> references, List<ReferencesResult> referencesResult, SubRefs refs) {
+            // in case (references != null &&  referencesResult == null) => no reference ids found for references 
+            if (references == null || referencesResult == null)
+                return;
+            for (int n = 0; n < references.Count; n++) {
+                References          reference    = references[n];
+                ReferencesResult    refResult    = referencesResult[n];
+                EntitySet           refContainer = set.intern.store._intern.setByName[refResult.container];
+                ReadRefsTask        subRef       = refs[reference.selector];
+                subRef.SetResult(refContainer, refResult.ids);
+
+                var subReferences = reference.references;
+                if (subReferences != null) {
+                    var readRefs = subRef.SubRefs;
+                    AddReferencesResult(subReferences, refResult.references, readRefs);
+                }
+            }
+        }
+        
+        internal override void PatchEntitiesResult(PatchEntities task, TaskResult result) {
+            if (result is TaskError) {
+                /* foreach (var patchPair in patches) {
+                    var patch = patchPair.Value;
+                    // patch.taskError = taskError; todo
+                } */
+                return;
+            }
+            // var patchResult = (PatchEntitiesResult)result;
+            var entityPatches = task.patches;
+            foreach (var entityPatchPair in entityPatches) {
+                var id = entityPatchPair.Key;
+                var peer = set.GetPeerById(id);
+                peer.SetPatchSource(peer.NextPatchSource);
+                peer.SetNextPatchSourceNull();
+            }
+        }
+
+        internal override void DeleteEntitiesResult(DeleteEntities task, TaskResult result) {
+            if (result is TaskError taskError) {
+                foreach (var deleteTask in deleteTasks) {
+                    deleteTask.state.SetError(new TaskErrorInfo(taskError));
+                }
+                return;
+            }
+            foreach (var id in task.ids) {
+                set.DeletePeer(id);
+            }
+            foreach (var deleteTask in deleteTasks) {
+                var entityErrorInfo = new TaskErrorInfo();
+                idsBuf.Clear();
+                deleteTask.GetIds(idsBuf);
+                foreach (var id in idsBuf) {
+                    if (deleteErrors.TryGetValue(id, out EntityError error)) {
+                        entityErrorInfo.AddEntityError(error);
+                    }
+                }
+                if (entityErrorInfo.HasErrors) {
+                    deleteTask.state.SetError(entityErrorInfo);
+                    continue;
+                }
+                deleteTask.state.Synced = true;
+            }
+        }
+    }
+}
