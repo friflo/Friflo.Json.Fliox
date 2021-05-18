0938e8dc1efb2142876ee1b7a0720b065da87d0e
Ullrich Praetz <ullrich@praetz.de>
2021-05-18 19:50:52 +0200

    fix Unity compiler error

--- Json/Burst/UnityExtension.cs
+++ Json/Burst/UnityExtension.cs
@@ -9,7 +9,7 @@ namespace System.Collections.Generic
 {
     public static class UnityExtensionGeneric
     {
-        public static bool TryAdd<TKey,TValue>(this Dictionary<TKey,TValue> dictionary, TKey key, TValue value) {
+        public static bool TryAdd<TKey,TValue>(this IDictionary<TKey,TValue> dictionary, TKey key, TValue value) {
             if (dictionary.ContainsKey(key))
                 return false;
             
