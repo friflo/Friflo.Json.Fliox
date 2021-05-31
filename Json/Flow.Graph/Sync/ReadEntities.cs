40de26c9162c21a490477480c37277777b278798
Ullrich Praetz <ullrich@praetz.de>
2021-05-31 15:00:22 +0200

    update ValidateEntities() docs

--- Json/Flow.Graph/Sync/ReadEntities.cs
+++ Json/Flow.Graph/Sync/ReadEntities.cs
@@ -24,16 +24,16 @@ namespace Friflo.Json.Flow.Sync
         /// <summary>
         /// Validate all <see cref="EntityValue.value"/>'s in the result set.
         /// Validation is required for all <see cref="EntityContainer"/> implementations which cannot ensure that the
-        /// value is valid JSON.
+        /// <see cref="EntityValue.Json"/> value of <see cref="entities"/> is valid JSON.
         /// 
         /// E.g. <see cref="FileContainer"/> cannot ensure this, as the file content can be written
-        /// or modified from an extern processes - for example by manually changing its JSON content with an editor.
+        /// or modified from extern processes - for example by manually changing its JSON content with an editor.
         /// 
         /// A <see cref="MemoryContainer"/> does not require validation as its key/values are always written via
         /// Database/Graph library - which generate valid JSON.
         /// 
         /// So database adapters which can ensure the JSON value is always valid made calling <see cref="ValidateEntities"/>
-        /// obsolete - like Postgres/JSONB, Azure Cosmos DB or MongoDB 
+        /// obsolete - like Postgres/JSONB, Azure Cosmos DB or MongoDB.
         /// </summary>
         public void ValidateEntities(string container, SyncContext syncContext) {
             using (var pooledValidator = syncContext.pools.JsonValidator.Get()) {
