06857b358a6f43eaa2e406903d5d8bf9e236b2d8
Ullrich Praetz <ullrich@praetz.de>
2021-06-12 07:56:41 +0200

    add docs

--- Json/Flow.Graph/Sync/DatabaseMessage.cs
+++ Json/Flow.Graph/Sync/DatabaseMessage.cs
@@ -5,13 +5,25 @@ namespace Friflo.Json.Flow.Sync
 {
     /// <summary>
     /// A container for different types of message classified into request, response and event messages.
-    /// It is used in communication protocols (HTTP) which support more than request/response schema.
-    /// So it is used for WebSocket's to notify about event messages having no related request message. 
+    /// It is used in communication protocols which support more than request/response schema.
+    /// Only one of its fields is set at a time.
+    /// 
+    /// <para>
+    ///     <see cref="DatabaseMessage"/>'s are used for WebSocket communication to notify about event messages having
+    ///     no corresponding request message.
+    /// </para>
+    /// <para>
+    ///     <see cref="DatabaseMessage"/>' are not not used for HTTP communication as HTTP support request / response
+    ///     pattern by itself and has not support for events. 
+    /// </para>
     /// </summary>
     public class DatabaseMessage
     {
+        /// <summary>A request message</summary>
         public DatabaseRequest  req;
+        /// <summary>A response message</summary>
         public DatabaseResponse resp;
+        /// <summary>An event message</summary>
         public DatabaseEvent    ev;
     }
 }
