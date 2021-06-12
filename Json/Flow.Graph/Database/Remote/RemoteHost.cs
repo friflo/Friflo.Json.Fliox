6c93b036bf0d1f02c3f1497218e02a0a74c62be9
Ullrich Praetz <ullrich@praetz.de>
2021-06-12 12:30:19 +0200

    moved ProtocolType to individual file

--- Json/Flow.Graph/Database/Remote/RemoteHost.cs
+++ Json/Flow.Graph/Database/Remote/RemoteHost.cs
@@ -9,19 +9,6 @@ using Friflo.Json.Flow.Sync;
 // Note! - Must not have any dependency to System.Net or System.Net.Http (or other HTTP stuff)
 namespace Friflo.Json.Flow.Database.Remote
 {
-    /// <summary>
-    /// Specify how <see cref="DatabaseRequest"/>'s and <see cref="DatabaseResponse"/>'s are send via the used
-    /// transmission protocol. E.g. HTTP or WebSockets.
-    /// In case of <see cref="BiDirect"/> (used for WebSockets) requests and responses are encapsulated in a <see cref="DatabaseMessage"/>.
-    /// Otherwise (HTTP) requests and responses are send / received as they are. Meaning they are not encapsulated.
-    /// </summary>
-    public enum ProtocolType {
-        /// requests and responses are not encapsulated.
-        ReqResp,
-        /// requests and responses are encapsulated in a <see cref="DatabaseMessage"/>.
-        BiDirect
-    }
-    
     public class RemoteHostDatabase : EntityDatabase
     {
         private readonly    EntityDatabase  local;
