d57e63ccec6720222340a9c63139c3a465842355
Ullrich Praetz <ullrich@praetz.de>
2021-06-16 12:28:36 +0200

    add docs

--- Json/Flow.Graph/Sync/DatabaseEvent.cs
+++ Json/Flow.Graph/Sync/DatabaseEvent.cs
@@ -13,8 +13,11 @@ namespace Friflo.Json.Flow.Sync
         // note for all fields
         // used { get; set; } to force properties on the top of JSON
         
+        /// Increasing event sequence number starting with 1
                                         public  int     seq      { get; set; }
+        /// The target the event is sent to
         [Fri.Property(Name = "target")] public  string  targetId { get; set; }
+        /// The client which caused the event. Specifically the client which made a database change.
         [Fri.Property(Name = "client")] public  string  clientId { get; set; }
 
         internal abstract   DatabaseEventType   EventType { get; }
