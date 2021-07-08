6243078a2b5a5b55f7859dd11d1b19519949c91f
Ullrich Praetz <ullrich@praetz.de>
2021-07-08 18:26:33 +0200

    add GraphUtils

---
+++ Json/Flow.Graph/Graph/Utils.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+using Friflo.Json.Flow.Database;
+using Friflo.Json.Flow.Sync;
+
+namespace Friflo.Json.Flow.Graph
+{
+    public static class GraphUtils
+    {
+        public static async Task DisposeStore(EntityStore store) {
+            if (store == null)
+                return;
+            await store.CancelPendingSyncs();
+            store.Dispose();
+        }
+        
+        public static void DisposeDatabase(EntityDatabase database) {
+            if (database == null)
+                return;
+            if (database.eventBroker != null) {
+                var eb = database.eventBroker;
+                database.eventBroker = null;
+                eb.Dispose();
+            }
+            database.Dispose();
+        }
+        
+        public static void DisposeCaches() {
+            Pools.SharedPools.Dispose();
+            SyncTypeStore.Dispose();
+        }
+    }
+}
