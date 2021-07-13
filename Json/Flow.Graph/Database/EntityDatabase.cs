82e360260452c75dacd5b06bdef6ca0adb81ba9e
Ullrich Praetz <ullrich@praetz.de>
2021-07-13 09:54:58 +0200

    moved TaskHandler to individual file

--- Json/Flow.Graph/Database/EntityDatabase.cs
+++ Json/Flow.Graph/Database/EntityDatabase.cs
@@ -124,29 +124,4 @@ namespace Friflo.Json.Flow.Database
             return response;
         }
     }
-    
-    public class TaskHandler
-    {
-        private static bool AuthorizeTask(DatabaseTask task, MessageContext messageContext, out TaskResult error) {
-            if (messageContext.Authorize(task, messageContext)) {
-                error = null;
-                return true;
-            }
-            var message = "not authorized";
-            var authError = messageContext.authState.Error; 
-            if (authError != null) {
-                message = $"{message} ({authError})";
-            }
-            error = DatabaseTask.PermissionDenied(message);
-            return false;
-        }
-        
-        public virtual Task<TaskResult> ExecuteTask (DatabaseTask task, EntityDatabase database, SyncResponse response, MessageContext messageContext) {
-            if (!AuthorizeTask(task, messageContext, out var error)) {
-                return Task.FromResult(error);
-            }
-            Task<TaskResult> result = task.Execute(database, response, messageContext);
-            return result;
-        }
-    }
 }
---
+++ Json/Flow.Graph/Database/TaskHandler.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+using Friflo.Json.Flow.Sync;
+
+namespace Friflo.Json.Flow.Database
+{
+    public class TaskHandler
+    {
+        private static bool AuthorizeTask(DatabaseTask task, MessageContext messageContext, out TaskResult error) {
+            if (messageContext.Authorize(task, messageContext)) {
+                error = null;
+                return true;
+            }
+            var message = "not authorized";
+            var authError = messageContext.authState.Error; 
+            if (authError != null) {
+                message = $"{message} ({authError})";
+            }
+            error = DatabaseTask.PermissionDenied(message);
+            return false;
+        }
+        
+        public virtual Task<TaskResult> ExecuteTask (DatabaseTask task, EntityDatabase database, SyncResponse response, MessageContext messageContext) {
+            if (!AuthorizeTask(task, messageContext, out var error)) {
+                return Task.FromResult(error);
+            }
+            Task<TaskResult> result = task.Execute(database, response, messageContext);
+            return result;
+        }
+    }
+}
