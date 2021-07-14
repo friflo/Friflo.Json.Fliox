d8ce43a02787bbc8af4a4f32b5c6d443a530a77a
Ullrich Praetz <ullrich@praetz.de>
2021-07-14 11:26:31 +0200

    add docs

--- Json/Flow.Graph/UserAuth/UserAuthenticator.cs
+++ Json/Flow.Graph/UserAuth/UserAuthenticator.cs
@@ -43,7 +43,7 @@ namespace Friflo.Json.Flow.UserAuth
     }
     
     /// <summary>
-    /// Performs user authentication by validating the "clientId" of a user with the given "token".
+    /// Performs user authentication by validating the "clientId" and the "token" assigned to an <see cref="Graph.EntityStore"/>
     /// <br></br>
     /// If authentication succeed it set the <see cref="AuthState.Authorizer"/> derived from the roles assigned to the user.
     /// If authentication fails the given default <see cref="Authorizer"/> is used for the user.
