7e1b942371b18bec89624123d5f76129b452d3c7
Ullrich Praetz <ullrich@praetz.de>
2021-07-14 09:17:10 +0200

    separate UserStorePool to individual file

--- Json/Flow.Graph/UserAuth/UserAuth.cs
+++ Json/Flow.Graph/UserAuth/UserAuth.cs
@@ -4,7 +4,7 @@
 using System;
 using System.Threading.Tasks;
 using Friflo.Json.Flow.Database;
-using Friflo.Json.Flow.Utils;
+
 
 namespace Friflo.Json.Flow.UserAuth
 {
@@ -27,44 +27,5 @@ namespace Friflo.Json.Flow.UserAuth
         public void Dispose() {
             storePool.Dispose();
         }
-    }
-    
-    
-    public struct UserStorePoolUsage {
-        internal int    userStoreCount;
-        
-        public void AssertEqual(in UserStorePoolUsage other) {
-            if (userStoreCount            != other.userStoreCount)          throw new InvalidOperationException("detect UserStore leak");
-        }
-    }
-    
-    public interface IUserStorePool
-    {
-        ObjectPool<UserStore>       UserStore           { get; }
-        UserStorePoolUsage          UserStorePoolUsage  { get; }
-    }
-    
-    public class UserStorePool : IUserStorePool, IDisposable
-    {
-        public              ObjectPool<UserStore>   UserStore     { get; }
-        
-        internal UserStorePool(EntityDatabase authDatabase, string clientId) {
-            UserStore      = new SharedPool<UserStore>      (() => new UserStore(authDatabase, clientId));
-        }
-        
-        internal UserStorePool(UserStorePool sharedPools) {
-            UserStore      = new LocalPool<UserStore>       (sharedPools.UserStore,      "UserStore");
-        }
-
-        public void Dispose() {
-            UserStore.    Dispose();
-        }
-
-        public UserStorePoolUsage UserStorePoolUsage { get {
-            var usage = new UserStorePoolUsage {
-                userStoreCount            = UserStore       .Usage,
-            };
-            return usage;
-        } }
     }
 }
