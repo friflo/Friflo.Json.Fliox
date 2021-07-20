ab89425e7452463bc4a5de2a5e2998a4f82baa71
Ullrich Praetz <ullrich@praetz.de>
2021-07-20 13:14:43 +0200

    JSON Schema - add runTest()

--- Json.Tests/assets/Schema/Typescript/playground/validate-json-schema.ts
+++ Json.Tests/assets/Schema/Typescript/playground/validate-json-schema.ts
@@ -4,35 +4,50 @@ import fs from 'fs';
 
 function run() {
     const ajv = new Ajv({allErrors: true}) // options can be passed, e.g. {allErrors: true}
+    // runTest(ajv);
 
     // var jsonSchemaString: string = fs.readFileSync("../JSON/json-schema.org/schema.json", "utf8");
     // var jsonSchema = JSON.parse(jsonSchemaString);
 
-    const schema = {
-      type: "object",
-      properties: {
-        foo: {type: "integer"},
-        bar: {type: "string"}
-      },
-      required: ["foo"],
-      additionalProperties: false,
-    }      
-    
-    const data = {
-      foo: "ssss",
-      bar: "abc"
-    }
 
     const validate = ajv.getSchema("http://json-schema.org/draft-07/schema#") as ValidateFunction;
+
+    const schema = {
+        type: "object",
+        properties: {
+          foo: {type: "integer"},
+          bar: {type: "string"}
+        },
+        required: ["foo"],
+        additionalProperties: false,
+    }  
+
     const valid = validate(schema);
     if (!valid)
         console.log(validate.errors)    
+}
+
+function runTest(ajv: Ajv) {
     
-    
-    /* const validate = ajv.compile(schema)
-    const valid = validate(schema)
+    const schema = {
+        type: "object",
+        properties: {
+          foo: {type: "integer"},
+          bar: {type: "string"}
+        },
+        required: ["foo"],
+        additionalProperties: false,
+    }      
+      
+    const data = {
+        foo: "should error",
+        bar: "abc"
+    }
+
+    const validate = ajv.compile(schema)
+    const valid = validate(data)
     if (!valid)
-        console.log(validate.errors) */
+        console.log(validate.errors)
 }
 
 run();
