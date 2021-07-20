073f5d132f0077523d6fd733526d2305d57e933a
Ullrich Praetz <ullrich@praetz.de>
2021-07-20 06:59:29 +0200

    Typescript - add playground examples

--- Json.Tests/assets/Schema/Typescript/playground/main.ts
+++ Json.Tests/assets/Schema/Typescript/playground/main.ts
@@ -4,14 +4,34 @@ var exampleRole: Role = {
     id: "some-id",
     rights: [
         {
-            type: "allow",
-            grant: true,
-            description: "allow description"
+            type:           "allow",
+            grant:          true,
+            description:    "allow description"
         },
         {
-            type: "database",
-            containers: { "Article": { operations:["read", "query", "update"], subscribeChanges: ["update"] }},
-            description: "test"
+            type:           "database",
+            containers:     { "Article": { operations:["read", "query", "update"], subscribeChanges: ["update"] }},
+            description:    "test"
+        },
+        {
+            type:           "message",
+            description:    "some text",
+            names:          ["test-mess*"]
+        },
+        {
+            type:           "subscribeMessage",
+            description:    "some text",
+            names:          ["test-sub*"]
+        },
+        { 
+            type:           "predicate",
+            description:    "some text",
+            names:          ["TestPredicate"]
+        },
+        {
+            type:           "task",
+            description:    "some text",
+            types:          ["read"]
         }
     ]
 }
