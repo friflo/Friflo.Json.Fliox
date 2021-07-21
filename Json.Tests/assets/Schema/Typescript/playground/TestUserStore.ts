1351cb38cb899529206063a4e2a1602e334d995d
Ullrich Praetz <ullrich@praetz.de>
2021-07-21 21:49:53 +0200

    Typescript: add example with Discriminated Union

--- Json.Tests/assets/Schema/Typescript/playground/TestUserStore.ts
+++ Json.Tests/assets/Schema/Typescript/playground/TestUserStore.ts
@@ -1,5 +1,7 @@
+import { Right, Right_Union } from "../UserStore/Friflo.Json.Flow.Auth.Rights"
 import { Role } from "../UserStore/Friflo.Json.Flow.UserAuth"
 
+// check assignment with using a type compiles successful
 var exampleRole: Role = {
     id: "some-id",
     rights: [
@@ -29,4 +31,19 @@ var exampleRole: Role = {
             types:          ["read"]
         }
     ]
+}
+
+// check using a Discriminated Union compiles successful
+function usePolymorphType (right: Right_Union) {
+    switch (right.type) {
+        case "allow":
+            var grant: boolean = right.grant;
+            break;
+        case "message":
+            var names: string[] = right.names;
+            break;
+        case "predicate":
+            var names: string[] = right.names;
+            break;
+    }
 }
