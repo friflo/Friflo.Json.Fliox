86728307f6665afe59b87c1dc59d0b2673ebce09
Ullrich Praetz <ullrich@praetz.de>
2021-07-26 13:37:04 +0200

    JsonTypeDef WIP

--- Json/Flow/Schema/JSON/JsonTypeDef.cs
+++ Json/Flow/Schema/JSON/JsonTypeDef.cs
@@ -8,27 +8,30 @@ using Friflo.Json.Flow.Schema.Definition;
 namespace Friflo.Json.Flow.Schema.JSON
 {
     public class JsonTypeDef : TypeDef {
+        private readonly    string              name;
         private readonly    JsonType            type;
         private readonly    FieldType           fieldType;
         
         // --- TypeDef
-        public  override    TypeDef             BaseType        { get; }
+        public  override    TypeDef             BaseType        => baseType;
         public  override    bool                IsComplex       => fields != null;
         public  override    List<Field>         Fields          => fields;
-        public  override    bool                IsArray         => fieldType?.items != null;
+        public  override    bool                IsArray         { get; }
         public  override    bool                IsDictionary    { get; }
         public  override    UnionType           UnionType       { get; }
         public  override    string              Discriminant    { get; }
-        public  override    bool                IsEnum          => type.enums != null;
-        public  override    ICollection<string> EnumValues      => type.enums;
+        public  override    bool                IsEnum          => EnumValues != null;
+        public  override    ICollection<string> EnumValues      { get; }
         public  override    TypeSemantic        TypeSemantic    => TypeSemantic.None;
 
-        public  override    string              ToString()      => type.name; 
+        public  override    string              ToString()      => name; 
 
         // --- private
+        private             TypeDef             baseType;
         private readonly    List<Field>         fields;
 
-        public JsonTypeDef (JsonType type) {
+        public JsonTypeDef (JsonType type, string name) {
+            this.name = name;
             this.type = type;
             var properties = type.properties;
             if (properties != null) {
@@ -43,6 +46,33 @@ namespace Friflo.Json.Flow.Schema.JSON
                     };
                     fields.Add(field);
                 }
+            }
+            if (type.enums != null) {
+                EnumValues = type.enums; 
+            }
+            if (type.oneOf != null) {
+                var types = new List<TypeDef>(type.oneOf.Count);
+                foreach (var item in type.oneOf) {
+                    // types.Add(i); todo
+                }
+                UnionType = new UnionType {
+                   types            = types,
+                   discriminator    = type.discriminator
+                };
+            }
+        }
+        
+        public JsonTypeDef (FieldType type, string name) {
+            this.name = name;
+            fieldType = type;
+            if (type.items != null) {
+                IsArray = true;
+            }
+            if (type.additionalProperties != null) {
+                IsDictionary = true;
+            }
+            if (type.discriminant != null) {
+                Discriminant = type.discriminant[0];
             }
         }
 
