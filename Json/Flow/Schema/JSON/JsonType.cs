016115d8cc242c5ef91f2823fddc4433bb54b356
Ullrich Praetz <ullrich@praetz.de>
2021-07-26 12:27:21 +0200

    add FieldType

--- Json/Flow/Schema/JSON/JsonType.cs
+++ Json/Flow/Schema/JSON/JsonType.cs
@@ -3,69 +3,35 @@
 
 using System.Collections.Generic;
 using Friflo.Json.Flow.Mapper;
-using Friflo.Json.Flow.Mapper.Map;
-using Friflo.Json.Flow.Mapper.Map.Val;
-using Friflo.Json.Flow.Schema.Definition;
 
 namespace Friflo.Json.Flow.Schema.JSON
 {
     public class JsonType
     {
         public  string                          discriminator;
-        public  List<JsonType>                  oneOf;
+        public  List<FieldType>                 oneOf;
         //
-        public  JsonValue                       type; // todo
-        public  Dictionary<string, JsonType>    properties;
+        public  string                          type; // null, "null", "object", "string", "number", "integer", "array", []
+        public  Dictionary<string, FieldType>   properties;
         public  List<string>                    required;
         public  bool                            additionalProperties;
         //
         [Fri.Property(Name = "$ref")]
         public  JsonType                        reference;
         //
-        public  List<JsonType>                  items;
-        //
         [Fri.Property(Name = "enum")]
         public  List<string>                    enums;
     }
+    
+    public class FieldType
+    {
+        public  string                          type; // "object", "string", "number", "integer", "array"
         
-    public class JsonTypeDef : TypeDef {
-        private readonly    JsonType            json;
+        public  List<FieldType>                 items;
         
-        // --- TypeDef
-        public  override    TypeDef             BaseType        { get; }
-        public  override    bool                IsComplex       => fields != null;
-        public  override    List<Field>         Fields          => fields;
-        public  override    bool                IsArray         => json.items != null;
-        public  override    bool                IsDictionary    { get; }
-        public  override    UnionType           UnionType       { get; }
-        public  override    string              Discriminant    { get; }
-        public  override    bool                IsEnum          => json.enums != null;
-        public  override    ICollection<string> EnumValues      => json.enums;
-        public  override    TypeSemantic        TypeSemantic    => TypeSemantic.None;
-        
-        // --- private
-        private readonly    List<Field>         fields;
+        [Fri.Property(Name = "$ref")]
+        public  FieldType                       reference;
 
-        public JsonTypeDef (JsonType json) {
-            this.json = json;
-            var properties = json.properties;
-            if (properties != null) {
-                fields = new List<Field>(properties.Count);
-                foreach (var property in properties) {
-                    string  fieldName       = property.Key;
-                    bool    requiredField   = json.required?.Contains(fieldName) ?? false;  
-                    var field = new Field {
-                        name        = fieldName,
-                        required    = requiredField,
-                        // type =  todo  
-                    };
-                    fields.Add(field);
-                }
-            }
-        }
-
-        public override bool IsDerivedField(Field field) {
-            throw new System.NotImplementedException();
-        }
+        public  JsonType                        additionalProperties;
     }
 }
---
+++ Json/Flow/Schema/JSON/JsonTypeDef.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using Friflo.Json.Flow.Mapper.Map;
+using Friflo.Json.Flow.Schema.Definition;
+
+namespace Friflo.Json.Flow.Schema.JSON
+{
+    public class JsonTypeDef : TypeDef {
+        private readonly    JsonType            type;
+        private readonly    FieldType           fieldType;
+        
+        // --- TypeDef
+        public  override    TypeDef             BaseType        { get; }
+        public  override    bool                IsComplex       => fields != null;
+        public  override    List<Field>         Fields          => fields;
+        public  override    bool                IsArray         => fieldType?.items != null;
+        public  override    bool                IsDictionary    { get; }
+        public  override    UnionType           UnionType       { get; }
+        public  override    string              Discriminant    { get; }
+        public  override    bool                IsEnum          => type.enums != null;
+        public  override    ICollection<string> EnumValues      => type.enums;
+        public  override    TypeSemantic        TypeSemantic    => TypeSemantic.None;
+        
+        // --- private
+        private readonly    List<Field>         fields;
+
+        public JsonTypeDef (JsonType type) {
+            this.type = type;
+            var properties = type.properties;
+            if (properties != null) {
+                fields = new List<Field>(properties.Count);
+                foreach (var property in properties) {
+                    string  fieldName       = property.Key;
+                    bool    requiredField   = type.required?.Contains(fieldName) ?? false;  
+                    var field = new Field {
+                        name        = fieldName,
+                        required    = requiredField,
+                        // type =  todo  
+                    };
+                    fields.Add(field);
+                }
+            }
+        }
+
+        public override bool IsDerivedField(Field field) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
