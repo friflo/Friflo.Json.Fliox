b72a412dc286bc2751fa897a703bc6b082f564c6
Ullrich Praetz <ullrich@praetz.de>
2021-07-26 11:35:00 +0200

    add model. JsonType - draft

---
+++ Json/Flow/Schema/JSON/JsonType.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using Friflo.Json.Flow.Mapper;
+using Friflo.Json.Flow.Mapper.Map.Val;
+
+namespace Friflo.Json.Flow.Schema.JSON
+{
+    public class JsonType
+    {
+        public  List<JsonType>                  oneOf;
+        //
+        public  JsonValue                       type; // todo
+        public  Dictionary<string, JsonType>    properties;
+        public  List<string>                    required;
+        public  bool                            additionalProperties;
+        //
+        [Fri.Property(Name = "$ref")]
+        public  JsonType                        reference;
+        //
+        public  List<JsonType>                  items;
+        //
+        [Fri.Property(Name = "enum")]
+        public  List<string>                    enums;
+    }
+}
---
+++ Json/Flow/Schema/JSON/JsonTypeSchema.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Friflo.Json.Flow.Schema.JSON
+{
+    public class JsonTypeSchema
+    {
+        public  List<JsonType>  definitions;
+    }
+}
