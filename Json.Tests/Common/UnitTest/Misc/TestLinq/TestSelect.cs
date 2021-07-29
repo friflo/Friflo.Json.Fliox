e57b29f08a48e82d804342ca07f83488e8c6a05f
Ullrich Praetz <ullrich@praetz.de>
2021-07-29 11:07:49 +0200

    removed obsolete evaluation stuff related to expressions

--- Json.Tests/Common/UnitTest/Misc/TestLinq/TestSelect.cs
+++ Json.Tests/Common/UnitTest/Misc/TestLinq/TestSelect.cs
@@ -2,51 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using Friflo.Json.Flow.Database;
-using Friflo.Json.Flow.Graph;
 using Friflo.Json.Flow.Mapper;
 using Friflo.Json.Tests.Common.UnitTest.Flow.Graph;
 using Friflo.Json.Tests.Unity.Utils;
 using NUnit.Framework;
 using static NUnit.Framework.Assert;
-using static Friflo.Json.Tests.Common.UnitTest.Misc.TestLinq.Graph;
-
-
 
 namespace Friflo.Json.Tests.Common.UnitTest.Misc.TestLinq
 {
 
     public class TestSelect : LeakTestsFixture
     {
-        [Test]
-        public void ElaborateQueryApi() {
-           
-            var query1 = TestQuery(
-                limit:      10,
-                orderBy:    c => c.name,
-                order:      Misc.TestLinq.Order.Asc,
-                @where:      c => c.name == "dddd",
-                @select:  () => new Customer {
-                    name    = default,
-                    id          = default }
-            );
-
-            var query2 = TestQuery(
-                limit:      10,
-                orderBy:    o => o.customer.Entity.name,
-                order:      Misc.TestLinq.Order.Desc,
-                @select: () => new Flow.Graph.Order {
-                    customer = {
-                        Entity = {
-                            id          = default,
-                            name    = default
-                        }
-                    },
-                    items = default
-                });
-        }
-        
         [Test]
         public void RunLinq() {
             using (var _        = Pools.SharedPools) // for LeakTestsFixture
@@ -58,19 +25,18 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc.TestLinq
                 var order1 = readOrders.Find("order-1");
                 store.Sync().Wait();
                 var orderResult = order1.Result;
-                var orders = new List<Flow.Graph.Order> { orderResult };
+                var orders = new List<Order> { orderResult };
 
                 var orderQuery =
                     from order in orders
                     // where order.id == "order-1"
-                    select new Flow.Graph.Order {
+                    select new Order {
                         id = order.id,
                         created = new DateTime(2021, 7, 22, 6, 0, 0, DateTimeKind.Utc),
                         customer =  order.customer,
                         items = order.items
                     };
 
-            
                 m.Pretty = true;
                 var jsonQuery = m.Write(orderQuery);
                 var json = m.Write(orders);
@@ -95,15 +61,15 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc.TestLinq
             }
         }
 
-        private static bool WhereOrderEqual(Flow.Graph.Order order, string test) {
+        private static bool WhereOrderEqual(Order order, string test) {
             return order.id == test;
         }
 
-        private static Flow.Graph.Order SelectOrder(Flow.Graph.Order order) {
+        private static Order SelectOrder(Order order) {
             return order;
         }
         
-        private static Flow.Graph.Order GetOrder(string id) {
+        private static Order GetOrder(string id) {
             using (var database = new MemoryDatabase())
             using (var store = new PocStore(database, "store")) {
                 TestRelationPoC.CreateStore(store).Wait();
@@ -120,11 +86,11 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc.TestLinq
             {
                 var order1 = GetOrder("order-1");
 
-                var orders = new List<Flow.Graph.Order> {order1};
+                var orders = new List<Order> {order1};
 
-                IQueryable<Flow.Graph.Order> queryable = orders.AsQueryable(); // for illustration only: Create queryable explicit from orders
+                IQueryable<Order> queryable = orders.AsQueryable(); // for illustration only: Create queryable explicit from orders
 
-                var gqlOrders = new GqlEnumerable<Flow.Graph.Order>(queryable); // <=> new GqlEnumerable<Order>(orders)
+                var gqlOrders = new GqlEnumerable<Order>(queryable); // <=> new GqlEnumerable<Order>(orders)
                 // var gqlOrders = new GqlEnumerable<Order>(orders);
 
                 var orderQuery =
@@ -151,7 +117,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc.TestLinq
                 var readOrders = store.orders.Read(); 
                 var order1 = readOrders.Find("order-1");
                 store.Sync().Wait();
-                var orders = new List<Flow.Graph.Order> {order1.Result};
+                var orders = new List<Order> {order1.Result};
 
                 var orderQuery =
                     from order in orders
@@ -163,39 +129,5 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc.TestLinq
                 }
             }
         }
-        
-        [Test]
-        public void TestUpdateField() {
-            var order = new Flow.Graph.Order();
-            order.customer = new Ref<Customer>();
-
-            Update (order, o => o.customer.Entity.name);
-            Update (order, o => o.items[1].amount);
-            
-            Update2 (order, o => new {
-                lastName = o.customer.Entity.name,
-                o.customer.id,
-                item = o.items.Sel(i => new {
-                    i.amount,
-                    i.article,
-                    i.article.Entity
-                }),
-            });
-            
-            Update3 (order, () => new Flow.Graph.Order{ items = null});
-
-
-            int index = 3;
-            Update2 (order, o => o.items[index].amount);
-        }
     }
 }
-
-
-
-
-
-
-
-
-
