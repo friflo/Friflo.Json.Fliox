bb628c3ee62aa9c9af79ed5d9463aa7944fb7405
Ullrich Praetz <ullrich@praetz.de>
2021-07-31 08:43:45 +0200

    optional data class parameter

--- Json.Tests/assets/Schema/Kotlin/src/main/kotlin/Main.kt
+++ Json.Tests/assets/Schema/Kotlin/src/main/kotlin/Main.kt
@@ -13,27 +13,29 @@ fun main(args: Array<String>) {
     var xxx = Json.encodeToString(data)
 }
 
+@Serializable
+data class Data(
+    val a:      Int,
+    val b:      String,
+    var list:   List<String>?               = null,   // optional parameter
+    var map:    HashMap<String, String>?    = null
+)
+
 
 fun learnTypes () {
-    var bool: Boolean = true
-    var str: String = "hello"
+    var bool:   Boolean = true
+    var str:    String = "hello"
 
-    var byte: Byte = 100;
-    var short: Short = 1000;
-    var int: Int = 10000000;
-    var long: Long = 2000000000000000000;
+    var byte:   Byte = 100;
+    var short:  Short = 1000;
+    var int:    Int = 10000000;
+    var long:   Long = 2000000000000000000;
 
-    var float: Float = 1.1f;
+    var float:  Float = 1.1f;
     var double: Double = 1.1;
 }
 
-@Serializable
-data class Data(
-    val a: Int,
-    val b: String,
-    var list: List<String>,
-    var map: HashMap<String, String>
-)
+
 
 enum class TestEnum {
     Test
