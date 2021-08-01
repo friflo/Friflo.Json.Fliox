02390615b90c17e5dcb980850654a409e2e85968
Ullrich Praetz <ullrich@praetz.de>
2021-08-01 10:07:01 +0200

    add BigIntegerSerializer

--- Json.Tests/assets/Schema/Kotlin/src/main/kotlin/Main.kt
+++ Json.Tests/assets/Schema/Kotlin/src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 import kotlinx.datetime.Instant
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.math.BigInteger
 import kotlin.collections.HashMap
 
 
@@ -10,8 +16,9 @@ fun main(args: Array<String>) {
     // Try adding program arguments at Run/Debug configuration
     println("Program arguments: ${args.joinToString()}")
 
-
-    var data = Data(42, "str", Instant.parse("2021-07-22T06:00:00.000Z"))
+    var instant = Instant.parse("2021-07-22T06:00:00.000Z");
+    var bigInt  = BigInteger("123");
+    var data = Data(42, "str", instant, bigInt)
 
     var json = Json.encodeToString(data)
     System.out.println("json: " + json);
@@ -19,13 +26,14 @@ fun main(args: Array<String>) {
 
 @Serializable
 class Data(
-    val a:      Int,
-    val b:      String,
-    // @Contextual
+    val a:          Int,
+    val b:          String,
     // @Serializable(with = InstantComponentSerializer::class, )
     var dateTime:   Instant,
-    var list:   List<String>?               = null,   // optional parameter
-    var map:    HashMap<String, String>?    = null,
+    @Serializable(with = BigIntegerSerializer::class, )
+    var bigInt:     BigInteger,
+    var list:       List<String>?               = null,   // optional parameter
+    var map:        HashMap<String, String>?    = null,
 )
 
 
@@ -43,8 +51,20 @@ fun learnTypes () {
     var dateTime:   Instant
 }
 
-
-
 enum class TestEnum {
     Test
+}
+
+object BigIntegerSerializer : KSerializer<BigInteger> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("BigInteger", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: BigInteger) {
+        val string = value.toString();
+        encoder.encodeString(string)
+    }
+
+    override fun deserialize(decoder: Decoder): BigInteger {
+        val string = decoder.decodeString()
+        return BigInteger(string)
+    }
 }
