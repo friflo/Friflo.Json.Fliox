6800e3cad805eec355bcf11a8e9380adb11d3ce1
Ullrich Praetz <ullrich@praetz.de>
2021-08-02 09:31:58 +0200

    add ValidationSchema

---
+++ Json/Flow/Schema/Validation/ValidationSchema.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using Friflo.Json.Flow.Schema.Definition;
+
+namespace Friflo.Json.Flow.Schema.Validation
+{
+    public class ValidationSchema : IDisposable
+    {
+        public  readonly    List<ValidationType>    types;
+
+        public ValidationSchema (TypeSchema schema) {
+            var schemaTypes = schema.Types;
+            types           = new List<ValidationType>(schemaTypes.Count);
+            foreach (var type in schemaTypes) {
+                var validationType = new ValidationType(type);
+                types.Add(validationType);
+            }
+        }
+
+        public void Dispose() {
+            foreach (var type in types) {
+                type.Dispose();
+            }
+        }
+    }
+}
