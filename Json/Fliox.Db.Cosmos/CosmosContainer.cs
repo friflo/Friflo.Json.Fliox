1a6b924d4983c61f6a4f042c4e4e92b9690ff9bf
Ullrich Praetz <ullrich@praetz.de>
2021-09-03 16:09:41 +0200

    sketch CosmosContainer implementation

--- Json/Fliox.Db.Cosmos/CosmosContainer.cs
+++ Json/Fliox.Db.Cosmos/CosmosContainer.cs
@@ -1,88 +1,113 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using Friflo.Json.Fliox.Database;
 using Friflo.Json.Fliox.Mapper;
 using Friflo.Json.Fliox.Sync;
+using Microsoft.Azure.Cosmos;
 
 namespace Friflo.Json.Fliox.Db.Cosmos
 {
     public class CosmosDatabase : EntityDatabase
     {
-        private  readonly   bool    pretty;
+        private  readonly   bool                            pretty;
+        private  readonly   Microsoft.Azure.Cosmos.Database cosmosDatabase;
 
-        public CosmosDatabase(bool pretty = false) {
-            this.pretty = pretty;
+        public CosmosDatabase(Microsoft.Azure.Cosmos.Database cosmosDatabase, bool pretty = false) {
+            this.cosmosDatabase = cosmosDatabase;
+            this.pretty         = pretty;
         }
         
         public override EntityContainer CreateContainer(string name, EntityDatabase database) {
-            return new MemoryContainer(name, database, pretty);
+            var container = cosmosDatabase.CreateContainerIfNotExistsAsync(name, "/LastName", 400).Result; // todo make CreateContainer async
+            return new CosmosContainer(name, database, container, pretty);
         }
     }
     
     public class CosmosContainer : EntityContainer
     {
-        private  readonly   Dictionary<JsonKey, string> keyValues = new Dictionary<JsonKey, string>(JsonKey.Equality);
-        
-        public   override   bool                        Pretty      { get; }
+        private  readonly   Container   cosmosContainer;
+        public   override   bool        Pretty      { get; }
 
-        public CosmosContainer(string name, EntityDatabase database, bool pretty) : base(name, database) {
-            Pretty = pretty;
+        public CosmosContainer(string name, EntityDatabase database, Container container, bool pretty) : base(name, database) {
+            cosmosContainer = container;
+            Pretty          = pretty;
         }
         
-        public override Task<CreateEntitiesResult> CreateEntities(CreateEntities command, MessageContext messageContext) {
+        public override async Task<CreateEntitiesResult> CreateEntities(CreateEntities command, MessageContext messageContext) {
             var entities = command.entities;
-            foreach (var entityPair in entities) {
-                var      key      = entityPair.Key;
-                EntityValue payload  = entityPair.Value;
-                if (keyValues.TryGetValue(key, out string _))
-                    throw new InvalidOperationException($"Entity with key '{key}' already in DatabaseContainer: {name}");
-                keyValues[key] = payload.Json;
+            using(MemoryStream memory   = new MemoryStream())
+            using(StreamWriter writer   = new StreamWriter(memory, Encoding.UTF8)) {
+                foreach (var entityPair in entities) {
+                    var         id      = entityPair.Key.AsString();
+                    EntityValue payload  = entityPair.Value;
+                    writer.Write(payload);
+                    memory.Seek(0, SeekOrigin.Begin);
+                    var partitionKey = new PartitionKey(id);
+                    // todo handle error;
+                    await cosmosContainer.CreateItemStreamAsync(memory, partitionKey);
+                }
             }
             var result = new CreateEntitiesResult();
-            return Task.FromResult(result);
+            return result;
         }
 
-        public override Task<UpdateEntitiesResult> UpdateEntities(UpdateEntities command, MessageContext messageContext) {
+        public override async Task<UpdateEntitiesResult> UpdateEntities(UpdateEntities command, MessageContext messageContext) {
             var entities = command.entities;
-            foreach (var entityPair in entities) {
-                var         key      = entityPair.Key;
-                EntityValue payload  = entityPair.Value;
-                keyValues[key] = payload.Json;
+            using(MemoryStream memory   = new MemoryStream())
+            using(StreamWriter writer   = new StreamWriter(memory, Encoding.UTF8)) {
+                foreach (var entityPair in entities) {
+                    var         key      = entityPair.Key.AsString();
+                    EntityValue payload  = entityPair.Value;
+                    writer.Write(payload);
+                    memory.Seek(0, SeekOrigin.Begin);
+                    var partitionKey = new PartitionKey(key);
+                    // todo handle error;
+                    await cosmosContainer.CreateItemStreamAsync(memory, partitionKey);
+                }
             }
             var result = new UpdateEntitiesResult();
-            return Task.FromResult(result);
+            return result;
         }
 
-        public override Task<ReadEntitiesResult> ReadEntities(ReadEntities command, MessageContext messageContext) {
+        public override async Task<ReadEntitiesResult> ReadEntities(ReadEntities command, MessageContext messageContext) {
             var keys = command.ids;
             var entities = new Dictionary<JsonKey, EntityValue>(keys.Count, JsonKey.Equality);
             foreach (var key in keys) {
-                keyValues.TryGetValue(key, out var payload);
-                var entry = new EntityValue(payload);
-                entities.TryAdd(key, entry);
+                var id              = key.AsString();
+                var partitionKey    = new PartitionKey(id);
+                // todo handle error;
+                ResponseMessage response = await cosmosContainer.ReadItemStreamAsync(id, partitionKey);
+                using (StreamReader reader = new StreamReader(response.Content)) {
+                    string payload = await reader.ReadToEndAsync();
+                    var entry = new EntityValue(payload);
+                    entities.TryAdd(key, entry);
+                }
             }
             var result = new ReadEntitiesResult{entities = entities};
-            return Task.FromResult(result);
-        }
-        
-        public override async Task<QueryEntitiesResult> QueryEntities(QueryEntities command, MessageContext messageContext) {
-            var ids     = keyValues.Keys.ToHashSet(JsonKey.Equality);
-            var result  = await FilterEntities(command, ids, messageContext).ConfigureAwait(false);
             return result;
         }
         
-        public override Task<DeleteEntitiesResult> DeleteEntities(DeleteEntities command, MessageContext messageContext) {
+        public override Task<QueryEntitiesResult> QueryEntities(QueryEntities command, MessageContext messageContext) {
+            // var ids     = keyValues.Keys.ToHashSet(JsonKey.Equality);
+            // var result  = await FilterEntities(command, ids, messageContext).ConfigureAwait(false);
+            return null;
+        }
+        
+        public override async Task<DeleteEntitiesResult> DeleteEntities(DeleteEntities command, MessageContext messageContext) {
             var keys = command.ids;
             foreach (var key in keys) {
-                keyValues.Remove(key);
+                var id              = key.AsString();
+                var partitionKey    = new PartitionKey(id);
+                // todo handle error;
+                await cosmosContainer.DeleteItemStreamAsync(id, partitionKey);
             }
             var result = new DeleteEntitiesResult();
-            return Task.FromResult(result);
+            return result;
         }
     }
 }
