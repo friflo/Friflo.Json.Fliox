b0d7587844755eb0b219db7453676da3e10db70e
Ullrich Praetz <ullrich@praetz.de>
2021-09-10 12:35:02 +0200

    removed unused import

--- Json.Tests/Common/UnitTest/Fliox/Graph/Happy/Test-RefAssignment.cs
+++ Json.Tests/Common/UnitTest/Fliox/Graph/Happy/Test-RefAssignment.cs
@@ -2,7 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using Friflo.Json.Fliox.DB.Graph;
 using Friflo.Json.Fliox.DB.Graph.Internal.KeyEntity;
@@ -130,7 +129,6 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Graph.Happy
             AreEqual("ref-id", reference.key);
             IsFalse (reference.TryEntity(out result));
             IsNull  (result);
-            
             var e = Throws<UnresolvedRefException>(() => _ = reference.Entity);
             AreEqual("Accessed unresolved reference. Ref<Article> (key: 'ref-id')", e.Message);
         }
