cab8d76cf2584504b92ea4fe301c568b3c8ad81c
Ullrich Praetz <ullrich@praetz.de>
2021-09-12 13:49:01 +0200

    add SequenceStore skeleton

--- Json/Fliox.DB/NoSQL/AutoIncrement.cs
+++ Json/Fliox.DB/NoSQL/AutoIncrement.cs
@@ -1,19 +1,32 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System.ComponentModel.DataAnnotations;
+using Friflo.Json.Fliox.DB.Graph;
+using Friflo.Json.Fliox.Mapper;
+
 namespace Friflo.Json.Fliox.DB.NoSQL
 {
-    public class AutoIncrement
-    {
-        
+    public class AutoIncrement {
+        public  int count;
     }
     
-    public class AutoIncrementResult
+    public class AutoIncrementResult {
+    }
+    
+    public class SequenceStore : EntityStore
     {
+        public  EntitySet <string, Sequence>    sequence; 
+        
+        public  SequenceStore(EntityDatabase database, TypeStore typeStore, string clientId) : base(database, typeStore, clientId) {
+        }
     }
     
     public class Sequence {
-        public  int     id;
+        [Key]
+        public  string  container;
         public  int     autoId;
+        [Fri.Property(Name = "_etag")]
+        public  int     etag;
     }
 }
