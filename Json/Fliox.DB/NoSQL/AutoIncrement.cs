80eca0dc18a7f29bbe6aaf61f057e5d6fd66a428
Ullrich Praetz <ullrich@praetz.de>
2021-09-12 13:54:53 +0200

    update AutoIncrementResult model

--- Json/Fliox.DB/NoSQL/AutoIncrement.cs
+++ Json/Fliox.DB/NoSQL/AutoIncrement.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Friflo.Json.Fliox.DB.Graph;
 using Friflo.Json.Fliox.Mapper;
@@ -8,18 +9,19 @@ using Friflo.Json.Fliox.Mapper;
 namespace Friflo.Json.Fliox.DB.NoSQL
 {
     public class AutoIncrement {
-        public  int count;
+        public  int         count;
     }
     
     public class AutoIncrementResult {
+        public  List<int>   ids;
     }
     
     public class SequenceStore : EntityStore
     {
         public  EntitySet <string, AutoIncSequence>    sequence; 
         
-        public  SequenceStore(EntityDatabase database, TypeStore typeStore, string clientId) : base(database, typeStore, clientId) {
-        }
+        public  SequenceStore(EntityDatabase database, TypeStore typeStore, string clientId)
+            : base(database, typeStore, clientId) { }
     }
     
     public class AutoIncSequence {
