1b14f3b57b0fb075a0e78c7b2f795cdfae30fd76
Ullrich Praetz <ullrich@praetz.de>
2021-09-14 17:29:51 +0200

    update AutoIncrement models

--- Json/Fliox.DB/NoSQL/AutoIncrement.cs
+++ Json/Fliox.DB/NoSQL/AutoIncrement.cs
@@ -1,37 +1,43 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
+using System;
 using Friflo.Json.Fliox.DB.Graph;
 using Friflo.Json.Fliox.Mapper;
 
+// ReSharper disable InconsistentNaming
 namespace Friflo.Json.Fliox.DB.NoSQL
 {
-    public class AutoIncrement {
-        [Fri.Required]
-        public  int         count;
+    // --- models
+    public class _ReservedKeys {
+        public  Guid    id;     // secret to ensure the client has reserved the keys
+        [Fri.Required]  public  int     start;
+        [Fri.Required]  public  int     count;
+        public  string  user;   // to track back who reserved key in case of abusing it
     }
     
-    public class AutoIncrementResult {
-        [Fri.Required]
-        public  List<long>  ids;
+    public class _Sequence {
+        [Fri.Key]       public  string  container;
+        [Fri.Required]  public  int     autoId;
+        [Fri.Required]  public  string  _etag;
     }
-    
+
     public class SequenceStore : EntityStore
     {
-        public  EntitySet <string, AutoIncSequence>    sequence; 
+        public  EntitySet <Guid,    _ReservedKeys>   reservedKeys;
+        public  EntitySet <string,  _Sequence>       sequence;
         
         public  SequenceStore(EntityDatabase database, TypeStore typeStore, string clientId)
             : base(database, typeStore, clientId) { }
     }
+
+    // --- protocol
+    public class AutoIncrement {
+        [Fri.Required]  public  int     count;
+    }
     
-    public class AutoIncSequence {
-        [Fri.Key]
-        public  string  container;
-        [Fri.Required]
-        public  long    autoId;
-        [Fri.Required]
-        [Fri.Property(Name = "_etag")]
-        public  string  etag;
+    public class AutoIncrementResult {
+        [Fri.Required]  public  int     start;
+        [Fri.Required]  public  int     count;
     }
 }
