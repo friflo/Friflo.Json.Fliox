c6f79413d785e04c5f07f93be6f43cf4d65c236d
Ullrich Praetz <ullrich@praetz.de>
2021-09-17 11:33:40 +0200

    removed Friflo.Playground.sln - to avoid confusion

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Friflo.Playground
+﻿using NUnit.Framework;
+
+namespace Friflo.Playground
 {
     public class Program
     {
+        public class Info
+        {
+            [Test]
+            public void Message() {
+                Assert.Fail("Playground test are not part of CI. They are used for evaluation and may fail. CI tests in: Friflo.Json.Tests.Common");
+            }
+        }
+        
         public static void Main(string[] args)
         {
         }
