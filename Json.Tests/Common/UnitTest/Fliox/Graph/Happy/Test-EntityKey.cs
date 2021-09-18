31429011a55dba8e7206e49e7ed6be1310376e42
Ullrich Praetz <ullrich@praetz.de>
2021-09-18 11:59:02 +0200

    add CosmosTestEntityKey()

--- Json.Tests/Common/UnitTest/Fliox/Graph/Happy/Test-EntityKey.cs
+++ Json.Tests/Common/UnitTest/Fliox/Graph/Happy/Test-EntityKey.cs
@@ -46,7 +46,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Graph.Happy
             }
         }
 
-        private static async Task AssertEntityKeyTests(EntityDatabase database, TypeStore typeStore) {
+        public static async Task AssertEntityKeyTests(EntityDatabase database, TypeStore typeStore) {
             var entityRef = new EntityRefs { id = "entity-ref-1" };
             
             // --- int as entity id ---
--- Playground/CosmosDB/TestCosmos.cs
+++ Playground/CosmosDB/TestCosmos.cs
@@ -4,6 +4,7 @@
 using System.Threading.Tasks;
 using Friflo.Json.Fliox.DB.Cosmos;
 using Friflo.Json.Fliox.DB.NoSQL;
+using Friflo.Json.Fliox.Mapper;
 using Friflo.Json.Tests.Common.UnitTest.Fliox.Graph;
 using Friflo.Json.Tests.Common.UnitTest.Fliox.Graph.Happy;
 using Friflo.Json.Tests.Common.Utils;
@@ -20,31 +21,40 @@ namespace Friflo.Playground.CosmosDB
         [OneTimeSetUp]    public static void  Init()       { TestGlobals.Init(); }
         [OneTimeTearDown] public static void  Dispose()    { TestGlobals.Dispose(); }
         
-        [Test] public static async Task  CosmosCreateAsync() { await CosmosCreate(); }
-
         private static IConfiguration InitConfiguration() {
             var appSettings     = CommonUtils.GetBasePath() + "appsettings.test.json";
             var privateSettings = CommonUtils.GetBasePath() + "appsettings.private.json";
             return new ConfigurationBuilder().AddJsonFile(appSettings).AddJsonFile(privateSettings).Build();
         }
         
-        private static async Task<DatabaseResponse> CosmosCreateDatabase() {
+        private static async Task<DatabaseResponse> CosmosCreateDatabase(string databaseName) {
             var config = InitConfiguration();
             var endpointUri = config["EndPointUri"];    // The Azure Cosmos DB endpoint for running this sample.
             var primaryKey  = config["PrimaryKey"];     // The primary key for the Azure Cosmos account.
             var options     = new CosmosClientOptions { ApplicationName = "Friflo.Playground" };
             var client      = new CosmosClient(endpointUri, primaryKey, options);
-            return await client.CreateDatabaseIfNotExistsAsync("PocStore");
+            return await client.CreateDatabaseIfNotExistsAsync(databaseName);
         }
 
-        private static async Task CosmosCreate() {
-            var database            = await CosmosCreateDatabase();
+        [Test]
+        public static async Task CosmosCreatePocStore() {
+            var cosmosDatabase      = await CosmosCreateDatabase(nameof(PocStore));
             using (var _            = Pools.SharedPools) // for LeakTestsFixture
-            using (var fileDatabase = new CosmosDatabase(database, 400))
-            using (var createStore  = new PocStore(fileDatabase, "createStore"))
-            using (var useStore     = new PocStore(fileDatabase, "useStore")) {
+            using (var database     = new CosmosDatabase(cosmosDatabase, 400))
+            using (var createStore  = new PocStore(database, "createStore"))
+            using (var useStore     = new PocStore(database, "useStore")) {
                 await TestRelationPoC.CreateStore(createStore);
                 await TestStore.TestStores(createStore, useStore);
+            }
+        }
+        
+        [Test] 
+        public static async Task CosmosTestEntityKey() {
+            var cosmosDatabase      = await CosmosCreateDatabase(nameof(EntityIdStore));
+            using (var _            = Pools.SharedPools) // for LeakTestsFixture
+            using (var typeStore    = new TypeStore())
+            using (var database     = new CosmosDatabase(cosmosDatabase, 400)) {
+                await TestEntityKey.AssertEntityKeyTests (database, typeStore);
             }
         }
     }
