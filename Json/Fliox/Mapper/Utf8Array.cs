e474de01f6b9c67b27222fe455e4da9ce3e0b5ed
Ullrich Praetz <ullrich@praetz.de>
2021-09-20 09:47:02 +0200

    support Utf8Array.array == null

--- Json/Fliox/Mapper/Utf8Array.cs
+++ Json/Fliox/Mapper/Utf8Array.cs
@@ -13,40 +13,50 @@ namespace Friflo.Json.Fliox.Mapper
 {
     public readonly struct Utf8Array {
         // array is internal to prevent potential side effects by application code mutating array elements
-        internal readonly   byte[]  array;
+        private  readonly   byte[]  array;                                          // can be null
+        internal            byte[]  Array       => array ?? Null;                   // never null
         
-        public              int     Length      => array.Length;
+        public              int     Length      => array?.Length ?? Null.Length;    // always > 0
         public   override   string  ToString()  => AsString();
         public              string  AsString()  => array == null ? "null" : Encoding.UTF8.GetString(array, 0, array.Length);
         
-        public  ArraySegment<byte>  AsArraySegment()        => new ArraySegment<byte>(array, 0, array.Length);
-        public  ByteArrayContent    AsByteArrayContent()    => new ByteArrayContent(array); // todo hm. dependency System.Net.Http 
+        public  ArraySegment<byte>  AsArraySegment()        => new ArraySegment<byte>(Array, 0, Array.Length);
+        public  ByteArrayContent    AsByteArrayContent()    => new ByteArrayContent(Array); // todo hm. dependency System.Net.Http 
 
         
-        private static readonly Utf8Array Null = new Utf8Array("null");
+        private static readonly byte[] Null =  {(byte)'n', (byte)'u', (byte)'l', (byte)'l'};
 
         public Utf8Array(byte[] array) {
+            if (array == null) {
+                this.array = null;
+                return;
+            }
+            if (array.Length == Null.Length && array.SequenceEqual(Null)) {
+                this.array = null;
+                return;
+            } 
             this.array  = array;
         }
         
         /// <summary> Prefer using <see cref="Utf8Array(byte[])"/> </summary>
         public Utf8Array(string value) {
+            if (value == null) {
+                array = null;
+                return;
+            }
+            if (value == "null") {
+                array = null;
+                return;
+            } 
             array  = Encoding.UTF8.GetBytes(value);
         }
         
         public bool IsNull() {
-            if (array == null)
-                return true;
-            return IsEqual(Null);
+            return array == null;
         }
 
         public bool IsEqual (Utf8Array value) {
-            if (array == null) {
-                return value.array == null;
-            }
-            if (value.array == null)
-                return false;
-            return array.SequenceEqual(value.array);
+            return Array.SequenceEqual(value.Array);
         }
         
         /// <summary>Use for testing only</summary>
@@ -70,7 +80,7 @@ namespace Friflo.Json.Fliox.Mapper
     public static class Utf8ArrayExtensions {
     
         public static void AppendArray(this ref Bytes bytes, Utf8Array array) {
-            AppendArray (ref bytes, array, 0, array.array.Length);
+            AppendArray (ref bytes, array, 0, array.Array.Length);
         }
         
         public static void AppendArray(this ref Bytes bytes, Utf8Array array, int offset, int len) {
@@ -78,7 +88,7 @@ namespace Friflo.Json.Fliox.Mapper
             int pos     = bytes.end;
             int arrEnd  = offset + len;
             ref var buf = ref bytes.buffer.array;
-            var src = array.array;
+            var src = array.Array;
             for (int n = offset; n < arrEnd; n++)
                 buf[pos++] = src[n];
             bytes.end += len;
@@ -86,11 +96,11 @@ namespace Friflo.Json.Fliox.Mapper
         }
         
         public static async Task WriteAsync(this Stream stream, Utf8Array array, int offset, int count) {
-            await stream.WriteAsync(array.array, offset, count).ConfigureAwait(false);
+            await stream.WriteAsync(array.Array, offset, count).ConfigureAwait(false);
         }
         
         public static void Write(this Stream stream, Utf8Array array, int offset, int count) {
-            stream.Write(array.array, offset, count);
+            stream.Write(array.Array, offset, count);
         }
     }
 }
