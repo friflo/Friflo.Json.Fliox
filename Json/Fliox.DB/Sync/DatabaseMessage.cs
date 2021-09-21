d87c87b4fd557c17a465cbce7963952f51a655bd
Ullrich Praetz <ullrich@praetz.de>
2021-09-21 04:21:04 +0200

    moved DatabaseRequest, DatabaseResponse & DatabaseEvent to DatabaseMessage

--- Json/Fliox.DB/Sync/DatabaseMessage.cs
+++ Json/Fliox.DB/Sync/DatabaseMessage.cs
@@ -44,6 +44,44 @@ namespace Friflo.Json.Fliox.DB.Sync
         internal abstract   MessageType     MessageType { get; }
     }
     
+    // ----------------------------------- request -----------------------------------
+    public abstract class DatabaseRequest   : DatabaseMessage {
+        // ReSharper disable once InconsistentNaming
+        /// <summary>Used only for <see cref="NoSQL.Remote.RemoteClientDatabase"/> to enable:
+        /// <para>
+        ///   1. Out of order response handling for their corresponding requests.
+        /// </para>
+        /// <para>
+        ///   2. Multiplexing of requests and their responses for multiple clients e.g. <see cref="Graph.EntityStore"/>
+        ///      using the same connection.
+        ///      This is not a common scenario but it enables using a single <see cref="NoSQL.Remote.WebSocketClientDatabase"/>
+        ///      used by multiple clients.
+        /// </para>
+        /// </summary>
+                                        public          int?            reqId       { get; set; }
+    }
+    
+    // ----------------------------------- response -----------------------------------
+    public abstract class DatabaseResponse : DatabaseMessage {
+        // ReSharper disable once InconsistentNaming
+        /// <summary>Set to the value of the corresponding <see cref="DatabaseRequest.reqId"/></summary>
+                                        public          int?            reqId       { get; set; }
+    }
+    
+    // ----------------------------------- event -----------------------------------
+    public abstract class DatabaseEvent     : DatabaseMessage {
+        // note for all fields
+        // used { get; set; } to force properties on the top of JSON
+        
+        /// Increasing event sequence number starting with 1.
+        /// Each target (subscriber) has its own sequence.  
+                                        public  int     seq      { get; set; }
+        /// The target the event is sent to
+        [Fri.Property(Name = "target")] public  string  targetId { get; set; }
+        /// The client which caused the event. Specifically the client which made a database change.
+        [Fri.Property(Name = "client")] public  string  clientId { get; set; }
+    }
+    
     public enum MessageType
     {
         subscription,
