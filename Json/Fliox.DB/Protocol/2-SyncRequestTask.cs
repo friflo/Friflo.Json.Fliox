afc6f96841202fab23f0e9400378c01d434f7647
Ullrich Praetz <ullrich@praetz.de>
2021-10-06 17:28:28 +0200

    moved SyncTaskResult to separate file

--- Json/Fliox.DB/Protocol/2-SyncRequestTask.cs
+++ Json/Fliox.DB/Protocol/2-SyncRequestTask.cs
@@ -83,40 +83,4 @@ namespace Friflo.Json.Fliox.DB.Protocol
             return true;
         }
     }
-    
-    // ----------------------------------- task result -----------------------------------
-    [Fri.Discriminator("task")]
-    [Fri.Polymorph(typeof(CreateEntitiesResult),    Discriminant = "create")]
-    [Fri.Polymorph(typeof(UpsertEntitiesResult),    Discriminant = "upsert")]
-    [Fri.Polymorph(typeof(ReadEntitiesListResult),  Discriminant = "read")]
-    [Fri.Polymorph(typeof(QueryEntitiesResult),     Discriminant = "query")]
-    [Fri.Polymorph(typeof(PatchEntitiesResult),     Discriminant = "patch")]
-    [Fri.Polymorph(typeof(DeleteEntitiesResult),    Discriminant = "delete")]
-    [Fri.Polymorph(typeof(SendMessageResult),       Discriminant = "message")]
-    [Fri.Polymorph(typeof(SubscribeChangesResult),  Discriminant = "subscribeChanges")]
-    [Fri.Polymorph(typeof(SubscribeMessageResult),  Discriminant = "subscribeMessage")]
-    [Fri.Polymorph(typeof(ReserveKeysResult),       Discriminant = "reserveKeys")]
-    //
-    [Fri.Polymorph(typeof(TaskErrorResult),         Discriminant = "error")]
-    public abstract class SyncTaskResult
-    {
-        internal abstract TaskType          TaskType { get; }
-    }
-    
-    // ReSharper disable InconsistentNaming
-    public enum TaskType
-    {
-        read,
-        query,
-        create,
-        upsert,
-        patch,
-        delete,
-        message,
-        subscribeChanges,
-        subscribeMessage,
-        reserveKeys,
-        //
-        error
-    }
 }
---
+++ Json/Fliox.DB/Protocol/2-SyncTaskResult.cs
@@ -0,0 +1,43 @@
+// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Friflo.Json.Fliox.Mapper;
+
+namespace Friflo.Json.Fliox.DB.Protocol
+{
+    // ----------------------------------- task result -----------------------------------
+    [Fri.Discriminator("task")]
+    [Fri.Polymorph(typeof(CreateEntitiesResult),    Discriminant = "create")]
+    [Fri.Polymorph(typeof(UpsertEntitiesResult),    Discriminant = "upsert")]
+    [Fri.Polymorph(typeof(ReadEntitiesListResult),  Discriminant = "read")]
+    [Fri.Polymorph(typeof(QueryEntitiesResult),     Discriminant = "query")]
+    [Fri.Polymorph(typeof(PatchEntitiesResult),     Discriminant = "patch")]
+    [Fri.Polymorph(typeof(DeleteEntitiesResult),    Discriminant = "delete")]
+    [Fri.Polymorph(typeof(SendMessageResult),       Discriminant = "message")]
+    [Fri.Polymorph(typeof(SubscribeChangesResult),  Discriminant = "subscribeChanges")]
+    [Fri.Polymorph(typeof(SubscribeMessageResult),  Discriminant = "subscribeMessage")]
+    [Fri.Polymorph(typeof(ReserveKeysResult),       Discriminant = "reserveKeys")]
+    //
+    [Fri.Polymorph(typeof(TaskErrorResult),         Discriminant = "error")]
+    public abstract class SyncTaskResult
+    {
+        internal abstract TaskType          TaskType { get; }
+    }
+    
+    // ReSharper disable InconsistentNaming
+    public enum TaskType
+    {
+        read,
+        query,
+        create,
+        upsert,
+        patch,
+        delete,
+        message,
+        subscribeChanges,
+        subscribeMessage,
+        reserveKeys,
+        //
+        error
+    }
+}
