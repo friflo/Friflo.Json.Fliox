06221879bc8f73c3f2cbc17bbc35d27ce1645893
Ullrich Praetz <ullrich@praetz.de>
2021-10-14 15:54:37 +0200

    test - add AssertAuthFailedResult()

--- Json.Tests/Common/UnitTest/Fliox/Client/Happy/Test-Monitoring.cs
+++ Json.Tests/Common/UnitTest/Fliox/Client/Happy/Test-Monitoring.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Friflo.Json.Fliox.DB.Auth;
+using Friflo.Json.Fliox.DB.Client;
 using Friflo.Json.Fliox.DB.Host;
 using Friflo.Json.Fliox.DB.Host.Monitor;
 using Friflo.Json.Fliox.DB.Remote;
@@ -32,7 +33,8 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client.Happy
                 await AssertNoAuthMonitoringDB (fileDatabase, monitorDb);
                 
                 await RunWithUserAuth(fileDatabase, async () => {
-                    await AssertAuthMonitoringDB (fileDatabase, monitorDb);
+                    await AssertAuthMonitoringDB        (fileDatabase, monitorDb);
+                    await AssertAuthFailedMonitoringDB  (fileDatabase, monitorDb);
                 });
             }
         }
@@ -48,7 +50,8 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client.Happy
                     await AssertNoAuthMonitoringDB(remoteDatabase, remoteMonitor);
                     
                     await RunWithUserAuth(fileDatabase, async () => {
-                        await AssertAuthMonitoringDB (fileDatabase, remoteMonitor);
+                        await AssertAuthMonitoringDB        (fileDatabase, remoteMonitor);
+                        await AssertAuthFailedMonitoringDB  (fileDatabase, remoteMonitor);
                     });
                 });
             }
@@ -91,27 +94,54 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client.Happy
                 // as clearing monitor stats subsequent call has same result
                 result = await Monitor(store, monitor, userId, clientId);
                 AssertAuthResult(result);
-                
-                await AssertMonitoringErrors(monitor);
             }
         }
         
-        private static void AssertNoAuthResult(MonitorResult result) {
-            AreEqual("{'id':'anonymous','clients':[],'stats':[{'db':'monitor','requests':1,'tasks':1}]}",   result.anonymous.ToString());
-            AreEqual("{'id':'poc-user','clients':['poc-client'],'stats':[{'requests':1,'tasks':2}]}",       result.user.ToString());
-            AreEqual(2, result.users.Count);
+        private  static async Task AssertAuthFailedMonitoringDB(EntityDatabase database, EntityDatabase monitorDb) {
+            const string userId     = "admin";
+            const string clientId   = "admin-xxx"; 
+            using (var store    = new PocStore(database, null))
+            using (var monitor  = new MonitorStore(monitorDb, TestGlobals.typeStore)) {
+                store.SetToken("invalid");
+                var result = await Monitor(store, monitor, userId, clientId);
+                AssertAuthFailedResult(result);
                 
-            AreEqual("{'id':'poc-client','user':'poc-user','stats':[{'requests':1,'tasks':2}]}",            result.client.ToString());
-            AreEqual(1, result.clients.Count);
+                // as clearing monitor stats subsequent call has same result
+                result = await Monitor(store, monitor, userId, clientId);
+                AssertAuthFailedResult(result);
+            }
+        }
+
+        private static void AssertNoAuthResult(MonitorResult result) {
+            var users   = result.users.Results;
+            var clients = result.clients.Results;
+            AreEqual("{'id':'anonymous','clients':[],'stats':[{'db':'monitor','requests':1,'tasks':1}]}",   users[User.AnonymousId].ToString());
+            AreEqual("{'id':'poc-user','clients':['poc-client'],'stats':[{'requests':1,'tasks':2}]}",       users[new JsonKey("poc-user")].ToString());
+            AreEqual(2, users.Count);
+                
+            AreEqual("{'id':'poc-client','user':'poc-user','stats':[{'requests':1,'tasks':2}]}",            clients[new JsonKey("poc-client")].ToString());
+            AreEqual(1, clients.Count);
+            
+            NotNull(result.user.Result);
+            NotNull(result.client.Result);
         }
 
         private static void AssertAuthResult(MonitorResult result) {
-            AreEqual("{'id':'anonymous','clients':[],'stats':[]}",                                          result.anonymous.ToString());
-            AreEqual("{'id':'admin','clients':['admin-client'],'stats':[{'requests':1,'tasks':2}]}",        result.user.ToString());
+            var users   = result.users.Results;
+            var clients = result.clients.Results;
+            AreEqual("{'id':'anonymous','clients':[],'stats':[]}",                                          users[User.AnonymousId].ToString());
+            AreEqual("{'id':'admin','clients':['admin-client'],'stats':[{'requests':1,'tasks':2}]}",        users[new JsonKey("admin")].ToString());
                 
-            AreEqual("{'id':'admin-client','user':'admin','stats':[{'requests':1,'tasks':2}]}",             result.client.ToString());
+            AreEqual("{'id':'admin-client','user':'admin','stats':[{'requests':1,'tasks':2}]}",             clients[new JsonKey("admin-client")].ToString());
+            NotNull(result.user.Result);
+            NotNull(result.client.Result);
         }
         
+        private static void AssertAuthFailedResult(MonitorResult result) {
+            // IsFalse(result.users.Success);
+            // IsFalse(result.clients.Success);
+        }
+
         private  static async Task AssertMonitoringErrors(MonitorStore monitor) {
             var deleteUser      = monitor.users.Delete(new JsonKey("123"));
             var createUser      = monitor.users.Create(new UserInfo{id = new JsonKey("abc")});
@@ -130,36 +160,24 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client.Happy
             
             store.articles.Read().Find("xxx");
             store.customers.Read().Find("yyy");
-            await store.Sync();
+            await store.TrySync();
             
-            var allUsers        = monitor.users.QueryAll();
-            var allClients      = monitor.clients.QueryAll();
-            var findUser        = monitor.users.Read().Find(userKey);
-            var findUserClient  = monitor.clients.Read().Find(clientKey);
-
-            await monitor.Sync();
-            
-            var users       = allUsers.Results;
-            var clients     = allClients.Results;
-            
-            var result = new MonitorResult() {
-                users       = users,
-                clients     = clients,
-                user        = users     [userKey], 
-                anonymous   = users     [User.AnonymousId],
-                client      = clients   [clientKey],
+            var result = new MonitorResult {
+                users       = monitor.users.QueryAll(),
+                clients     = monitor.clients.QueryAll(),
+                user        = monitor.users.Read().Find(userKey),
+                client      = monitor.clients.Read().Find(clientKey),
+                sync        = await monitor.Sync()
             };
-            NotNull(findUser.Result);
-            NotNull(findUserClient.Result);
             return result;
         }
         
         internal class MonitorResult {
-            internal    Dictionary<JsonKey, UserInfo>   users;     
-            internal    Dictionary<JsonKey, ClientInfo> clients;  
-            internal    UserInfo                        user;      
-            internal    UserInfo                        anonymous; 
-            internal    ClientInfo                      client; 
+            internal    SyncResult                      sync;
+            internal    QueryTask<JsonKey, UserInfo>    users;
+            internal    QueryTask<JsonKey, ClientInfo>  clients;
+            internal    Find<JsonKey, UserInfo>         user;
+            internal    Find<JsonKey, ClientInfo>       client;
         }
     }
 }
