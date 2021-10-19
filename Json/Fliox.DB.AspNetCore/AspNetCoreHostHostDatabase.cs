22261ce3f482afd83e57bcac14895e0c0b5deb5d
Ullrich Praetz <ullrich@praetz.de>
2021-10-19 10:09:47 +0200

    add library Fliox.DB.AspNetCore

---
+++ Json/Fliox.DB.AspNetCore/AspNetCoreHostHostDatabase.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace Friflo.Json.Fliox.DB.AspNetCore
+{
+    public class AspNetCoreHostHostDatabase
+    {
+    }
+}
