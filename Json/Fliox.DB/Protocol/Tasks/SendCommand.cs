73c6ff429a8b6179e3f8a4085e4ad1b4b7f937ff
Ullrich Praetz <ullrich@praetz.de>
2021-10-24 07:43:55 +0200

    Protocol: Moved SendMessage / SendMessageResult to individual file

--- Json/Fliox.DB/Protocol/Tasks/SendCommand.cs
+++ Json/Fliox.DB/Protocol/Tasks/SendCommand.cs
@@ -2,31 +2,12 @@
 // See LICENSE file in the project root for full license information.
 
 using System.Threading.Tasks;
-using Friflo.Json.Fliox.DB.Client;
 using Friflo.Json.Fliox.DB.Host;
-using Friflo.Json.Fliox.DB.Host.Internal;
-using Friflo.Json.Fliox.DB.Protocol.Models;
 using Friflo.Json.Fliox.Mapper;
 
 namespace Friflo.Json.Fliox.DB.Protocol.Tasks
 {
     // ----------------------------------- task -----------------------------------
-    public class SendMessage : SyncRequestTask
-    {
-        [Fri.Required]  public  string          name;
-                        public  JsonValue       value;
-            
-        internal override       TaskType        TaskType => TaskType.message;
-        public   override       string          TaskName => $"name: '{name}'";
-
-        internal override Task<SyncTaskResult> Execute(EntityDatabase database, SyncResponse response, MessageContext messageContext) {
-            if (name == null)
-                return Task.FromResult<SyncTaskResult>(MissingField(nameof(name)));
-            SyncTaskResult result = new SendMessageResult();
-            return Task.FromResult(result);
-        }
-    }
-    
     public sealed class SendCommand : SendMessage
     {
         internal override       TaskType        TaskType => TaskType.command;
@@ -46,14 +27,6 @@ namespace Friflo.Json.Fliox.DB.Protocol.Tasks
     }
     
     // ----------------------------------- task result -----------------------------------
-    public class SendMessageResult : SyncTaskResult, ICommandResult
-    {
-        public CommandError                 Error { get; set; }
-
-        internal override   TaskType        TaskType => TaskType.message;
-    }
-
-
     public sealed class SendCommandResult : SendMessageResult
     {
         public              JsonValue       result;
---
+++ Json/Fliox.DB/Protocol/Tasks/SendMessage.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+using Friflo.Json.Fliox.DB.Host;
+using Friflo.Json.Fliox.DB.Protocol.Models;
+using Friflo.Json.Fliox.Mapper;
+
+namespace Friflo.Json.Fliox.DB.Protocol.Tasks
+{
+    // ----------------------------------- task -----------------------------------
+    public class SendMessage : SyncRequestTask
+    {
+        [Fri.Required]  public  string          name;
+                        public  JsonValue       value;
+            
+        internal override       TaskType        TaskType => TaskType.message;
+        public   override       string          TaskName => $"name: '{name}'";
+
+        internal override Task<SyncTaskResult> Execute(EntityDatabase database, SyncResponse response, MessageContext messageContext) {
+            if (name == null)
+                return Task.FromResult<SyncTaskResult>(MissingField(nameof(name)));
+            SyncTaskResult result = new SendMessageResult();
+            return Task.FromResult(result);
+        }
+    }
+    
+    
+    // ----------------------------------- task result -----------------------------------
+    public class SendMessageResult : SyncTaskResult, ICommandResult
+    {
+        public CommandError                 Error { get; set; }
+
+        internal override   TaskType        TaskType => TaskType.message;
+    }
+}
