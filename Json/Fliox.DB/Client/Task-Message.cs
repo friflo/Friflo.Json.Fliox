51cc54759285e091c2110415fd6b03d6b5453121
Ullrich Praetz <ullrich@praetz.de>
2021-10-27 11:22:12 +0200

    update MessageTask docs

--- Json/Fliox.DB/Client/Task-Message.cs
+++ Json/Fliox.DB/Client/Task-Message.cs
@@ -9,8 +9,9 @@ namespace Friflo.Json.Fliox.DB.Client
 {
     /// <summary>
     /// A <see cref="MessageTask"/> contains the message (or command) <see cref="name"/> and <see cref="value"/> sent to
-    /// an <see cref="EntityDatabase"/>. It is used to send to the message (or command) as en event to all clients which
-    /// successful subscribed the message by its <see cref="name"/>.
+    /// an <see cref="EntityDatabase"/> by <see cref="FlioxClient.SendMessage"/>.<br/>
+    /// It is used to send to the message (or command) as en event to all clients which successful subscribed the
+    /// message by its <see cref="name"/>.
     /// If the message was sent successful <see cref="SyncTask.Success"/> is true.
     /// <br/>
     /// <b>Note</b>: A message returns no result. To get a result send a command by <see cref="FlioxClient.SendCommand{TCommand,TResult}"/> 
@@ -33,10 +34,13 @@ namespace Friflo.Json.Fliox.DB.Client
     }
 
     /// <summary>
+    /// A <see cref="CommandTask"/> is created when a command is send to an <see cref="EntityDatabase"/> by
+    /// <see cref="FlioxClient.SendCommand{TResult}"/>.<br/>
     /// Additional to a <see cref="MessageTask"/> a <see cref="CommandTask"/> also provide a command <see cref="ResultJson"/>
     /// after the task is synced successful.
     /// <br/>
-    /// <b>Note</b>: For type safe access to the result use <see cref="CommandTask{TResult}"/>
+    /// <b>Note</b>: For type safe access to the result use <see cref="CommandTask{TResult}"/> returned by
+    /// <see cref="FlioxClient.SendCommand{TCommand,TResult}"/>
     /// </summary>
     public class CommandTask : MessageTask
     {
@@ -93,7 +97,9 @@ namespace Friflo.Json.Fliox.DB.Client
     }
 
     /// <summary>
-    /// Additional to a <see cref="MessageTask"/> a <see cref="CommandTask{TResult}"/> also provide a type safe access
+    /// A <see cref="CommandTask{TResult}"/> is created when a command is send to an <see cref="EntityDatabase"/> by
+    /// <see cref="FlioxClient.SendCommand{TResult}"/>.<br/>
+    /// Additional to a <see cref="MessageTask"/> a <see cref="CommandTask{TResult}"/> also provide type safe access
     /// to the command <see cref="Result"/> after the task is synced successful.
     /// </summary>
     public sealed class CommandTask<TResult> : CommandTask
