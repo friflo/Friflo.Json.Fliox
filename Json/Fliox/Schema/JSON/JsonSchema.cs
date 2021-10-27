50941ddb7ef2d0aace6a2b9faa25cc82467a0ee0
Ullrich Praetz <ullrich@praetz.de>
2021-10-27 10:40:09 +0200

    update JsonSchema docs

--- Json/Fliox/Schema/JSON/JsonSchema.cs
+++ Json/Fliox/Schema/JSON/JsonSchema.cs
@@ -19,6 +19,7 @@ namespace Friflo.Json.Fliox.Schema.JSON
     ///     <item><see cref="JsonType.discriminator"/></item>
     ///     <item><see cref="JsonType.isStruct"/></item>
     ///     <item><see cref="JsonType.isAbstract"/></item>
+    ///     <item><see cref="JsonType.commands"/></item>
     ///     <item><see cref="FieldType.isKey"/></item>
     /// </list>
     /// The restriction of <see cref="JsonSchema"/> are:
