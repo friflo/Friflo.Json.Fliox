548908565d1b5c0aedd02c83e934d380e9653f76
Ullrich Praetz <ullrich@praetz.de>
2021-10-30 07:23:06 +0200

    adjust allocated bytes for CI (Ubuntu)

--- Json.Tests/Common/UnitTest/Fliox/Client/TestUtils.cs
+++ Json.Tests/Common/UnitTest/Fliox/Client/TestUtils.cs
@@ -150,7 +150,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client
                 var diff = GC.GetAllocatedBytesForCurrentThread() - start;
                 
                 Console.WriteLine($"PocStore memory: {diff}");
-                var expected = Is.InRange(6840, 7024);
+                var expected = Is.InRange(6808, 7024);
                 That(diff, expected);
             }
         }
