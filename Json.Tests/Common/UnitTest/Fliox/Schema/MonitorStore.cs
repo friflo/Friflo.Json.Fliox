250decca181d9b70505c643104d3bcbfac7122f3
Ullrich Praetz <ullrich@praetz.de>
2021-11-13 11:35:19 +0100

    tests - generate Typescript Schema for MonitorStore

---
+++ Json.Tests/Common/UnitTest/Fliox/Schema/MonitorStore.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Friflo.Json.Fliox.Hub.Host.Monitor;
+using Friflo.Json.Fliox.Schema;
+using Friflo.Json.Tests.Common.Utils;
+using NUnit.Framework;
+
+namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema
+{
+    public static class MonitorStoreGen
+    {
+        // -------------------------------------- input: C# --------------------------------------
+        
+        /// C# -> Typescript
+        [Test]
+        public static void CS_Typescript () {
+            var options     = new NativeTypeOptions(typeof(MonitorStore));
+            var generator   = TypescriptGenerator.Generate(options);
+            generator.WriteFiles(CommonUtils.GetBasePath() + "assets~/Schema/Typescript/MonitorStore");
+        }
+    }
+}
