93b0bc00647d05e94c0e0b90d7d43b1326a6e4ca
Ullrich Praetz <ullrich@praetz.de>
2021-12-16 08:45:52 +0100

    demo-page - add filter link

--- Json.Tests/www/index.js
+++ Json.Tests/www/index.js
@@ -787,13 +787,19 @@ class App {
     }
 
     runFilter(event) {
-        // console.log(event);
-        if (event.code == 'Enter') {
-            var filter  = entityFilter.value;
-            var query   = filter == "" ? null : `filter=${encodeURIComponent(filter)}`;
-            var params  = { database: this.filter.database, container: this.filter.container };
-            this.loadEntities(params, query);
-        }
+        if (event.code != 'Enter')
+            return;
+        var filter  = entityFilter.value;
+        var query   = filter == "" ? null : `filter=${encodeURIComponent(filter)}`;
+        var params  = { database: this.filter.database, container: this.filter.container };
+        this.loadEntities(params, query);
+    }
+
+    updateFilterLink() {
+        var filter  = entityFilter.value;
+        var query   = filter == "" ? "" : `?filter=${encodeURIComponent(filter)}`;
+        const url = `./rest/${this.filter.database}/${this.filter.container}${query}`;
+        filterLink.href = url;
     }
 
     async loadEntities (p, query) {
