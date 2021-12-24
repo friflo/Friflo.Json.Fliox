03b5369ed7cad16823ea843831c8d34bbe8daf2c
Ullrich Praetz <ullrich@praetz.de>
2021-12-24 10:31:38 +0100

    separate Scalar implementations to Scalar-compare.cs & Scalar-operation.cs

---
+++ Json/Fliox/Transform/Scalar-compare.cs
@@ -0,0 +1,180 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Text;
+using Friflo.Json.Fliox.Transform.Query.Ops;
+
+// ReSharper disable SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+// ReSharper disable CompareOfFloatsByEqualityOperator
+namespace Friflo.Json.Fliox.Transform {
+    
+    public readonly partial struct Scalar
+    {
+        // ------------------------------- compare equality of two scalars -------------------------------
+        public Scalar EqualsTo(in Scalar other, Operation operation) {
+            switch (type) {
+                case ScalarType.String:
+                    if (other.IsString)
+                        return stringValue == other.stringValue ? True : False;
+                    return EqualsDefault(other, operation);
+                case ScalarType.Double:
+                    if (other.IsDouble)
+                        return DoubleValue == other.DoubleValue ? True : False;
+                    if (other.IsLong)
+                        return EqualsDouble(DoubleValue, other.LongValue);
+                    return EqualsDefault(other, operation);
+                case ScalarType.Long:
+                    if (other.IsDouble)
+                        return EqualsDouble(LongValue, other.DoubleValue);
+                    if (other.IsLong)
+                        return LongValue == other.LongValue ? True : False;
+                    return EqualsDefault(other, operation);
+                case ScalarType.Bool:
+                    if (other.IsBool)
+                        return primitiveValue == other.primitiveValue ? True : False; // possible primitive values: 0 or 1
+                    return EqualsDefault(other, operation);
+                case ScalarType.Null:
+                    if (other.IsNull)
+                        return True;
+                    return Null;
+                case ScalarType.Object:
+                case ScalarType.Array:
+                    return EqualsError("invalid operand", other, operation);
+                case ScalarType.Error:
+                    return this;
+                default:
+                    throw new NotSupportedException($"Scalar does not support EqualsTo() for type: {type}");                
+            }
+        }
+        
+        private static Scalar EqualsDouble(double left, double right) {
+            return left == right ? True : False;
+        }
+        
+        private Scalar EqualsDefault(in Scalar other, Operation operation) {
+            switch (other.type) {
+                case ScalarType.Null:
+                    return Null;
+                case ScalarType.Array:
+                case ScalarType.Object:
+                    return EqualsError("invalid operand", other, operation);
+                default:
+                    return EqualsError("incompatible operands", other, operation);
+            }
+        }
+        
+        private Scalar EqualsError(string error, in Scalar other, Operation operation) {
+            var sb = new StringBuilder();
+            sb.Append(error);
+            sb.Append(": ");
+            AppendTo(sb);
+            sb.Append(" == ");
+            other.AppendTo(sb);
+            if (operation != null) {
+                sb.Append(" in ");
+                var appendCx = new AppendCx(sb);
+                operation.AppendLinq(appendCx);
+            }
+            return Error(sb.ToString());
+        }
+        
+        // ------------------------------- compare order of two scalars -------------------------------
+        public long CompareTo(in Scalar other, Operation operation, out Scalar result) {
+            switch (type) {
+                case ScalarType.String:
+                    if (other.IsString) {
+                        result = default;
+                        return string.Compare(stringValue, other.stringValue, StringComparison.Ordinal);
+                    }
+                    return CompareDefault(other, operation, out result);
+                case ScalarType.Double:
+                    if (other.IsDouble) {
+                        result = default;
+                        return CompareDouble(DoubleValue, other.DoubleValue);
+                    }
+                    if (other.IsLong) {
+                        result = default;
+                        return CompareDouble(DoubleValue, other.LongValue);
+                    }
+                    return CompareDefault(other, operation, out result);
+                case ScalarType.Long:
+                    if (other.IsDouble) {
+                        result = default;
+                        return CompareDouble(LongValue, other.DoubleValue);
+                    }
+                    if (other.IsLong) {
+                        result = default;
+                        return LongValue - other.LongValue;
+                    }
+                    return CompareDefault(other, operation, out result);
+                case ScalarType.Bool:
+                    if (other.IsBool) {
+                        result = default;
+                        return primitiveValue - other.primitiveValue; // possible primitive values: 0 or 1
+                    }
+                    return CompareDefault(other, operation, out result);
+                case ScalarType.Null:
+                    if (other.IsNull) {
+                        result = default;
+                        return 0;
+                    }
+                    result = Null;
+                    return 0;
+                case ScalarType.Object:
+                case ScalarType.Array:
+                    result = CompareError("invalid operand", other, operation);
+                    return 0;
+                case ScalarType.Error:
+                    result = this;
+                    return 0;
+                default:
+                    throw new NotSupportedException($"Scalar does not support CompareTo() for type: {type}");                
+            }
+        }
+
+        private static int CompareDouble(double left, double right) {
+            double dif = left - right;
+            return dif < 0 ? -1 : (dif > 0 ? +1 : 0);
+        }
+        
+        private int CompareDefault(in Scalar other, Operation operation, out Scalar result) {
+            switch (other.type) {
+                case ScalarType.Null:
+                    result = Null;
+                    return 0;
+                case ScalarType.Array:
+                case ScalarType.Object:
+                    result = CompareError("invalid operand", other, operation);
+                    return 0;
+                case ScalarType.Error:
+                    result = other;
+                    return 0;
+                default:
+                    result = CompareError("incompatible operands", other, operation);                
+                    return 0;
+            }
+        }
+        
+        private Scalar CompareError(string error, in Scalar other, Operation operation) {
+            var sb = new StringBuilder();
+            sb.Append(error);
+            sb.Append(": ");
+            AppendTo(sb);
+            if (operation != null) {
+                sb.Append(' ');
+                sb.Append(operation.OperationName);
+                sb.Append(' ');
+            } else {
+                sb.Append(" with ");
+            }
+            other.AppendTo(sb);
+            if (operation != null) {
+                var appendCx = new AppendCx(sb);
+                sb.Append(" in ");
+                operation.AppendLinq(appendCx);
+            }
+            return Error(sb.ToString());
+        }
+    }
+}
---
+++ Json/Fliox/Transform/Scalar-operation.cs
@@ -0,0 +1,190 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Text;
+using Friflo.Json.Fliox.Transform.Query.Ops;
+
+// ReSharper disable SwitchStatementMissingSomeEnumCasesNoDefault
+namespace Friflo.Json.Fliox.Transform
+{
+    public readonly partial struct Scalar
+    {
+        // ------------------------------- unary arithmetic operations -------------------------------
+        public Scalar Abs(Operation operation) {
+            switch (type) {
+                case ScalarType.Null:   return Null;
+                case ScalarType.Double: return new Scalar(Math.Abs(DoubleValue));
+                case ScalarType.Long:   return new Scalar(Math.Abs(LongValue));
+            } 
+            return ExpectNumber(operation);
+        }
+        
+        public Scalar Ceiling(Operation operation) {
+            switch (type) {
+                case ScalarType.Null:   return Null;
+                case ScalarType.Double: return new Scalar(Math.Ceiling(DoubleValue));
+                case ScalarType.Long:   return new Scalar(             LongValue);
+            } 
+            return ExpectNumber(operation);
+        }
+        
+        public Scalar Floor(Operation operation) {
+            switch (type) {
+                case ScalarType.Null:   return Null;
+                case ScalarType.Double: return new Scalar(Math.Floor(DoubleValue));
+                case ScalarType.Long:   return new Scalar(           LongValue);
+            } 
+            return ExpectNumber(operation);
+        }
+        
+        public Scalar Exp(Operation operation) {
+            switch (type) {
+                case ScalarType.Null:   return Null;
+                case ScalarType.Double: return new Scalar(Math.Exp(DoubleValue));
+                case ScalarType.Long:   return new Scalar(Math.Exp(LongValue));
+            } 
+            return ExpectNumber(operation);
+        }
+        
+        public Scalar Log(Operation operation) {
+            switch (type) {
+                case ScalarType.Null:   return Null;
+                case ScalarType.Double: return new Scalar(Math.Log(DoubleValue));
+                case ScalarType.Long:   return new Scalar(Math.Log(LongValue));
+            } 
+            return ExpectNumber(operation);
+        }
+        
+        public Scalar Sqrt(Operation operation) {
+            switch (type) {
+                case ScalarType.Null:   return Null;
+                case ScalarType.Double: return new Scalar(Math.Sqrt(DoubleValue));
+                case ScalarType.Long:   return new Scalar(Math.Sqrt(LongValue));
+            }
+            return ExpectNumber(operation);
+        }
+
+        private Scalar ExpectNumber(Operation operation) {
+            var sb = new StringBuilder();
+            sb.Append("expect numeric operand. was: ");
+            AppendTo(sb);
+            if (operation != null) {
+                var appendCx = new AppendCx(sb);
+                sb.Append(" in ");
+                operation.AppendLinq(appendCx);
+            }
+            return Error(sb.ToString());
+        }
+        
+        // ------------------------------- binary arithmetic operations -------------------------------
+        public Scalar Add(in Scalar other, Operation operation) {
+            if (!AssertBinaryNumbers(other, operation, out Scalar error))
+                return error;
+            if (IsDouble) {
+                if (other.IsDouble)
+                    return new Scalar(DoubleValue + other.DoubleValue);
+                return     new Scalar(DoubleValue + other.LongValue);
+            }
+            if (other.IsDouble)
+                return     new Scalar(LongValue   + other.DoubleValue);
+            return         new Scalar(LongValue   + other.LongValue);
+        }
+        
+        public Scalar Subtract(in Scalar other, Operation operation) {
+            if (!AssertBinaryNumbers(other, operation, out Scalar error))
+                return error;
+            if (IsDouble) {
+                if (other.IsDouble)
+                    return new Scalar(DoubleValue - other.DoubleValue);
+                return     new Scalar(DoubleValue - other.LongValue);
+            }
+            if (other.IsDouble)
+                return     new Scalar(LongValue   - other.DoubleValue);
+            return         new Scalar(LongValue   - other.LongValue);
+        }
+        
+        public Scalar Multiply(in Scalar other, Operation operation) {
+            if (!AssertBinaryNumbers(other, operation, out Scalar error))
+                return error;
+            if (IsDouble) {
+                if (other.IsDouble)
+                    return new Scalar(DoubleValue * other.DoubleValue);
+                return     new Scalar(DoubleValue * other.LongValue);
+            }
+            if (other.IsDouble)
+                return     new Scalar(LongValue   * other.DoubleValue);
+            return         new Scalar(LongValue   * other.LongValue);
+        }
+        
+        public Scalar Divide(in Scalar other, Operation operation) {
+            if (!AssertBinaryNumbers(other, operation, out Scalar error))
+                return error;
+            if (IsDouble) {
+                if (other.IsDouble)
+                    return new Scalar(DoubleValue / other.DoubleValue);
+                return     new Scalar(DoubleValue / other.LongValue);
+            }
+            if (other.IsDouble)
+                return     new Scalar(LongValue   / other.DoubleValue);
+            return         new Scalar(LongValue   / other.LongValue);
+        }
+        
+        private bool AssertBinaryNumbers(in Scalar other, Operation operation, out Scalar error) {
+            if (IsNumber && other.IsNumber) {
+                error = default;
+                return true;
+            }
+            var sb = new StringBuilder();
+            sb.Append("expect numeric operands. left: ");
+            AppendTo(sb);
+            sb.Append(", right: ");
+            other.AppendTo(sb);
+            if (operation != null) {
+                sb.Append(" in ");
+                var cx = new AppendCx(sb);
+                operation.AppendLinq(cx);
+            }
+            error = Error(sb.ToString());
+            return false;
+        }
+        
+        // ------------------------------- binary string operations -------------------------------
+        public Scalar Contains(in Scalar other, Operation operation) {
+            if (!AssertBinaryString(other, operation, out Scalar error))
+                return error;
+            return stringValue.Contains(other.stringValue) ? True : False;
+        }
+        
+        public Scalar StartsWith(in Scalar other, Operation operation) {
+            if (!AssertBinaryString(other, operation, out Scalar error))
+                return error;
+            return stringValue.StartsWith(other.stringValue) ? True : False;
+        }
+        
+        public Scalar EndsWith(in Scalar other, Operation operation) {
+            if (!AssertBinaryString(other, operation, out Scalar error))
+                return error;
+            return stringValue.EndsWith(other.stringValue) ? True : False;
+        }
+        
+        private bool AssertBinaryString(in Scalar other, Operation operation, out Scalar error) {
+            if (IsString && other.IsString) {
+                error = default;
+                return true;
+            }
+            var sb = new StringBuilder();
+            sb.Append("expect string operands. left: ");
+            AppendTo(sb);
+            sb.Append(", right: ");
+            other.AppendTo(sb);
+            if (operation != null) {
+                sb.Append(" in ");
+                var cx = new AppendCx(sb);
+                operation.AppendLinq(cx);
+            }
+            error = Error(sb.ToString());
+            return false;
+        }
+    }
+}
