ff1b38f239ff3b7e0443b9436dd5a871887c2c7a
Ullrich Praetz <ullrich@praetz.de>
2022-02-04 07:47:05 +0100

    CreateHttpHost() docs

--- Json.Tests/Main/Program.cs
+++ Json.Tests/Main/Program.cs
@@ -45,14 +45,14 @@ namespace Friflo.Json.Tests.Main
         /// <br/>
         /// The features of a <see cref="HttpHostHub"/> instance are:
         /// <list type="bullet">
-        ///   <item> Providing all common database operations to query, read, create, updates and delete records</item>
+        ///   <item> Providing all common database operations to query, read, create, update, delete and patch records </item>
         ///   <item> Expose access to the service in two ways:<br/>
         ///     1. POST via a single path ./ enabling batching multiple tasks in a single request<br/>
         ///     2. Common REST API to POST, GET, PUT, DELETE and PATCH with via a path like ./rest/database/container/id
         ///   </item>
         ///   <item> Enable Messaging and Pub-Sub to send messages or commands and setup subscriptions by multiple clients </item>
         ///   <item> Enable user authentication and authorization of tasks requested by a user </item>
-        ///   <item> Access and change user permission and roles required for authorization via the extension database: user_db</item>
+        ///   <item> Access and change user permission and roles required for authorization via the extension database: user_db </item>
         ///   <item> Expose server Monitoring as an extension database to get statistics about requests and tasks executed by users and clients </item>
         ///   <item> Adding a database schema to: <br/>
         ///     1. validate records written to the database by its schema definition <br/>
