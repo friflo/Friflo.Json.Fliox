95af077f3b6bec88385f4aaca530335c685e96a4
Ullrich Praetz <ullrich@praetz.de>
2022-02-05 13:06:42 +0100

    remove copyright header in DemoHub files

--- DemoHub/DemoHandler.cs
+++ DemoHub/DemoHandler.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Ullrich Praetz. All rights reserved.
-// See LICENSE file in the project root for full license information.
-
 using Friflo.Json.Fliox.Hub.Host;
 
 namespace Friflo.Json.Fliox.DemoHub
