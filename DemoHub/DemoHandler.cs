3a3d756d4324dcb49e96addebc76e7fde25d847e
Ullrich Praetz <ullrich@praetz.de>
2022-02-07 10:30:37 +0100

    DemoHandler docs

--- DemoHub/DemoHandler.cs
+++ DemoHub/DemoHandler.cs
@@ -3,7 +3,9 @@ using Friflo.Json.Fliox.Hub.Host;
 namespace Friflo.Json.Fliox.DemoHub
 {
     /// <summary>
-    /// Implementation of custom database commands declared by <see cref="DemoStore"/>.
+    /// Implementation of custom database commands declared by <see cref="DemoStore"/>. <br/>
+    /// Every method using <see cref="Command{TValue}"/> as parameter is added as a command handler.
+    /// Their method names need to match the commands declared in <see cref="DemoStore"/>.
     /// </summary> 
     public class DemoHandler : TaskHandler
     {
