c425c290f9c0bf6dece98f17dce2bd96613d60a7
Ullrich Praetz <ullrich@praetz.de>
2022-02-08 13:19:50 +0100

    DemoHub - moved FakeUtils to separate file

--- DemoHub/DemoHandler.cs
+++ DemoHub/DemoHandler.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Threading.Tasks;
-using Bogus;
 using Friflo.Json.Fliox.Hub.Host;
 
 // ReSharper disable UnusedMember.Local
@@ -57,33 +55,6 @@ namespace Friflo.Json.Fliox.DemoHub
             
             await demoStore.SyncTasks();
             
-            return result;
-        }
-    }
-
-    internal class FakeUtils
-    {
-        private             int             articleCounter;
-        private readonly    Faker<Article>  articleFaker;
-        
-        internal FakeUtils()
-        {
-            Randomizer.Seed = new Random(1337);
-            
-            articleFaker = new Faker<Article>()
-                .RuleFor(a => a.id,     f => $"article-{articleCounter++}")
-                .RuleFor(a => a.name,   f => f.Commerce.Product());
-        }
-        
-        internal FakeResult CreateFakes(Fake fake) {
-            var result = new FakeResult();
-            
-            if (fake.articles.HasValue) {
-                result.articles = new Article[fake.articles.Value];
-                for (int n = 0; n < fake.articles; n++) {
-                    result.articles[n] = articleFaker.Generate();
-                }
-            }
             return result;
         }
     }
---
+++ DemoHub/FakeUtils.cs
@@ -0,0 +1,32 @@
+using System;
+using Bogus;
+
+namespace Friflo.Json.Fliox.DemoHub
+{
+    internal class FakeUtils
+    {
+        private             int             articleCounter;
+        private readonly    Faker<Article>  articleFaker;
+        
+        internal FakeUtils()
+        {
+            Randomizer.Seed = new Random(1337);
+            
+            articleFaker = new Faker<Article>()
+                .RuleFor(a => a.id,     f => $"article-{articleCounter++}")
+                .RuleFor(a => a.name,   f => f.Commerce.Product());
+        }
+        
+        internal FakeResult CreateFakes(Fake fake) {
+            var result = new FakeResult();
+            
+            if (fake.articles.HasValue) {
+                result.articles = new Article[fake.articles.Value];
+                for (int n = 0; n < fake.articles; n++) {
+                    result.articles[n] = articleFaker.Generate();
+                }
+            }
+            return result;
+        }
+    }
+}
