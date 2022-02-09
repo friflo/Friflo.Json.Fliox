fd09794108f8a3b9732cfbcffc1fafe48c0e5013
Ullrich Praetz <ullrich@praetz.de>
2022-02-09 12:11:26 +0100

    add FakeResult.filter

--- DemoHub/FakeUtils.cs
+++ DemoHub/FakeUtils.cs
@@ -126,7 +126,7 @@ namespace Friflo.Json.Fliox.DemoHub
                 producers   = result.producers? .Length,
                 employees   = result.employees? .Length,
             };
-            result.filter = $"o.id.Contains('-{fakeCounter}.')";
+            result.filter = $"use lambda as container filter: o.id.Contains('-{fakeCounter}.')";
             result.added = added;
             return result;
         }
