b67f2b82934c14c147749babd5ee3ad7f9b5542c
Ullrich Praetz <ullrich@praetz.de>
2022-02-17 12:29:40 +0100

    trim trailing spaces

--- Json/Fliox.Hub/Client/Internal/SyncSet.cs
+++ Json/Fliox.Hub/Client/Internal/SyncSet.cs
@@ -30,68 +30,68 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
         // Note!
         // All fields & getters must be private by all means to ensure that all scheduled tasks of a SyncTasks() call
         // managed by this instance can be mapped to their task results safely.
-        
+
         private     readonly EntitySet<TKey, T>             set;
-        
+
         // --- backing fields for lazy-initialized getters
         private     List<ReadTask<TKey, T>>                 _reads;
-        
+
         private     Dictionary<string, QueryTask<TKey, T>>  _queries;
-        
+
         private     List<AggregateTask<TKey, T>>            _aggregates;
         private     int                                     aggregatesTasksIndex;
-        
+
         private     List<CloseCursorsTask>                  _closeCursors;
         private     int                                     closeCursorsIndex;
 
         private     HashSet<T>                              _autos;
-        
+
         private     ReserveKeysTask<TKey,T>                 _reserveKeys;
-            
+
         private     Dictionary<JsonKey, Peer<T>>            _creates;
         private     List<WriteTask>                         _createTasks;
-        
+
         private     Dictionary<JsonKey, Peer<T>>            _upserts;
         private     List<WriteTask>                         _upsertTasks;
-        
+
         private     Dictionary<JsonKey, EntityPatch>        _patches;
         private     List<PatchTask<T>>                      _patchTasks;
-        
+
         private     HashSet<TKey>                           _deletes;
         private     List<DeleteTask<TKey, T>>               _deleteTasks;
 
         // --- lazy-initialized getters => they behave like readonly fields
         private     List<ReadTask<TKey, T>>                 Reads()      => _reads       ?? (_reads       = new List<ReadTask<TKey, T>>());
-        
+
         /// key: <see cref="QueryTask{TKey,T}.filterLinq"/>
         private     Dictionary<string, QueryTask<TKey, T>>  Queries()    => _queries     ?? (_queries     = new Dictionary<string, QueryTask<TKey, T>>());
-        
+
         private     List<AggregateTask<TKey, T>>            Aggregates() => _aggregates  ?? (_aggregates  = new List<AggregateTask<TKey, T>>());
-        
+
         private     List<CloseCursorsTask>                  CloseCursors()=> _closeCursors ?? (_closeCursors  = new List<CloseCursorsTask>());
-        
+
         private     SubscribeChangesTask<T>                 subscribeChanges;
 
         private     HashSet<T>                              Autos()      => _autos       ?? (_autos        = new HashSet<T>(EntityEqualityComparer<T>.Instance));
 
         private     Dictionary<JsonKey, Peer<T>>            Creates()    => _creates     ?? (_creates     = new Dictionary<JsonKey, Peer<T>>(JsonKey.Equality));
         private     List<WriteTask>                         CreateTasks()=> _createTasks ?? (_createTasks = new List<WriteTask>());
-        
+
         private     Dictionary<JsonKey, Peer<T>>            Upserts()    => _upserts     ?? (_upserts     = new Dictionary<JsonKey, Peer<T>>(JsonKey.Equality));
         private     List<WriteTask>                         UpsertTasks()=> _upsertTasks ?? (_upsertTasks = new List<WriteTask>());
 
         private     Dictionary<JsonKey, EntityPatch>        Patches()    => _patches     ?? (_patches     = new Dictionary<JsonKey, EntityPatch>(JsonKey.Equality));
         private     List<PatchTask<T>>                      PatchTasks() => _patchTasks  ?? (_patchTasks  = new List<PatchTask<T>>());
-        
+
         private     HashSet<TKey>                           Deletes()    => _deletes     ?? (_deletes     = CreateHashSet<TKey>(0));
         private     List<DeleteTask<TKey, T>>               DeleteTasks()=> _deleteTasks ?? (_deleteTasks = new List<DeleteTask<TKey, T>>());
-        
+
         private     DeleteAllTask<TKey, T>                  _deleteTaskAll;
-        
+
         internal SyncSet(EntitySet<TKey, T> set) {
             this.set = set;
         }
-        
+
         internal override bool AddCreate (Peer<T> peer) {
             peer.assigned = true;
             Creates().TryAdd(peer.id, peer);    // sole place a peer (entity) is added
@@ -101,7 +101,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             }
             return false;
         }
-        
+
         internal override void AddUpsert (Peer<T> peer) {
             peer.assigned = true;
             Upserts().TryAdd(peer.id, peer);      // sole place a peer (entity) is added
@@ -109,42 +109,42 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
                 peer.updated = true;            // sole place created set to true
             }
         }
-        
+
         internal void AddDelete (TKey id) {
             Deletes().Add(id);
         }
-        
+
         internal void AddDeleteRange (ICollection<TKey> keys) {
             var deletes = Deletes();
             deletes.EnsureCapacity(deletes.Count + keys.Count);
             deletes.UnionWith(keys);
         }
-        
+
         // --- Read
         internal ReadTask<TKey, T> Read() {
             var read = new ReadTask<TKey, T>(set);
             Reads().Add(read);
             return read;
         }
-        
+
         // --- Query
         internal QueryTask<TKey, T> QueryFilter(FilterOperation filter) {
             var filterLinq = filter.Linq;
-            var queries = Queries(); 
+            var queries = Queries();
             if (queries.TryGetValue(filterLinq, out QueryTask<TKey, T> query))
                 return query;
             query = new QueryTask<TKey, T>(filter, set.intern.store);
             queries.Add(filterLinq, query);
             return query;
         }
-        
+
         internal CloseCursorsTask CloseCursors(IEnumerable<string> cursors) {
-            var closeCursors = CloseCursors(); 
+            var closeCursors = CloseCursors();
             var closeCursor = new CloseCursorsTask(cursors);
             closeCursors.Add(closeCursor);
             return closeCursor;
         }
-        
+
         // --- Aggregate
         internal AggregateTask<TKey, T> AggregateFilter(AggregateType type, FilterOperation filter) {
             var aggregates = Aggregates();
@@ -152,7 +152,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             aggregates.Add(aggregate);
             return  aggregate;
         }
-        
+
         // --- SubscribeChanges
         internal SubscribeChangesTask<T> SubscribeChangesFilter(IEnumerable<Change> changes, FilterOperation filter) {
             if (subscribeChanges == null)
@@ -160,17 +160,17 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             subscribeChanges.Set(changes, filter);
             return subscribeChanges;
         }
-        
+
         // --- ReserveKeys
         internal ReserveKeysTask<TKey, T> ReserveKeys(int count) {
             var reserve = _reserveKeys;
             if (reserve == null) {
-                return _reserveKeys = new ReserveKeysTask<TKey,T>(count); 
+                return _reserveKeys = new ReserveKeysTask<TKey,T>(count);
             }
             reserve.count += count;
             return reserve;
         }
-        
+
         // --- Create
         internal CreateTask<T> Create(T entity) {
             if (set.intern.autoIncrement) {
@@ -186,7 +186,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             CreateTasks().Add(create);
             return create;
         }
-        
+
         internal CreateTask<T> CreateRange(ICollection<T> entities) {
             foreach (var entity in entities) {
                 var peer = set.CreatePeer(entity);
@@ -196,7 +196,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             CreateTasks().Add(create);
             return create;
         }
-        
+
         // --- Upsert
         internal UpsertTask<T> Upsert(T entity) {
             var peer = set.CreatePeer(entity);
@@ -205,7 +205,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             UpsertTasks().Add(upsert);
             return upsert;
         }
-        
+
         internal UpsertTask<T> UpsertRange(ICollection<T> entities) {
             foreach (var entity in entities) {
                 var peer = set.CreatePeer(entity);
@@ -215,20 +215,20 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             UpsertTasks().Add(upsert);
             return upsert;
         }
-        
+
         // --- Patch
         internal PatchTask<T> Patch(Peer<T> peer) {
             var patchTask  = new PatchTask<T>(peer, set);
             PatchTasks().Add(patchTask);
             return patchTask;
         }
-        
+
         internal PatchTask<T> PatchRange(ICollection<Peer<T>> peers) {
             var patchTask  = new PatchTask<T>(peers, set);
             PatchTasks().Add(patchTask);
             return patchTask;
         }
-        
+
         // --- Delete
         internal DeleteTask<TKey, T> Delete(TKey key) {
             AddDelete(key);
@@ -236,21 +236,21 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             DeleteTasks().Add(delete);
             return delete;
         }
-        
+
         internal DeleteTask<TKey, T> DeleteRange(ICollection<TKey> keys) {
             AddDeleteRange(keys);
             var delete = new DeleteTask<TKey, T>(keys.ToList(), this);
             DeleteTasks().Add(delete);
             return delete;
         }
-        
+
         internal DeleteAllTask<TKey, T> DeleteAll() {
             if (_deleteTaskAll != null)
                 return _deleteTaskAll;
             _deleteTaskAll = new DeleteAllTask<TKey, T>();
             return _deleteTaskAll;
         }
-        
+
         // --- Log changes -> create patches
         internal void LogSetChanges(Dictionary<TKey, Peer<T>> peers, LogTask logTask, ObjectMapper mapper) {
             foreach (var peerPair in peers) {
@@ -270,7 +270,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
         /// In case the given entity is already <see cref="Peer{T}.created"/> or <see cref="Peer{T}.updated"/> trace
         /// the entity to find changes in referenced entities in <see cref="Ref{TKey,T}"/> fields of the given entity.
         /// In these cases <see cref="Map.RefMapper{TKey,T}.Trace"/> add untracked entities (== have no <see cref="Peer{T}"/>)
-        /// which is not already assigned) 
+        /// which is not already assigned)
         private void GetEntityChanges(Peer<T> peer, LogTask logTask, ObjectMapper mapper) {
             ref var intern = ref set.intern;
             var tracer = intern.GetTracer();
@@ -282,7 +282,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             var patchSource = peer.PatchSource;
             if (patchSource != null) {
                 var entity = peer.Entity;
-                var objectPatcher   = intern.store._intern.ObjectPatcher(); 
+                var objectPatcher   = intern.store._intern.ObjectPatcher();
                 var diff = objectPatcher.differ.GetDiff(patchSource, entity, mapper.writer);
                 if (diff == null)
                     return;
@@ -290,11 +290,11 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
                 var entityPatch = new EntityPatch {
                     patches = patchList
                 };
-                SetNextPatchSource(peer, mapper); // todo next patch source need to be set on Synchronize() 
+                SetNextPatchSource(peer, mapper); // todo next patch source need to be set on Synchronize()
                 var id = peer.id;
                 Patches()[id] = entityPatch;
                 logTask.AddPatch(this, id);
-                
+
                 intern.store._intern.tracerLogTask = logTask;
                 tracer.Trace(entity);
             }
@@ -314,8 +314,8 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             DeleteAll           (tasks);
             SubscribeChanges    (tasks);
         }
-        
-        
+
+
         private void ReserveKeys(List<SyncRequestTask> tasks) {
             if (_reserveKeys == null)
                 return;
@@ -325,9 +325,9 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             };
             tasks.Add(req);
         }
-        
+
         private void CreateEntities(List<SyncRequestTask> tasks, ObjectMapper mapper) {
-            var createCount = _creates?.Count   ?? 0; 
+            var createCount = _creates?.Count   ?? 0;
             var autoCount   = _autos?.Count     ?? 0;
             var count       = createCount + autoCount;
             if (count == 0)
@@ -368,7 +368,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             };
             tasks.Add(req);
         }
-        
+
         private void UpsertEntities(List<SyncRequestTask> tasks, ObjectMapper mapper) {
             if (_upserts == null || _upserts.Count == 0)
                 return;
@@ -377,7 +377,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             writer.WriteNullMembers = set.intern.writeNull;
             var entries = new List<JsonValue>   (_upserts.Count);
             var keys    = new List<JsonKey>    (_upserts.Count);
-            
+
             foreach (var upsertPair in _upserts) {
                 T entity    = upsertPair.Value.Entity;
                 var json    = writer.WriteAsArray(entity);
@@ -423,7 +423,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             }
             tasks.Add(readList);
         }
-        
+
         private void QueryEntities(List<SyncRequestTask> tasks) {
             if (_queries == null || _queries.Count == 0)
                 return;
@@ -442,7 +442,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
                 var req = new QueryEntities {
                     container   = set.name,
                     keyName     = SyncKeyName(set.GetKeyName()),
-                    isIntKey    = IsIntKey(set.IsIntKey()),  
+                    isIntKey    = IsIntKey(set.IsIntKey()),
                     filterTree  = filterTree,
                     filter      = query.filterLinq,
                     references  = references,
@@ -464,23 +464,23 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
                 }
                 var req = new AggregateEntities {
                     container   = set.name,
-                    type        = aggregate.type,  
+                    type        = aggregate.type,
                 //  keyName     = SyncKeyName(set.GetKeyName()),
-                //  isIntKey    = IsIntKey(set.IsIntKey()),  
+                //  isIntKey    = IsIntKey(set.IsIntKey()),
                     filterTree  = filterTree,
                     filter      = aggregate.filterLinq
                 };
                 tasks.Add(req);
             }
         }
-        
+
         private void CloseCursors(List<SyncRequestTask> tasks) {
             if (_closeCursors == null || _closeCursors.Count == 0)
                 return;
             foreach (var closeCursor in _closeCursors) {
                 var req = new CloseCursors {
                     container   = set.name,
-                    cursors     = closeCursor.cursors 
+                    cursors     = closeCursor.cursors
                 };
                 tasks.Add(req);
             }
@@ -490,15 +490,15 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
         {
             var patches     = _patches;
             var patchTasks  = _patchTasks;
-            
+
             if (patchTasks != null && patchTasks.Count > 0) {
-                patches = Patches();    
+                patches = Patches();
                 var writer = mapper.writer;
                 foreach (var patchTask in patchTasks) {
                     // todo performance: cache MemberAccess instances with members as key
                     var memberAccess    = new MemberAccess(patchTask.members);
                     var memberAccessor  = new MemberAccessor(writer);
-                    
+
                     foreach (var peer in patchTask.peers) {
                         var entity  = peer.Entity;
                         var id      = peer.id;
@@ -548,7 +548,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             tasks.Add(req);
             deletes.Clear();
         }
-        
+
         private void DeleteAll(List<SyncRequestTask> tasks) {
             var deleteAll = _deleteTaskAll;
             if (deleteAll == null)
@@ -559,7 +559,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             };
             tasks.Add(req);
         }
-        
+
         private void SubscribeChanges(List<SyncRequestTask> tasks) {
             var sub = subscribeChanges;
             if (sub == null)
@@ -571,14 +571,14 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
             };
             tasks.Add(req);
         }
-        
+
         // ----------------------------------- helper methods -----------------------------------
         private static void AddReferences(List<References> references, SubRefs refs) {
             foreach (var readRefs in refs) {
                 var queryReference = new References {
                     container   = readRefs.Container,
                     keyName     = SyncKeyName(readRefs.KeyName),
-                    isIntKey    = IsIntKey(readRefs.IsIntKey), 
+                    isIntKey    = IsIntKey(readRefs.IsIntKey),
                     selector    = readRefs.Selector
                 };
                 references.Add(queryReference);
@@ -589,7 +589,7 @@ namespace Friflo.Json.Fliox.Hub.Client.Internal
                 }
             }
         }
-        
+
         private void SetNextPatchSource(Peer<T> peer, ObjectMapper mapper) {
             var jsonArray   = mapper.writer.WriteAsArray(peer.Entity);
             var json        = new JsonValue(jsonArray);
