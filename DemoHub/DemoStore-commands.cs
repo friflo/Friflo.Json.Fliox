16005810e8eeb7d394f81958c084e794dc87c624
Ullrich Praetz <ullrich@praetz.de>
2022-02-17 13:39:43 +0100

    DemoHub cleanup

--- DemoHub/DemoStore-commands.cs
+++ DemoHub/DemoStore-commands.cs
@@ -9,13 +9,13 @@ namespace Friflo.Json.Fliox.DemoHub
     public partial class DemoStore {
         // --- commands
         /// <summary> Generate random entities (records) in the containers listed in the <see cref="DemoHub.Fake"/> param </summary>
-        public CommandTask<FakeResult>Fake (Fake        param)  => SendCommand<Fake, FakeResult>("demo.Fake", param);
+        public CommandTask<FakeResult>  Fake (Fake      param)  => SendCommand<Fake, FakeResult>("demo.Fake", param);
         
         /// <summary> simple command adding two numbers - no database access. </summary>
-        public CommandTask<double>    Add  (Operands    param)  => SendCommand<Operands, double>("demo.Add",  param);
+        public CommandTask<double>      Add  (Operands  param)  => SendCommand<Operands, double>("demo.Add",  param);
         
         /// <summary> simple command multiplying two numbers - no database access. </summary>
-        public CommandTask<double>    Mul  (Operands    param)  => SendCommand<Operands, double>("demo.Mul",  param);
+        public CommandTask<double>      Mul  (Operands  param)  => SendCommand<Operands, double>("demo.Mul",  param);
     }
     
     public class Operands {
