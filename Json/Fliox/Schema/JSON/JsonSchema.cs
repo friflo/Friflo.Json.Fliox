3abdb0d2d0bb55f6657daa07c2de6afeeec89a1c
Ullrich Praetz <ullrich@praetz.de>
2022-02-21 06:38:01 +0100

    JsonSchema indentation

--- Json/Fliox/Schema/JSON/JsonSchema.cs
+++ Json/Fliox/Schema/JSON/JsonSchema.cs
@@ -4,6 +4,9 @@
 using System.Collections.Generic;
 using Friflo.Json.Fliox.Mapper;
 
+using Req       = Friflo.Json.Fliox.Mapper.Fri.RequiredAttribute;
+using Ignore    = Friflo.Json.Fliox.Mapper.Fri.IgnoreAttribute;
+
 // ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable UnassignedField.Global
 // ReSharper disable CollectionNeverUpdated.Global
@@ -12,7 +15,7 @@ namespace Friflo.Json.Fliox.Schema.JSON
     /// <summary>
     /// Compatible subset of JSON Schema with some extensions required for code generation.
     /// JSON Schema specification: https://json-schema.org/specification.html
-    /// 
+    ///
     /// Following extensions are added to JSON Schema:
     /// <list type="bullet">
     ///     <item><see cref="JsonType.extends"/></item>
@@ -28,8 +31,8 @@ namespace Friflo.Json.Fliox.Schema.JSON
     ///   <item>
     ///     A schema property cannot nest anonymous types by "type": "object" with "properties": { ... }.
     ///     The property type needs to be a known type like "string", ... or a referenced ("$ref") type.
-    ///     This restriction enables generation of code and types for languages without support of anonymous types. 
-    ///     It also enables concise error messages for validation errors when using <see cref="Validation.TypeValidator"/>. 
+    ///     This restriction enables generation of code and types for languages without support of anonymous types.
+    ///     It also enables concise error messages for validation errors when using <see cref="Validation.TypeValidator"/>.
     ///   </item>
     ///   <item>
     ///     Note: Arrays and dictionaries are also valid schema properties. E.g. <br></br>
@@ -44,115 +47,105 @@ namespace Friflo.Json.Fliox.Schema.JSON
     /// </summary>
     public sealed class JsonSchema
     {
-        [Fri.Property(Name ="$ref")]public  string                          rootRef;
-        
-        [Fri.Required]              public  Dictionary<string, JsonType>    definitions;
-        
+        [Fri.Property(Name =                           "$ref")]
+                public  string                          rootRef;
+
+        [Req]   public  Dictionary<string, JsonType>    definitions;
+
         /// <summary>file name is <see cref="name"/> + ".json".
         /// E.g. <see cref="name"/>: Standard.json, <see cref="name"/>: "Standard</summary>
-        [Fri.Ignore]                public  string                          fileName;
-        [Fri.Ignore]                public  string                          name;
-        [Fri.Ignore]                public  Dictionary<string, JsonTypeDef> typeDefs;
+        [Ignore]public  string                          fileName;
+        [Ignore]public  string                          name;
+        [Ignore]public  Dictionary<string, JsonTypeDef> typeDefs;
 
-        public override                     string                          ToString() => fileName;
+        public override string                          ToString() => fileName;
     }
-    
+
     public sealed class JsonType
     {
-        public  TypeRef                         extends;
-        
-        public  string                          discriminator;
-        public  List<FieldType>                 oneOf;
-        public  bool?                           isAbstract;
-        //
-        // public  SchemaType?                  type; // todo use this
-        public  string                          type; // null or SchemaType
-        public  string                          key;  // if null a property named "id" must exist
-        public  Dictionary<string, FieldType>   properties;
-        public  Dictionary<string, CommandType> commands;
-        public  bool?                           isStruct;
-        public  List<string>                    required;
-        public  bool                            additionalProperties;
-        //
-        [Fri.Property(Name =                   "enum")]
-        public  List<string>                    enums;
-        
-        [Fri.Ignore]
-        public  string                          name;
-        
-        public  string                          description;
+                public  TypeRef                         extends;
 
-        public override string                  ToString() => name;
+                public  string                          discriminator;
+                public  List<FieldType>                 oneOf;
+                public  bool?                           isAbstract;
+        //
+             // public  SchemaType?                     type; // todo use this
+                public  string                          type; // null or SchemaType
+                public  string                          key;  // if null a property named "id" must exist
+                public  Dictionary<string, FieldType>   properties;
+                public  Dictionary<string, CommandType> commands;
+                public  bool?                           isStruct;
+                public  List<string>                    required;
+                public  bool                            additionalProperties;
+        //
+        [Fri.Property(Name =                           "enum")]
+                public  List<string>                    enums;
+
+        [Ignore]public  string                          name;
+
+                public  string                          description;
+
+        public override string                          ToString() => name;
     }
-    
+
     public sealed class TypeRef {
-        [Fri.Property (Name =           "$ref")]
-        [Fri.Required]  public  string  reference;
-                        public  string  type;   // not used - was used for nullable array elements
+        [Fri.Property (Name =          "$ref")]
+        [Req]   public  string          reference;
+                public  string          type;   // not used - was used for nullable array elements
 
-        public override         string  ToString() => reference;
+        public override string          ToString() => reference;
     }
-    
+
     public sealed class FieldType
     {
-        public  JsonValue       type;           // SchemaType or SchemaType[]
-        
-        [Fri.Property(Name =   "enum")]
-        public  List<string>    discriminant;   // contains exactly one element for a specific type or a list is inside an abstract type
-        
-        public  FieldType       items;
-        
-        public  List<FieldType> oneOf;
-        
-        public  long?           minimum;
-        public  long?           maximum;
-        
-        public  string          pattern;
-        public  string          format;  // "date-time"
+                public  JsonValue       type;           // SchemaType or SchemaType[]
 
-        [Fri.Property(Name =   "$ref")]
-        public  string          reference;
+        [Fri.Property(Name =           "enum")]
+                public  List<string>    discriminant;   // contains exactly one element for a specific type or a list is inside an abstract type
 
-        public  FieldType       additionalProperties;
-        
-        [Fri.Ignore]
-        public  string          name;
-        [Fri.Ignore]
-        public  bool?           isKey;
-        public  bool?           isAutoIncrement;
-        
-        public  string          relation;
-        
-        public  string          description; 
-        
-        public override string  ToString() => name;
+                public  FieldType       items;
+
+                public  List<FieldType> oneOf;
+
+                public  long?           minimum;
+                public  long?           maximum;
+
+                public  string          pattern;
+                public  string          format;  // "date-time"
+
+        [Fri.Property(Name =           "$ref")]
+                public  string          reference;
+
+                public  FieldType       additionalProperties;
+
+        [Ignore]public  string          name;
+        [Ignore]public  bool?           isKey;
+                public  bool?           isAutoIncrement;
+
+                public  string          relation;
+
+                public  string          description;
+
+        public override string          ToString() => name;
     }
- 
+
     public sealed class CommandType
     {
-        [Fri.Ignore]
-        public  string          name;
-        public  FieldType       param;
-        public  FieldType       result;
-        public  string          description;
+        [Ignore]public  string          name;
+                public  FieldType       param;
+                public  FieldType       result;
+                public  string          description;
 
-        public override string  ToString() => name;
+                public override string  ToString() => name;
     }
-    
+
     public enum SchemaType {
-        [Fri.EnumValue(Name = "null")]
-        Null,
-        [Fri.EnumValue(Name = "object")]
-        Object,
-        [Fri.EnumValue(Name = "string")]
-        String,
-        [Fri.EnumValue(Name = "boolean")]
-        Boolean,
-        [Fri.EnumValue(Name = "number")]
-        Number,
-        [Fri.EnumValue(Name = "integer")]
-        Integer,
-        [Fri.EnumValue(Name = "array")]
-        Array
+        [Fri.EnumValue(Name = "null")]      Null,
+        [Fri.EnumValue(Name = "object")]    Object,
+        [Fri.EnumValue(Name = "string")]    String,
+        [Fri.EnumValue(Name = "boolean")]   Boolean,
+        [Fri.EnumValue(Name = "number")]    Number,
+        [Fri.EnumValue(Name = "integer")]   Integer,
+        [Fri.EnumValue(Name = "array")]     Array
     }
 }
