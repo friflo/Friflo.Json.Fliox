a54a3bb9fa9d651b4844050aa7964b5272451d3d
Ullrich Praetz <ullrich@praetz.de>
2022-02-25 11:19:26 +0100

    demo-page - add boolean default value for command param

--- Json/Fliox.Hub.Explorer/www~/src/entity-editor.ts
+++ Json/Fliox.Hub.Explorer/www~/src/entity-editor.ts
@@ -667,8 +667,9 @@ export class EntityEditor
         switch (resolvedDef.type) {
             case "object":  return '{}';
             case "array":   return '[]';
-            case "number":  return '0';
             case "string":  return '""';
+            case "number":  return '0';
+            case "boolean": return 'false';
             default:        return 'null';
         }
     }
