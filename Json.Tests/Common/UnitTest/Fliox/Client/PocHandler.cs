8d4e62e1085cefd0cc36764d31a3dfca9654da18
Ullrich Praetz <ullrich@praetz.de>
2022-02-27 14:56:56 +0100

    add test - assert Command.Name / .ToString()

--- Json.Tests/Common/UnitTest/Fliox/Client/PocHandler.cs
+++ Json.Tests/Common/UnitTest/Fliox/Client/PocHandler.cs
@@ -3,6 +3,7 @@
 
 using System.Threading.Tasks;
 using Friflo.Json.Fliox.Hub.Host;
+using static NUnit.Framework.Assert;
 
 // ReSharper disable UnusedParameter.Local
 // ReSharper disable UnusedMember.Local
@@ -25,6 +26,10 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client
         }
         
         private static bool TestCommand(Command<TestCommand> command) {
+            AreEqual("TestCommand", command.Name);
+            var expectToString = $"{command.Name}(param: {command.JsonParam.AsString()})";
+            AreEqual(expectToString, command.ToString());
+            command.WriteNull = true; // ensure API available
             return true;
         }
     }
@@ -34,7 +39,6 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client
     /// </summary>
     public class TestCommandsHandler {
         private static string Command1(Command<string> command) {
-            command.WriteNull = true; // ensure API available
             return "hello Command1";
         }
         
