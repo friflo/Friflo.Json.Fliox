bfa2d89ef5bc3b4f263101c051578327210583af
Ullrich Praetz <ullrich@praetz.de>
2022-02-28 18:08:06 +0100

    cleanup

--- Json/Fliox.Hub/Client/HubCommands.cs
+++ Json/Fliox.Hub/Client/HubCommands.cs
@@ -57,25 +57,25 @@ namespace Friflo.Json.Fliox.Hub.Client
         
         // Declared only to generate command in Schema
         /// <summary>echos the given parameter to assure the database is working appropriately. </summary>
-        internal CommandTask<JsonValue>     Echo(JsonValue _) => throw new InvalidOperationException("unexpected call of DbEcho command");
+        internal CommandTask<JsonValue> Echo(JsonValue _) => throw new InvalidOperationException("unexpected call of DbEcho command");
 
         // --- commands: database
         /// <summary>echos the given parameter to assure the database is working appropriately. </summary>
-        public CommandTask<TParam>          Echo<TParam> (TParam param) => SendCommand<TParam,TParam>  (Std.Echo,   param);
+        public CommandTask<TParam>      Echo<TParam> (TParam param) => SendCommand<TParam,TParam>   (Std.Echo,       param);
         /// <summary>list all containers of the database</summary>
-        public CommandTask<DbContainers>    Containers()=>  SendCommand<JsonValue, DbContainers>(Std.Containers,    new JsonValue());
+        public CommandTask<DbContainers>Containers()        => SendCommand<JsonValue, DbContainers> (Std.Containers, new JsonValue());
         /// <summary>list all commands exposed by the database</summary>
-        public CommandTask<DbCommands>      Commands()  =>  SendCommand<JsonValue, DbCommands>  (Std.Commands,      new JsonValue());
+        public CommandTask<DbCommands>  Commands()          => SendCommand<JsonValue, DbCommands>   (Std.Commands,   new JsonValue());
         /// <summary>return the JSON Schema assigned to the database</summary>
-        public CommandTask<DbSchema>        Schema()    =>  SendCommand<JsonValue, DbSchema>    (Std.Schema,        new JsonValue());
+        public CommandTask<DbSchema>    Schema()            => SendCommand<JsonValue, DbSchema>     (Std.Schema,     new JsonValue());
         /// <summary>return the number of entities of all containers (or the given container) of the database</summary>
-        public CommandTask<DbStats>         Stats(string param) => SendCommand<string,DbStats>  (Std.Stats,  param);
+        public CommandTask<DbStats>     Stats(string param) => SendCommand<string,    DbStats>      (Std.Stats,      param);
         
         // --- commands: host
         /// <summary>returns descriptive information about the Hub like version, host, project and environment name</summary>
-        public CommandTask<HostDetails>     Details()   =>  SendCommand<JsonValue, HostDetails> (Std.HostDetails,   new JsonValue());
+        public CommandTask<HostDetails> Details()           => SendCommand<JsonValue, HostDetails>  (Std.HostDetails,new JsonValue());
         /// <summary>list all databases and their containers hosted by the Hub</summary>
-        public CommandTask<HostCluster>     Cluster()   =>  SendCommand<JsonValue, HostCluster> (Std.HostCluster,   new JsonValue());
+        public CommandTask<HostCluster> Cluster()           => SendCommand<JsonValue, HostCluster>  (Std.HostCluster,new JsonValue());
     }
     
     
