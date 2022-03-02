f02391cf601dd9db97b56a89fc29076735074c52
Ullrich Praetz <ullrich@praetz.de>
2022-03-02 13:32:13 +0100

    add Command<> comment

--- Json/Fliox.Hub/Host/Command.cs
+++ Json/Fliox.Hub/Host/Command.cs
@@ -22,6 +22,7 @@ namespace Friflo.Json.Fliox.Hub.Host
     /// - a <see cref="Pool"/> mainly providing common utilities to transform JSON <br/>
     /// </summary>
     /// <typeparam name="TParam">Type of the command input parameter</typeparam>
+    // Note: Implement IMessage to ensure Message & Command<> have the same API - not really necessary 
     public class Command<TParam> : IMessage {
         public              string          Name            { get; }
         public              IPool           Pool            => messageContext.pool;
