0bf609be1f9d5ab12ac7e8a150786adebed8be39
Ullrich Praetz <ullrich@praetz.de>
2022-03-04 10:02:40 +0100

    introduced MessageContext

--- Json/Fliox.Hub/Host/MessageContext.cs
+++ Json/Fliox.Hub/Host/MessageContext.cs
@@ -18,8 +18,7 @@ namespace Friflo.Json.Fliox.Hub.Host
     /// - the <see cref="Hub"/> exposing general Hub information <br/>
     /// - a <see cref="Pool"/> mainly providing common utilities to transform JSON <br/>
     /// </summary>
-    /// <remarks>For consistency the API to access the command param is same a <see cref="IMessage"/></remarks>
-    public class CommandContext { // : IMessage { // uncomment to check API consistency
+    public class MessageContext {
         public              string          Name            { get; }
         public              IPool           Pool            => executeContext.pool;
         public              FlioxHub        Hub             => executeContext.hub;
@@ -27,24 +26,40 @@ namespace Friflo.Json.Fliox.Hub.Host
         public              EntityDatabase  Database        => executeContext.Database;
         public              User            User            => executeContext.User;
         public              JsonKey         ClientId        => executeContext.clientId;
+        
+        [DebuggerBrowsable(Never)]  internal            ExecuteContext  ExecuteContext  => executeContext;
+        [DebuggerBrowsable(Never)]  protected readonly  ExecuteContext  executeContext;
+
+        public   override   string          ToString()      => Name;
+        
+        internal MessageContext(string name, ExecuteContext executeContext) {
+            Name                = name;
+            this.executeContext = executeContext;
+        }
+    }
+    
+    /// <summary>
+    /// <see cref="CommandContext"/> expose all data relevant for command execution as properties or methods. <br/>
+    /// - the command <see cref="MessageContext.Name"/> == method name <br/>
+    /// - the <see cref="MessageContext.DatabaseName"/> <br/>
+    /// - the <see cref="MessageContext.Database"/> instance <br/>
+    /// - the <see cref="MessageContext.Hub"/> exposing general Hub information <br/>
+    /// - a <see cref="MessageContext.Pool"/> mainly providing common utilities to transform JSON <br/>
+    /// </summary>
+    /// <remarks>For consistency the API to access the command param is same a <see cref="IMessage"/></remarks>
+    public class CommandContext : MessageContext { // : IMessage { // uncomment to check API consistency
+
         public              bool            WriteNull       { get; set; }
         public              bool            WritePretty     { get; set; }
         
         internal            string          error;
 
-        [DebuggerBrowsable(Never)]  internal            ExecuteContext  ExecuteContext  => executeContext;
-        [DebuggerBrowsable(Never)]  private   readonly  ExecuteContext  executeContext;
-
-        public   override   string          ToString()      => Name;
-        
         public              UserInfo        UserInfo { get {
             var user = executeContext.User;
             return new UserInfo (user.userId, user.token, executeContext.clientId);
         } }
 
-        internal CommandContext(string name, ExecuteContext executeContext) {
-            Name                = name;
-            this.executeContext = executeContext;
+        internal CommandContext(string name, ExecuteContext executeContext) : base (name, executeContext) {
             WritePretty         = true;
         }
         
