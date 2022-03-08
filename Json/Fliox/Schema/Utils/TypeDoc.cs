5e3561952428c1ad2018aad236779ec56d3b3e55
Ullrich Praetz <ullrich@praetz.de>
2022-03-08 08:11:52 +0100

    Schema - enhance TypeDoc performance by using append approach

--- Json/Fliox/Schema/Utils/TypeDoc.cs
+++ Json/Fliox/Schema/Utils/TypeDoc.cs
@@ -11,17 +11,28 @@ namespace Friflo.Json.Fliox.Schema.Utils
         public static string HtmlToText (string html, string indent, string start, string newLine, string end) {
             if (html == null)
                 return "";
-            var xml     = $"<summary>{html}</summary>";
-            var docs        = XDocument.Parse(xml);
-            var root        = docs.Root;
+            var sb      = new StringBuilder();
+            // --- create valid xml to enable parsing
+            sb.Append("<summary>");
+            sb.Append(html);
+            sb.Append("</summary>");
+            var xml     = sb.ToString();
+            var docs    = XDocument.Parse(xml);
+            var root    = docs.Root;
             if (root == null)
                 return "";
-            var str         = XElementToString(root);
-            var lines       = str.Split('\n');
+            
+            // --- convert html to code documentation string with markdown style
+            sb.Clear();
+            AppendElementText(sb, root);
+            var str     = sb.ToString();
+            
+            // --- format markdown as code (Typescript) documentation
+            var lines   = str.Split('\n');
             if (lines.Length == 1) {
                 return $"{indent}{start} {lines[0]}{end}\n";   
             }
-            var sb          = new StringBuilder();
+            sb.Clear();
             var firstLine   = true;
             sb.Append(indent);
             sb.Append(start);
@@ -48,49 +59,50 @@ namespace Friflo.Json.Fliox.Schema.Utils
             return sb.ToString();
         }
         
-        private static string XElementToString (XElement element) {
-            var sb = new StringBuilder();
-            return GetElementText(sb, element);
-        }
-        
-        private static string GetElementText(StringBuilder sb, XElement element) {
+        private static void AppendElementText(StringBuilder sb, XElement element) {
             var nodes = element.DescendantNodes();
             // var nodes = element.DescendantsAndSelf();
             // if (element.Value.Contains("Check some new lines")) { int i = 42; }
             foreach (var node in nodes) {
                 if (node.Parent != element)
                     continue;
-                var nodeText = GetNodeText(node);
-                sb.Append(nodeText);
+                AppendNodeText(sb, node);
             }
-            var text = sb.ToString();
-            sb.Clear();
-            return text;
         }
         
-        private static string GetNodeText (XNode node) {
+        private static void AppendNodeText (StringBuilder sb, XNode node) {
             if (node is XText xtext) {
-                return xtext.Value;
+                sb.Append(xtext.Value);
+                return;
             }
             if (node is XElement element) {
                 var name    = element.Name.LocalName;
                 switch (name) {
-                    case "ul":  return XElementToString(element);
-                    case "li":  return GetLiText(element);
-                    case "b":   return $"**{XElementToString(element)}**";
-                    case "i":   return $"*{XElementToString(element)}*";
+                    case "ul":  AppendElementText(sb, element);    return;
+                    case "li":  AppendLiText(sb, element);         return;
+                    case "b":
+                        sb.Append("**");
+                        AppendElementText(sb, element);
+                        sb.Append("**");
+                        return;
+                    case "i":
+                        sb.Append("*");
+                        AppendElementText(sb, element);
+                        sb.Append("*");
+                        return;
                     default:
-                        return XElementToString(element);
+                        AppendElementText(sb, element);
+                        return;
                 }
             }
-            return "";
         }
         
-        private static string GetLiText (XElement element) {
-            var text        = XElementToString(element);
+        private static void AppendLiText (StringBuilder sb, XElement element) {
+            var localSb = new StringBuilder();
+            AppendElementText(localSb, element);
+            var text        = localSb.ToString();
             text            = text.Trim();
             var lines       = text.Split("\n");
-            var sb          = new StringBuilder();
             var firstLine   = true;
             foreach (var line in lines) {
                 sb.Append(firstLine ? "- " : "  ");
@@ -98,7 +110,6 @@ namespace Friflo.Json.Fliox.Schema.Utils
                 sb.Append(line);
                 sb.Append('\n');
             }
-            return sb.ToString();
         }
     }
 }
