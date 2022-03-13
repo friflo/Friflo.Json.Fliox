45f6e300fab832d0d0e946acd76cff0ac92f6c4a
Ullrich Praetz <ullrich@praetz.de>
2022-03-13 14:52:09 +0100

    Combined Authorizers: Allow-Deny, Any-All

--- Json/Fliox.Hub/Host/Auth/Authorizers/Allow-Deny.cs
+++ Json/Fliox.Hub/Host/Auth/Authorizers/Allow-Deny.cs
@@ -1,25 +1,25 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
 using Friflo.Json.Fliox.Hub.Protocol;
 using Friflo.Json.Fliox.Hub.Protocol.Tasks;
 
 // ReSharper disable once CheckNamespace
 namespace Friflo.Json.Fliox.Hub.Host.Auth
 {
-    public sealed class AuthorizeAny : Authorizer {
-        private readonly    ICollection<Authorizer>     list;
-        
-        public AuthorizeAny(ICollection<Authorizer> list) {
-            this.list = list;    
-        }
+    public sealed class AuthorizeAllow : AuthorizerDatabase {
+        public override string ToString() => $"database: {dbLabel}";
+
+        public AuthorizeAllow () { }
+        public AuthorizeAllow (string database) : base (database) { }
         
         public override bool Authorize(SyncRequestTask task, ExecuteContext executeContext) {
-            foreach (var item in list) {
-                if (item.Authorize(task, executeContext))
-                    return true;
-            }
+            return AuthorizeDatabase(executeContext);
+        }
+    }
+    
+    public sealed class AuthorizeDeny : Authorizer {
+        public override bool Authorize(SyncRequestTask task, ExecuteContext executeContext) {
             return false;
         }
     }
--- Json/Fliox.Hub/Host/Auth/Authorizers/Any-All.cs
+++ Json/Fliox.Hub/Host/Auth/Authorizers/Any-All.cs
@@ -8,6 +8,22 @@ using Friflo.Json.Fliox.Hub.Protocol.Tasks;
 // ReSharper disable once CheckNamespace
 namespace Friflo.Json.Fliox.Hub.Host.Auth
 {
+    public sealed class AuthorizeAny : Authorizer {
+        private readonly    ICollection<Authorizer>     list;
+        
+        public AuthorizeAny(ICollection<Authorizer> list) {
+            this.list = list;    
+        }
+        
+        public override bool Authorize(SyncRequestTask task, ExecuteContext executeContext) {
+            foreach (var item in list) {
+                if (item.Authorize(task, executeContext))
+                    return true;
+            }
+            return false;
+        }
+    }
+    
     public sealed class AuthorizeAll : Authorizer {
         private readonly    ICollection<Authorizer>     list;
         
