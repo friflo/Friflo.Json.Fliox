8cbf92d6dd1d496c865456693d0a1fefd2d03412
Ullrich Praetz <ullrich@praetz.de>
2022-03-13 17:11:39 +0100

    add Cluster docs

--- Json/Fliox.Hub/DB/Cluster/ClusterStore.cs
+++ Json/Fliox.Hub/DB/Cluster/ClusterStore.cs
@@ -27,48 +27,69 @@ namespace Friflo.Json.Fliox.Hub.DB.Cluster
         public ClusterStore (FlioxHub hub, string database = null) : base(hub, database) { }
     }
     
+    /// <summary><see cref="containers"/> and <see cref="storage"/> type of a database</summary>
     public class DbContainers {
+        /// <summary>database name</summary>
         [Req]   public  string                          id;
+        /// <summary><see cref="storage"/> type. e.g. memory, file-system, ...</summary>
         [Req]   public  string                          storage;
+        /// <summary>container names</summary>
         [Req]   public  string[]                        containers;
                         
         public override string                          ToString() => JsonSerializer.Serialize(this).Replace("\"", "'");
     }
+    
+    /// <summary><see cref="commands"/> and <see cref="messages"/> of a database</summary>
     public class DbMessages {
+        /// <summary>database name</summary>
         [Req]   public  string                          id;
+        /// <summary>command names</summary>
         [Req]   public  string[]                        commands;
+        /// <summary>message names</summary>
         [Req]   public  string[]                        messages;
                         
         public override string  ToString() => JsonSerializer.Serialize(this).Replace("\"", "'");
     }
     
+    /// <summary>schema assigned to a database</summary>
     public class DbSchema {
+        /// <summary>database name</summary>
         [Req]   public  string                          id;
+        /// <summary>refer a type definition of the JSON Schema referenced with <see cref="schemaPath"/></summary>
         [Req]   public  string                          schemaName;
+        /// <summary>refer a JSON Schema in <see cref="jsonSchemas"/></summary>
         [Req]   public  string                          schemaPath;
-        [Req]   public  Dictionary<string,JsonValue>    jsonSchemas;
+        /// <summary>map of JSON Schemas and their name</summary>
+        [Req]   public  Dictionary<string, JsonValue>   jsonSchemas;
                         
         public override         string  ToString() => JsonSerializer.Serialize(this).Replace("\"", "'");
     }
     
     // --- commands
+    /// <summary>list of container statistics. E.g. entity <see cref="ContainerStats.count"/> per container</summary>
     public class DbStats {
                 public  ContainerStats[]                containers;
     }
     
+    /// <summary>statistics of a single container. E.g. entity <see cref="ContainerStats.count"/></summary>
     public class ContainerStats {
+        /// <summary>container <see cref="name"/></summary>
         [Req]   public  string                          name;
+        /// <summary><see cref="count"/> of entities / records</summary>
                 public  long                            count;
             
         public override string                          ToString() => $"{name} - count: {count}";
     }
     
-    
+    /// <summary>general information about a Hub</summary>
     public class HostDetails {
         [Req]   public  string                          version;
                 public  string                          hostName;
+                /// <summary>project name</summary>
                 public  string                          projectName;
+                /// <summary>link to a website describing project and Hub</summary>
                 public  string                          projectWebsite;
+                /// <summary>environment name. e.g. 'dev', 'test', 'staging', 'prod'</summary>
                 public  string                          envName;
                 /// <summary>
                 /// the color used to display the environment name in GUI's using CSS color format.<br/>
@@ -77,6 +98,7 @@ namespace Friflo.Json.Fliox.Hub.DB.Cluster
                 public  string                          envColor;
     }
     
+    /// <summary>list of all databases of a Hub</summary>
     public class HostCluster {
         [Req]   public  List<DbContainers>              databases;
     }
