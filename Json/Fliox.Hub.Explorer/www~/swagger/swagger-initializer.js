7d6dce8c069cc0f366f494f2a66a63f990fc977e
Ullrich Praetz <ullrich@praetz.de>
2022-04-02 13:16:26 +0200

    Explorer - use original Swagger UI files

--- Json/Fliox.Hub.Explorer/www~/swagger/swagger-initializer.js
+++ Json/Fliox.Hub.Explorer/www~/swagger/swagger-initializer.js
@@ -3,11 +3,9 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    // url: "http://localhost:8010/fliox/openapi.json",
-    url: "http://localhost:8010/fliox/schema/main_db/json-schema/openapi.json",
+    url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
-    docExpansion: 'none',
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
@@ -15,9 +13,7 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout",
-    
-
+    layout: "StandaloneLayout"
   });
 
   //</editor-fold>
