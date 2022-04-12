516cfbeac9a28ad825f104064debd82b3cce5392
Ullrich Praetz <ullrich@praetz.de>
2022-04-12 10:24:41 +0200

    GraphQL - prepare SelectionNode

--- Json/Fliox.Hub.GraphQL/SelectionNode.cs
+++ Json/Fliox.Hub.GraphQL/SelectionNode.cs
@@ -1,19 +1,35 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using GraphQLParser;
+using GraphQLParser.AST;
+
 #if !UNITY_5_3_OR_NEWER
 
 namespace Friflo.Json.Fliox.Hub.GraphQL
 {
-    internal struct SelectionNode
+    internal readonly struct SelectionNode
     {
-        internal SelectionNode (in Query query) {
-            var set = query.graphQL.SelectionSet;
-            if (set == null) {
+        internal  readonly      SelectionNode[] nodes;
+        internal  readonly      ROM             name;
+
+        public override         string          ToString() => name.ToString();
+
+        internal SelectionNode (in Query query)
+            : this (null, query.graphQL.SelectionSet)
+        { }
+            
+        private SelectionNode  (GraphQLName name, GraphQLSelectionSet selectionSet) {
+            this.name = name;
+            if (selectionSet == null) {
+                nodes = null;
                 return;
             }
-            foreach (var selection in set.Selections) {
-                
+            var selections  = selectionSet.Selections;
+            nodes           = new SelectionNode[selections.Count];
+            for (int n = 0; n < selections.Count; n++) {
+                var selection   = (GraphQLField)selections[n];
+                nodes[n]        = new SelectionNode(selection.Name, selection.SelectionSet);
             }
         }
     }
