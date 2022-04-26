09653e53205c5f65a1550c04e6a3308b5f30e491
Ullrich Praetz <ullrich@praetz.de>
2022-04-26 12:24:15 +0200

    GraphQL - add Echo test

--- Json.Tests/Common/UnitTest/Fliox/Remote/GraphQL.cs
+++ Json.Tests/Common/UnitTest/Fliox/Remote/GraphQL.cs
@@ -11,12 +11,20 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Remote
     public partial class TestRemote
     {
         [Test]
-        public static async Task TestQueries() {
-            var query       = File.ReadAllText(TestFolder + "query.graphql");
+        public static async Task TestGQL_queries() {
+            var query       = File.ReadAllText(TestFolder + "queries.graphql");
 
             var request     = await GraphQLRequest("/graphql/main_db", query);
             
-            File.WriteAllBytes(TestFolder + "query.json", request.Response.AsByteArray());
+            File.WriteAllBytes(TestFolder + "queries.json", request.Response.AsByteArray());
+        }
+        
+        [Test]
+        public static async Task TestGQL_std_Echo() {
+            var query   = "{ std_Echo (param: 123) }";
+            var request = await GraphQLRequest("/graphql/main_db", query);
+            
+            File.WriteAllBytes(TestFolder + "std_Echo.json", request.Response.AsByteArray());
         }
     }
 }
