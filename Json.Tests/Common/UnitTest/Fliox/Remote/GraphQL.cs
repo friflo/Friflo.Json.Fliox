e3eadfda8eb4fe5ad95714062c46375b409eb421
Ullrich Praetz <ullrich@praetz.de>
2022-04-26 12:56:19 +0200

    TestRemote - add GraphQL__Init()

--- Json.Tests/Common/UnitTest/Fliox/Remote/GraphQL.cs
+++ Json.Tests/Common/UnitTest/Fliox/Remote/GraphQL.cs
@@ -10,17 +10,27 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Remote
 {
     public partial class TestRemote
     {
+        /// <summary> use name <see cref="GraphQL__Init"/> to run as first test.
+        /// This forces loading all required GraphQL code and subsequent tests show the real execution time</summary>
         [Test]
-        public static async Task TestGQL_queries() {
+        public static async Task GraphQL__Init() {
+            var query       = File.ReadAllText(TestFolder + "queries.graphql");
+            await GraphQLRequest("/graphql/main_db", query);
+        }
+        
+        [Test]
+        public static async Task GraphQL_queries() {
             var query       = File.ReadAllText(TestFolder + "queries.graphql");
 
             var request     = await GraphQLRequest("/graphql/main_db", query);
             
+            // for (int n = 0; n < 100000; n++) { await GraphQLRequest("/graphql/main_db", query); }
+            
             File.WriteAllBytes(TestFolder + "queries.json", request.Response.AsByteArray());
         }
-        
+
         [Test]
-        public static async Task TestGQL_std_Echo() {
+        public static async Task GraphQL_std_Echo() {
             var query   = "{ std_Echo (param: 123) }";
             var request = await GraphQLRequest("/graphql/main_db", query);
             
