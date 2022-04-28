6385a5ac114008521e882db472bef6a225e3695c
Ullrich Praetz <ullrich@praetz.de>
2022-04-28 09:21:43 +0200

    TestRemote - Schema: add resource requests

---
+++ Json.Tests/Common/UnitTest/Fliox/Remote/Schema.cs
@@ -0,0 +1,62 @@
+// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+#if !UNITY_5_3_OR_NEWER
+
+using System.IO;
+using System.Threading.Tasks;
+using Friflo.Json.Fliox.Hub.Remote;
+using Friflo.Json.Tests.Common.Utils;
+using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+// ReSharper disable MethodHasAsyncOverload
+namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Remote
+{
+    public partial class TestRemote
+    {
+        [Test]
+        public static async Task Schema_main_db_index() {
+            var request = await RestRequest("GET", "/schema/main_db/index.html");
+            AssertRequest(request, 200, "text/html");
+        }
+        
+        [Test]
+        public static async Task Schema_main_db_json_schema_index() {
+            var request = await RestRequest("GET", "/schema/main_db/json-schema/index.html");
+            AssertRequest(request, 200, "text/html");
+        }
+        
+        [Test]
+        public static async Task Schema_main_db_json_schema() {
+            var request = await RestRequest("GET", "/schema/main_db/json-schema.json");
+            AssertRequest(request, 200, "application/json");
+        }
+        
+        [Test]
+        public static async Task Schema_main_db_json_schema_directory() {
+            var request = await RestRequest("GET", "/schema/main_db/json-schema/directory");
+            AssertRequest(request, 200, "application/json");
+        }
+        
+        [Test]
+        public static async Task Schema_main_db_json_schema_zip() {
+            var request = await RestRequest("GET", "/schema/main_db/json-schema/PocStore.json-schema.zip");
+            AssertRequest(request, 200, "application/zip");
+        }
+        
+        [Test]
+        public static async Task Schema_main_db_open_api() {
+            var request = await RestRequest("GET", "/schema/main_db/open-api.html");
+            AssertRequest(request, 200, "text/html");
+        }
+        
+        [Test]
+        public static async Task Schema_main_db_typescript_schema() {
+            var request = await RestRequest("GET", "/schema/main_db/typescript/unknown");
+            AssertRequest(request, 404, "text/plain", "schema error > file not found: 'unknown'");
+        }
+    }
+}
+
+#endif
