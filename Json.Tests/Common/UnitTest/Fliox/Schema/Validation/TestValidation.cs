199f30b8f1b01679b1fdb45e2bcc00607030ac80
Ullrich Praetz <ullrich@praetz.de>
2022-05-06 14:12:49 +0200

    TestValidation - indentation

--- Json.Tests/Common/UnitTest/Fliox/Schema/Validation/TestValidation.cs
+++ Json.Tests/Common/UnitTest/Fliox/Schema/Validation/TestValidation.cs
@@ -30,26 +30,26 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
         public void ValidateInt() {
             var validation = validationSet.GetValidationType(typeof(int));
             
-            success = validator.Validate(new JsonValue("123"),         validation, out error);
+            success = validator.Validate(new JsonValue("123"),          validation, out error);
             IsTrue(success);
                 
-            success = validator.Validate(new JsonValue("null"),        validation, out error);
+            success = validator.Validate(new JsonValue("null"),         validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: null, expect: int32 (root), pos: 4", error);
             
-            success = validator.Validate(new JsonValue("\"noInt\""),        validation, out error);
+            success = validator.Validate(new JsonValue("\"noInt\""),    validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: 'noInt', expect: int32 (root), pos: 7", error);
                 
-            success = validator.Validate(new JsonValue("{}"),          validation, out error);
+            success = validator.Validate(new JsonValue("{}"),           validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: object, expect: int32 at int32 > (root), pos: 1", error);
             
-            success = validator.Validate(new JsonValue("[]"),          validation, out error);
+            success = validator.Validate(new JsonValue("[]"),           validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: array, expect: int32 [], pos: 1", error);
                 
-            success = validator.Validate(new JsonValue("xxx"),         validation, out error);
+            success = validator.Validate(new JsonValue("xxx"),          validation, out error);
             IsFalse(success);
             AreEqual("unexpected character while reading value. Found: x", error);
         }
@@ -61,27 +61,27 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
             success = validator.Validate(new JsonValue("true"),         validation, out error);
             IsTrue(success);
                 
-            success = validator.Validate(new JsonValue("null"),        validation, out error);
+            success = validator.Validate(new JsonValue("null"),         validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: null, expect: boolean (root), pos: 4", error);
             
-            success = validator.Validate(new JsonValue("555"),        validation, out error);
+            success = validator.Validate(new JsonValue("555"),          validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: 555, expect: boolean (root), pos: 3", error);
 
-            success = validator.Validate(new JsonValue("\"noBool\""),        validation, out error);
+            success = validator.Validate(new JsonValue("\"noBool\""),   validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: 'noBool', expect: boolean (root), pos: 8", error);
                 
-            success = validator.Validate(new JsonValue("{}"),          validation, out error);
+            success = validator.Validate(new JsonValue("{}"),           validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: object, expect: boolean at boolean > (root), pos: 1", error);
             
-            success = validator.Validate(new JsonValue("[]"),          validation, out error);
+            success = validator.Validate(new JsonValue("[]"),           validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: array, expect: boolean [], pos: 1", error);
                 
-            success = validator.Validate(new JsonValue("yyy"),         validation, out error);
+            success = validator.Validate(new JsonValue("yyy"),          validation, out error);
             IsFalse(success);
             AreEqual("unexpected character while reading value. Found: y", error);
         }
@@ -90,21 +90,21 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
         public void ValidateIntArray() {
             var validation = validationSet.GetValidationType(typeof(int[]));
             
-            success = validator.Validate(new JsonValue("[1,2,3]"),     validation, out error);
+            success = validator.Validate(new JsonValue("[1,2,3]"),      validation, out error);
             IsTrue(success);
                 
-            success = validator.Validate(new JsonValue("null"),        validation, out error);
+            success = validator.Validate(new JsonValue("null"),         validation, out error);
             IsTrue(success);
                 
-            success = validator.Validate(new JsonValue("[\"abc\"]"),   validation, out error);
+            success = validator.Validate(new JsonValue("[\"abc\"]"),    validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: 'abc', expect: int32 [0], pos: 6", error);
             
-            success = validator.Validate(new JsonValue("\"no array\""),   validation, out error);
+            success = validator.Validate(new JsonValue("\"no array\""), validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: 'no array', expect: int32 (root), pos: 10", error);
             
-            success = validator.Validate(new JsonValue("{}"),   validation, out error);
+            success = validator.Validate(new JsonValue("{}"),           validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: object, expect: int32 at int32 > (root), pos: 1", error);
         }
@@ -113,13 +113,13 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
         public void ValidateIntNull() {
             var validation = validationSet.GetValidationType(typeof(int?));
             
-            success = validator.Validate(new JsonValue("456"),         validation, out error);
+            success = validator.Validate(new JsonValue("456"),          validation, out error);
             IsTrue(success);
                 
-            success = validator.Validate(new JsonValue("null"),        validation, out error);
+            success = validator.Validate(new JsonValue("null"),         validation, out error);
             IsTrue(success);
 
-            success = validator.Validate(new JsonValue("true"),        validation, out error);
+            success = validator.Validate(new JsonValue("true"),         validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: true, expect: int32 (root), pos: 4", error);
         }
@@ -127,21 +127,21 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
         [Test]
         public void ValidateString() {
             var validation       = validationSet.GetValidationType(typeof(string));
-            success = validator.Validate(new JsonValue("\"xyz\""),     validation, out error);
+            success = validator.Validate(new JsonValue("\"xyz\""),      validation, out error);
             IsTrue(success);
                 
-            success = validator.Validate(new JsonValue("null"),        validation, out error);
+            success = validator.Validate(new JsonValue("null"),         validation, out error);
             IsTrue(success);
                 
-            success = validator.Validate(new JsonValue("42"),          validation, out error);
+            success = validator.Validate(new JsonValue("42"),           validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: 42, expect: string (root), pos: 2", error);
 
-            success = validator.Validate(new JsonValue("[]"),          validation, out error);
+            success = validator.Validate(new JsonValue("[]"),           validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: array, expect: string [], pos: 1", error);
             
-            success = validator.Validate(new JsonValue("{}"),          validation, out error);
+            success = validator.Validate(new JsonValue("{}"),           validation, out error);
             IsFalse(success);
             AreEqual("Incorrect type. was: object, expect: string at string > (root), pos: 1", error);
         }
@@ -149,13 +149,13 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
         [Test]
         public void ValidateJsonValue() {
             var validation       = validationSet.GetValidationType(typeof(JsonValue));
-            success = validator.Validate(new JsonValue("\"xyz\""),     validation, out error);
+            success = validator.Validate(new JsonValue("\"xyz\""),      validation, out error);
             IsTrue(success);
             
-            success = validator.Validate(new JsonValue("222"),     validation, out error);
+            success = validator.Validate(new JsonValue("222"),          validation, out error);
             IsTrue(success);
             
-            success = validator.Validate(new JsonValue("true"),     validation, out error);
+            success = validator.Validate(new JsonValue("true"),         validation, out error);
             IsTrue(success);
 
             // todo
