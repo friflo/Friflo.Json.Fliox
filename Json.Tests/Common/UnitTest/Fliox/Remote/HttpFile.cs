b2338b7440042dff0efcaa5776b2167d15f3101a
Ullrich Praetz <ullrich@praetz.de>
2022-05-13 11:11:38 +0200

    HttpFileRequest - add ReadHeaders()

--- Json.Tests/Common/UnitTest/Fliox/Remote/HttpFile.cs
+++ Json.Tests/Common/UnitTest/Fliox/Remote/HttpFile.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using Friflo.Json.Fliox.Hub.Remote;
 
 // ReSharper disable MemberCanBePrivate.Global
@@ -48,7 +49,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Remote
             // skip first entry containing variables
             for (int n = 1; n < sections.Length; n++) {
                 var section = sections[n];
-                var request = new HttpFileRequest(section);
+                var request = new HttpFileRequest(section, this);
                 requests.Add(request);
             }
         }
@@ -64,7 +65,8 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Remote
                     continue;
                 var name    = line.Substring(1, assignPos - 1).Trim(); 
                 var value   = line.Substring(assignPos + 1).Trim();
-                result.Add(name, value);
+                var key     = $"{{{{{name}}}}}";
+                result.Add(key, value);
             }
             return result;
         }
@@ -108,10 +110,11 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Remote
     
     public class HttpFileRequest
     {
-        public  readonly    string  method;
-        public  readonly    string  path;
-        public  readonly    string  query;
-        public  readonly    string  body;
+        public  readonly    string                      method;
+        public  readonly    string                      path;
+        public  readonly    string                      query;
+        public  readonly    string                      body;
+        public  readonly    Dictionary<string, string>  headers;
 
         public  override    string  ToString() {
             if (query == "")
@@ -119,14 +122,15 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Remote
             return $"{method} {path}?{query}";
         }
 
-        private const   string  BaseVariable = "{{base}}";
+        private const           string  BaseVariable    = "{{base}}";
+        private static readonly Regex   RegExVariables  = new Regex(@"\{{([^}]+)\}}");
         
-        public HttpFileRequest (string request) {
+        public HttpFileRequest (string request, HttpFile httpFile) {
             var parts       = request.Split(new [] { "\r\n\r\n" }, StringSplitOptions.None);
             var head        = parts[0];
             body            = parts.Length > 1 ? parts[1] : null;
-            var headers     = head.Split(new [] {"\r\n"}, StringSplitOptions.None);
-            var requestLine = headers[0];
+            var lines       = head.Split(new [] {"\r\n"}, StringSplitOptions.None);
+            var requestLine = lines[0];
             
             var spacePos    = requestLine.IndexOf(' ');
             method          = requestLine.Substring(0, spacePos);
@@ -139,6 +143,37 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Remote
             }
             path            = path.Substring(BaseVariable.Length);
             query           = queryPos == -1 ? "" : urlPath.Substring(queryPos + 1);
+            headers         = ReadHeaders(lines, httpFile);
+        }
+        
+        private static Dictionary<string, string> ReadHeaders(string[] lines, HttpFile httpFile) {
+            var result      = new Dictionary<string, string>();
+            var sb          = new StringBuilder();
+            var variables   = httpFile.variables;
+            
+            for (int n = 1; n < lines.Length; n++) {
+                sb.Clear();
+                var line    = lines[n].Trim();
+                if (line == "")
+                    continue;
+                var matches = RegExVariables.Matches(line);
+                var pos = 0;
+                for (int i = 0; i < matches.Count; i++) {
+                    var match       = matches[i];
+                    var count       = match.Index - pos;
+                    sb.Append(line, pos, count);
+                    var variable    = match.Value;
+                    var varValue    = variables[variable]; 
+                    sb.Append(varValue);
+                    pos            +=  count + match.Length;
+                }
+                var resultLine  = sb.ToString();
+                var colonPos    = resultLine.IndexOf(':');
+                var name        = resultLine.Substring(0, colonPos).Trim();
+                var value       = resultLine.Substring(colonPos + 1).Trim();
+                result.Add(name, value);
+            }            
+            return result;
         }
     }
 }
