d6428f7dae6e3186859b0bf764438072abfd083e
Ullrich Praetz <ullrich@praetz.de>
2022-05-19 14:02:28 +0200

    cleanup

--- Json/Fliox.Hub/Client/Event/EntityChanges.cs
+++ Json/Fliox.Hub/Client/Event/EntityChanges.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-// ReSharper disable once CheckNamespace
-
 using System.Collections.Generic;
 using System.Diagnostics;
 using Friflo.Json.Fliox.Hub.Client.Internal;
