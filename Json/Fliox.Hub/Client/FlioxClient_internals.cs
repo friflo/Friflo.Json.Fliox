8f888d338c641f0c3b57f564aa5a9f8f7222ac50
Ullrich Praetz <ullrich@praetz.de>
2022-05-25 19:30:29 +0200

    removed unused import

--- Json/Fliox.Hub/Client/FlioxClient_internals.cs
+++ Json/Fliox.Hub/Client/FlioxClient_internals.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
-using Friflo.Json.Fliox.Hub.Client.Event;
 using Friflo.Json.Fliox.Hub.Client.Internal;
 using Friflo.Json.Fliox.Hub.Host;
 using Friflo.Json.Fliox.Hub.Protocol;
