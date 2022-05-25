45d5e14905cb6e160119358e770f20ad554d070e
Ullrich Praetz <ullrich@praetz.de>
2022-05-25 07:35:54 +0200

    ExecuteSyncResult - moved to ExecuteContext.cs

--- Json/Fliox.Hub/Host/ExecuteContext.cs
+++ Json/Fliox.Hub/Host/ExecuteContext.cs
@@ -98,4 +98,31 @@ namespace Friflo.Json.Fliox.Hub.Host
             startUsage.AssertEqual(pool.PoolUsage);
         }
     }
+    
+    /// <summary>
+    /// Contains the result of a <see cref="FlioxHub.ExecuteSync"/> call. <br/>
+    /// After execution either <see cref="success"/> or <see cref="error"/> is set. Never both.
+    /// </summary>
+    public readonly struct ExecuteSyncResult {
+        public   readonly   SyncResponse    success;
+        public   readonly   ErrorResponse   error;
+
+        public ExecuteSyncResult (SyncResponse successResponse) {
+            success = successResponse ?? throw new ArgumentNullException(nameof(successResponse));
+            error   = null;
+        }
+        
+        public ExecuteSyncResult (string errorMessage, ErrorResponseType errorType) {
+            success = null;
+            error   = new ErrorResponse { message = errorMessage, type = errorType };
+        }
+        
+        public  ProtocolResponse Result { get {
+            if (success != null)
+                return success;
+            return error;
+        } }
+
+        public override string ToString() => success != null ? success.ToString() : error.ToString();
+    }
 }
