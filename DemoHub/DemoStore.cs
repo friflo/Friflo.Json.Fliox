85edeb0b8ee89147cd765e041a3497a38eb14f0f
Ullrich Praetz <ullrich@praetz.de>
2022-06-02 11:07:57 +0200

    DemoHub - organized DemoStore & Models in their own files

--- DemoHub/DemoStore.cs
+++ DemoHub/DemoStore.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using Friflo.Json.Fliox;
+﻿using Friflo.Json.Fliox;
 using Friflo.Json.Fliox.Hub.Client;
 using Friflo.Json.Fliox.Hub.Host;
 
-// ReSharper disable All
+// ReSharper disable UnassignedReadonlyField
 namespace Fliox.DemoHub
 {
     /// <summary>
@@ -28,59 +25,28 @@ namespace Fliox.DemoHub
         ContactName = "Ullrich Praetz", ContactUrl = "https://github.com/friflo/Friflo.Json.Fliox/issues",
         LicenseName = "MIT",            LicenseUrl = "https://spdx.org/licenses/MIT.html")]
     [OpenAPIServer(Description = "public DemoHub API", Url = "http://ec2-174-129-178-18.compute-1.amazonaws.com/fliox/rest/main_db")]
-    public partial class DemoStore : FlioxClient {
+    [MessagePrefix("demo.")]
+    public class DemoStore : FlioxClient {
         // --- containers
         public readonly EntitySet <long, Article>     articles;
         public readonly EntitySet <long, Customer>    customers;
         public readonly EntitySet <long, Employee>    employees;
         public readonly EntitySet <long, Order>       orders;
         public readonly EntitySet <long, Producer>    producers;
+        
+        // --- commands
+        /// <summary> generate random entities (records) in the containers listed in the <see cref="DemoHub.Fake"/> param </summary>
+        public CommandTask<Records>     FakeRecords (Fake param)    => SendCommand<Fake, Records>   ("demo.FakeRecords", param);
+
+        /// <summary> count records added to containers within the last param seconds. default 60</summary>
+        public CommandTask<Counts>      CountLatest (int? param)    => SendCommand<int?, Counts>    ("demo.CountLatest", param);
+        
+        /// <summary> return records added to containers within the last param seconds. default 60</summary>
+        public CommandTask<Records>     LatestRecords(int? param)   => SendCommand<int?, Records>   ("demo.LatestRecords", param);
+
+        /// <summary> simple command adding two numbers - no database access. </summary>
+        public CommandTask<double>      Add  (Operands  param)      => SendCommand<Operands, double>("demo.Add", param);
 
         public DemoStore(FlioxHub hub) : base (hub) { }
-    }
-    
-    // ------------------------------ entity models ------------------------------
-    public class Article {
-        [Required]  public  long            id { get; set; }
-        [Required]  public  string          name;
-        [Relation(nameof(DemoStore.producers))]
-                    public  long            producer;
-                    public  DateTime?       created;
-    }
-
-    public class Customer {
-        [Required]  public  long            id { get; set; }
-        [Required]  public  string          name;
-                    public  DateTime?       created;
-    }
-    
-    public class Employee {
-        [Required]  public  long            id { get; set; }
-        [Required]  public  string          firstName;
-                    public  string          lastName;
-                    public  DateTime?       created;
-    }
-
-    public class Order {
-        [Required]  public  long            id { get; set; }
-        [Relation(nameof(DemoStore.customers))]
-                    public  long            customer;
-                    public  DateTime        created;
-                    public  List<OrderItem> items = new List<OrderItem>();
-    }
-
-    public class OrderItem {
-        [Relation(nameof(DemoStore.articles))]
-        [Required]  public  long            article;
-                    public  int             amount;
-                    public  string          name;
-    }
-
-    public class Producer {
-        [Required]  public  long            id { get; set; }
-        [Required]  public  string          name;
-        [Relation(nameof(DemoStore.employees))]
-                    public  List<long>      employees;
-                    public  DateTime?       created;
     }
 }
---
+++ DemoHub/Models.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Friflo.Json.Fliox;
+
+// ReSharper disable All
+namespace Fliox.DemoHub
+{
+    // ------------------------------ entity models ------------------------------
+    public class Article {
+        [Required]  public  long            id { get; set; }
+        [Required]  public  string          name;
+        [Relation(nameof(DemoStore.producers))]
+                    public  long            producer;
+                    public  DateTime?       created;
+    }
+
+    public class Customer {
+        [Required]  public  long            id { get; set; }
+        [Required]  public  string          name;
+                    public  DateTime?       created;
+    }
+    
+    public class Employee {
+        [Required]  public  long            id { get; set; }
+        [Required]  public  string          firstName;
+                    public  string          lastName;
+                    public  DateTime?       created;
+    }
+
+    public class Order {
+        [Required]  public  long            id { get; set; }
+        [Relation(nameof(DemoStore.customers))]
+                    public  long            customer;
+                    public  DateTime        created;
+                    public  List<OrderItem> items = new List<OrderItem>();
+    }
+
+    public class OrderItem {
+        [Relation(nameof(DemoStore.articles))]
+        [Required]  public  long            article;
+                    public  int             amount;
+                    public  string          name;
+    }
+
+    public class Producer {
+        [Required]  public  long            id { get; set; }
+        [Required]  public  string          name;
+        [Relation(nameof(DemoStore.employees))]
+                    public  List<long>      employees;
+                    public  DateTime?       created;
+    }
+    
+    
+    // ------------------------------ command models ------------------------------
+    public class Operands {
+        public  double      left;
+        public  double      right;
+    }
+    
+    public class Fake {
+        /// <summary>if false generated entities are nor added to the <see cref="Records"/> result</summary>
+        public  bool?       addResults;
+        public  int?        articles;
+        public  int?        customers;
+        public  int?        employees;
+        public  int?        orders;
+        public  int?        producers;
+    }
+    
+    public class Counts {
+        public  int         articles;
+        public  int         customers;
+        public  int         employees;
+        public  int         orders;
+        public  int         producers;
+    }
+    
+    public class Records {
+        /// <summary>contains a filter that can be used to filter the generated entities in a container</summary>
+        public  string      info;
+        /// <summary>number of entities generated in each container</summary>
+        public  Counts      counts;
+        public  Article[]   articles;
+        public  Customer[]  customers;
+        public  Employee[]  employees;
+        public  Order[]     orders;
+        public  Producer[]  producers;
+    }
+}
