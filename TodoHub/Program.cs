8e6d4dc93cc5a40e790297376daa17e7f644d722
Ullrich Praetz <ullrich@praetz.de>
2022-06-14 11:04:04 +0200

    TodoHub - simplify minimal setup

--- TodoHub/Program.cs
+++ TodoHub/Program.cs
@@ -17,42 +17,21 @@ namespace Fliox.TodoHub
 
         /// <summary>
         /// This method is a blueprint showing how to setup a <see cref="HttpHost"/> utilizing a minimal features set
-        /// available supporting HTTP and WebSockets by using a <see cref="System.Net.HttpListener"/>
+        /// available via HTTP and WebSockets
         /// </summary>
-        internal static HttpHost CreateHttpHost() {
-            var c                   = new Config();
+        private static HttpHost CreateHttpHost() {
             var typeSchema          = NativeTypeSchema.Create(typeof(TodoStore)); // optional - create TypeSchema from Type
-            var databaseSchema      = new DatabaseSchema(typeSchema);
-            var database            = CreateDatabase(c, databaseSchema, null);
+            var database            = new FileDatabase("main_db", "./DB~/main_db");
+            database.Schema         = new DatabaseSchema(typeSchema);
 
             var hub                 = new FlioxHub(database);
-            hub.Info.projectName    = "TodoHub";                                                        // optional
+            hub.Info.projectName    = "TodoHub";                    // optional
             hub.AddExtensionDB (new ClusterDB("cluster", hub));     // optional - expose info of hosted databases. Required by Hub Explorer
             hub.EventDispatcher     = new EventDispatcher(true);    // optional - enables sending events for subscriptions
             
-            var httpHost            = new HttpHost(hub, "/fliox/") { CacheControl = c.cache };
-            httpHost.AddHandler      (new StaticFileHandler(c.www) { CacheControl = c.cache }); // optional - serve static web files of Hub Explorer
+            var httpHost            = new HttpHost(hub, "/fliox/");
+            httpHost.AddHandler      (new StaticFileHandler(HubExplorer.Path)); // optional - serve static web files of Hub Explorer
             return httpHost;
-        }
-        
-        private class Config {
-            internal readonly string  mainDbPath          = "./DB~/main_db";
-            internal readonly string  www                 = HubExplorer.Path;
-            internal readonly string  cache               = null; // "max-age=600"; // HTTP Cache-Control
-            internal readonly bool    useMemoryDbClone    = false;
-        }
-        
-        private static EntityDatabase CreateDatabase(Config c, DatabaseSchema schema, TaskHandler handler) {
-            var fileDb = new FileDatabase("main_db", c.mainDbPath, handler);
-            fileDb.Schema = schema;
-            if (!c.useMemoryDbClone)
-                return fileDb;
-            // As the DemoHub is also deployed as a demo service in the internet it uses a memory database
-            // to minimize operation cost and prevent abuse as a free persistent database.   
-            var memoryDB = new MemoryDatabase("main_db", handler);
-            memoryDB.Schema = schema;
-            memoryDB.SeedDatabase(fileDb).Wait();
-            return memoryDB;
         }
     }
 }
