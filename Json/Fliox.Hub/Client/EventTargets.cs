459fb64c49862ceedef53f2900de686f9b16afe0
Ullrich Praetz <ullrich@praetz.de>
2022-06-27 09:37:19 +0200

    EventTargetsExtension - moved to EventTargets.cs

--- Json/Fliox.Hub/Client/EventTargets.cs
+++ Json/Fliox.Hub/Client/EventTargets.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 // ReSharper disable ClassNeverInstantiated.Global
 namespace Friflo.Json.Fliox.Hub.Client
 {
+    // ---------------------------------------------- EventTargets ----------------------------------------------
     /// <summary> prototype WIP </summary>
     public class EventTargets
     {
@@ -58,6 +59,71 @@ namespace Friflo.Json.Fliox.Hub.Client
         internal void AddClients (ICollection<JsonKey> clients) {
             if (this.clients == null) this.clients = new List<JsonKey>(clients.Count);
             this.clients.AddRange(clients);
+        }
+    }
+
+
+    // ------------------------------------------ EventTargetsExtension ------------------------------------------
+    public static class EventTargetsExtension
+    {
+        // --- user
+        /// <summary> Send the <paramref name="message"/> as an event only to the given <paramref name="user"/> </summary>
+        public static  TTask  EventTargetUser<TTask> (this TTask message, string  user) where TTask : MessageTask {
+            var targets = message.GetOrCreateTargets();
+            targets.AddUser(user);
+            return message;
+        }
+        
+        /// <summary> Send the <paramref name="message"/> as an event only to the given <paramref name="user"/> </summary>
+        public static  TTask  EventTargetUser<TTask> (this TTask message, in JsonKey user) where TTask : MessageTask {
+            var targets = message.GetOrCreateTargets();
+            targets.AddUser(user);
+            return message;
+        }
+        
+        // --- client
+        /// <summary> Send the <paramref name="message"/> as an event only to the given <paramref name="client"/> </summary>
+        public static  TTask  EventTargetClient<TTask> (this TTask message, string client) where TTask : MessageTask {
+            var targets = message.GetOrCreateTargets();
+            targets.AddClient(client);
+            return message;
+        }
+        
+        /// <summary> Send the <paramref name="message"/> as an event only to the given <paramref name="client"/> </summary>
+        public static  TTask  EventTargetClient<TTask> (this TTask message, in JsonKey client) where TTask : MessageTask {
+            var targets = message.GetOrCreateTargets();
+            targets.AddClient(client);
+            return message;
+        }
+        
+        // --- users
+        /// <summary> Send the <paramref name="message"/> as an event only to the given <paramref name="users"/> </summary>
+        public static  TTask  EventTargetUsers<TTask> (this TTask message, ICollection<string> users) where TTask : MessageTask {
+            var targets = message.GetOrCreateTargets();
+            targets.AddUsers(users);
+            return message;
+        }
+        
+        /// <summary> Send the <paramref name="message"/> as an event only to the given <paramref name="users"/> </summary>
+        public static  TTask  EventTargetUsers<TTask> (this TTask message, ICollection<JsonKey> users) where TTask : MessageTask {
+            var targets = message.GetOrCreateTargets();
+            targets.AddUsers (users);
+            return message;
+        }
+        
+        // --- clients
+        /// <summary> Send the <paramref name="message"/> as an event only to the given <paramref name="clients"/> </summary>
+        public static  TTask  EventTargetClients<TTask> (this TTask message, ICollection<string>  clients) where TTask : MessageTask {
+            var targets = message.GetOrCreateTargets();
+            targets.AddClients(clients);
+            return message;
+        }
+        
+        /// <summary> Send the <paramref name="message"/> as an event only to the given <paramref name="clients"/> </summary>
+        public static  TTask  EventTargetClients<TTask> (this TTask message, ICollection<JsonKey>  clients) where TTask : MessageTask {
+            var targets = message.GetOrCreateTargets();
+            targets.AddClients (clients);
+            return message;
         }
     }
 }
