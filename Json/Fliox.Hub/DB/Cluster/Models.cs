8ce924c597066ce9b60dd03253d713352632d155
Ullrich Praetz <ullrich@praetz.de>
2022-06-28 12:31:25 +0200

    Hub.DB.* - cleanup: moved models classes to separate files -> Models.cs

---
+++ Json/Fliox.Hub/DB/Cluster/Models.cs
@@ -0,0 +1,98 @@
+// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Friflo.Json.Fliox.Hub.DB.Cluster
+{
+    // ---------------------------------- entity models ----------------------------------
+    /// <summary><see cref="containers"/> and <see cref="storage"/> type of a database</summary>
+    public sealed class DbContainers {
+        /// <summary>database name</summary>
+        [Required]  public  string                          id;
+        /// <summary><see cref="storage"/> type. e.g. memory, file-system, ...</summary>
+        [Required]  public  string                          storage;
+        /// <summary>list of database <see cref="containers"/></summary>
+        [Required]  public  string[]                        containers;
+                        
+        public override string  ToString() => JsonSerializer.Serialize(this).Replace("\"", "'");
+    }
+    
+    /// <summary><see cref="commands"/> and <see cref="messages"/> of a database</summary>
+    public sealed class DbMessages {
+        /// <summary>database name</summary>
+        [Required]  public  string                          id;
+        /// <summary>list of database <see cref="commands"/></summary>
+        [Required]  public  string[]                        commands;
+        /// <summary>list of database <see cref="messages"/></summary>
+        [Required]  public  string[]                        messages;
+                        
+        public override string  ToString() => JsonSerializer.Serialize(this).Replace("\"", "'");
+    }
+    
+    /// <summary>
+    /// A <see cref="DbSchema"/> can be assigned to a database to specify its <b>containers</b>, <b>commands</b> and <b>messages</b>.<br/>
+    /// The types used by the Schema are declared within <see cref="jsonSchemas"/>.<br/>
+    /// The type referenced by the tuple <see cref="schemaName"/> / <see cref="schemaPath"/> specify the
+    /// database containers, commands and messages. 
+    /// </summary>
+    public sealed class DbSchema {
+        /// <summary>database name</summary>
+        [Required]  public  string                          id;
+        /// <summary>refer a type definition of the JSON Schema referenced with <see cref="schemaPath"/></summary>
+        [Required]  public  string                          schemaName;
+        /// <summary>refer a JSON Schema in <see cref="jsonSchemas"/></summary>
+        [Required]  public  string                          schemaPath;
+        /// <summary>map of <b>JSON Schemas</b> each containing a set of type definitions.<br/>
+        /// Each JSON Schema is identified by its unique path</summary>
+        [Required]  public  Dictionary<string, JsonValue>   jsonSchemas;
+                        
+        public override string  ToString() => JsonSerializer.Serialize(this).Replace("\"", "'");
+    }
+    
+    // ---------------------------- command models - aka DTO's ---------------------------
+    /// <summary>list of container statistics. E.g. the number of entities per container</summary>
+    public sealed class DbStats {
+        /// <summary>list of container statistics - number of entities per container</summary>
+                    public  ContainerStats[]    containers;
+    }
+    
+    /// <summary>statistics of a single container. E.g. the number of entities in a container</summary>
+    public sealed class ContainerStats {
+        /// <summary>container name</summary>
+        [Required]  public  string              name;
+        /// <summary>number of entities / records within a container</summary>
+                    public  long                count;
+            
+        public override     string              ToString() => $"{name} - count: {count}";
+    }
+    
+    /// <summary>general information about a Hub</summary>
+    public sealed class HostDetails {
+        /// <summary>host version</summary>
+        [Required]  public  string              version;
+        /// <summary>host name. Used as <see cref="DB.Monitor.HostHits.id"/> in
+        /// <see cref="DB.Monitor.MonitorStore.hosts"/> of database <b>monitor</b></summary>
+                    public  string              hostName;
+        /// <summary>project name</summary>
+                    public  string              projectName;
+        /// <summary>link to a website describing project and Hub</summary>
+                    public  string              projectWebsite;
+        /// <summary>environment name. e.g. 'dev', 'test', 'staging', 'prod'</summary>
+                    public  string              envName;
+        /// <summary>
+        /// the color used to display the environment name in GUI's using CSS color format.<br/>
+        /// E.g. using red for a production environment: "#ff0000" or "rgb(255 0 0)"
+        /// </summary>
+                    public  string              envColor;
+        /// <summary>routes configures by <see cref="Remote.HttpHost"/> - commonly below <c>/fliox</c></summary>
+        [Required]  public  List<string>        routes; 
+    }
+    
+    /// <summary>All <see cref="databases"/> hosted by Hub</summary>
+    public sealed class HostCluster {
+        /// <summary>list of <see cref="databases"/> hosted by Hub</summary>
+        [Required]  public  List<DbContainers>  databases;
+    }
+}
--- Json/Fliox.Hub/DB/UserAuth/UserStore.cs
+++ Json/Fliox.Hub/DB/UserAuth/UserStore.cs
@@ -1,17 +1,11 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Friflo.Json.Fliox.Hub.Client;
 using Friflo.Json.Fliox.Hub.Host;
-using Friflo.Json.Fliox.Hub.Host.Auth.Rights;
 
 // ReSharper disable UnassignedReadonlyField
-// ReSharper disable ClassNeverInstantiated.Global
-// ReSharper disable UnassignedField.Global
-// ReSharper disable CollectionNeverUpdated.Global
 namespace Friflo.Json.Fliox.Hub.DB.UserAuth
 {
     /// <summary>
@@ -56,59 +50,6 @@ namespace Friflo.Json.Fliox.Hub.DB.UserAuth
         }
     }
 
-    // ---------------------------------- entity models ----------------------------------
-    /// <summary>contains a <see cref="token"/> assigned to a user used for authentication</summary>
-    public sealed class UserCredential {
-        /// <summary>user name</summary>
-        [Required]  public  JsonKey         id;
-        /// <summary>user token</summary>
-                    public  string          token;
-                        
-        public override     string          ToString() => JsonSerializer.Serialize(this);
-    }
-    
-    /// <summary>Set of <see cref="roles"/> assigned to a user used for authorization</summary>
-    public sealed class UserPermission {
-        /// <summary>user name</summary>
-        [Required]  public  JsonKey         id;
-        /// <summary>set of <see cref="roles"/> assigned to a user</summary>
-        [Relation(nameof(UserStore.roles))]
-                    public  List<string>    roles;
 
-        public override     string          ToString() => JsonSerializer.Serialize(this);
-    }
-    
-    /// <summary>Contains a set of <see cref="rights"/> used for task authorization</summary>
-    public sealed class Role {
-        /// <summary><see cref="Role"/> name</summary>
-        [Required]  public  string          id;
-        /// <summary>a set of <see cref="rights"/> used for task authorization</summary>
-        [Required]  public  List<Right>     rights;
-        /// <summary>optional <see cref="description"/> explaining a <see cref="Role"/></summary>
-                    public  string          description;
-                        
-        public override     string          ToString() => JsonSerializer.Serialize(this);
-    }
-    
-    // ---------------------------- command models - aka DTO's ---------------------------
-    /// <summary>user <see cref="Credentials"/> used for authentication</summary>
-    public sealed class Credentials {
-        [Required]  public  JsonKey         userId;
-        [Required]  public  string          token;
-
-        public override     string          ToString() => $"userId: {userId}";
-    }
-    
-    /// <summary>Result of <see cref="UserStore.AuthenticateUser"/> command</summary>
-    public sealed class AuthResult {
-        /// <summary>true if authentication was successful</summary>
-                    public  bool            isValid;
-
-        public override     string          ToString() => $"isValid: {isValid}";
-    }
-    
-    public sealed class ValidateUserDbResult {
-                    public  string[]        errors;
-    }
     
 }
