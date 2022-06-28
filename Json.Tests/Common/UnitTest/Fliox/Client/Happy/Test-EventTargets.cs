3df8620cb9b4771017ddcbe62e9832739283d753
Ullrich Praetz <ullrich@praetz.de>
2022-06-28 07:19:58 +0200

    TestEventTargets() simplify

--- Json.Tests/Common/UnitTest/Fliox/Client/Happy/Test-EventTargets.cs
+++ Json.Tests/Common/UnitTest/Fliox/Client/Happy/Test-EventTargets.cs
@@ -23,69 +23,66 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client.Happy
             using (var client2          = new PocStore(hub))
             using (hub.EventDispatcher  = new EventDispatcher(false)) // dispatch events synchronous to simplify test
             {
+                client1.ClientId    = "client-1";
+                client1.UserId      = "user-1";
                 for (int n = 0; n < 1; n++) {
-                    AssertEventTargets(client1, client2);
+                    int eventCount      = 0;
+                    client1.SubscribeMessage("*", (message, context) => { eventCount++; });
+                    client1.SubscriptionEventHandler = context => {
+                        AreEqual("user-1", context.SrcUserId.ToString());
+                        var expect = new [] { "msg-1", "msg-2", "msg-3", "msg-4", "msg-5", "msg-6", "msg-7", "msg-8", "Command1" };
+                        var actual = context.Messages.Select(msg => msg.Name);
+                        AreEqual(expect, actual);
+                    };
+                    SendMessages(client1);
+                    AreEqual(9, eventCount);
+                }
+                //
+                client2.ClientId    = "client-2";
+                client2.UserId      = "user-2";
+                for (int n = 0; n < 1; n++) {
+                    int eventCount      = 0;
+                    client2.SubscribeMessage("*", (message, context) => { eventCount++; });
+                    client2.SubscriptionEventHandler = context => {
+                        Fail("unexpected events");
+                    };
+                    SendMessages(client2);
+                    AreEqual(0, eventCount);
                 }
             }
         }
         
-        /// <summary>
-        /// all events targeting only <paramref name="client1"/> either by userId or clientId. <br/>
-        /// <paramref name="client2"/> is used to verify no events are received
-        /// </summary>
-        private static void AssertEventTargets(PocStore client1, FlioxClient client2) {
+        private static void SendMessages(PocStore client) {
             // --- client1 receive all events
-            var clientId1   = "client-1";
-            var user1Id     = "user-1";
-            var client1Key  = new JsonKey(clientId1);   // test interface using a JsonKey
-            var user1Key    = new JsonKey(user1Id);     // test interface using a JsonKey
-            client1.ClientId  = clientId1;
-            client1.UserId    = user1Id;
-            int count = 0;
-            client1.SubscribeMessage("*", (message, context) => { count++; });
-            client1.SubscriptionEventHandler = context => {
-                AreEqual("user-1", context.SrcUserId.ToString());
-                var expect = new [] { "msg-1", "msg-2", "msg-3", "msg-4", "msg-5", "msg-6", "msg-7", "msg-8", "Command1" };
-                var actual = context.Messages.Select(msg => msg.Name);
-                AreEqual(expect, actual);
-            };
-            
-            // --- client2 receive no events
-            client2.ClientId  = "client-2";
-            client2.SubscribeMessage("*", (message, context) => {
-                Fail("unexpected events");
-            });
+            var clientId    = "client-1";
+            var userId      = "user-1";
+            var clientKey   = new JsonKey(clientId);   // test interface using a JsonKey
+            var userKey     = new JsonKey(userId);     // test interface using a JsonKey
 
             // --- single target
-            client1.SendMessage("msg-1").EventTargetUser(user1Id);
-            client1.SendMessage("msg-2").EventTargetUser(user1Key);
+            client.SendMessage("msg-1").EventTargetUser(userId);
+            client.SendMessage("msg-2").EventTargetUser(userKey);
             
-            client1.SendMessage("msg-3").EventTargetClient(clientId1);
-            client1.SendMessage("msg-4").EventTargetClient(client1Key);
+            client.SendMessage("msg-3").EventTargetClient(clientId);
+            client.SendMessage("msg-4").EventTargetClient(clientKey);
 
             // --- multi target
-            client1.SendMessage("msg-5").EventTargetUsers (new[] { user1Id });
-            client1.SendMessage("msg-6").EventTargetUsers (new[] { user1Key });
+            client.SendMessage("msg-5").EventTargetUsers (new[] { userId });
+            client.SendMessage("msg-6").EventTargetUsers (new[] { userKey });
 
-            client1.SendMessage("msg-7").EventTargetClients (new[] { clientId1 });
-            client1.SendMessage("msg-8").EventTargetClients (new[] { client1Key });
+            client.SendMessage("msg-7").EventTargetClients (new[] { clientId });
+            client.SendMessage("msg-8").EventTargetClients (new[] { clientKey });
             
             // var eventTargets = new EventTargets();
             // store.SendMessage("msg-5" ).EventTargets = eventTargets;
             
-            client1.Command1().EventTargetUser(user1Id);
-        //  todo store.CommandInt(111).EventTargetUser(user1Key);
+            client.Command1().EventTargetUser(userId);
+        //  todo client.CommandInt(111).EventTargetUser(user1Key);
             
-            client1.SyncTasks().Wait();
-            client2.SyncTasks().Wait();
+            client.SyncTasks().Wait();
             
-            AreEqual(9, count);
-            
-            client1.UnsubscribeMessage("*", null);
-            client1.SyncTasks().Wait();
-            
-            client2.UnsubscribeMessage("*", null);
-            client2.SyncTasks().Wait();
+            client.UnsubscribeMessage("*", null);
+            client.SyncTasks().Wait();
         }
     }
 }
