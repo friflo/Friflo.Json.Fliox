f2faaae5b60c65257b1e0da5710008aa8931f018
Ullrich Praetz <ullrich@praetz.de>
2022-07-02 11:58:13 +0200

    check-markdown.ts - get anchors

--- scripts/check-markdown.ts
+++ scripts/check-markdown.ts
@@ -3,7 +3,7 @@ import * as fs              from 'fs';
 
 import { promisify }        from 'util';
 import { fromMarkdown }     from 'mdast-util-from-markdown'
-import { Root } from 'mdast-util-from-markdown/lib';
+import { Content, Parent, Root } from 'mdast-util-from-markdown/lib';
 
 
 const readdirAsync      = promisify(fs.readdir);
@@ -14,6 +14,7 @@ async function scanMarkdownFiles(directoryName: string, results : string[] = [])
     let files = await readdirAsync(directoryName);
     for (let f of files) {
         let fullPath: string = path.join(directoryName, f);
+        fullPath = fullPath.replaceAll("\\", "/");
         let stat = await statAsync(fullPath);
         if (stat.isDirectory()) {
             if (directoryName == "node_modules") {
@@ -33,30 +34,54 @@ async function scanMarkdownFiles(directoryName: string, results : string[] = [])
 }
 
 type Markdown = {
+    path:       string,
     links:      string[],
     anchors:    string[]
 }
 
-function markdownFromTree(tree: Root, markdown: Markdown) {
-    for (const name in tree.children) {
-        const child = tree.children[name];
-        var node: any = child;
+function textFromNode(node: Parent, texts: string[])  {
+    const children = node.children
+    for (const name in children) {
+        const child = children[name];
+        if (child.type == "text") {
+            texts.push(child.value);
+        }
+        const node: any = child;
+        if (node.children) {
+            textFromNode(node, texts);
+        }  
+    }
+}
+
+function markdownFromTree(tree: Parent, markdown: Markdown) {
+    const children = tree.children
+    for (const name in children) {
+        const child = children[name];
+        var node = child as Parent;
         if (node.children) {
             markdownFromTree(node, markdown);
         }
         if (node.type == "link") {
             markdown.links.push(node.url);
-        }        
+        }
+        if (node.type == "heading") {    
+            const texts: string[] = [];
+            textFromNode (node, texts);
+            const label = texts.join("").trim();
+            markdown.anchors.push(label);
+        }    
     }
 }
 
-function parseMarkdown(path: string) {
+function parseMarkdown(path: string) : Markdown {
     const content       = fs.readFileSync(path, {encoding: 'utf8'});
     const tree: Root    = fromMarkdown(content);
     const markdown = {
-        links:   [],
-        anchors: []
+        path:       path,
+        links:      [],
+        anchors:    []
     };
+    // if (path != "README.md") { return markdown; }
     markdownFromTree (tree, markdown);
     // get links in markdown
     return markdown;
@@ -70,10 +95,11 @@ async function main() {
 
     const markdownMap : { [path: string] : Markdown } = {};
     for (const file of files) {
-        if (file != "README.md") continue;
         const markdown = parseMarkdown(file);
         markdownMap[file] = markdown;
+        console.log(markdown);
     }
+    // console.log(markdownMap["README.md"]);
     console.log(files);
 }
 
