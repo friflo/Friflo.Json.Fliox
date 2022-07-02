bdd41db67809f81470dc7f9e8c6409ba5f9b9cd4
Ullrich Praetz <ullrich@praetz.de>
2022-07-02 18:27:31 +0200

    TodoStore - simplify docs

--- TodoHub/TodoStore.cs
+++ TodoHub/TodoStore.cs
@@ -10,12 +10,7 @@ namespace Fliox.TodoHub
     /// The <see cref="TodoStore"/> offer two functionalities: <br/>
     /// 1. Defines a database <b>schema</b> by declaring its containers, commands and messages<br/>
     /// 2. Is a database <b>client</b> providing type-safe access to its containers, commands and messages <br/>
-    /// <br/>
     /// </summary>
-    /// <remarks>
-    /// <see cref="TodoStore"/> containers are fields or properties of type <see cref="EntitySet{TKey,T}"/>. <br/>
-    /// <see cref="TodoStore"/> instances can be used on client and server side. <br/>
-    /// </remarks>
     public class TodoStore : FlioxClient {
         // --- containers
         public readonly EntitySet <long, Todo>        todos;
