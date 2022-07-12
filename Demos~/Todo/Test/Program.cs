1724a109da5d5fe55991d55486432ec1db6820bc
Ullrich Praetz <ullrich@praetz.de>
2022-07-12 14:52:10 +0200

    Demo projects: changed LangVersion -> 7.3 (required by TargetFramework: netcoreapp3.1)

--- Demos~/Todo/Test/Program.cs
+++ Demos~/Todo/Test/Program.cs
@@ -5,34 +5,35 @@ using Friflo.Json.Fliox.Hub.Host;
 using Friflo.Json.Fliox.Hub.Remote;
 using Todo;
 
-namespace TodoTest;
+namespace TodoTest {
 
-internal static class Program
-{
-    internal static async Task Main(string[] args) {
-        var option  = args.FirstOrDefault() ?? "http";
-        var hub     = CreateHub(option);
-        var client  = new TodoClient(hub);
-        var jobs    = client.jobs.QueryAll();
-        await client.SyncTasks();
+    internal static class Program
+    {
+        internal static async Task Main(string[] args) {
+            var option  = args.FirstOrDefault() ?? "http";
+            var hub     = CreateHub(option);
+            var client  = new TodoClient(hub);
+            var jobs    = client.jobs.QueryAll();
+            await client.SyncTasks();
+                
+            foreach (var job in jobs.Result) {
+                Console.WriteLine($"id: {job.id}, title: {job.title}, completed: {job.completed}");
+            }
+        }
             
-        foreach (var job in jobs.Result) {
-            Console.WriteLine($"id: {job.id}, title: {job.title}, completed: {job.completed}");
+        private static FlioxHub CreateHub(string option) {            
+            switch (option) {
+                case "http":
+                    return new HttpClientHub("main_db", "http://localhost:8010/fliox/");
+                case "ws":
+                    var wsHub = new WebSocketClientHub("main_db", "ws://localhost:8010/fliox/");
+                    wsHub.Connect().Wait();
+                    return wsHub;
+                case "file":
+                    var db = new FileDatabase("main_db", "../TodoHub/DB/main_db");
+                    return new FlioxHub(db);
+            }
+            throw new InvalidOperationException($"unknown option: '{option}' use: [http, ws, file]");
         }
-    }
-        
-    private static FlioxHub CreateHub(string option) {            
-        switch (option) {
-            case "http":
-                return new HttpClientHub("main_db", "http://localhost:8010/fliox/");
-            case "ws":
-                var wsHub = new WebSocketClientHub("main_db", "ws://localhost:8010/fliox/");
-                wsHub.Connect().Wait();
-                return wsHub;
-            case "file":
-                var db = new FileDatabase("main_db", "../TodoHub/DB/main_db");
-                return new FlioxHub(db);
-        }
-        throw new InvalidOperationException($"unknown option: '{option}' use: [http, ws, file]");
     }
 }
