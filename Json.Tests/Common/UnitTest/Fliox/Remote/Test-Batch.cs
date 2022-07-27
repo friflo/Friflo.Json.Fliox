53d553875b69a36d69198425fd484213fdc3230e
Ullrich Praetz <ullrich@praetz.de>
2022-07-27 12:49:45 +0200

    add Remote/Batch test - assert error: subscribing events requires an eventReceiver

---
+++ Json.Tests/Common/UnitTest/Fliox/Remote/Test-Batch.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using NUnit.Framework;
+
+namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Remote
+{
+    public partial class TestRemote
+    {
+        [Test, Order(0)]
+        public static void Batch_main_db() {
+            ExecuteHttpFile("Batch/main_db/sync.http", "Batch/main_db/sync.result.http");
+        }
+    }
+}
