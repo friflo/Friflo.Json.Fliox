9468f5f6b87a627b7c1389d7f30ad08e01016b19
Ullrich Praetz <ullrich@praetz.de>
2022-08-03 09:41:15 +0200

    test: push nuget packages to nuget.org

--- .nupkgs/push-packages.bat
+++ .nupkgs/push-packages.bat
@@ -1,13 +1,13 @@
 
-rem dotnet pack -p:PackageVersion=0.1.2 /p:Version=0.1.2 /p:FileVersion=0.1.2 /p:AssemblyVersion=0.1.2 --output .nupkgs -c Release
+rem dotnet pack -p:PackageVersion=0.2.0 /p:Version=0.2.0 /p:FileVersion=0.2.0 /p:AssemblyVersion=0.2.0 --output .nupkgs -c Release
 rem cd .nupkgs
 rem push-packages.bat
 
-dotnet nuget push Friflo.Json.Burst.0.1.2.nupkg                 --source "github"
-dotnet nuget push Friflo.Json.Fliox.0.1.2.nupkg                 --source "github"
-dotnet nuget push Friflo.Json.Fliox.Annotation.0.1.2.nupkg      --source "github"
-dotnet nuget push Friflo.Json.Fliox.Hub.0.1.2.nupkg             --source "github"
-dotnet nuget push Friflo.Json.Fliox.Hub.AspNetCore.0.1.2.nupkg  --source "github"
-dotnet nuget push Friflo.Json.Fliox.Hub.Cosmos.0.1.2.nupkg      --source "github"
-dotnet nuget push Friflo.Json.Fliox.Hub.Explorer.0.1.2.nupkg    --source "github"
-dotnet nuget push Friflo.Json.Fliox.Hub.GraphQL.0.1.2.nupkg     --source "github"
+dotnet nuget push Friflo.Json.Burst.0.2.0.nupkg                 --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
+dotnet nuget push Friflo.Json.Fliox.0.2.0.nupkg                 --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
+dotnet nuget push Friflo.Json.Fliox.Annotation.0.2.0.nupkg      --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
+dotnet nuget push Friflo.Json.Fliox.Hub.0.2.0.nupkg             --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
+dotnet nuget push Friflo.Json.Fliox.Hub.AspNetCore.0.2.0.nupkg  --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
+dotnet nuget push Friflo.Json.Fliox.Hub.Cosmos.0.2.0.nupkg      --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
+dotnet nuget push Friflo.Json.Fliox.Hub.Explorer.0.2.0.nupkg    --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
+dotnet nuget push Friflo.Json.Fliox.Hub.GraphQL.0.2.0.nupkg     --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
