c79fb4d339ac3777c7ef47da613fe5794ea9606f
Ullrich Praetz <ullrich@praetz.de>
2022-08-04 10:47:08 +0200

    nuget - pack: removed -p:SymbolPackageFormat=snupkg

--- .nupkgs/push-packages.bat
+++ .nupkgs/push-packages.bat
@@ -1,7 +1,8 @@
 
-rem dotnet pack -p:PackageVersion=0.2.0 /p:Version=0.2.0 /p:FileVersion=0.2.0 /p:AssemblyVersion=0.2.0 --output .nupkgs -c Release
+rem dotnet pack -p:PackageVersion=0.2.2 /p:Version=0.2.2 /p:FileVersion=0.2.2 /p:AssemblyVersion=0.2.2 --output .nupkgs -c Release -p:IncludeSymbols=true -p:IncludeSymbols=true
 rem cd .nupkgs
 rem push-packages.bat
+rem [IncludeSource and --include-source are ignored if SymbolPackageFormat is set to snupkg · Issue #8589 · NuGet/Home] https://github.com/NuGet/Home/issues/8589
 
 dotnet nuget push Friflo.Json.Burst.0.2.0.nupkg                 --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
 dotnet nuget push Friflo.Json.Fliox.0.2.0.nupkg                 --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
