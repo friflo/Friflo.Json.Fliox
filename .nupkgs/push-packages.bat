94e03b60321207bb7cd57e6d88a6915d314a4601
Ullrich Praetz <ullrich@praetz.de>
2022-08-04 11:17:48 +0200

    all csproj files: include symbols / sources to pdb file

--- .nupkgs/push-packages.bat
+++ .nupkgs/push-packages.bat
@@ -1,8 +1,7 @@
 
-rem dotnet pack -p:PackageVersion=0.2.2 /p:Version=0.2.2 /p:FileVersion=0.2.2 /p:AssemblyVersion=0.2.2 --output .nupkgs -c Release -p:IncludeSymbols=true -p:IncludeSymbols=true
+rem dotnet pack -p:PackageVersion=0.2.4 /p:Version=0.2.4 /p:FileVersion=0.2.4 /p:AssemblyVersion=0.2.4 --output .nupkgs -c Release -p:SymbolPackageFormat=snupkg
 rem cd .nupkgs
 rem push-packages.bat
-rem [IncludeSource and --include-source are ignored if SymbolPackageFormat is set to snupkg · Issue #8589 · NuGet/Home] https://github.com/NuGet/Home/issues/8589
 
 dotnet nuget push Friflo.Json.Burst.0.2.0.nupkg                 --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
 dotnet nuget push Friflo.Json.Fliox.0.2.0.nupkg                 --source https://api.nuget.org/v3/index.json -k %PUSH_JSON_FLIOX%
