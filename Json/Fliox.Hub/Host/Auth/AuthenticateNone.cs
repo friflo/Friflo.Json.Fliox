02c637e78bcd88f282640ae7f5116647eb5858b9
Ullrich Praetz <ullrich@praetz.de>
2022-09-04 11:06:00 +0200

    Host.Auth - add AuthenticateNone docs

--- Json/Fliox.Hub/Host/Auth/AuthenticateNone.cs
+++ Json/Fliox.Hub/Host/Auth/AuthenticateNone.cs
@@ -1,12 +1,17 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System;
 using System.Threading.Tasks;
 using Friflo.Json.Fliox.Hub.Protocol;
 
 namespace Friflo.Json.Fliox.Hub.Host.Auth
 {
+    /// <summary>
+    /// Used as default <see cref="Authenticator"/> in a <see cref="FlioxHub"/>.<br/>
+    /// All users accessing a <see cref="FlioxHub"/> are not authenticated - <see cref="SyncContext.Authenticated"/> = false <br/>
+    /// Execution of all user tasks are authorized. The <see cref="User.TaskAuthorizer"/> and <see cref="User.HubPermission"/>
+    /// of <see cref="SyncContext.User"/> grant full access.
+    /// </summary>
     public sealed class AuthenticateNone : Authenticator
     {
         public override Task Authenticate(SyncRequest syncRequest, SyncContext syncContext) {
