0138c8130bc0d9c2f4c84c1269587bd7c37283bb
Ullrich Praetz <ullrich@praetz.de>
2022-09-04 11:14:34 +0200

    add missing copyright headers

--- Json/Fliox.Hub/Host/Auth/Rights/HubRights.cs
+++ Json/Fliox.Hub/Host/Auth/Rights/HubRights.cs
@@ -1,5 +1,7 @@
-// ReSharper disable ClassNeverInstantiated.Global
+// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
 
+// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable UnassignedField.Global
 namespace Friflo.Json.Fliox.Hub.Host.Auth.Rights
 {
