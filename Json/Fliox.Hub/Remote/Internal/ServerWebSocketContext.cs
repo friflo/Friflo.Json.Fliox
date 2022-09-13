4c0bbba7d5b381d5c32f77d8d3da0fd56a633023
Ullrich Praetz <ullrich@praetz.de>
2022-09-13 09:56:18 +0200

    Remote - ServerWebSocket: WIP

--- Json/Fliox.Hub/Remote/Internal/ServerWebSocketContext.cs
+++ Json/Fliox.Hub/Remote/Internal/ServerWebSocketContext.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Net;
 using System.Net.WebSockets;
+using System.Security.Cryptography;
 using System.Security.Principal;
+using System.Text;
 using System.Threading.Tasks;
 
 // ReSharper disable UnassignedGetOnlyAutoProperty
@@ -35,16 +37,34 @@ namespace Friflo.Json.Fliox.Hub.Remote.Internal
     internal static class ServerWebSocketExtensions
     {
         internal static async Task<ServerWebSocketContext> AcceptWebSocket(this HttpListenerContext context) {
-            var websocket   = new ServerWebSocket();
-            var wsContext   = new ServerWebSocketContext (websocket);
-            var response    = context.Response;
+            var websocket           = new ServerWebSocket();
+            var wsContext           = new ServerWebSocketContext (websocket);
+            var headers             = context.Request.Headers;
+            var secWebSocketKey     = headers["Sec-WebSocket-Key"];
+            var secWebSocketProtocol= headers["Sec-WebSocket-Protocol"];
+            var secWebSocketVersion = headers["Sec-WebSocket-Version"];
             
-            var headers     = new Dictionary<string, string> {
-                { "Upgrade",        "websocket" },
-                { "Connection",     "Upgrade"}
-            };
-            await HttpListenerExtensions.WriteResponseString(response, null, 101, "", headers).ConfigureAwait(false);
+            // --- create response
+            // [WebSocket - Wikipedia] https://en.wikipedia.org/wiki/WebSocket
+            // secWebSocketKey = "x3JJHMbDL1EzLkh9GBhXDw=="; // test from Wikipedia
+            var secWebSocketAccept      = Sha1Hash(secWebSocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+            var response                = context.Response;
+            response.StatusCode         = 101;
+            response.StatusDescription  = "Switching Protocols";
+            var responseHeaders         = response.Headers;
+            responseHeaders["Upgrade"]                  = "websocket";
+            responseHeaders["Connection"]               = "Upgrade";
+            responseHeaders["Sec-WebSocket-Accept"]     = secWebSocketAccept;
+            
+            await response.OutputStream.FlushAsync();
             return wsContext;
+        }
+        
+        private static string Sha1Hash(string input) {
+            using (SHA1Managed sha1 = new SHA1Managed()) {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return Convert.ToBase64String(hash);
+            }
         }
     }
 }
