2f380675fd2dd327922bf0c233a2443bc9c62138
Ullrich Praetz <ullrich@praetz.de>
2022-09-14 08:38:40 +0200

    Unity - WebSockets: add RFC6455 links

--- Json/Fliox.Hub/Remote/WebSockets/RFC6455.cs
+++ Json/Fliox.Hub/Remote/WebSockets/RFC6455.cs
@@ -6,6 +6,10 @@ using System;
 
 namespace Friflo.Json.Fliox.Hub.Remote.WebSockets
 {
+    /// <summary>
+    /// RFC 6455: The WebSocket Protocol https://www.rfc-editor.org/rfc/rfc6455#section-5.2 <br/>
+    /// RFC 6455 in pretty: https://greenbytes.de/tech/webdav/rfc6455.html#baseframing
+    /// </summary>
     [Flags]
     internal enum FrameFlags {
         Fin                 = 0x80,
@@ -15,7 +19,8 @@ namespace Friflo.Json.Fliox.Hub.Remote.WebSockets
         //
         Opcode              = 0x0f,
     }
-    
+
+    /// <summary> RFC 6455: The WebSocket Protocol https://www.rfc-editor.org/rfc/rfc6455#section-5.2 - Opcode</summary>
     internal enum Opcode {
         ContinuationFrame   = 0x00,
         TextFrame           = 0x01,
@@ -25,6 +30,7 @@ namespace Friflo.Json.Fliox.Hub.Remote.WebSockets
         Pong                = 0x0a
     }
 
+    /// <summary> RFC 6455: The WebSocket Protocol https://www.rfc-editor.org/rfc/rfc6455#section-5.2 - Mask</summary>
     [Flags]
     internal enum LenFlags {
         Mask                = 0x80
