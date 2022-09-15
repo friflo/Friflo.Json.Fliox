44adc06a98bf6fea045ebc81c1c35bf08d4eb4ff
Ullrich Praetz <ullrich@praetz.de>
2022-09-15 08:14:07 +0200

    WebSockets - cleanup

--- Json/Fliox.Hub/Remote/WebSockets/ServerWebSocketContext.cs
+++ Json/Fliox.Hub/Remote/WebSockets/ServerWebSocketContext.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.WebSockets;
@@ -48,7 +47,7 @@ namespace Friflo.Json.Fliox.Hub.Remote.WebSockets
             var secWebSocketProtocol= headers["Sec-WebSocket-Protocol"];
             var secWebSocketVersion = headers["Sec-WebSocket-Version"];
             
-            var items = headers.AllKeys.SelectMany(headers.GetValues, (k, v) => new {key = k, value = v});
+            // var items = headers.AllKeys.SelectMany(headers.GetValues, (k, v) => new {key = k, value = v});
             
             // --- create response
             var secWebSocketAccept      = Sha1Hash(secWebSocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
