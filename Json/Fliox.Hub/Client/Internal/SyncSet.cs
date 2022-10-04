3de28c3c388a56382916fd3879f1d6daa62833dd
Ullrich Praetz <ullrich@praetz.de>
2022-10-04 08:17:12 +0200

    removed unused import

--- Json/Fliox.Hub/Client/Internal/SyncSet.cs
+++ Json/Fliox.Hub/Client/Internal/SyncSet.cs
@@ -11,7 +11,6 @@ using Friflo.Json.Fliox.Hub.Protocol.Models;
 using Friflo.Json.Fliox.Hub.Protocol.Tasks;
 using Friflo.Json.Fliox.Mapper;
 using Friflo.Json.Fliox.Transform;
-using Friflo.Json.Fliox.Transform.Query.Ops;
 
 // ReSharper disable RedundantArgumentDefaultValue
 // ReSharper disable InconsistentNaming
