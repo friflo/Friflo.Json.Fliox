09ca1ec7f05beeadfe2d432734dea17d89243052
Ullrich Praetz <ullrich@praetz.de>
2022-10-07 03:55:10 +0200

    Tests - add TestVarGetAndSet

---
+++ Json.Tests/Common/UnitTest/Fliox/Mapper/TestVar.cs
@@ -0,0 +1,65 @@
+// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using Friflo.Json.Fliox.Mapper.Map.Object.Reflect;
+using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Mapper
+{
+    internal class TestVarObject { }
+    
+    public class TestVar
+    {
+        [Test] public void  TestVarGetAndSet()
+        {
+            // --- object
+            var testObj1 = new TestVarObject();
+            var testObj2 = new TestVarObject();
+            
+            var obj1A   = new Var(testObj1);
+            var obj1B   = new Var(testObj1);
+            var obj2    = new Var(testObj2);
+            var objNull = new Var((object)null);
+            
+            IsFalse (obj2.IsNull);
+            IsTrue  (objNull.IsNull);
+            
+            IsTrue  (obj1A == obj1B);
+            IsTrue  (obj1A != obj2);
+            
+            // --- string
+            var abc1 = new string("abc");
+            var abc2 = new string("abc");
+            IsFalse(ReferenceEquals(abc1, abc2));
+            
+            var str1A   = new Var(abc1);
+            var str1B   = new Var(abc2);
+            var str2    = new Var("xyz");
+            var strNull = new Var((string)null); 
+            
+            IsFalse (str1A.IsNull);
+            IsTrue  (strNull.IsNull);
+
+            IsTrue  (str1A == str1B);
+            IsTrue  (str1A != str2);
+            IsTrue  (str1A != strNull);
+            
+            // --- long
+            var long0       = new Var(0);
+            var long1A      = new Var(1);
+            var long1B      = new Var(1);
+            var long2       = new Var(2);
+            var longNull    = new Var((long?)null);
+            
+            IsFalse (long0.IsNull);
+            IsTrue (longNull.IsNull);
+            IsFalse (long1A.IsNull);
+            
+            IsTrue  (long1A == long1B);
+            IsTrue  (long1A != long2);
+            IsFalse (long1A == longNull);
+        }
+    }
+}
--- Json/Fliox/Mapper/Map/Object/Reflect/Var.cs
+++ Json/Fliox/Mapper/Map/Object/Reflect/Var.cs
@@ -5,20 +5,20 @@ using System;
 
 namespace Friflo.Json.Fliox.Mapper.Map.Object.Reflect
 {
-    internal readonly struct Var
+    public readonly struct Var
     {
         public override bool    Equals(object obj)  => throw new InvalidOperationException("not implemented intentionally");
         public override int     GetHashCode()       => throw new InvalidOperationException("not implemented intentionally");
 
-        internal  readonly  VarType type;
+        private   readonly  VarType type;
         internal  readonly  object  obj;
         internal  readonly  long    lng;
         internal  readonly  double  dbl;   // can merge with lng using BitConverter.DoubleToInt64Bits()
         
-        internal            bool    IsEqual(in Var other)   =>  type.AreEqual(this, other);
-        internal            bool    IsNull                  =>  type.IsNull(this);
-        internal            bool    NotNull                 => !type.IsNull(this);
-        internal            string  AsString()              =>  type.AsString(this);
+        public              bool    IsEqual(in Var other)   =>  type.AreEqual(this, other);
+        public              bool    IsNull                  =>  type.IsNull(this);
+        public              bool    NotNull                 => !type.IsNull(this);
+        public              string  AsString()              =>  type.AsString(this);
         
         public    override  string  ToString()              =>  AsString();
 
@@ -26,14 +26,14 @@ namespace Friflo.Json.Fliox.Mapper.Map.Object.Reflect
         public static bool operator != (in Var val1, in Var val2) => !val1.type.AreEqual(val1, val2);
         
         // --- object ---
-        internal Var (object value) {
+        public Var (object value) {
             type    = VarTypeObject.Instance;
             obj     = value;
             lng     = 0;
             dbl     = 0;
         }
         
-        internal Var (string value) {
+        public Var (string value) {
             type    = VarTypeString.Instance;
             obj     = value;
             lng     = 0;
@@ -41,14 +41,14 @@ namespace Friflo.Json.Fliox.Mapper.Map.Object.Reflect
         }
         
         // --- long (int64) ---
-        internal Var (long value) {
+        public Var (long value) {
             type    = VarTypeLong.Instance;
             obj     = HasValue;
             lng     = value;
             dbl     = 0;
         }
 
-        internal Var (long? value) {
+        public Var (long? value) {
             type    = VarTypeNullableLong.Instance;
             obj     = value.HasValue ? HasValue : null;
             lng     = value ?? 0L;
@@ -56,14 +56,14 @@ namespace Friflo.Json.Fliox.Mapper.Map.Object.Reflect
         }
         
         // --- double (64 bit) ---
-        internal Var (double value) {
+        public Var (double value) {
             type    = VarTypeDbl.Instance;
             obj     = HasValue;
             lng     = 0;
             dbl     = value;
         }
 
-        internal Var (double? value) {
+        public Var (double? value) {
             type    = VarTypeNullableDbl.Instance;
             obj     = value.HasValue ? HasValue : null;
             lng     = 0;
