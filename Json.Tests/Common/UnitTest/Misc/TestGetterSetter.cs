49feaefe2d256e4c6eb224e212dd21a9bde10dd7
Ullrich Praetz <ullrich@praetz.de>
2022-10-08 19:46:28 +0200

    Tests - add example for property access to TestGetterSetter

--- Json.Tests/Common/UnitTest/Misc/TestGetterSetter.cs
+++ Json.Tests/Common/UnitTest/Misc/TestGetterSetter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Reflection;
 using NUnit.Framework;
 
+// ReSharper disable PossibleNullReferenceException
 namespace Friflo.Json.Tests.Common.UnitTest.Misc
 {
     public struct TestStruct
@@ -11,6 +12,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc
     
     public class TestClass {
         public int      intValue;
+        public int      Int2 { get; set; }
         public object   objValue;
     }
     
@@ -81,22 +83,31 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc
     {
         [Test]
         public static void  Test() {
-            var testClass = new TestClass { intValue = 123 };
+            var testClass = new TestClass { intValue = 123, Int2 = 456 };
             
             var setIntMethod    = typeof(SampleGen).GetMethod(nameof(SampleGen.TestClass_intValue_set));
             var setIntDelegate  = DelegateUtils.CreateSetDelegate<TestClass, int>(setIntMethod);
             
             var getIntMethod    = typeof(SampleGen).GetMethod(nameof(SampleGen.TestClass_intValue_get));
             var getIntDelegate  = DelegateUtils.CreateGetDelegate<TestClass, int>(getIntMethod);
+            //
             
+            var getInt2Method   = typeof(TestClass).GetProperty(nameof(TestClass.Int2)).GetGetMethod(true);
+            var getInt2Delegate = DelegateUtils.CreateGetDelegate<TestClass, int>(getInt2Method);
+            
+            var setInt2Method   = typeof(TestClass).GetProperty(nameof(TestClass.Int2)).GetSetMethod(true);
+            var setInt2Delegate = DelegateUtils.CreateSetDelegate<TestClass, int>(setInt2Method);
+            
+            //
             var getObjMethod    = typeof(SampleGen).GetMethod(nameof(SampleGen.TestClass_objValue_get));
             var getObjDelegate  = DelegateUtils.CreateGetDelegate<TestClass, object>(getObjMethod);
             
             var setObjMethod    = typeof(SampleGen).GetMethod(nameof(SampleGen.TestClass_objValue_set));
             var setObjDelegate  = DelegateUtils.CreateSetDelegate<TestClass, object>(setObjMethod);
             
-            var intAccess       = new ObjectMemberAccessInt<TestClass>       { get = getIntDelegate, set = setIntDelegate };
-            var objAccess       = new ObjectMemberAccessObject<TestClass>    { get = getObjDelegate, set = setObjDelegate };
+            var intAccess       = new ObjectMemberAccessInt<TestClass>       { get = getIntDelegate,  set = setIntDelegate };
+            var int2Access      = new ObjectMemberAccessInt<TestClass>       { get = getInt2Delegate, set = setInt2Delegate };
+            var objAccess       = new ObjectMemberAccessObject<TestClass>    { get = getObjDelegate,  set = setObjDelegate };
 
             
             var intField = typeof(TestClass).GetField("intValue");
@@ -105,9 +116,10 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc
             var start = GC.GetAllocatedBytesForCurrentThread();
             
             var intValue = new BuildInValue(1,BuildInType.Int32);
-            for (int n = 0; n < 50; n++) {
+            for (int n = 0; n < 50_000_000; n++) {
                 // var value = testClass.intValue;                      // 50_000_000    12 ms, memory             0 bytes
                 BuildInValue value = intAccess.GetValue(testClass);     // 50_000_000   110 ms, memory             0 bytes
+                // BuildInValue value = int2Access.GetValue(testClass); // 50_000_000   110 ms, memory             0 bytes (Property)
                 // intAccess.SetValue(testClass, intValue);             // 50_000_000   110 ms, memory             0 bytes
                 // var value = (int)intField.GetValue(testClass);       // 50_000_000  2296 ms, memory 1.200.000.080 bytes
                 
