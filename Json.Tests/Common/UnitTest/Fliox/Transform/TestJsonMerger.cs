812a53efbba1d0e0ee0141d900bdac91bcec65bb
Ullrich Praetz <ullrich@praetz.de>
2022-10-19 09:59:34 +0200

    Tests - simplify TestJsonMerger tests

--- Json.Tests/Common/UnitTest/Fliox/Transform/TestJsonMerger.cs
+++ Json.Tests/Common/UnitTest/Fliox/Transform/TestJsonMerger.cs
@@ -18,43 +18,97 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Transform
             public  int     childInt;
         }
         
-        private  class MergeClass {
-            public  int         int1;
-            public  MergeChild  child1;
-            public  MergeChild  child2;
-            public  string      str1;
-            public  int         int2;
-            public  int         int3;
+        private  class MergePrimitives {
+            public  int         intEqual;
+            public  int         intNotEqual;
+            public  bool        boolEqual;
+            public  bool        boolNotEqual;
+            public  string      strEqual;
+            public  string      strEqualNull;
+            public  string      strNotEqual;
+            public  string      strOnlyLeft;
+            public  string      strOnlyRight;
         }
         
         [Test]
-        public void TestJsonMerge() {
+        public void TestJsonMergePrimitives() {
             using (var typeStore        = new TypeStore()) 
             using (var differ           = new ObjectDiffer(typeStore))
             using (var jsonDiff         = new JsonDiff(typeStore))
             using (var writer           = new ObjectWriter(typeStore))
             using (var merger           = new JsonMerger())
             {
-                var left    = new MergeClass {
-                    int1    =  1,
-                    int2    =  2,
-                    child1  = new MergeChild { childInt =  3 },
-                    child2  = null,
-                    str1    = "Test",
-                    int3    =  5
+                var left    = new MergePrimitives {
+                    intEqual        = 1,
+                    intNotEqual     = 2,
+                    boolEqual       = true,
+                    boolNotEqual    = true,
+                    strEqual        = "Test",
+                    strEqualNull    = null, 
+                    strNotEqual     = "Str-1",
+                    strOnlyLeft     = "only left",
+                    strOnlyRight    = null
                 };
-                var right   = new MergeClass {
-                    int1    = 11,
-                    int2    = 12,
-                    child1  = new MergeChild { childInt = 13 },
-                    child2  = new MergeChild { childInt = 14 },
-                    str1    = null,  
-                    int3    =  5
+                var right   = new MergePrimitives {
+                    intEqual        = 1,
+                    intNotEqual     = 22,
+                    boolEqual       = true,
+                    boolNotEqual    = false,
+                    strEqual        = "Test",
+                    strEqualNull    = null,
+                    strNotEqual     = "Str-2",
+                    strOnlyLeft     = null,
+                    strOnlyRight    = "only right"
                 };
-
                 PrepareMerge(left, right, differ, jsonDiff, writer, out var value, out var patch);
+                
                 var merge       = merger.Merge(value, patch);
-                var expect      = "{'int1':11,'child1':{'childInt':13},'int2':12,'int3':5,'child2':{'childInt':14}}".Replace('\'', '"');
+                var expect      =
+"{'intEqual':1,'intNotEqual':22,'boolEqual':true,'boolNotEqual':false,'strEqual':'Test','strNotEqual':'Str-2','strOnlyRight':'only right'}".Replace('\'', '"');
+                AreEqual(expect, merge.AsString());
+                
+                AssertAlloc(value, patch, 1, merger);
+            }
+        }
+        
+        private  class MergeArray {
+            public  int[]       int1;
+            public  int[]       int2;
+            public  bool[]      bool1;
+            public  bool[]      bool2;
+            public  string[]    str1;
+            public  string[]    str2;
+        }
+        
+        [Test]
+        public void TestJsonMergeArrays() {
+            using (var typeStore        = new TypeStore()) 
+            using (var differ           = new ObjectDiffer(typeStore))
+            using (var jsonDiff         = new JsonDiff(typeStore))
+            using (var writer           = new ObjectWriter(typeStore))
+            using (var merger           = new JsonMerger())
+            {
+                var left = new MergeArray {
+                    int1  = new [] { 10 },
+                    int2  = new [] { 20 },
+                    bool1 = new [] { true },
+                    bool2 = new [] { true },
+                    str1  = new [] { "A" },
+                    str2  = new [] { "B" },
+                };
+                var right = new MergeArray {
+                    int1  = new [] { 10 },
+                    int2  = new [] { 21 },
+                    bool1 = new [] { true },
+                    bool2 = new [] { false },
+                    str1  = new [] { "A" },
+                    str2  = new [] { "C" },
+                };
+                PrepareMerge(left, right, differ, jsonDiff, writer, out var value, out var patch);
+                
+                var merge       = merger.Merge(value, patch);
+                var expect      =
+"{'int1':[10],'int2':[21],'bool1':[true],'bool2':[false],'str1':['A'],'str2':['C']}".Replace('\'', '"');
                 AreEqual(expect, merge.AsString());
                 
                 AssertAlloc(value, patch, 1, merger);
