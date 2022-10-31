76d6b60479104b9f0ab3e3e5c93c0172db47ce8d
Ullrich Praetz <ullrich@praetz.de>
2022-10-31 06:22:33 +0100

    Mapper - add Read() / Write() used by generated code

--- Json/Fliox/Mapper/Gen/ReaderGen.cs
+++ Json/Fliox/Mapper/Gen/ReaderGen.cs
@@ -46,6 +46,38 @@ namespace Friflo.Json.Fliox.Mapper.Map
             return success;
         }
         
+        // ------------------------------------------- bool ---------------------------------------------
+        public bool Read (string name, PropField field, ref bool value) {
+            if (parser.Event != JsonEvent.ValueBool)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsBool(out bool success);
+            return success;
+        }
+        
+        // --- nullable
+        public bool Read (string name, PropField field, ref bool? value) {
+            if (parser.Event != JsonEvent.ValueBool)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsBool(out bool success);
+            return success;
+        }
+        
+        // ------------------------------------------- number ---------------------------------------------
+        // --- integer
+        public bool Read (string name, PropField field, ref byte value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsByte(out bool success);
+            return success;
+        }
+        
+        public bool Read (string name, PropField field, ref short value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsShort(out bool success);
+            return success;
+        }
+        
         public bool Read (string name, PropField field, ref int value) {
             if (parser.Event != JsonEvent.ValueNumber)
                 return HandleEventGen(field.fieldType, ref value);
@@ -53,11 +85,101 @@ namespace Friflo.Json.Fliox.Mapper.Map
             return success;
         }
         
+        public bool Read (string name, PropField field, ref long value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsLong(out bool success);
+            return success;
+        }
+        
+        // --- floating point ---
         public bool Read (string name, PropField field, ref float value) {
             if (parser.Event != JsonEvent.ValueNumber)
                 return HandleEventGen(field.fieldType, ref value);
             value = parser.ValueAsFloat(out bool success);
             return success;
+        }
+        
+        public bool Read (string name, PropField field, ref double value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsDouble(out bool success);
+            return success;
+        }
+        
+        // --------------------------------- nullable number ------------------------------------------
+        // --- integer
+        public bool Read (string name, PropField field, ref byte? value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsByte(out bool success);
+            return success;
+        }
+        
+        public bool Read (string name, PropField field, ref short? value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsShort(out bool success);
+            return success;
+        }
+        
+        public bool Read (string name, PropField field, ref int? value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsInt(out bool success);
+            return success;
+        }
+        
+        public bool Read (string name, PropField field, ref long? value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsLong(out bool success);
+            return success;
+        }
+        
+        // --- floating point ---
+        public bool Read (string name, PropField field, ref float? value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsFloat(out bool success);
+            return success;
+        }
+        
+        public bool Read (string name, PropField field, ref double? value) {
+            if (parser.Event != JsonEvent.ValueNumber)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.ValueAsDouble(out bool success);
+            return success;
+        }
+        
+        // ------------------------------------------- string ---------------------------------------------
+        public bool Read (string name, PropField field, ref string value) {
+            if (parser.Event != JsonEvent.ValueString)
+                return HandleEventGen(field.fieldType, ref value);
+            value = parser.value.GetString(ref charBuf);
+            return true;
+        }
+        
+        public bool Read (string name, PropField field, ref JsonKey value) {
+            if (parser.Event != JsonEvent.ValueString)
+                return HandleEventGen(field.fieldType, ref value);
+            value = new JsonKey(ref parser.value, ref parser.valueParser);
+            return true;
+        }
+        
+        // ------------------------------------------- any ---------------------------------------------
+        // ReSharper disable once RedundantAssignment
+        public bool Read (string name, PropField field, ref JsonValue value) {
+            var stub = jsonWriterStub;
+            if (stub == null)
+                jsonWriterStub = stub = new Utf8JsonWriterStub();
+            
+            ref var serializer = ref stub.jsonWriter;
+            serializer.InitSerializer();
+            serializer.WriteTree(ref parser);
+            var json    = serializer.json.AsArray();
+            value       = new JsonValue (json);
+            return true;
         }
     }
 }
--- Json/Fliox/Mapper/Gen/WriterGen.cs
+++ Json/Fliox/Mapper/Gen/WriterGen.cs
@@ -24,14 +24,130 @@ namespace Friflo.Json.Fliox.Mapper.Map
             mapper.Write(ref this, value);
         }
         
+        private void WriteKeyNull (PropField field, ref bool firstMember) {
+            if (!writeNullMembers)
+                return;
+            WriteFieldKey(field, ref firstMember);
+            AppendNull();
+        }
+        
+        // ------------------------------------------- bool ---------------------------------------------
+        public void Write (string name, PropField field, bool value, ref bool firstMember) {
+            WriteFieldKey(field, ref firstMember);
+            format.AppendBool(ref bytes, value);
+        }
+        
+        // --- nullable
+        public void Write (string name, PropField field, bool? value, ref bool firstMember) {
+            if (value == null) { WriteKeyNull(field, ref firstMember); return; }
+            WriteFieldKey(field, ref firstMember);
+            format.AppendBool(ref bytes, value.Value);
+        }
+        
+        // ------------------------------------------- number ---------------------------------------------
+        // --- integer
+        public void Write (string name, PropField field, byte value, ref bool firstMember) {
+            WriteFieldKey(field, ref firstMember);
+            format.AppendInt(ref bytes, value);
+        }
+        
+        public void Write (string name, PropField field, short value, ref bool firstMember) {
+            WriteFieldKey(field, ref firstMember);
+            format.AppendInt(ref bytes, value);
+        }
+        
         public void Write (string name, PropField field, int value, ref bool firstMember) {
             WriteFieldKey(field, ref firstMember);
             format.AppendInt(ref bytes, value);
         }
         
+        public void Write (string name, PropField field, long value, ref bool firstMember) {
+            WriteFieldKey(field, ref firstMember);
+            format.AppendLong(ref bytes, value);
+        }
+        
+        // --- floating point
         public void Write (string name, PropField field, float value, ref bool firstMember) {
             WriteFieldKey(field, ref firstMember);
             format.AppendFlt(ref bytes, value);
+        }
+        
+        public void Write (string name, PropField field, double value, ref bool firstMember) {
+            WriteFieldKey(field, ref firstMember);
+            format.AppendDbl(ref bytes, value);
+        }
+        
+        // -------------------------------- nullable number ------------------------------------------
+        // --- integer
+        public void Write (string name, PropField field, byte? value, ref bool firstMember) {
+            if (value == null) { WriteKeyNull(field, ref firstMember); return; }
+            WriteFieldKey(field, ref firstMember);
+            format.AppendInt(ref bytes, value.Value);
+        }
+        
+        public void Write (string name, PropField field, short? value, ref bool firstMember) {
+            if (value == null) { WriteKeyNull(field, ref firstMember); return; }
+            WriteFieldKey(field, ref firstMember);
+            format.AppendInt(ref bytes, value.Value);
+        }
+        
+        public void Write (string name, PropField field, int? value, ref bool firstMember) {
+            if (value == null) { WriteKeyNull(field, ref firstMember); return; }
+            WriteFieldKey(field, ref firstMember);
+            format.AppendInt(ref bytes, value.Value);
+        }
+        
+        public void Write (string name, PropField field, long? value, ref bool firstMember) {
+            if (value == null) { WriteKeyNull(field, ref firstMember); return; }
+            WriteFieldKey(field, ref firstMember);
+            format.AppendLong(ref bytes, value.Value);
+        }
+        
+        // --- floating point
+        public void Write (string name, PropField field, float? value, ref bool firstMember) {
+            if (value == null) { WriteKeyNull(field, ref firstMember); return; }
+            WriteFieldKey(field, ref firstMember);
+            format.AppendFlt(ref bytes, value.Value);
+        }
+        
+        public void Write (string name, PropField field, double? value, ref bool firstMember) {
+            if (value == null) { WriteKeyNull(field, ref firstMember); return; }
+            WriteFieldKey(field, ref firstMember);
+            format.AppendDbl(ref bytes, value.Value);
+        }
+        
+        // ------------------------------------------- string ---------------------------------------------
+        public void Write (string name, PropField field, string value, ref bool firstMember) {
+            if (value == null) { WriteKeyNull(field, ref firstMember); return; }
+            WriteFieldKey(field, ref firstMember);
+            WriteString(value);
+        }
+
+        public void Write (string name, PropField field, in JsonKey value, ref bool firstMember) {
+            switch (value.type) {
+                case JsonKeyType.Long:
+                    bytes.AppendChar('\"');
+                    format.AppendLong(ref bytes, value.lng);
+                    bytes.AppendChar('\"');
+                    break;
+                case JsonKeyType.String:
+                    WriteString(value.str);
+                    break;
+                case JsonKeyType.Guid:
+                    WriteGuid(value.guid);
+                    break;
+                case JsonKeyType.Null:
+                    AppendNull();
+                    break;
+            }
+        }
+        
+        // ------------------------------------------- any ---------------------------------------------
+        public void Write (string name, PropField field, in JsonValue value, ref bool firstMember) {
+            if (!value.IsNull())
+                bytes.AppendArray(value);
+            else
+                AppendNull();
         }
     }
 }
