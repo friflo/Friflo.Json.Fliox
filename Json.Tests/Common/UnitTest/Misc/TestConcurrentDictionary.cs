22f0c79e06ae01abd920e2f84a658e8189179a14
Ullrich Praetz <ullrich@praetz.de>
2022-11-10 13:04:13 +0100

    Tests - update TestConcurrentDictionary

--- Json.Tests/Common/UnitTest/Misc/TestConcurrentDictionary.cs
+++ Json.Tests/Common/UnitTest/Misc/TestConcurrentDictionary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using Friflo.Json.Burst;
 using NUnit.Framework;
 using static NUnit.Framework.Assert;
 
@@ -28,6 +29,56 @@ namespace Friflo.Json.Tests.Common.UnitTest.Misc
             
             AreSame(valAbc, abc);
             AreEqual(0, dif);
+        }
+
+        private const int Count = 10;
+        
+        [Test]
+        public void ConcurrentPerf()
+        {
+            int len = 0;
+            var start = GC.GetAllocatedBytesForCurrentThread();
+            for (int n = 0; n < Count; n++) {
+                var str  = new string("0123456789"); 
+                len     += str.Length;
+            }
+            var dif  = GC.GetAllocatedBytesForCurrentThread() - start;
+            Console.WriteLine($"{len}, dif: {dif}");
+        }
+        
+        [Test]
+        public void ConcurrentStringIntern()
+        {
+            int len     = 0;
+            var key     = new Bytes("0123456789");
+            var intern  = new StringIntern();
+            intern.Get(ref key);
+            intern.Get(ref key);
+            intern.Get(ref key);
+            
+            var start = GC.GetAllocatedBytesForCurrentThread();
+            for (int n = 0; n < Count; n++) {
+                var str  = intern.Get(ref key);
+                len     += str.Length;
+            }
+            var dif  = GC.GetAllocatedBytesForCurrentThread() - start;
+            AreEqual(0, dif);
+            Console.WriteLine($"{len}, dif: {dif}");
+        }
+    }
+    
+    class StringIntern
+    {
+        private readonly ConcurrentDictionary<Bytes, string> interns = new ConcurrentDictionary<Bytes, string>(Bytes.Equality);
+        
+        public string Get(ref Bytes value) {
+            value.UpdateHashCode();
+            if (interns.TryGetValue(value, out var str)) {
+                return str;
+            }
+            str = value.AsString();
+            interns.TryAdd(value, str);
+            return str;
         }
     }
 }
