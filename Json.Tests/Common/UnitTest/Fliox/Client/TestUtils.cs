690cf2c1da1ab2434a423c17b50ed158ab5de7d4
Ullrich Praetz <ullrich@praetz.de>
2022-11-28 10:13:45 +0100

    CI (Linux) - adjust expected memory allocation

--- Json.Tests/Common/UnitTest/Fliox/Client/TestUtils.cs
+++ Json.Tests/Common/UnitTest/Fliox/Client/TestUtils.cs
@@ -144,7 +144,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client
             var diff = GC.GetAllocatedBytesForCurrentThread() - start;
             var platform    = Environment.OSVersion.Platform;
             var isWindows   = platform == PlatformID.Win32NT; 
-            var expected    = isWindows ? 1736 : 2120;  // Test Windows & Linux
+            var expected    = isWindows ? 1736 : 1736;  // Test Windows & Linux
             Console.WriteLine($"PocStore allocation. platform: {platform}, memory: {diff}");
             AreEqual(expected, diff);
         }
