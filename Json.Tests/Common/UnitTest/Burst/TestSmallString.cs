b890d26cd97f69987fc876f688d6b6951dbb64fe
Ullrich Praetz <ullrich@praetz.de>
2022-12-04 06:09:13 +0100

    Utils - support null in SmallString

--- Json.Tests/Common/UnitTest/Burst/TestSmallString.cs
+++ Json.Tests/Common/UnitTest/Burst/TestSmallString.cs
@@ -14,24 +14,45 @@ namespace Friflo.Json.Tests.Common.UnitTest.Burst
     {
         [Test]
         public static void SmallStringIsEqual() {
+            var smallNull = new SmallString();
             var sb = new StringBuilder();
-            for (int n = 0; n < 20; n++) {
+            for (int n = 0; n < 30; n++) {
                 sb.Clear();
                 for (int i = 0; i < n; i++) {
                     char c = (char)('a' + i);
                     sb.Append(c);
                 }
                 var str     = sb.ToString();
-                var small  = new SmallString(str);
+                var small   = new SmallString(str);
                 
-                IsTrue(small.IsEqual(small));
+                AreEqual(n, small.Length);
+                IsFalse (small.IsNull());
+                IsTrue  (small.IsEqual(small));
+                
+                IsFalse (smallNull.IsEqual(small));
+                IsFalse (small.IsEqual(smallNull));
             }
+        }
+        
+        [Test]
+        public static void SmallStringNull() {
+            var null1 = new SmallString();
+            IsTrue(null1.IsNull());
+            
+            var null2 = new SmallString(null);
+            IsTrue(null2.IsNull());
+            
+            IsNull(null1.ToString());
+            
+            Throws<NullReferenceException>(() => {
+                var _ = null1.Length;
+            });
         }
         
         [Test]
         public static void SmallStringNotEqual() {
             var sb = new StringBuilder();
-            for (int n = 0; n < 20; n++) {
+            for (int n = 0; n < 30; n++) {
                 sb.Clear();
                 for (int i = 0; i < n; i++) {
                     char c = (char)('a' + i);
--- Json/Burst/Utils/SmallString.cs
+++ Json/Burst/Utils/SmallString.cs
@@ -13,7 +13,14 @@ namespace Friflo.Json.Burst.Utils
     /// </remarks>
     public readonly struct SmallString
     {
-        private readonly    int     len;
+        /// <summary>
+        ///  0   => null string <br/>
+        /// -1   => empty string <br/>
+        /// -2   => small string Length = 1  <br/>  
+        /// -17  => small string Length = 16 <br/>
+        /// > 16 => string Length
+        /// </summary>
+        private readonly    int     lenCode;
         
         private readonly    ulong   c00;
         private readonly    ulong   c04;
@@ -21,8 +28,10 @@ namespace Friflo.Json.Burst.Utils
         private readonly    ulong   c12;
         
         public  readonly    string  value;
-
-        public  override    string  ToString()          => value;
+        
+        public              int     Length      => lenCode > 0 ? lenCode : lenCode < 0 ? -lenCode - 1 : throw new NullReferenceException();
+        public              bool    IsNull()    => lenCode == 0;
+        public  override    string  ToString()  => value;
         
         public override bool Equals(object obj) {
             throw new NotImplementedException("not implemented by intention to avoid boxing. Use IsEqual() or SmallString.Equality comparer");
@@ -33,46 +42,52 @@ namespace Friflo.Json.Burst.Utils
         }
         
         public bool IsEqual(in SmallString str) {
-            return len <= 16 ?
-                !(c00 != str.c00 ||
-                  c04 != str.c04 ||
-                  c08 != str.c08 ||
-                  c12 != str.c12) :
-                 value  == str.value;
+            return lenCode <= 0
+                ? !(lenCode != str.lenCode  ||
+                    c00     != str.c00      ||
+                    c04     != str.c04      ||
+                    c08     != str.c08      ||
+                    c12     != str.c12)
+                : lenCode == str.lenCode &&
+                  value   == str.value;
         }
 
         public unsafe  SmallString(string str) {
             value   = str;
-            var s   = str.AsSpan();
-            len     = s.Length;
-            if (len == 0) {
-                c00 = 0; c04 = 0; c08 = 0; c12 = 0;
+            if (str == null) {
+                lenCode = 0; c00 = 0; c04 = 0; c08 = 0; c12 = 0;
+                return;
+            }
+            var s       = str.AsSpan();
+            var strLen  = s.Length;
+            if (strLen == 0) {
+                lenCode = -1; c00 = 0; c04 = 0; c08 = 0; c12 = 0;
                 return;
             }
             fixed (char*  srcPtr = &s[0])
             {
-                switch (len) {
-                    case 1:     c00 = s[0];                                             c04 = 0; c08 = 0; c12 = 0;  return;
-                    case 2:     c00 = s[0] + ((ulong)s[1] << 16);                       c04 = 0; c08 = 0; c12 = 0;  return;
-                    case 3:     c00 = s[0] + ((ulong)s[1] << 16) + ((ulong)s[2] << 32); c04 = 0; c08 = 0; c12 = 0;  return;
+                switch (strLen) {
+                    case 1:  lenCode = -2;  c00 = s[0];                                             c04 = 0; c08 = 0; c12 = 0;  return;
+                    case 2:  lenCode = -3;  c00 = s[0] + ((ulong)s[1] << 16);                       c04 = 0; c08 = 0; c12 = 0;  return;
+                    case 3:  lenCode = -4;  c00 = s[0] + ((ulong)s[1] << 16) + ((ulong)s[2] << 32); c04 = 0; c08 = 0; c12 = 0;  return;
                     
-                    case 4:                                                             c04 = 0; c08 = 0; c12 = 0;  goto Pre0;
-                    case 5:     c04 = s[4];                                                      c08 = 0; c12 = 0;  goto Pre0;
-                    case 6:     c04 = s[4] + ((ulong)s[5] << 16);                                c08 = 0; c12 = 0;  goto Pre0;
-                    case 7:     c04 = s[4] + ((ulong)s[5] << 16) + ((ulong)s[6] << 32);          c08 = 0; c12 = 0;  goto Pre0;
+                    case 4:  lenCode = -5;                                                          c04 = 0; c08 = 0; c12 = 0;  goto Pre0;
+                    case 5:  lenCode = -6;  c04 = s[4];                                                      c08 = 0; c12 = 0;  goto Pre0;
+                    case 6:  lenCode = -7;  c04 = s[4] + ((ulong)s[5] << 16);                                c08 = 0; c12 = 0;  goto Pre0;
+                    case 7:  lenCode = -8;  c04 = s[4] + ((ulong)s[5] << 16) + ((ulong)s[6] << 32);          c08 = 0; c12 = 0;  goto Pre0;
 
-                    case 8:                                                                      c08 = 0; c12 = 0;  goto Pre4;
-                    case 9:     c08 = s[8];                                                               c12 = 0;  goto Pre4;
-                    case 10:    c08 = s[8] + ((ulong)s[9] << 16);                                         c12 = 0;  goto Pre4;
-                    case 11:    c08 = s[8] + ((ulong)s[9] << 16) + ((ulong)s[10] << 32);                  c12 = 0;  goto Pre4;
+                    case 8:  lenCode = -9;                                                                   c08 = 0; c12 = 0;  goto Pre4;
+                    case 9:  lenCode = -10; c08 = s[8];                                                               c12 = 0;  goto Pre4;
+                    case 10: lenCode = -11; c08 = s[8] + ((ulong)s[9] << 16);                                         c12 = 0;  goto Pre4;
+                    case 11: lenCode = -12; c08 = s[8] + ((ulong)s[9] << 16) + ((ulong)s[10] << 32);                  c12 = 0;  goto Pre4;
                         
-                    case 12:                                                                              c12 = 0;  goto Pre8;
-                    case 13:    c12 = s[12];                                                                        goto Pre8;
-                    case 14:    c12 = s[12]+ ((ulong)s[13] << 16);                                                  goto Pre8;
-                    case 15:    c12 = s[12]+ ((ulong)s[13] << 16)+ ((ulong)s[14] << 32);                            goto Pre8;
+                    case 12: lenCode = -13;                                                                           c12 = 0;  goto Pre8;
+                    case 13: lenCode = -14; c12 = s[12];                                                                        goto Pre8;
+                    case 14: lenCode = -15; c12 = s[12]+ ((ulong)s[13] << 16);                                                  goto Pre8;
+                    case 15: lenCode = -16; c12 = s[12]+ ((ulong)s[13] << 16)+ ((ulong)s[14] << 32);                            goto Pre8;
                         
-                    case 16:                                                                                        goto Pre12;
-                    default:    c00 = 0; c04 = 0; c08 = 0; c12 = 0;                                                 return;
+                    case 16: lenCode = -17;                                                                                     goto Pre12;
+                    default: lenCode = strLen; c00 = 0; c04 = 0; c08 = 0; c12 = 0;                                              return;
                 }
                 Pre12:  c12 = *(ulong*)(srcPtr + 12);
                 Pre8:   c08 = *(ulong*)(srcPtr +  8);
