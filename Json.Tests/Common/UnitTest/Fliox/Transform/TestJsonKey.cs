a85fbae041fefabe63cb0b227ec89716139e281a
Ullrich Praetz <ullrich@praetz.de>
2023-01-24 15:05:32 +0100

    Tests - add add JsonKeyTests_StringPerf()

--- Json.Tests/Common/UnitTest/Fliox/Transform/TestJsonKey.cs
+++ Json.Tests/Common/UnitTest/Fliox/Transform/TestJsonKey.cs
@@ -2,7 +2,10 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.Text;
+using Friflo.Json.Burst;
+using Friflo.Json.Burst.Utils;
 using Friflo.Json.Fliox;
 using NUnit.Framework;
 using static NUnit.Framework.Assert;
@@ -56,6 +59,22 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Transform
                 var str = new JsonKey ("UTF-8 string with more than 15 characters");
                 AreEqual("UTF-8 string with more than 15 characters", str.AsString());
             }
+        }
+        
+        /// <summary>
+        /// Compare performance of <see cref="ShortString"/> optimization using 15 / 16 characters 
+        /// </summary>
+        [Test]
+        public static void JsonKeyTests_StringPerf () {
+            int count       = 10; // 20_000_000;
+            var value       = new Bytes("0---------1----"); // 15 characters. 
+            var list        = new List<JsonKey>(count);
+            var valueParser = new ValueParser();
+            for (int n = 0; n < count; n++) {
+                var key = new JsonKey (ref value, ref valueParser, default);
+                list.Add(key);
+            }
+            AreEqual(count, list.Count);
         }
     }
 }
