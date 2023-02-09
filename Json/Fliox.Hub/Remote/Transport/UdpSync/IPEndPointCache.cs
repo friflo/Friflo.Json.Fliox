bc666770eb8a120bf756a26fc2275f0f02a98b50
Ullrich Praetz <ullrich@praetz.de>
2023-02-09 14:41:16 +0100

    Remote.Transport - Udp/Unity: add AddressKey for reliable SocketAddress hash code

--- Json/Fliox.Hub/Remote/Transport/UdpSync/IPEndPointCache.cs
+++ Json/Fliox.Hub/Remote/Transport/UdpSync/IPEndPointCache.cs
@@ -13,17 +13,18 @@ namespace Friflo.Json.Fliox.Hub.Remote.Transport.Udp
     /// </summary>
     public class IPEndPointCache : IPEndPoint
     {
-        private readonly    SocketAddress                               address;
-        private readonly    int                                         hashCode;
-        private readonly    Dictionary<SocketAddress, IPEndPointReuse>  cachedIPEndPoints = new Dictionary<SocketAddress, IPEndPointReuse>();
+        private readonly    SocketAddress                           address;
+        private readonly    int                                     hashCode;
+        private readonly    Dictionary<AddressKey, IPEndPointReuse> cachedIPEndPoints;
         
-        public  override    int                                         GetHashCode()   => hashCode;
+        public  override    int                                     GetHashCode()   => hashCode;
         /// Method is called from Socket.ReceiveFrom() in Unity 2021.3.9f1. It is not called in MS CLR
-        public  override    SocketAddress                               Serialize()     => address;
+        public  override    SocketAddress                           Serialize()     => address;
 
         private IPEndPointCache(IPAddress address, int port) : base(address, port) {
-            this.address    = base.Serialize();
-            hashCode        = base.GetHashCode();
+            this.address        = base.Serialize();
+            hashCode            = base.GetHashCode();
+            cachedIPEndPoints   = new Dictionary<AddressKey, IPEndPointReuse>(AddressKey.Equality);
         }
         
         public static IPEndPoint Create(IPAddress address, int port) {
@@ -36,16 +37,56 @@ namespace Friflo.Json.Fliox.Hub.Remote.Transport.Udp
 
         /// Method is called from Socket.ReceiveFrom() in Unity 2021.3.9f1. It is not called in MS CLR
         public override EndPoint Create(SocketAddress socketAddress) {
-            // var hc = socketAddress.GetHashCode(); // test if hash code is cached 
-            if (cachedIPEndPoints.TryGetValue(socketAddress, out var endPoint)) {
+            // Unity/Mono SocketAddress.GetHashCode() implementation is not reliable. Its hash key it mutable.
+            // => use immutable implementation
+            var key = new AddressKey(socketAddress);
+            
+            if (cachedIPEndPoints.TryGetValue(key, out var endPoint)) {
                 return endPoint;
             }
             var newIpEndPoint   = (IPEndPoint)base.Create(socketAddress);
             var newAddress      = newIpEndPoint.Address;
             var newPort         = newIpEndPoint.Port;
             var newEndpoint     = new IPEndPointReuse(newAddress, newPort);
-            cachedIPEndPoints[socketAddress] = newEndpoint;
+            cachedIPEndPoints[key] = newEndpoint;
             return newEndpoint;
+        }
+    }
+    
+    internal readonly struct AddressKey
+    {
+        internal readonly int       hashCode;
+        // IPv6AddressSize = 28  =>  4 long's provide 32 bytes 
+        internal readonly long      bytes0;     // byte[0 .. 7]
+        internal readonly long      bytes8;     // byte[8 ..15]
+        internal readonly long      bytes16;    // byte[16..23]
+        internal readonly long      bytes24;    // byte[24..31]
+        
+        internal static readonly  AddressKeyEqualityComparer Equality    = new AddressKeyEqualityComparer();
+
+        internal AddressKey(SocketAddress address) {
+            int size    = address.Size;
+            
+            long B(int index) => index < size ? address[index] : 0;
+            
+            bytes0  = B (0) | B (1) <<  8 | B (2) << 16 | B (3) << 24 | B (4) << 32 | B (5) << 40  | B (6) << 48 | B (7) << 56;
+            bytes8  = B (8) | B (9) <<  8 | B(10) << 16 | B(11) << 24 | B(12) << 32 | B(13) << 40  | B(14) << 48 | B(15) << 56;
+            bytes16 = B(16) | B(17) <<  8 | B(18) << 16 | B(19) << 24 | B(20) << 32 | B(21) << 40  | B(22) << 48 | B(23) << 56;
+            bytes24 = B(24) | B(25) <<  8 | B(26) << 16 | B(27) << 24 | B(28) << 32 | B(29) << 40  | B(30) << 48 | B(31) << 56;
+            
+            hashCode    = unchecked((int) bytes0) ^ (int)( bytes0 >> 32) ^
+                          unchecked((int) bytes8) ^ (int)( bytes8 >> 32) ^
+                          unchecked((int)bytes16) ^ (int)(bytes16 >> 32) ^
+                          unchecked((int)bytes24) ^ (int)(bytes24 >> 32);
+        }
+    }
+    
+    internal sealed class AddressKeyEqualityComparer : IEqualityComparer<AddressKey>
+    {
+        public int  GetHashCode(AddressKey value)       => value.hashCode;
+        
+        public bool Equals(AddressKey x, AddressKey y) {
+            return x.bytes0 == y.bytes0 && x.bytes8 == y.bytes8 && x.bytes16 == y.bytes16 && x.bytes24 == y.bytes24;
         }
     }
 }
