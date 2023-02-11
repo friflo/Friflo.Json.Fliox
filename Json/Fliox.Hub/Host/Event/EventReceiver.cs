8e31d2348a597918c6556b43a033bd67be50cf6b
Ullrich Praetz <ullrich@praetz.de>
2023-02-11 11:32:13 +0100

    Host.Event - add EventReceiver docs

--- Json/Fliox.Hub/Host/Event/EventReceiver.cs
+++ Json/Fliox.Hub/Host/Event/EventReceiver.cs
@@ -5,6 +5,12 @@ using Friflo.Json.Fliox.Hub.Protocol;
 
 namespace Friflo.Json.Fliox.Hub.Host.Event
 {
+    /// <summary>
+    /// An <see cref="EventReceiver"/> is used to send events to clients they have subscribed before.<br/>
+    /// A single <see cref="EventReceiver"/> can be shared by multiple clients to enable using a single
+    /// remote connection. To address a specific client in case of a shared remote connection the
+    /// <see cref="ClientEvent.dstClientId"/> is used.
+    /// </summary>
     public abstract class EventReceiver {
         protected internal abstract bool    IsOpen ();
         protected internal abstract bool    IsRemoteTarget ();
