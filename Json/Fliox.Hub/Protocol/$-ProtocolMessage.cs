e7c0f6db5b4c96b6d4ccad9cb7d1a27d083c42d3
Ullrich Praetz <ullrich@praetz.de>
2023-02-21 20:55:26 +0100

    update nuget dependencies

--- Json/Fliox.Hub/Protocol/$-ProtocolMessage.cs
+++ Json/Fliox.Hub/Protocol/$-ProtocolMessage.cs
@@ -2,7 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
-using System.ComponentModel.DataAnnotations;
 using Friflo.Json.Fliox.Hub.Host;
 using Friflo.Json.Fliox.Hub.Protocol.Tasks;
 using Friflo.Json.Fliox.Hub.Remote;
