7748bda8779946d96a93432d28b23e2c51bd98bc
Ullrich Praetz <ullrich@praetz.de>
2023-03-12 21:43:14 +0100

    add .NET Framework example

---
+++ Demos~/NetFramework/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Friflo.Json.Fliox;
+
+class TestClass
+{
+    public int val;
+}
+
+internal  static class  Program {
+    
+    public static void Main(string[] args) {
+        var test = new TestClass { val = 42 };
+        var json = JsonSerializer.Serialize(test);
+        Console.WriteLine($"JSON:  {json}");
+    }
+}
