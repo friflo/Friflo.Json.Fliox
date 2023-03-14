141a56f70be53c0dcddbb2352d2dec86d6e7fcde
Ullrich Praetz <ullrich@praetz.de>
2023-03-14 10:04:48 +0100

    Tests - add TestJsonValidator tests

--- Json.Tests/Common/UnitTest/Fliox/Schema/Validation/TestJsonValidator.cs
+++ Json.Tests/Common/UnitTest/Fliox/Schema/Validation/TestJsonValidator.cs
@@ -3,7 +3,9 @@
 
 #pragma warning disable CS0649
 
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Friflo.Json.Fliox;
 using Friflo.Json.Fliox.Schema;
 using NUnit.Framework;
 using static NUnit.Framework.Assert;
@@ -15,20 +17,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
 {
     public static class TestJsonValidator
     {
-        class OptionalFields
-        {
-            public  int?    age;
-            public  string  name;
-            public  int[]   intArray;
-        }
-        
-        class RequiredFields
-        {
-                       public   int     age;
-            [Required] public   string  name;
-            [Required] public   int[]   intArray;
-        }
-        
+        // --- primitive types
         [Test]
         public static void TestValidatePrimitives() {
             {
@@ -48,6 +37,33 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
                 IsFalse(success);
                 AreEqual("Incorrect type. was: 'xyz', expect: int32 (root), pos: 5", error);
             }
+        }
+        
+        // --- array types
+        [Test]
+        public static void TestValidateArray() {
+            {
+                var success = JsonValidator.Validate("[1]", typeof(int[]), out var error);
+                IsTrue(success);
+            } {
+                var success = JsonValidator.Validate("[1,2,3]", typeof(List<int>), out var error);
+                IsTrue(success);
+            }
+        }
+        
+        // --- optional class fields
+        class OptionalFields
+        {
+            public  int?    age;
+            public  string  name;
+            public  int[]   intArray;
+        }
+        
+        class RequiredFields
+        {
+            public   int     age;
+            [Required] public   string  name;
+            [Required] public   int[]   intArray;
         }
         
         [Test]
@@ -73,6 +89,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
             }
         }
         
+        // --- required class fields
         [Test]
         public static void TestValidateRequiredFields() {
             {
@@ -84,6 +101,34 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Schema.Validation
                 var success = JsonValidator.Validate(json, typeof(RequiredFields), out var  error);
                 IsFalse(success);
                 AreEqual("Missing required fields: [age, name, intArray] at RequiredFields > (root), pos: 2", error);
+            }
+        }
+        
+        // --- polymorph type
+        [Discriminator("vehicleType")]
+        [PolymorphType(typeof(Car),     "car")]
+        [PolymorphType(typeof(Bike),    "bike")]
+        class Vehicle { }
+        
+        class Car : Vehicle {
+            public int  seatCount;
+        }
+        
+        class Bike : Vehicle {
+            public bool hasLuggageRack;
+        }
+        
+        [Test]
+        public static void TestValidatePolymorphType() {
+            {
+                var json = "{\"vehicleType\":\"car\",\"seatCount\":1}";
+                var success = JsonValidator.Validate(json, typeof(Vehicle), out var  error);
+                IsTrue(success);
+            } {
+                var json = "{}";
+                var success = JsonValidator.Validate(json, typeof(Vehicle), out var  error);
+                IsFalse(success);
+                AreEqual("Expect discriminator as first member. was: ObjectEnd, expect: 'vehicleType' at Vehicle > (root), pos: 2", error);
             }
         }
     }
