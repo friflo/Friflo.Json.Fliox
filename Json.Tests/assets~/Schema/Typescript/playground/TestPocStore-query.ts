df9ccfb8ee2d0340f3fff0d1c92c4f18e10094c0
Ullrich Praetz <ullrich@praetz.de>
2023-03-30 14:31:57 +0200

    Tests - update query filter interface using entity types

--- Json.Tests/assets~/Schema/Typescript/playground/TestPocStore-query.ts
+++ Json.Tests/assets~/Schema/Typescript/playground/TestPocStore-query.ts
@@ -1,7 +1,7 @@
 import { Article, Order, TestType } from "../PocStore/UnitTest.Fliox.Client"
 
 type List<T> = {
-    Length : number,
+    readonly Length : number,
 
     Any  (filter: (o: T) => boolean) : boolean;
     All  (filter: (o: T) => boolean) : boolean;
@@ -13,6 +13,7 @@ type List<T> = {
     Count(filter: (o: T) => boolean) : number;
 }
 
+/*
 type UnArray<T> = T extends Array<infer U> ? U : T;
 
 type RecursiveObject<T> = T extends Array<T> ? never : UnArray<UnArray<T>>
@@ -21,13 +22,35 @@ export type Filter<TModel> = {
     [Key in keyof TModel]-? : TModel[Key] extends RecursiveObject<TModel[Key]>  // -? declare all fields required
         ? Filter<TModel[Key]>                                                   // convert nested fields recursive
         : List<NonNullable<UnArray<TModel[Key]>>>;                              // convert T[] -> List<T>, NonNullable<> remove null | undefined from array type
-};
+}; */
 
-function query<T>(filter: (o: Filter<T>) => boolean) { }
+// type Without<T, K> = Pick<T, Exclude<keyof T, K>>;
+// type Str = Exclude<string, "length"> | { Length: number };
+
+type FilterTypes<T> =
+    T extends string ? string & {
+        readonly Length : number,
+                 StartsWith  (str: string) : boolean,
+                 EndsWith    (str: string) : boolean,
+                 Contains    (str: string) : boolean,
+    } :
+    T extends Array<infer U>    ? List<U>
+    : Filter<T>
+
+type Filter<T> = {
+    readonly [K in keyof T]-? : FilterTypes<T[K]> & { }     // type NonNullable<T> = T & {};
+}
+
+function query<T>(filter: (o: FilterTypes<T>) => boolean) { }
 
 query(o => true);
 
-query<Article>(o => o.id == "ddd");
+query<Article>(o => o.id.Length == 3);
+query<Article>(o => o.id.StartsWith("abc"));
+query<Article>(o => o.id.EndsWith("abc"));
+query<Article>(o => o.id.Contains("abc"));
+
+query<Article>(o => o.id.at(1) == "ddd"); // expect error
 
 query<TestType>(o => o.derivedClass.amount == 1);
 
