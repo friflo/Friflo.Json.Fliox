e039fd278964e84c3b84f7796e97f2c4ca9ce95f
Ullrich Praetz <ullrich@praetz.de>
2023-03-30 15:05:51 +0200

    Tests - update query filter interface using entity types

--- Json.Tests/assets~/Schema/Typescript/playground/TestPocStore-query.ts
+++ Json.Tests/assets~/Schema/Typescript/playground/TestPocStore-query.ts
@@ -1,16 +1,24 @@
 import { Article, Order, TestType } from "../PocStore/UnitTest.Fliox.Client"
 
+function Abs    (value: number) : number { return 0; }
+function Log    (value: number) : number { return 0; }
+function Exp    (value: number) : number { return 0; }
+function Sqrt   (value: number) : number { return 0; }
+function Floor  (value: number) : number { return 0; }
+function Ceiling(value: number) : number { return 0; }
+
 type List<T> = {
     readonly Length : number,
 
-    Any  (filter: (o: T) => boolean) : boolean;
-    All  (filter: (o: T) => boolean) : boolean;
+    Any     (filter: (o: T) => boolean) : boolean;
+    All     (filter: (o: T) => boolean) : boolean;
 
-    Min  (filter: (o: T) => string | number | boolean) : number;
-    Max  (filter: (o: T) => string | number | boolean) : number;
-    Sum  (filter: (o: T) => string | number | boolean) : number;
+    Min     (filter: (o: T) => string | number | boolean) : number;
+    Max     (filter: (o: T) => string | number | boolean) : number;
+    Sum     (filter: (o: T) => string | number | boolean) : number;
+    Average (filter: (o: T) => string | number | boolean) : number;
 
-    Count(filter: (o: T) => boolean) : number;
+    Count   (filter: (o: T) => boolean) : number;
 }
 
 /*
@@ -25,16 +33,18 @@ export type Filter<TModel> = {
 }; */
 
 // type Without<T, K> = Pick<T, Exclude<keyof T, K>>;
-// type Str = Exclude<string, "length"> | { Length: number };
+type Str = Exclude<string, "length"> | { Length: number };
+
+type StringFilter = {
+    readonly Length : number,
+             StartsWith  (str: string) : boolean,
+             EndsWith    (str: string) : boolean,
+             Contains    (str: string) : boolean,
+}
 
 type FilterTypes<T> =
-    T extends string ? string & {
-        readonly Length : number,
-                 StartsWith  (str: string) : boolean,
-                 EndsWith    (str: string) : boolean,
-                 Contains    (str: string) : boolean,
-    } :
-    T extends Array<infer U>    ? List<U>
+    T extends string         ? StringFilter & string : // Exclude<StringFilter & string, "length"> :
+    T extends Array<infer U> ? List<U>
     : Filter<T>
 
 type Filter<T> = {
@@ -47,21 +57,26 @@ query(o => true);
 
 query<Article>(o => o.id.Length == 3);
 query<Article>(o => o.id.StartsWith("abc"));
-query<Article>(o => o.id.EndsWith("abc"));
-query<Article>(o => o.id.Contains("abc"));
+query<Article>(o => o.id.EndsWith  ("abc"));
+query<Article>(o => o.id.Contains  ("abc"));
 
-query<Article>(o => o.id.at(1) == "ddd"); // expect error
+query<Article>(o => o.id.length == 3); // expect error!
 
 query<TestType>(o => o.derivedClass.amount == 1);
 
 query<Order>(o => o.customer == "dddd");
+query<Order>(o => o.customer != "dddd");
+
 query<Order>(o => o.items.Length == 1);
 
 query<Order>(o => o.items.Any   (o => o.amount == 1));
 query<Order>(o => o.items.All   (o => o.amount == 1));
 
-query<Order>(o => o.items.Min   (o => o.amount) == 1);
-query<Order>(o => o.items.Max   (o => o.amount) == 2);
-query<Order>(o => o.items.Sum   (o => o.amount) == 6);
+query<Order>(o => o.items.Min    (o => o.amount) == 1);
+query<Order>(o => o.items.Max    (o => o.amount) == 2);
+query<Order>(o => o.items.Sum    (o => o.amount) == 6);
+query<Order>(o => o.items.Average(o => o.amount) == 3);
 
 query<Order>(o => o.items.Count (o => o.name == "Camera") == 2);
+
+query<Order>(o => Abs(o.items.Length) == 1);
