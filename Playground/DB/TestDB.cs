44ebf8842fca6439352ab5dfde002a3e12c65516
Ullrich Praetz <ullrich@praetz.de>
2023-04-11 12:01:41 +0200

    Playground - add read test

--- Playground/DB/TestDB.cs
+++ Playground/DB/TestDB.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Friflo.Playground.Client;
 using NUnit.Framework;
 using static NUnit.Framework.Assert;
+using static Friflo.Playground.DB.Env;
 
 namespace Friflo.Playground.DB
 {
@@ -12,23 +13,41 @@ namespace Friflo.Playground.DB
     {
         [OneTimeSetUp]
         public static void Init() {
-            Env.Setup();
+            Setup();
         }
         
         private static TestClient GetClient(string db) {
-            var hub    = Env.GetDatabaseHub(db);
+            var hub    = GetDatabaseHub(db);
             return new TestClient(hub);
         }
 
-        [TestCase(Env.File,   Category = Env.File)]
-        [TestCase(Env.Memory, Category = Env.Memory)]
-        [TestCase(Env.Cosmos, Category = Env.Cosmos)]
-        public static async Task TestQuery(string db) {
+        [TestCase(File, Category = File)] [TestCase(Memory, Category = Memory)] [TestCase(Cosmos, Category = Cosmos)]
+        public static async Task TestQueryAll(string db) {
             var store = GetClient(db);
             var query = store.articles.QueryAll();
             await store.SyncTasks();
-            AreEqual(1, query.Result.Count);
+            AreEqual(2, query.Result.Count);
         }
+        
+        [TestCase(File, Category = File)] [TestCase(Memory, Category = Memory)] [TestCase(Cosmos, Category = Cosmos)]
+        public static async Task TestRead_One(string db) {
+            var store = GetClient(db);
+            var find  = store.articles.Read().Find("a-1");
+            await store.SyncTasks();
+            NotNull(find.Result);
+        }
+        
+        [TestCase(File, Category = File)] [TestCase(Memory, Category = Memory)] [TestCase(Cosmos, Category = Cosmos)]
+        public static async Task TestRead_Many(string db) {
+            var store = GetClient(db);
+            var read  = store.articles.Read();
+            var find1  = read.Find("a-1");
+            var find2  = read.Find("a-2");
+            await store.SyncTasks();
+            NotNull(find1.Result);
+            NotNull(find2.Result);
+        }
+
     }
 }
 
