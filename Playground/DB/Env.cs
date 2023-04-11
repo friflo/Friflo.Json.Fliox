26f2e6f35818abb1436f64fc440a696406e44aa5
Ullrich Praetz <ullrich@praetz.de>
2023-04-11 12:16:40 +0200

    Playground - create test databases on demand

--- Playground/DB/Env.cs
+++ Playground/DB/Env.cs
@@ -14,14 +14,14 @@ namespace Friflo.Playground.DB
         public const string  Memory = "memory";
         public const string  Cosmos = "cosmos";
             
-        private static  FlioxHub _memoryHub;
-        private static  FlioxHub _fileHub;
-        private static  FlioxHub _cosmosHub;
+        private static  FlioxHub        _memoryHub;
+        private static  FlioxHub        _fileHub;
+        private static  FlioxHub        _cosmosHub;
         
         private static readonly string TestDbFolder = CommonUtils.GetBasePath() + "assets~/DB/test_db";
             
         internal static EntityDatabase CreateMemoryDatabase(EntityDatabase sourceDB) {
-            var memoryDB = new MemoryDatabase("memory_db") { Schema = sourceDB.Schema};
+            var memoryDB = new MemoryDatabase("memory_db") { Schema = sourceDB.Schema };
             memoryDB.SeedDatabase(sourceDB).Wait();
             return memoryDB;
         }
@@ -29,29 +29,27 @@ namespace Friflo.Playground.DB
         internal static EntityDatabase CreateCosmosDatabase(EntityDatabase sourceDB) {
             var client              = TestCosmosDB.CreateCosmosClient();
             var createDatabase      = client.CreateDatabaseIfNotExistsAsync("cosmos_db").Result;
-            var cosmosDatabase      = new CosmosDatabase("cosmos_db", createDatabase) { Throughput = 400 };
-            cosmosDatabase.Schema   = sourceDB.Schema;
+            var cosmosDatabase      = new CosmosDatabase("cosmos_db", createDatabase)
+                { Throughput = 400, Schema = sourceDB.Schema };
             cosmosDatabase.SeedDatabase(sourceDB).Wait();
             return cosmosDatabase;
         }
                 
         internal static EntityDatabase CreateFileDatabase(DatabaseSchema schema) {
-            return new FileDatabase("file_db", TestDbFolder) { Schema = schema};;
+            return new FileDatabase("file_db", TestDbFolder) { Schema = schema };
         }
         
         internal static void Setup() {
-            var typeSchema          = NativeTypeSchema.Create(typeof(TestClient)); // optional - create TypeSchema from Type 
-            var databaseSchema      = new DatabaseSchema(typeSchema);
-            _fileHub    = new FlioxHub(CreateFileDatabase(databaseSchema));
-            _memoryHub  = new FlioxHub(CreateMemoryDatabase(_fileHub.database));
-            _cosmosHub  = new FlioxHub(CreateCosmosDatabase(_fileHub.database));
+            var typeSchema      = NativeTypeSchema.Create(typeof(TestClient)); // optional - create TypeSchema from Type 
+            var databaseSchema  = new DatabaseSchema(typeSchema);
+            _fileHub            = new FlioxHub(CreateFileDatabase(databaseSchema));
         }
 
         internal static FlioxHub GetDatabaseHub(string db) {
             switch (db) {
-                case Memory:    return _memoryHub;
+                case Memory:    return _memoryHub   ??= new FlioxHub(CreateMemoryDatabase(_fileHub.database));
                 case File:      return _fileHub;
-                case Cosmos:    return _cosmosHub;
+                case Cosmos:    return _cosmosHub   ??= new FlioxHub(CreateCosmosDatabase(_fileHub.database));
             }
             throw new InvalidOperationException($"invalid database Env: {db}");
         }
