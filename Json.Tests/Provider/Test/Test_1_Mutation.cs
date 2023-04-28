c9734a8984bb89c4b4bfaa2adfcd55a8c70f5873
Ullrich Praetz <ullrich@praetz.de>
2023-04-28 10:59:16 +0200

    Tests > Provider: enable TestMutation > delete by id

--- Json.Tests/Provider/Test/Test_1_Mutation.cs
+++ Json.Tests/Provider/Test/Test_1_Mutation.cs
@@ -87,8 +87,7 @@ namespace Friflo.Json.Tests.Provider.Test
         }
         
         // --- delete by id
-        // todo
-        // [TestCase(memory_db, Category = memory_db)] [TestCase(test_db, Category = test_db)] [TestCase(sqlite_db, Category = sqlite_db)]
+        [TestCase(memory_db, Category = memory_db)] [TestCase(test_db, Category = test_db)] [TestCase(sqlite_db, Category = sqlite_db)]
         public static async Task TestMutation_5_DeleteById(string db) {
             var client      = await GetClient(db);
             var deleteAll   = client.testMutate.DeleteAll();
