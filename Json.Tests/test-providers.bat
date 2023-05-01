87fc5f02df537d2e52210f345cc114e50a3afcdb
Ullrich Praetz <ullrich@praetz.de>
2023-05-01 20:36:49 +0200

    Tests - add batch file for all db db providers

---
+++ Json.Tests/test-providers.bat
@@ -0,0 +1,9 @@
+
+dotnet test -c Release --consoleloggerparameters:ErrorsOnly --filter TestCategory=test_db --no-build --environment TEST_DB_PROVIDER=file
+dotnet test -c Release --consoleloggerparameters:ErrorsOnly --filter TestCategory=test_db --no-build --environment TEST_DB_PROVIDER=sqlite
+dotnet test -c Release --consoleloggerparameters:ErrorsOnly --filter TestCategory=test_db --no-build --environment TEST_DB_PROVIDER=postgres
+dotnet test -c Release --consoleloggerparameters:ErrorsOnly --filter TestCategory=test_db --no-build --environment TEST_DB_PROVIDER=mysql
+dotnet test -c Release --consoleloggerparameters:ErrorsOnly --filter TestCategory=test_db --no-build --environment TEST_DB_PROVIDER=mariadb
+dotnet test -c Release --consoleloggerparameters:ErrorsOnly --filter TestCategory=test_db --no-build --environment TEST_DB_PROVIDER=cosmos
+
+pause
