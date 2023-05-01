8336f5de56448930fc684b63f47f6ada912c01d8
Ullrich Praetz <ullrich@praetz.de>
2023-05-01 20:52:45 +0200

    Test - add dotnet build to provider tests

--- Json.Tests/test-providers.bat
+++ Json.Tests/test-providers.bat
@@ -1,4 +1,6 @@
 
+dotnet build -c Release --verbosity:quiet
+
 dotnet test -c Release --consoleloggerparameters:ErrorsOnly --filter TestCategory=test_db --no-build --environment TEST_DB_PROVIDER=file
 dotnet test -c Release --consoleloggerparameters:ErrorsOnly --filter TestCategory=test_db --no-build --environment TEST_DB_PROVIDER=sqlite
 dotnet test -c Release --consoleloggerparameters:ErrorsOnly --filter TestCategory=test_db --no-build --environment TEST_DB_PROVIDER=postgres
