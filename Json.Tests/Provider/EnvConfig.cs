107d227d0dd5df898bada129ab09e82e40eee03c
Ullrich Praetz <ullrich@praetz.de>
2023-05-06 15:03:39 +0200

    Tests - EnvConfig cleanup

--- Json.Tests/Provider/EnvConfig.cs
+++ Json.Tests/Provider/EnvConfig.cs
@@ -9,16 +9,17 @@ namespace Friflo.Json.Tests.Provider
 {
     public static class EnvConfig
     {
-        private static IConfiguration InitConfiguration() {
+        private static readonly IConfiguration Configuration;
+        
+        static EnvConfig() {
             var basePath        = CommonUtils.GetBasePath();
             var appSettings     = basePath + "appsettings.test.json";
             var privateSettings = basePath + "appsettings.private.json";
-            return new ConfigurationBuilder().AddJsonFile(appSettings).AddJsonFile(privateSettings).Build();
+            Configuration = new ConfigurationBuilder().AddJsonFile(appSettings).AddJsonFile(privateSettings).Build();
         }
         
         public static string GetConnectionString(string provider) {
-            var config              = InitConfiguration();
-            string connectionString = config[provider];
+            string connectionString = Configuration[provider];
             if (connectionString == null) {
                 throw new ArgumentException($"provider not found in appsettings. provider: {provider}");
             }
@@ -27,9 +28,8 @@ namespace Friflo.Json.Tests.Provider
         
         // --- CosmosDB
         public static CosmosClient CreateCosmosClient() {
-            var config      = InitConfiguration();
-            var endpointUri = config["EndPointUri"];    // The Azure Cosmos DB endpoint for running this sample.
-            var primaryKey  = config["PrimaryKey"];     // The primary key for the Azure Cosmos account.
+            var endpointUri = Configuration["EndPointUri"];    // The Azure Cosmos DB endpoint for running this sample.
+            var primaryKey  = Configuration["PrimaryKey"];     // The primary key for the Azure Cosmos account.
             var options     = new CosmosClientOptions { ApplicationName = "Friflo.Playground" };
             return new CosmosClient(endpointUri, primaryKey, options);
         }
