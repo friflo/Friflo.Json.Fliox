3a0ec9998aa0801ac9948fe3f432e92832251122
Ullrich Praetz <ullrich@praetz.de>
2023-05-27 11:08:32 +0200

    cleanup

--- Json/Fliox.Hub/Host/Base/Result.cs
+++ Json/Fliox.Hub/Host/Base/Result.cs
@@ -29,7 +29,7 @@ namespace Friflo.Json.Fliox.Hub.Host
 
         internal            bool        Success => error.message == null;      
         
-        public Result (T value) {
+        internal Result (T value) {
             this.value  = value;
             this.error  = default;
         }
