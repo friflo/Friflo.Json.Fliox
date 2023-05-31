138cd4d41359fdeb519cb6ff91445c72f22dd37a
Ullrich Praetz <ullrich@praetz.de>
2023-05-31 17:26:44 +0200

    Host - add Param<> docs

--- Json/Fliox.Hub/Host/Message/Param.cs
+++ Json/Fliox.Hub/Host/Message/Param.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System;
 using static System.Diagnostics.DebuggerBrowsableState;
 using Browse = System.Diagnostics.DebuggerBrowsableAttribute;
 
@@ -9,7 +10,14 @@ namespace Friflo.Json.Fliox.Hub.Host
 {
     public readonly struct Param<TParam>
     {
+        /// <summary>
+        /// Returns the <see cref="Param{TParam}"/> <see cref="RawValue"/> of a message / command without validation.<br/>
+        /// </summary>
                         public              JsonValue   RawValue    => rawValue;
+        /// <summary>
+        /// Returns the <see cref="Param{TParam}"/> <see cref="Value"/> of a message / command without validation.<br/>
+        /// Throws an exception in case deserialization fails.
+        /// </summary>
                         public              TParam      Value       => GetValue();
                         public  override    string      ToString()  => rawValue.AsString();
                         
@@ -25,11 +33,17 @@ namespace Friflo.Json.Fliox.Hub.Host
         private TParam GetValue() {
             using (var pooled = syncContext.pool.ObjectMapper.Get()) {
                 var reader = pooled.instance.reader;
-                return reader.Read<TParam>(rawValue);
+                var value = reader.Read<TParam>(rawValue);
+                if (reader.Error.ErrSet) {
+                    throw new InvalidOperationException(reader.Error.msg.ToString());
+                }
+                return value;
             }
         }
 
-        /// <summary>Return the command <paramref name="value"/></summary> without validation 
+        /// <summary>
+        /// Return the command / message <see cref="Param{TParam}"/> <paramref name="value"/> without validation
+        /// </summary> 
         /// <param name="value">the param value if conversion successful</param>
         /// <param name="error">contains the error message if conversion failed</param>
         /// <returns> true if successful; false otherwise </returns>
@@ -37,7 +51,10 @@ namespace Friflo.Json.Fliox.Hub.Host
             return Get<TParam>(out value, out error);
         }
         
-        /// <summary>Return the command <paramref name="value"/> as the given type <typeparamref name="T"/> without validation</summary>
+        /// <summary>
+        /// Return the command / message <see cref="Param{TParam}"/> <paramref name="value"/>
+        /// as the given type <typeparamref name="T"/> without validation
+        /// </summary>
         /// <param name="value">the param value if conversion successful</param>
         /// <param name="error">contains the error message if conversion failed</param>
         /// <returns> true if successful; false otherwise </returns>
@@ -54,7 +71,9 @@ namespace Friflo.Json.Fliox.Hub.Host
             }
         }
 
-        /// <summary>Return the validated command <paramref name="value"/></summary>
+        /// <summary>
+        /// Return the validated command / message <see cref="Param{TParam}"/> <paramref name="value"/>
+        /// </summary>
         /// <param name="value">the param value if conversion successful</param>
         /// <param name="error">contains the error message if conversion failed</param>
         /// <returns> true if successful; false otherwise </returns>
@@ -62,9 +81,12 @@ namespace Friflo.Json.Fliox.Hub.Host
             return GetValidate<TParam>(out value, out error);
         }
         
-        /// <summary>Return the validated command <paramref name="value"/> as the given type <typeparamref name="T"/></summary>
-        /// <param name="value">the param value if conversion successful</param>
-        /// <param name="error">contains the error message if conversion failed</param>
+        /// <summary>
+        /// Return the validated command / message <see cref="Param{TParam}"/> <paramref name="value"/>
+        /// as the given type <typeparamref name="T"/>
+        /// </summary>
+        /// <param name="value">the param value if deserialization is successful</param>
+        /// <param name="error">contains the error message if deserialization fails</param>
         /// <returns> true if successful; false otherwise </returns>
         public bool GetValidate<T>(out T value, out string error) {
             if (!Validate<T>(out error)) {
