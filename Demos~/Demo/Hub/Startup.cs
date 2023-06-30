151f7954c3da52822798ccb00414a475e02b4599
Ullrich Praetz <ullrich@praetz.de>
2023-06-30 14:06:34 +0200

    DemoHub - configure Kestrel http port by code - for Docker

--- Demos~/Demo/Hub/Startup.cs
+++ Demos~/Demo/Hub/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Friflo.Json.Fliox.Hub.AspNetCore;
 using Friflo.Json.Fliox.Hub.Remote;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 
 namespace DemoHub;
 
@@ -10,7 +11,7 @@ public static class Startup
     public static void Run(string[] args, HttpHost httpHost)
     {
         var builder     = WebApplication.CreateBuilder(args);
-        // builder.WebHost.ConfigureKestrel(options => options.ListenAnyIP(5000));
+        builder.WebHost.ConfigureKestrel(options => options.ListenAnyIP(5000));
         var app         = builder.Build();
 
         httpHost.UseAspNetCoreLogger(app.Services);
