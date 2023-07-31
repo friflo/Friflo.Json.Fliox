574a319f92ff32e679235d02fba56b50751462cf
Ullrich Praetz <ullrich@praetz.de>
2023-07-31 08:41:29 +0200

    Tests - set upper bounds for memory consumption for ReadRange() - will fail on CI for now

--- Json.Tests/Common/UnitTest/Fliox/Client/TestUtils.cs
+++ Json.Tests/Common/UnitTest/Fliox/Client/TestUtils.cs
@@ -235,7 +235,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Client
                 await store.SyncTasks();
             }
             var diff = Mem.GetAllocationDiff(start);
-            var expected = IsDebug() ? new LongRange(17368, 27500) : new LongRange(14808, 25000); // Test Debug & Release  SYNC_READ : adjust max
+            var expected = IsDebug() ? new LongRange(17368, 17368) : new LongRange(14808, 14808);
             Mem.InRange(expected, diff);
         }
         
