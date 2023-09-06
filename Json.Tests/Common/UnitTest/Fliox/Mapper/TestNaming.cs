889618bb05dc96a6b3cf468ec0b9ed9869b2432f
Ullrich Praetz <ullrich@praetz.de>
2023-09-06 19:36:08 +0200

    Test - add test for camelCase / PascalCase naming

--- Json.Tests/Common/UnitTest/Fliox/Mapper/TestNaming.cs
+++ Json.Tests/Common/UnitTest/Fliox/Mapper/TestNaming.cs
@@ -73,7 +73,51 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Mapper
                 AreEqual(expect, result);
             }
         }
-        
 
+        [JsonNaming(JsonNamingType.CamelCase)]
+        class CamelCaseClass {
+            public int      lowerProperty { get; set; }
+            public int      UpperProperty { get; set; }
+            
+            public int      lowerField;
+            public int      UpperField;
+        }
+        
+        [Test] public void TestCamelCase()
+        {
+            var camelCase = new CamelCaseClass { lowerProperty = 1, UpperProperty = 2, lowerField = 3, UpperField = 4,  };
+            var json = JsonSerializer.Serialize(camelCase, new SerializerOptions { Pretty = false });
+            AreEqual("{\"lowerProperty\":1,\"upperProperty\":2,\"lowerField\":3,\"upperField\":4}", json);
+            
+            var result = JsonSerializer.Deserialize<CamelCaseClass>(json);
+            AreEqual(1, result.lowerProperty);
+            AreEqual(2, result.UpperProperty);
+            AreEqual(3, result.lowerField);
+            AreEqual(4, result.UpperField);
+        }
+        
+        [JsonNaming(JsonNamingType.PascalCase)]
+        class PascalCaseClass {
+            public int      lowerProperty { get; set; }
+            public int      UpperProperty { get; set; }
+            
+            public int      lowerField;
+            public int      UpperField;
+        }
+
+        
+        [Test] public void TestPascalCase()
+        {
+            var camelCase = new PascalCaseClass { lowerProperty = 1, UpperProperty = 2, lowerField = 3, UpperField = 4,  };
+            var json = JsonSerializer.Serialize(camelCase, new SerializerOptions { Pretty = false });
+            AreEqual("{\"LowerProperty\":1,\"UpperProperty\":2,\"LowerField\":3,\"UpperField\":4}", json);
+            
+            var result = JsonSerializer.Deserialize<PascalCaseClass>(json);
+            AreEqual(1, result.lowerProperty);
+            AreEqual(2, result.UpperProperty);
+            AreEqual(3, result.lowerField);
+            AreEqual(4, result.UpperField);
+        }
+        
     }
 }
