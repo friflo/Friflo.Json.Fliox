91ecda23e63710aa5de1955c3edd8744619ca4ab
Ullrich Praetz <ullrich@praetz.de>
2023-09-13 08:38:06 +0200

    MsgPack - WIP: made MsgReader.CreateErrorMessage() pure functional

--- Json/Fliox/MsgPack/Reader.error.cs
+++ Json/Fliox/MsgPack/Reader.error.cs
@@ -11,63 +11,51 @@ namespace Friflo.Json.Fliox.MsgPack
             if (state != MsgReaderState.Ok) {
                 return;
             }
-            var sb  = StopReader(error, type, cur);
-            CreateErrorMessage(sb);
-            SetMessage(sb, cur);
+            StopReader(error, type, cur);
+            this.error = CreateErrorMessage();
         }
         
         private void SetRangeError(MsgFormat type, int cur) {
             if (state != MsgReaderState.Ok) {
                 return;
             }
-            var sb  = StopReader(MsgReaderState.RangeError, type, cur);
-            CreateErrorMessage(sb);
-            SetMessage(sb, cur);
+            StopReader(MsgReaderState.RangeError, type, cur);
+            error = CreateErrorMessage();
         }
         
         private void SetEofError(int cur) {
             if (state != MsgReaderState.Ok) {
                 return;
             }
-            var sb  = StopReader(MsgReaderState.UnexpectedEof, MsgFormat.root, cur);
-            var msg = MsgPackUtils.Error(MsgReaderState.UnexpectedEof);
-            sb.Append($"MessagePack error - {msg}.");
-            SetMessage(sb, cur);
+            StopReader(MsgReaderState.UnexpectedEof, MsgFormat.root, cur);
+            error = CreateErrorMessage();
         }
         
         private void SetEofErrorType(MsgFormat type, int cur) {
             if (state != MsgReaderState.Ok) {
                 return;
             }
-            var sb  = StopReader(MsgReaderState.UnexpectedEof, type, cur);
-            CreateErrorMessage(sb);
-            SetMessage(sb, cur);
+            StopReader(MsgReaderState.UnexpectedEof, type, cur);
+            error = CreateErrorMessage();
         }
         
         // ----------------------------------------- utils -----------------------------------------
-        private void CreateErrorMessage(StringBuilder sb) {
+        private string CreateErrorMessage()
+        {
+            var sb = new StringBuilder();
             sb.Append("MessagePack error - ");
             sb.Append(MsgPackUtils.Error(state));
             sb.Append('.');
-            sb.Append(" was: ");
-            if (state == MsgReaderState.RangeError) {
-                MsgPackUtils.AppendValue(sb, data, errorType, errorPos);
-                sb.Append(' ');
+            if (errorType != MsgFormat.root) {
+                sb.Append(" was: ");
+                if (state == MsgReaderState.RangeError) {
+                    MsgPackUtils.AppendValue(sb, data, errorType, errorPos);
+                    sb.Append(' ');
+                }
+                sb.Append($"{MsgPackUtils.Name(errorType)}(0x{(int)errorType:X})");
             }
-            sb.Append($"{MsgPackUtils.Name(errorType)}(0x{(int)errorType:X})");
-        }
-        
-        private StringBuilder StopReader(MsgReaderState state, MsgFormat type, int cur) {
-            this.state  = state;
-            pos         = MsgError;
-            errorType   = type;
-            errorPos    = cur;
-            return new StringBuilder();
-        }
-        
-        private void SetMessage(StringBuilder sb, int cur) {
             sb.Append(" pos: ");
-            sb.Append(cur);
+            sb.Append(errorPos);
             if (keyName == null) {
                 sb.Append(" (root)");
             } else {
@@ -76,7 +64,14 @@ namespace Friflo.Json.Fliox.MsgPack
                 sb.Append(key);
                 sb.Append('\'');
             }
-            error   = sb.ToString();
+            return sb.ToString();
+        }
+        
+        private void StopReader(MsgReaderState state, MsgFormat type, int cur) {
+            this.state  = state;
+            pos         = MsgError;
+            errorType   = type;
+            errorPos    = cur;
         }
     }
 }
