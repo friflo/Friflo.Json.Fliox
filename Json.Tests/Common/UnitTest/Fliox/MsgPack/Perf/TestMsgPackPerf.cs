36edef5e77f201be031306df8922a9a4c2036599
Ullrich Praetz <ullrich@praetz.de>
2023-09-14 22:00:03 +0200

    MsgPack - WIP: moved perf tests to separate folder

---
+++ Json.Tests/Common/UnitTest/Fliox/MsgPack/Perf/TestMsgPackPerf.cs
@@ -0,0 +1,53 @@
+﻿// ReSharper disable InconsistentNaming
+
+using System;
+using Friflo.Json.Fliox.MsgPack;
+using Gen.Friflo.Json.Tests.Common.UnitTest.Fliox.MsgPack;
+using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+namespace Friflo.Json.Tests.Common.UnitTest.Fliox.MsgPack.Perf
+{
+    public static class TestMsgRead
+    {
+        private const long Count = 1; // 100_000_000L;
+
+        [Test]
+        public static void Perf_MapperRead()
+        {
+            var sample = new Sample { x = int.MaxValue };
+            var data = Sample.Test;
+            MsgPackMapper.DeserializeTo(data, ref sample);
+            AreEqual(int.MaxValue, sample.x);
+            
+            long start = GC.GetAllocatedBytesForCurrentThread();
+            for (long n = 0; n < Count; n++) {
+                MsgPackMapper.DeserializeTo(data, ref sample);
+            }
+            long diff = GC.GetAllocatedBytesForCurrentThread() - start;
+            AreEqual(0, diff);
+        }
+        
+        [Test]
+        public static void Perf_Read_2()
+        {
+            Perf_Read();
+        }
+        
+        [Test]
+        public static void Perf_Read()
+        {
+            var data = Sample.Test;
+            var reader = new MsgReader();
+            var sample = new Sample();
+            reader.Init(data);
+            reader.ReadMsg(ref sample);
+            AreEqual(int.MaxValue, sample.x);
+            
+            for (long n = 0; n < Count; n++) {
+                reader.Init(data);
+                reader.ReadMsg(ref sample);
+            }
+        }
+    }
+}
--- Json.Tests/Common/UnitTest/Fliox/MsgPack/Perf/TestMsgPackWrite.cs
+++ Json.Tests/Common/UnitTest/Fliox/MsgPack/Perf/TestMsgPackWrite.cs
@@ -1,5 +1,5 @@
-﻿// ReSharper disable InconsistentNaming
-
+﻿
+// ReSharper disable InconsistentNaming
 using System;
 using Friflo.Json.Fliox.MsgPack;
 using Gen.Friflo.Json.Tests.Common.UnitTest.Fliox.MsgPack;
@@ -7,9 +7,10 @@ using NUnit.Framework;
 using static NUnit.Framework.Assert;
 using static Friflo.Json.Fliox.MsgPack.MsgPackUtils;
 
-namespace Friflo.Json.Tests.Common.UnitTest.Fliox.MsgPack.Test
+namespace Friflo.Json.Tests.Common.UnitTest.Fliox.MsgPack.Perf
 {
-    public static class TestMsgPackPerf {
+    public static class TestMsgWrite
+    {
         private const long Count = 1; // 100_000_000L;
 
         [Test]
@@ -59,44 +60,6 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.MsgPack.Test
             }
             long diff = GC.GetAllocatedBytesForCurrentThread() - start;
             AreEqual(0, diff);
-        }
-        
-        [Test]
-        public static void Perf_MapperRead()
-        {
-            var sample = new Sample { x = int.MaxValue };
-            var data = Sample.Test;
-            MsgPackMapper.DeserializeTo(data, ref sample);
-            AreEqual(int.MaxValue, sample.x);
-            
-            long start = GC.GetAllocatedBytesForCurrentThread();
-            for (long n = 0; n < Count; n++) {
-                MsgPackMapper.DeserializeTo(data, ref sample);
-            }
-            long diff = GC.GetAllocatedBytesForCurrentThread() - start;
-            AreEqual(0, diff);
-        }
-        
-        [Test]
-        public static void Perf_Read_2()
-        {
-            Perf_Read();
-        }
-        
-        [Test]
-        public static void Perf_Read()
-        {
-            var data = Sample.Test;
-            var reader = new MsgReader();
-            var sample = new Sample();
-            reader.Init(data);
-            reader.ReadMsg(ref sample);
-            AreEqual(int.MaxValue, sample.x);
-            
-            for (long n = 0; n < Count; n++) {
-                reader.Init(data);
-                reader.ReadMsg(ref sample);
-            }
         }
     }
 }
