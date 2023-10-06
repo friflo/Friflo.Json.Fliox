261d2ad3d3773fa609d42ed0d992abafc2d99762
Ullrich Praetz <ullrich@praetz.de>
2023-10-06 16:59:30 +0200

    Engine - removed obsolete struct DecomposedGuid

--- Engine/src/ECS/Struct/Types.cs
+++ Engine/src/ECS/Struct/Types.cs
@@ -1,20 +1,8 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System;
-using System.Runtime.InteropServices;
-
 // ReSharper disable once CheckNamespace
 namespace Friflo.Fliox.Engine.ECS;
-
-[StructLayout(LayoutKind.Explicit)]
-internal struct DecomposedGuid
-{
-    [FieldOffset(00)] public Guid Value;
-    [FieldOffset(00)] public long Hi;
-    [FieldOffset(08)] public long Lo;
-    public DecomposedGuid(Guid value) : this() => Value = value;
-}
 
 internal readonly struct ArchetypeInfo
 {
