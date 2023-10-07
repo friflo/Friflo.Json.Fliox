f64d991c3e594dcbc3ec3c88d2056a618c41d346
Ullrich Praetz <ullrich@praetz.de>
2023-10-07 11:04:57 +0200

    Engine - cleanup Enums.cs

--- Engine/src/ECS/Base/Enums.cs
+++ Engine/src/ECS/Base/Enums.cs
@@ -13,26 +13,26 @@ namespace Friflo.Fliox.Engine.ECS;
 /// <summary>Describe the ownership state of a <see cref="GameEntity"/></summary>
 public enum StoreOwnership
 {
-    /// <summary>The entity is owned by an <see cref="EntityStore"/></summary>
-    /// <remarks>
-    /// Entities created with <see cref="EntityStore.CreateEntity()"/> are automatically <see cref="attached"/> to its <see cref="EntityStore"/><br/>
-    /// </remarks>
-    attached = 1,
     /// <summary>The entity is not owned by an <see cref="EntityStore"/></summary>
     /// <remarks>
     /// When calling <see cref="GameEntity.DeleteEntity"/> its state changes to <see cref="detached"/>.<br/>
     /// </remarks>
-    detached = 0,
+    detached    = 0,
+    /// <summary>The entity is owned by an <see cref="EntityStore"/></summary>
+    /// <remarks>
+    /// Entities created with <see cref="EntityStore.CreateEntity()"/> are automatically <see cref="attached"/> to its <see cref="EntityStore"/><br/>
+    /// </remarks>
+    attached    = 1,
 }
 
 /// <summary>Describe the membership of a <see cref="GameEntity"/> to the tree of an <see cref="EntityStore"/></summary>
 /// <remarks>Requirement: The entity must be <see cref="attached"/> to an <see cref="EntityStore"/></remarks>
 public enum TreeMembership
 {
-    /// <summary>The entity is member of the <see cref="EntityStore"/> tree</summary>
-    treeNode = 1,
     /// <summary>The entity is not member of the <see cref="EntityStore"/> tree</summary>
-    floating = 0,
+    floating    = 0,
+    /// <summary>The entity is member of the <see cref="EntityStore"/> tree</summary>
+    treeNode    = 1,
 }
 
 [Flags]
@@ -65,12 +65,12 @@ public enum PidType
     /// - When add entities in a database id clashes with entities added by other users are very likely.<br/>
     /// - High probability of merge conflicts caused by id clashes by adding the same entity ids by multiple users. 
     /// </remarks>
-    UsePidAsId,
+    UsePidAsId  = 0,
     /// <summary>
     /// Map random <see cref="EntityNode.Pid"/>'s to internal used <see cref="EntityNode.Id"/>'s.<br/>
     /// This method is intended to be used to store entities of a scene in JSON files or in a database. 
     /// </summary>
-    RandomPids
+    RandomPids  = 1,
 }
 
 /// <summary>
@@ -79,7 +79,7 @@ public enum PidType
 public enum ComponentKind
 {
     /// <summary>Declare a <see cref="ComponentType"/> is a <b>struct</b> component</summary>
-    Struct  = 0,
+    Struct      = 0,
     /// <summary>Declare a <see cref="ComponentType"/> is a <b>class</b> component</summary>
-    Class   = 1,
+    Class       = 1,
 }
