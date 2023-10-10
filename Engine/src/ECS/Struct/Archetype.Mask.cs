3c1c4e7cf48abd302ef82cf63dfbb66f49dbdf99
Ullrich Praetz <ullrich@praetz.de>
2023-10-10 08:17:48 +0200

    Engine - implement IEnumerable<ComponentType> by ArchetypeMask

--- Engine/src/ECS/Struct/Archetype.Mask.cs
+++ Engine/src/ECS/Struct/Archetype.Mask.cs
@@ -1,46 +1,161 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 
 // ReSharper disable ConvertToAutoProperty
 // ReSharper disable once CheckNamespace
 namespace Friflo.Fliox.Engine.ECS;
 
-public readonly struct ArchetypeMask
+public struct ArchetypeMask : IEnumerable<ComponentType>
 {
-    private readonly    BitSet  mask;
+    internal             BitSet  bitSet;
     
-    public  override    string  ToString() => GetString();
+   
+    public ArchetypeMaskEnumerator  GetEnumerator()                             => new ArchetypeMaskEnumerator (this);
+    // --- IEnumerable
+    IEnumerator                     IEnumerable.GetEnumerator()                 => new ArchetypeMaskEnumerator (this);
+    // --- IEnumerable<>
+    IEnumerator<ComponentType>      IEnumerable<ComponentType>.GetEnumerator()  => new ArchetypeMaskEnumerator (this);
+
+    public   override               string    ToString() => GetString();
+    
+    private ArchetypeMask(in BitSet bitSet) {
+        this.bitSet = bitSet;
+    }
     
     internal ArchetypeMask(StructHeap[] heaps) {
         foreach (var heap in heaps) {
-            mask.SetBit(heap.structIndex);
+            bitSet.SetBit(heap.structIndex);
         }
     }
     
     internal ArchetypeMask(Signature signature) {
         foreach (var type in signature.types) {
-            mask.SetBit(type.structIndex);
+            bitSet.SetBit(type.structIndex);
         }
     }
-
+    
+    // ----------------------------------------- read mask -----------------------------------------
     internal bool Has(in ArchetypeMask other) {
-        return (mask.value & other.mask.value) == mask.value;
+        return (bitSet.value & other.bitSet.value) == bitSet.value;
+    }
+    
+    public  bool    Has<T> ()
+        where T : struct, IStructComponent
+    {
+        return bitSet.Has(StructHeap<T>.StructIndex);
+    }
+    
+    public  bool    Has<T1, T2> ()
+        where T1 : struct, IStructComponent
+        where T2 : struct, IStructComponent
+    {
+        return bitSet.Has(StructHeap<T1>.StructIndex) &&
+               bitSet.Has(StructHeap<T2>.StructIndex);
+    }
+
+    public  bool    Has<T1, T2, T3> ()
+        where T1 : struct, IStructComponent
+        where T2 : struct, IStructComponent
+        where T3 : struct, IStructComponent
+    {
+        return bitSet.Has(StructHeap<T1>.StructIndex) &&
+               bitSet.Has(StructHeap<T2>.StructIndex) &&
+               bitSet.Has(StructHeap<T3>.StructIndex);
+    }
+    
+    public  bool    HasAll (in ArchetypeMask mask)
+    {
+        return bitSet.HasAll(mask.bitSet);
+    }
+    
+    public  bool    HasAny (in ArchetypeMask mask)
+    {
+        return bitSet.HasAny(mask.bitSet);
+    }
+    
+    // ----------------------------------------- mutate Mask -----------------------------------------
+    
+    public void Add<T>()
+        where T : struct, IStructComponent
+    {
+        bitSet.SetBit(StructHeap<T>.StructIndex);
+    }
+    
+    public void Add(in ArchetypeMask mask)
+    {
+        bitSet.value |= mask.bitSet.value;
+    }
+    
+    public void Remove<T>()
+        where T : struct, IStructComponent
+    {
+        bitSet.ClearBit(StructHeap<T>.StructIndex);
+    }
+    
+    public void Remove(in ArchetypeMask mask)
+    {
+        bitSet.value &= ~mask.bitSet.value;
+    }
+    
+    // ----------------------------------------- static methods -----------------------------------------    
+    public static ArchetypeMask Get<T>()
+        where T : struct, IStructComponent
+    {
+        BitSet bitSet = default;
+        bitSet.SetBit(StructHeap<T>.StructIndex);
+        return new ArchetypeMask(bitSet);
+    }
+    
+    public static ArchetypeMask Get<T1, T2>()
+        where T1 : struct, IStructComponent
+        where T2 : struct, IStructComponent
+    {
+        BitSet bitSet = default;
+        bitSet.SetBit(StructHeap<T1>.StructIndex);
+        bitSet.SetBit(StructHeap<T2>.StructIndex);
+        return new ArchetypeMask(bitSet);
     }
     
     private string GetString()
     {
         var sb = new StringBuilder();
         sb.Append("Mask: [");
-        foreach (var index in mask) {
-            var tagType = EntityStore.Static.ComponentSchema.GetStructComponentAt(index);
+        var hasTypes    = false;
+        foreach (var index in bitSet) {
+            var structType = EntityStore.Static.ComponentSchema.GetStructComponentAt(index);
             sb.Append('#');
-            sb.Append(tagType.type.Name);
+            sb.Append(structType.type.Name);
             sb.Append(", ");
+            hasTypes = true;
         }
-        sb.Length -= 2;
+        if (hasTypes) {
+            sb.Length -= 2;
+        }
         sb.Append(']');
         return sb.ToString();
     }
+}
+
+public struct ArchetypeMaskEnumerator : IEnumerator<ComponentType>
+{
+    private BitSetEnumerator    bitSetEnumerator;
+
+    // --- IEnumerator
+    public  void                Reset()             => bitSetEnumerator.Reset();
+
+    object              IEnumerator.Current => Current;
+
+    public  ComponentType       Current => EntityStore.Static.ComponentSchema.GetStructComponentAt(bitSetEnumerator.Current);
+    
+    internal ArchetypeMaskEnumerator(in ArchetypeMask mask) {
+        bitSetEnumerator = mask.bitSet.GetEnumerator();
+    }
+    
+    // --- IEnumerator
+    public bool MoveNext() => bitSetEnumerator.MoveNext();
+    public void Dispose() { }
 }
---
+++ Engine/tests/ECS/Test_ArchetypeMask.cs
@@ -0,0 +1,107 @@
+using Friflo.Fliox.Engine.ECS;
+using NUnit.Framework;
+using Tests.Utils;
+using static NUnit.Framework.Assert;
+
+// ReSharper disable UseObjectOrCollectionInitializer
+// ReSharper disable InconsistentNaming
+namespace Tests.ECS;
+
+
+public static class Test_ArchetypeMask
+{
+    [Test]
+    public static void Test_ArchetypeMask_basics()
+    {
+        var twoStructs = ArchetypeMask.Get<Position, Rotation>();
+        AreEqual("Mask: [#Position, #Rotation]", twoStructs.ToString());
+        
+        var structs    = new ArchetypeMask();
+        AreEqual("Mask: []",                    structs.ToString());
+        IsFalse(structs.Has<Position>());
+        IsFalse(structs.HasAll(twoStructs));
+        IsFalse(structs.HasAny(twoStructs));
+        
+        structs.Add<Position>();
+        IsTrue (structs.Has<Position>());
+        IsFalse(structs.HasAll(twoStructs));
+        IsTrue (structs.HasAny(twoStructs));
+        
+        AreEqual("Mask: [#Position]",            structs.ToString());
+        
+        structs.Add<Rotation>();
+        AreEqual("Mask: [#Position, #Rotation]", structs.ToString());
+        IsTrue (structs.Has<Position, Rotation>());
+        IsTrue (structs.HasAll(twoStructs));
+        IsTrue (structs.HasAny(twoStructs));
+
+        var copy = new ArchetypeMask();
+        copy.Add(structs);
+        AreEqual("Mask: [#Position, #Rotation]", copy.ToString());
+        
+        copy.Remove<Position>();
+        AreEqual("Mask: [#Rotation]",           copy.ToString());
+        
+        copy.Remove(structs);
+        AreEqual("Mask: []",                    copy.ToString());
+        
+        var store   = new EntityStore();
+        var entity  = store.CreateEntity();
+        
+        // AreEqual("Mask: []", entity.Tags.ToString());
+    }
+    
+    [Test]
+    public static void Test_ArchetypeMask_Get()
+    {
+        var schema          = EntityStore.GetComponentSchema();
+        var testStructType  = schema.ComponentTypeByType[typeof(Position)];
+        var testStructType2  = schema.ComponentTypeByType[typeof(Rotation)];
+        
+        var struct1    = ArchetypeMask.Get<Position>();
+        AreEqual("Mask: [#Position]", struct1.ToString());
+        int count1 = 0;
+        foreach (var structType in struct1) {
+            AreSame(testStructType, structType);
+            count1++;
+        }
+        AreEqual(1, count1);
+        
+        var count2 = 0;
+        var struct2 = ArchetypeMask.Get<Position, Rotation>();
+        AreEqual("Mask: [#Position, #Rotation]", struct2.ToString());
+        foreach (var _ in struct2) {
+            count2++;
+        }
+        AreEqual(2, count2);
+        
+        AreEqual(struct2, ArchetypeMask.Get<Position, Rotation>());
+    }
+    
+    [Test]
+    public static void Test_ArchetypeMask_Get_Mem()
+    {
+        var struct1    = ArchetypeMask.Get<Position>();
+        foreach (var _ in struct1) { }
+        
+        // --- 1 struct
+        var start   = Mem.GetAllocatedBytes();
+        int count1 = 0;
+        foreach (var _ in struct1) {
+            count1++;
+        }
+        Mem.AssertNoAlloc(start);
+        AreEqual(1, count1);
+        
+        // --- 2 structs
+        start       = Mem.GetAllocatedBytes();
+        var struct2    = ArchetypeMask.Get<Position, Rotation>();
+        var count2 = 0;
+        foreach (var _ in struct2) {
+            count2++;
+        }
+        Mem.AssertNoAlloc(start);
+        AreEqual(2, count2);
+    }
+}
+
