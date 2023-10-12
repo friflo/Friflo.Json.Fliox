a1666576ac142abb383ecfdea2929ab5ce1b7a8d
Ullrich Praetz <ullrich@praetz.de>
2023-10-12 17:03:15 +0200

    Engine - fixed compiler warning in ArchetypeQuery<,>

--- Engine/src/ECS/Struct/Query.cs
+++ Engine/src/ECS/Struct/Query.cs
@@ -150,9 +150,9 @@ public sealed class ArchetypeQuery<T1, T2> : ArchetypeQuery // : IEnumerable <> 
         return this;
     }
     
-    public QueryChunks      <T1,T2> Chunks                                      => new (this);
-    public QueryEnumerator  <T1,T2> GetEnumerator()                             => new (this);
-    public QueryForEach     <T1,T2> ForEach(Action<Ref<T1>, Ref<T2>> lambda)    => new (this, lambda);
+    public      QueryChunks    <T1,T2>  Chunks                                      => new (this);
+    public new  QueryEnumerator<T1,T2>  GetEnumerator()                             => new (this);
+    public      QueryForEach   <T1,T2>  ForEach(Action<Ref<T1>, Ref<T2>> lambda)    => new (this, lambda);
 }
 
 public sealed class ArchetypeQuery<T1, T2, T3> : ArchetypeQuery
