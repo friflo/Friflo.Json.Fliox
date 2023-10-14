cf0aa589a43c1b61b3dfaefd83425dabc16ee7b7
Ullrich Praetz <ullrich@praetz.de>
2023-10-14 18:52:40 +0200

    Engine - removed unused import

--- Engine/src/ECS/Base/EntityNode.cs
+++ Engine/src/ECS/Base/EntityNode.cs
@@ -2,7 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
-using System.Runtime.InteropServices;
 using System.Text;
 using static Friflo.Fliox.Engine.ECS.NodeFlags;
 using static System.Diagnostics.DebuggerBrowsableState;
