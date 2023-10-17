93cd3d9c17e04cd1266d0c4981e2f52d79dec312
Ullrich Praetz <ullrich@praetz.de>
2023-10-17 17:31:20 +0200

    Engine - removed unused using declaration

--- Engine/src/ECS/RawEntity/RawEntity.cs
+++ Engine/src/ECS/RawEntity/RawEntity.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using static System.Diagnostics.DebuggerBrowsableState;
 using Browse = System.Diagnostics.DebuggerBrowsableAttribute;
 
 // ReSharper disable once CheckNamespace
