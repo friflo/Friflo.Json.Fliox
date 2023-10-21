0c28d311536ba76c3a6b71b7ef6181b21adedf92
Ullrich Praetz <ullrich@praetz.de>
2023-10-21 09:34:37 +0200

    Engine - fixed docs

--- Engine/src/ECS/Enums/TreeMembership.cs
+++ Engine/src/ECS/Enums/TreeMembership.cs
@@ -8,14 +8,13 @@ using static Friflo.Fliox.Engine.ECS.StoreOwnership;
 namespace Friflo.Fliox.Engine.ECS;
 
 /// <summary>
-/// Describe the membership of a <see cref="GameEntity"/> to the <see cref="GameEntityStore"/>
-/// <see cref="GameEntityStore"/> tree graph.
+/// Describe the membership of a <see cref="GameEntity"/> to the <see cref="GameEntityStore"/> tree graph.
 /// </summary>
 /// <remarks>Requirement: The entity must be <see cref="attached"/> to an <see cref="GameEntityStore"/></remarks>
 public enum TreeGraphMembership
 {
-    /// <summary>The entity is not member of the <see cref="GameEntityStore"/> <see cref="GameEntityStore"/> tree graph</summary>
+    /// <summary>The entity is not member of the <see cref="GameEntityStore"/> tree graph</summary>
     floating    = 0,
-    /// <summary>The entity is member of the <see cref="GameEntityStore"/> <see cref="GameEntityStore"/> tree graph</summary>
+    /// <summary>The entity is member of the <see cref="GameEntityStore"/> tree graph</summary>
     graphNode   = 1,
 }
