364412ea40f1b75d11dfd54122f8f8b5b5fba489
Ullrich Praetz <ullrich@praetz.de>
2023-10-27 18:01:30 +0200

    Engine - add HTTP server to Editor

--- Engine/Editor/Program.cs
+++ Engine/Editor/Program.cs
@@ -1,4 +1,8 @@
-﻿using Friflo.Fliox.Engine.ECS;
+﻿using Friflo.Fliox.Engine.Client;
+using Friflo.Fliox.Engine.ECS;
+using Friflo.Json.Fliox.Hub.Explorer;
+using Friflo.Json.Fliox.Hub.Host;
+using Friflo.Json.Fliox.Hub.Remote;
 
 namespace Friflo.Fliox.Editor;
 
@@ -12,6 +16,23 @@ public static class Program
         var entity  = store.CreateEntity();
         entity.AddComponent(new Position());
         Console.WriteLine($"entity: {entity}");
+        
+        RunServer();
+    }
+    
+    private static void RunServer()
+    {
+        var schema      = DatabaseSchema.Create<GameClient>();
+        var database    = new MemoryDatabase("game", schema);
+        var hub         = new FlioxHub(database);
+        hub.Info.Set ("Editor", "dev", "https://github.com/friflo/Friflo.Json.Fliox/tree/main/Engine", "rgb(91,21,196)"); // optional
+        hub.UseClusterDB(); // required by HubExplorer
+        hub.UsePubSub();    // optional - enables Pub-Sub
+        // --- create HttpHost
+        var httpHost    = new HttpHost(hub, "/fliox/");
+        httpHost.UseStaticFiles(HubExplorer.Path); // nuget: https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.Explorer
+    
+        HttpServer.RunHost("http://localhost:5000/", httpHost); // http://localhost:5000/fliox/
     }
 }
 
