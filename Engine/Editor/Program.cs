39bad6574c5adaafe214a95be57b8e3e287b4244
Ullrich Praetz <ullrich@praetz.de>
2023-10-27 17:48:51 +0200

    Engine - add Editor.csproj

---
+++ Engine/Editor/Program.cs
@@ -0,0 +1,17 @@
+﻿using Friflo.Fliox.Engine.ECS;
+
+namespace Friflo.Fliox.Editor;
+
+using System;
+
+public static class Program
+{
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+        var store   = new GameEntityStore();
+        var entity  = store.CreateEntity();
+        entity.AddComponent(new Position());
+        Console.WriteLine($"entity: {entity}");
+    }
+}
+
