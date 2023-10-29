0e123ffca10b27ad67c7bc2d7fb585663dcd11d3
Ullrich Praetz <ullrich@praetz.de>
2023-10-29 12:56:39 +0100

    Engine - hide field EntityName.Utf8 in debugger

--- Engine/src/ECS/Components/EntityName.cs
+++ Engine/src/ECS/Components/EntityName.cs
@@ -2,6 +2,7 @@
 // See LICENSE file in the project root for full license information.
 
 using System.Text;
+using static System.Diagnostics.DebuggerBrowsableState;
 using Browse = System.Diagnostics.DebuggerBrowsableAttribute;
 
 // ReSharper disable once CheckNamespace
@@ -10,11 +11,11 @@ namespace Friflo.Fliox.Engine.ECS;
 [Component("name")]
 public struct EntityName : IComponent
 {
-    public          string  value;  //  8
+                    public  string  value;  //  8
     
-    public          byte[]  Utf8 => value == null ? null : Encoding.UTF8.GetBytes(value);
+    [Browse(Never)] public  byte[]  Utf8 => value == null ? null : Encoding.UTF8.GetBytes(value);
     
-    public override string  ToString() => $"EntityName: '{value}'";
+    public override         string  ToString() => $"EntityName: '{value}'";
 
     public EntityName (string value) {
         this.value = value;
