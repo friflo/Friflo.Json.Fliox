cff9b48365b987e65fffea879311d1fa4d00006c
Ullrich Praetz <ullrich@praetz.de>
2023-10-30 11:50:46 +0100

    Engine - add assertions to Test_Unresolved tests

--- Engine/Tests/ECS/Sync/Test_Unresolved.cs
+++ Engine/Tests/ECS/Sync/Test_Unresolved.cs
@@ -14,22 +14,23 @@ public static class Test_Unresolved
     [Test]
     public static void Test_Unresolved_components()
     {
-        var store       = new GameEntityStore(PidType.UsePidAsId);
-        var converter   = EntityConverter.Default;
-        var source1     = new DataEntity { pid = 1, components = new JsonValue("{ \"xxx1\": { \"foo1\":1 }}") };
+        var store           = new GameEntityStore(PidType.UsePidAsId);
+        var converter       = EntityConverter.Default;
+        var source1         = new DataEntity { pid = 1, components = new JsonValue("{ \"xxx1\": { \"foo1\":1 }}") };
+        var unresolvedTypes = store.GetArchetype(Signature.Get<Unresolved>());
         
         for (int n = 0; n < 2; n++)
         {
             var gameEntity  = converter.DataToGameEntity(source1, store, out _);
             var unresolved  = gameEntity.GetComponent<Unresolved>();
             
-            AreEqual(1,                                 unresolved.components.Length);
-            AreEqual("'xxx1': { \"foo1\":1 }",          unresolved.components[0].ToString());
-            
-            AreEqual("unresolved components: 'xxx1'",   unresolved.ToString());
+            AreEqual(1,                                         unresolved.components.Length);
+            AreEqual("'xxx1': { \"foo1\":1 }",                  unresolved.components[0].ToString());
+            AreEqual("unresolved components: 'xxx1'",           unresolved.ToString());
+            AreEqual(1,                                         unresolvedTypes.EntityCount);
             
             var targetEntity = converter.GameToDataEntity(gameEntity);
-            AreEqual("{\"xxx1\":{ \"foo1\":1 }}",        targetEntity.components.ToString());
+            AreEqual("{\"xxx1\":{ \"foo1\":1 }}",               targetEntity.components.ToString());
         }
         
         var source2     = new DataEntity { pid = 1, components = new JsonValue("{ \"xxx2\": { \"foo2\":2 }}") };
@@ -41,6 +42,7 @@ public static class Test_Unresolved
             AreEqual("'xxx1': { \"foo1\":1 }",                  unresolved.components[0].ToString());
             AreEqual("'xxx2': { \"foo2\":2 }",                  unresolved.components[1].ToString());
             AreEqual("unresolved components: 'xxx1', 'xxx2'",   unresolved.ToString());
+            AreEqual(1,                                         unresolvedTypes.EntityCount);
             
             var targetEntity = converter.GameToDataEntity(gameEntity);
             AreEqual("{\"xxx1\":{ \"foo1\":1 },\"xxx2\":{ \"foo2\":2 }}", targetEntity.components.ToString());
@@ -50,22 +52,22 @@ public static class Test_Unresolved
     [Test]
     public static void Test_Unresolved_tags()
     {
-        var store       = new GameEntityStore(PidType.UsePidAsId);
-        var converter   = EntityConverter.Default;
-        var source1     = new DataEntity { pid = 1, tags = new List<string>{"yyy1"} };
+        var store           = new GameEntityStore(PidType.UsePidAsId);
+        var converter       = EntityConverter.Default;
+        var source1         = new DataEntity { pid = 1, tags = new List<string>{"yyy1"} };
+        var unresolvedTypes = store.GetArchetype(Signature.Get<Unresolved>());
         
         for (int n = 0; n < 2; n++)
         {
             var gameEntity  = converter.DataToGameEntity(source1, store, out _);
             var unresolved  = gameEntity.GetComponent<Unresolved>();
             
-            AreEqual(1, unresolved.tags.Length);
+            AreEqual(1,                                 unresolved.tags.Length);
             IsTrue  (unresolved.tags.Contains("yyy1"));
-            
-            AreEqual("unresolved tags: 'yyy1'", unresolved.ToString());
+            AreEqual("unresolved tags: 'yyy1'",         unresolved.ToString());
+            AreEqual(1,                                 unresolvedTypes.EntityCount);
             
             var targetEntity = converter.GameToDataEntity(gameEntity);
-            
             AreEqual(1, targetEntity.tags.Count);
             IsTrue  (targetEntity.tags.Contains("yyy1"));
             IsTrue  (targetEntity.components.IsNull()); // Unresolved component is not serialized.
@@ -77,14 +79,13 @@ public static class Test_Unresolved
             var gameEntity  = converter.DataToGameEntity(source2, store, out _);
             var unresolved  = gameEntity.GetComponent<Unresolved>();
             
-            AreEqual(2, unresolved.tags.Length);
+            AreEqual(2,                                 unresolved.tags.Length);
             IsTrue  (unresolved.tags.Contains("yyy1"));
             IsTrue  (unresolved.tags.Contains("yyy2"));
-            
             AreEqual("unresolved tags: 'yyy1', 'yyy2'", unresolved.ToString());
+            AreEqual(1,                                 unresolvedTypes.EntityCount);
             
             var targetEntity = converter.GameToDataEntity(gameEntity);
-            
             AreEqual(2, targetEntity.tags.Count);
             IsTrue  (targetEntity.tags.Contains("yyy1"));
             IsTrue  (targetEntity.tags.Contains("yyy2"));
