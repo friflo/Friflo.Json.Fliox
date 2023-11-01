2889ef5d08d5924a9eeee84f6abc9f3dab3daff1
Ullrich Praetz <ullrich@praetz.de>
2023-11-01 15:51:01 +0100

    Engine - integrate Avalonia into Editor

--- Engine/Editor/Program.cs
+++ Engine/Editor/Program.cs
@@ -1,12 +1,32 @@
-﻿namespace Friflo.Fliox.Editor;
+﻿using System;
+using Avalonia;
+using GetStartedApp;
+
+namespace Friflo.Fliox.Editor;
 
 public static class Program
 {
+    [STAThread]
     public static void Main(string[] args)
     {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        AppBuilder builder = BuildAvaloniaApp();
+        builder.StartWithClassicDesktopLifetime(args);
+        
         var editor = new Editor();
         editor.Init(args).Wait();
         editor.Run();
+    }
+    
+    // Avalonia configuration, don't remove; also used by visual designer.
+    private static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
     }
 }
 
