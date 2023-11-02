3ef83ff8e8972eabfc456208d93ac410a23017ca
Ullrich Praetz <ullrich@praetz.de>
2023-11-02 08:39:13 +0100

    Engine - Editor: add TestPanel

---
+++ Engine/Editor/UI/TestPanel.axaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Friflo.Fliox.Editor.UI;
+
+public partial class TestPanel : UserControl
+{
+    public TestPanel()
+    {
+        InitializeComponent();
+    }
+    
+    public void OnButtonClick(object sender, RoutedEventArgs routedEventArgs)
+    {
+        Console.WriteLine("Click");
+    }
+}
