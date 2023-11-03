c4947c86cdc21d359f2fc60534624f15e3a8cb17
Ullrich Praetz <ullrich@praetz.de>
2023-11-03 16:43:58 +0100

    Engine - Editor - fixed Drag/Drop exception in Explorer

--- Engine/Editor/UI/Panels/Models/DragDropItem.cs
+++ Engine/Editor/UI/Panels/Models/DragDropItem.cs
@@ -7,20 +7,20 @@ namespace Friflo.Fliox.Editor.UI.Models
     // see: https://github.com/AvaloniaUI/Avalonia.Controls.TreeDataGrid/blob/master/samples/TreeDataGridDemo/Models/DragDropItem.cs
     public class DragDropItem 
     {
-        public  ObservableCollection<DragDropItem>  children;
-        public  string                              Name { get; }
-        public  bool                                flag;
+        public  readonly    ObservableCollection<DragDropItem>  children;   // Must always be present. Drop on an item with children == null result in COMException.
+        public              string                              Name { get; }
+        public              bool                                flag;
 
-        public DragDropItem(string name) => Name = name;
+        public DragDropItem(string name) {
+            Name        = name;
+            children    = new ObservableCollection<DragDropItem>();
+        }
 
         public static ObservableCollection<DragDropItem> CreateRandomItems()
         {
-            var root = new DragDropItem ("root") {
-                children = new ObservableCollection<DragDropItem> {
-                    new DragDropItem("child 1"),
-                    new DragDropItem("child 12")
-                }
-            };
+            var root = new DragDropItem ("root");
+            root.children.Add(new DragDropItem("child 1"));
+            root.children.Add(new DragDropItem("child 2"));
             return new ObservableCollection<DragDropItem> { root };
         }
     }
