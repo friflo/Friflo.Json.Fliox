8723a3dd2c430d285fbfc5b12acfda1f5f030455
Ullrich Praetz <ullrich@praetz.de>
2023-11-04 09:41:30 +0100

    Engine - Editor: ObservableList evaluation

--- Engine/Editor/UI/Panels/Models/ObservableList.cs
+++ Engine/Editor/UI/Panels/Models/ObservableList.cs
@@ -20,29 +20,11 @@ public class ObservableList<T> : IList<T>, IList, IReadOnlyList<T>, INotifyColle
     public  event       PropertyChangedEventHandler         PropertyChanged;
     private readonly    Collection<T>                       collection;
     
+    // ReSharper disable once ConvertConstructorToMemberInitializers
     public ObservableList() {
         collection = new Collection<T>();
-        CollectionChanged += (sender, args) => {
-            _ = 1;
-        };
-        PropertyChanged += (sender, args) => {
-            _ = 2;  
-        };
-    }
-    static void Test() {
-        var col = new ObservableCollection<object>();
-        col.Insert(1, null);
     }
     
-    private void OnCollectionChanged(Op action, object item, int index) {
-        var args = new NotifyCollectionChangedEventArgs(action, item, index);
-        CollectionChanged?.Invoke(this, args);
-    }
-    
-    private void OnCollectionChanged(Args args) {
-        CollectionChanged?.Invoke(this, args);
-    }
-
     public IEnumerator<T> GetEnumerator() {
         return collection.GetEnumerator();
     }
@@ -56,56 +38,6 @@ public class ObservableList<T> : IList<T>, IList, IReadOnlyList<T>, INotifyColle
         collection.Add(item);
         var index   = collection.Count - 1;
         OnCollectionChanged(Op.Add, item, index);
-    }
-
-    public int Add(object value) {
-        collection.Add((T)value);
-        var index   = collection.Count - 1;
-        OnCollectionChanged(Op.Add, value, index);
-        return index;
-    }
-
-    void IList.Clear()  {
-        collection.Clear();
-        OnCollectionChanged(Op.Reset, null, -1);
-    }
-
-    public bool Contains(object value) {
-        return collection.Contains((T)value);
-    }
-
-    public int IndexOf(object value) {
-        return collection.IndexOf((T)value);
-    }
-
-    public void Insert(int index, object value) {
-        collection.Insert(index, (T)value);
-        OnCollectionChanged(Op.Add, value, index);
-    }
-
-    public void Remove(object value) {
-        collection.Remove((T)value);
-        throw new NotImplementedException();
-    }
-
-    void IList.RemoveAt(int index) {
-        var item = collection[index];
-        collection.RemoveAt(index);
-        OnCollectionChanged(Op.Remove, item, index);
-    }
-
-    public bool IsFixedSize => false;
-
-    bool IList.IsReadOnly => false;
-
-    object IList.this[int index] {
-        get => collection[index];
-        set {
-            var oldItem         = collection[index];
-            collection[index]   = (T)value;
-            var args = new Args(Op.Replace, value, oldItem, index);
-            OnCollectionChanged(args);
-        }
     }
 
     void ICollection<T>.Clear() {
@@ -126,15 +58,6 @@ public class ObservableList<T> : IList<T>, IList, IReadOnlyList<T>, INotifyColle
         throw new NotImplementedException();
         return collection.Remove(item);
     }
-
-    public void CopyTo(Array array, int index) {
-        throw new NotImplementedException();
-    }
-
-    int ICollection.Count => collection.Count;
-
-    public bool IsSynchronized  => false;
-    public object SyncRoot      => collection;
 
     int ICollection<T>.Count => collection.Count;
 
@@ -170,4 +93,70 @@ public class ObservableList<T> : IList<T>, IList, IReadOnlyList<T>, INotifyColle
     }
 
     int IReadOnlyCollection<T>.Count => collection.Count;
+    
+    // --- private methods
+    private void OnCollectionChanged(Op action, object item, int index) {
+        var args = new NotifyCollectionChangedEventArgs(action, item, index);
+        CollectionChanged?.Invoke(this, args);
+    }
+    
+    private void OnCollectionChanged(Args args) {
+        CollectionChanged?.Invoke(this, args);
+    }
+    
+    // --------------------------------------- IList crab :) ---------------------------------------
+    void IList.Clear()  {
+        collection.Clear();
+        OnCollectionChanged(Op.Reset, null, -1);
+    }
+    void IList.RemoveAt(int index) {
+        var item = collection[index];
+        collection.RemoveAt(index);
+        OnCollectionChanged(Op.Remove, item, index);
+    }
+    
+    public int Add(object value) {
+        collection.Add((T)value);
+        var index   = collection.Count - 1;
+        OnCollectionChanged(Op.Add, value, index);
+        return index;
+    }
+
+    object IList.this[int index] {
+        get => collection[index];
+        set {
+            var oldItem         = collection[index];
+            collection[index]   = (T)value;
+            var args = new Args(Op.Replace, value, oldItem, index);
+            OnCollectionChanged(args);
+        }
+    }
+    
+    public bool Contains(object value) {
+        return collection.Contains((T)value);
+    }
+
+    public int IndexOf(object value) {
+        return collection.IndexOf((T)value);
+    }
+
+    public void Insert(int index, object value) {
+        collection.Insert(index, (T)value);
+        OnCollectionChanged(Op.Add, value, index);
+    }
+
+    public void Remove(object value) {
+        collection.Remove((T)value);
+        throw new NotImplementedException();
+    }
+    
+    public void CopyTo(Array array, int index) {
+        throw new NotImplementedException();
+    }
+    
+    public  bool    IsFixedSize         => false;
+            bool    IList.IsReadOnly    => false;    
+            int     ICollection.Count   => collection.Count;
+    public  bool    IsSynchronized      => false;
+    public  object  SyncRoot            => collection;
 }
