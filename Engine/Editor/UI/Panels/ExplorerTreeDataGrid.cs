f42a615641c9c04878359a9ca249e72083a91f10
Ullrich Praetz <ullrich@praetz.de>
2023-11-15 16:41:43 +0100

    Engine - add ExplorerTreeDataGrid

---
+++ Engine/Editor/UI/Panels/ExplorerTreeDataGrid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+
+
+namespace Friflo.Fliox.Editor.UI.Panels;
+
+/// <summary>
+/// Extended <see cref="TreeDataGrid"/> should not be necessary. But is needed to get <see cref="OnKeyDown"/> callbacks.<br/>
+/// <br/>
+/// On Windows it is sufficient to handle these events in <see cref="ExplorerPanel.OnKeyDown"/>
+/// but on macOS this method is not called.
+/// </summary>
+public class ExplorerTreeDataGrid : TreeDataGrid
+{
+    public ExplorerTreeDataGrid() { }
+        
+    // https://stackoverflow.com/questions/71815213/how-can-i-show-my-own-control-in-avalonia
+    protected override Type StyleKeyOverride => typeof(TreeDataGrid);
+
+    protected override void OnKeyDown(KeyEventArgs e) {
+        Console.WriteLine($"ExplorerTreeDataGrid - OnKeyDown: {e.Key} {e.KeyModifiers}");
+        base.OnKeyDown(e);
+    }
+}
