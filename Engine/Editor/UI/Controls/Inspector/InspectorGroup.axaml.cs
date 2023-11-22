24e49e157948350c83a15a1af68bfddb6a9c1048
Ullrich Praetz <ullrich@praetz.de>
2023-11-22 15:30:41 +0100

    Engine - Editor: layout Inspector add buttons

--- Engine/Editor/UI/Controls/Inspector/InspectorGroup.axaml.cs
+++ Engine/Editor/UI/Controls/Inspector/InspectorGroup.axaml.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System.Threading;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using AP = Avalonia.AvaloniaProperty;
 
@@ -13,8 +16,8 @@ public partial class InspectorGroup : UserControl
     public static readonly StyledProperty<string>   GroupProperty       = AP.Register<InspectorGroup, string>(nameof(Group), "group");
     public static readonly StyledProperty<bool>     ExpandedProperty    = AP.Register<InspectorGroup, bool>  (nameof(Expanded), true);
     
-    public string   Group  { get => GetValue(GroupProperty);  set => SetValue(GroupProperty, value); }
-    public bool     Expanded        { get => GetValue(ExpandedProperty);        set => SetValue(ExpandedProperty,       value); }
+    public string   Group       { get => GetValue(GroupProperty);       set => SetValue(GroupProperty,      value); }
+    public bool     Expanded    { get => GetValue(ExpandedProperty);    set => SetValue(ExpandedProperty,   value); }
     
     public InspectorGroup()
     {
@@ -24,4 +27,20 @@ public partial class InspectorGroup : UserControl
     private void Button_OnClick(object sender, RoutedEventArgs e) {
         Expanded = !Expanded;
     }
+    
+    bool entered;
+
+    private async void InputElement_OnPointerEntered(object sender, PointerEventArgs e) {
+        entered = true;
+        await Task.Delay(50);
+        if (entered) {
+            Add.IsVisible = true;
+        }
+    }
+
+    private void InputElement_OnPointerExited(object sender, PointerEventArgs e) {
+        entered = false;
+        Add.IsVisible = false;
+    }
 }
+
