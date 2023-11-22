e00d58e24525099cecf600eb4e6274a8051dac53
Ullrich Praetz <ullrich@praetz.de>
2023-11-22 08:44:53 +0100

    Engine - Editor: removed field controls from InspectorComponent.axaml

---
+++ Engine/Editor/UI/Controls/Inspector/Separator.axaml.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Avalonia;
+using Avalonia.Controls;
+using AP = Avalonia.AvaloniaProperty;
+
+namespace Friflo.Fliox.Editor.UI.Controls.Inspector;
+
+public partial class Separator : UserControl
+{
+    
+    public Separator()
+    {
+        InitializeComponent();
+    }
+}
