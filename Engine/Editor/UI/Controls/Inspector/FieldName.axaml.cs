7000b2e2406e8542c2e23876ea021f6a2a170e51
Ullrich Praetz <ullrich@praetz.de>
2023-11-22 07:03:42 +0100

    Engine - Editor: add Inspector control FieldName

---
+++ Engine/Editor/UI/Controls/Inspector/FieldName.axaml.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using AP = Avalonia.AvaloniaProperty;
+
+namespace Friflo.Fliox.Editor.UI.Controls.Inspector;
+
+public partial class FieldName : UserControl
+{
+    public static readonly StyledProperty<string>   TextProperty  = AP.Register<InspectorComponent, string>(nameof(Text), "name");
+    
+    public string   Text  { get => GetValue(TextProperty);  set => SetValue(TextProperty, value); }
+    
+    public FieldName()
+    {
+        InitializeComponent();
+    }
+}
