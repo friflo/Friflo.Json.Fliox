d7845c0a364b07f706521ebb280c23feb89ba783
Ullrich Praetz <ullrich@praetz.de>
2023-11-24 08:53:12 +0100

    Engine - Editor: add InspectorControl.AddComponentFields() & .AddScriptFields()

--- Engine/Editor/UI/Inspector/InspectorControl.axaml.cs
+++ Engine/Editor/UI/Inspector/InspectorControl.axaml.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Friflo.Fliox.Engine.ECS;
@@ -38,11 +39,11 @@ public partial class InspectorControl : UserControl
             
             var entity = item?.Entity;
             if (entity != null) {
-                AddComponentControls(entity);
+                AddEntityControls(entity);
             }
         }
         
-        private void AddComponentControls(Entity entity)
+        private void AddEntityControls(Entity entity)
         {
             // Console.WriteLine($"--- Inspector entity: {entity}");
             var tags        = inspector.Tags.Children;
@@ -62,13 +63,49 @@ public partial class InspectorControl : UserControl
             {
                 var component = new InspectorComponent { ComponentTitle = componentType.type.Name };
                 components.Add(component);
-                _ = archetype.GetEntityComponent(entity, componentType);
+                var panel = new StackPanel();
+                AddComponentFields(entity, componentType, panel);
+                
+                // <StackPanel IsVisible="{Binding #Comp1.Expanded}"
+                var expanded = component.GetObservable(InspectorComponent.ExpandedProperty);
+                // same as: AvaloniaObjectExtensions.GetObservable(component, InspectorComponent.ExpandedProperty);
+                panel.Bind(IsVisibleProperty, expanded);
+                
+                components.Add(panel);
             }
             // --- scripts
             foreach (var script in entity.Scripts)
             {
                 var component = new InspectorComponent { ComponentTitle = script.GetType().Name };
                 scripts.Add(component);
+                var panel = new StackPanel();
+                AddScriptFields(entity, script, panel);
+                
+                // <StackPanel IsVisible="{Binding #Comp1.Expanded}"
+                var expanded = component.GetObservable(InspectorComponent.ExpandedProperty);
+                panel.Bind(IsVisibleProperty, expanded);
+                
+                scripts.Add(panel);
+            }
+        }
+        
+        private static void AddComponentFields(Entity entity, ComponentType componentType, StackPanel panel)
+        {
+            for (int n = 1; n <= 2; n++) {
+                var dock = new DockPanel();
+                dock.Children.Add(new FieldName   { Text  = $"Field {n}"} );
+                dock.Children.Add(new StringField { Value = $"value {n}"} );
+                panel.Children.Add(dock);
+            }
+        }
+        
+        private static void AddScriptFields(Entity entity, Script script, StackPanel panel)
+        {
+            for (int n = 1; n <= 1; n++) {
+                var dock = new DockPanel();
+                dock.Children.Add(new FieldName   { Text  = $"Field {n}"} );
+                dock.Children.Add(new StringField { Value = $"value {n}"} );
+                panel.Children.Add(dock);
             }
         }
     }
