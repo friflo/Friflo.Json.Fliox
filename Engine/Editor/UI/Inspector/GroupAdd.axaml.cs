4f4d32d66d83369667c15e482c28932c96fadc91
Ullrich Praetz <ullrich@praetz.de>
2023-11-27 12:08:34 +0100

    Engine - Editor: add GroupAdd control skeleton

---
+++ Engine/Editor/UI/Inspector/GroupAdd.axaml.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Avalonia.Controls;
+using AP = Avalonia.AvaloniaProperty;
+
+namespace Friflo.Fliox.Editor.UI.Inspector;
+
+public partial class GroupAdd : UserControl
+{
+    
+    public GroupAdd()
+    {
+        InitializeComponent();
+    }
+}
