b1ac77affd691889ff672026c279e189242fbf6b
Ullrich Praetz <ullrich@praetz.de>
2023-12-02 16:32:36 +0100

    Engine - OpenGL moved OpenGL test drawing To TestDraw to disable unsafe code in Editor

--- Engine/Editor/OpenGL/SilkOpenGLControl.cs
+++ Engine/Editor/OpenGL/SilkOpenGLControl.cs
@@ -1,84 +1,40 @@
 using System;
-using System.Drawing;
 using Avalonia.OpenGL;
 using Avalonia.OpenGL.Controls;
 using Avalonia.Threading;
-using Silk.NET.OpenGL;
 
+// ReSharper disable ConvertConstructorToMemberInitializers
 // ReSharper disable InconsistentNaming
 namespace Friflo.Fliox.Editor.OpenGL
 {
     public class SilkOpenGLControl : OpenGlControlBase
     {
-        internal Action OpenGlReady;
-            
-        private GL Gl;
-        private BufferObject<float> Vbo;
-        private BufferObject<uint> Ebo;
-        private VertexArrayObject<float, uint> Vao;
-        private Shader Shader;
-
-        private static readonly float[] Vertices =
-        {
-            //X    Y      Z     R  G  B  A
-            0.5f,  0.5f, 0.0f, 1, 0, 0, 1,
-            0.5f, -0.5f, 0.0f, 0, 0, 0, 1,
-            -0.5f, -0.5f, 0.0f, 0, 0, 1, 1,
-            -0.5f,  0.5f, 0.5f, 0, 0, 0, 1
-        };
-
-        private static readonly uint[] Indices =
-        {
-            0, 1, 3,
-            1, 2, 3
-        };
-
-
-
+        internal            Action      OpenGlReady;
+        private             DrawTest    test;
+        
         protected override void OnOpenGlInit(GlInterface gl)
         {
             base.OnOpenGlInit(gl);
             // Console.WriteLine($"--- SilkOpenGLControl.OnOpenGlInit() - startup {Program.startTime.ElapsedMilliseconds} ms");
-            Gl = GL.GetApi(gl.GetProcAddress);
-            
+            Func<string,nint> procAddress = gl.GetProcAddress;
+            test = new DrawTest();
+            test.OpenGlInit(procAddress);
 
-            //Instantiating our new abstractions
-            Ebo = new BufferObject<uint>(Gl, Indices, BufferTargetARB.ElementArrayBuffer);
-            Vbo = new BufferObject<float>(Gl, Vertices, BufferTargetARB.ArrayBuffer);
-            Vao = new VertexArrayObject<float, uint>(Gl, Vbo, Ebo);
-
-            //Telling the VAO object how to lay out the attribute pointers
-            Vao.VertexAttributePointer(0, 3, VertexAttribPointerType.Float, 7, 0);
-            Vao.VertexAttributePointer(1, 4, VertexAttribPointerType.Float, 7, 3);
-
-            Shader = new Shader(Gl, "OpenGL/Shader/shader.vert", "OpenGL/Shader/shader.frag");
             Dispatcher.UIThread.Post(OpenGlReady, DispatcherPriority.ApplicationIdle);
         }
-       
 
         protected override void OnOpenGlDeinit(GlInterface gl)
         {
-            Vbo.Dispose();
-            Ebo.Dispose();
-            Vao.Dispose();
-            Shader.Dispose();
+            test.OpenGlDeinit();
             base.OnOpenGlDeinit(gl);
         }
 
-        protected override unsafe void OnOpenGlRender(GlInterface gl, int fb)
+        protected override void OnOpenGlRender(GlInterface gl, int fb)
         {
-            Gl.ClearColor(Color.Gray);
-            Gl.Clear((uint)(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit));
-            Gl.Enable(EnableCap.DepthTest);
-            Gl.Viewport(0,0, (uint)Bounds.Width, (uint)Bounds.Height);
+            var width   = Bounds.Width;
+            var height  = Bounds.Height;
+            test.OpenGlRender(width, height);
             
-            Ebo.Bind();
-            Vbo.Bind();
-            Vao.Bind();
-            Shader.Use();
-            Shader.SetUniform("uBlue", (float) Math.Sin(DateTime.Now.Millisecond / 1000f * Math.PI));
-
-            Gl.DrawElements(PrimitiveType.Triangles, (uint) Indices.Length, DrawElementsType.UnsignedInt, null);
             Dispatcher.UIThread.Post(RequestNextFrameRendering, DispatcherPriority.Background);
         }
     }
---
+++ Engine/OpenGL/DrawTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using Silk.NET.OpenGL;
+
+// ReSharper disable InconsistentNaming
+namespace Friflo.Fliox.Editor.OpenGL;
+
+public class DrawTest
+{
+    private GL                              Gl;
+    private BufferObject<float>             Vbo;
+    private BufferObject<uint>              Ebo;
+    private VertexArrayObject<float, uint>  Vao;
+    private Shader                          Shader;
+
+    private static readonly float[] Vertices =
+    {
+        //X    Y      Z     R  G  B  A
+        0.5f,  0.5f, 0.0f, 1, 0, 0, 1,
+        0.5f, -0.5f, 0.0f, 0, 0, 0, 1,
+        -0.5f, -0.5f, 0.0f, 0, 0, 1, 1,
+        -0.5f,  0.5f, 0.5f, 0, 0, 0, 1
+    };
+
+    private static readonly uint[] Indices =
+    {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+
+
+    public void OpenGlInit(Func<string,nint> procAddress)
+    {
+        Gl = GL.GetApi(procAddress);
+
+        //Instantiating our new abstractions
+        Ebo = new BufferObject<uint>(Gl, Indices, BufferTargetARB.ElementArrayBuffer);
+        Vbo = new BufferObject<float>(Gl, Vertices, BufferTargetARB.ArrayBuffer);
+        Vao = new VertexArrayObject<float, uint>(Gl, Vbo, Ebo);
+
+        //Telling the VAO object how to lay out the attribute pointers
+        Vao.VertexAttributePointer(0, 3, VertexAttribPointerType.Float, 7, 0);
+        Vao.VertexAttributePointer(1, 4, VertexAttribPointerType.Float, 7, 3);
+
+        Shader = new Shader(Gl, "OpenGL/Shader/shader.vert", "OpenGL/Shader/shader.frag");
+    }
+
+    public void OpenGlDeinit()
+    {
+        Vbo.Dispose();
+        Ebo.Dispose();
+        Vao.Dispose();
+        Shader.Dispose();
+    }
+    
+    public unsafe void OpenGlRender(double width, double height)
+    {
+        Gl.ClearColor(Color.Gray);
+        Gl.Clear((uint)(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit));
+        Gl.Enable(EnableCap.DepthTest);
+        Gl.Viewport(0,0, (uint)width, (uint)width);
+        
+        Ebo.Bind();
+        Vbo.Bind();
+        Vao.Bind();
+        Shader.Use();
+        Shader.SetUniform("uBlue", (float) Math.Sin(DateTime.Now.Millisecond / 1000f * Math.PI));
+
+        Gl.DrawElements(PrimitiveType.Triangles, (uint) Indices.Length, DrawElementsType.UnsignedInt, null);
+    }
+}
