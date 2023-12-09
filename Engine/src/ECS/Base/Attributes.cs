935aceb844f3b3f33e05fe03f53bdb318cb3e59f
Ullrich Praetz <ullrich@praetz.de>
2023-12-09 10:28:59 +0100

    Engine - ECS: update attribute classes docs

--- Engine/src/ECS/Base/Attributes.cs
+++ Engine/src/ECS/Base/Attributes.cs
@@ -8,18 +8,19 @@ using System;
 namespace Friflo.Fliox.Engine.ECS;
 
 /// <summary>
-/// Annotated struct a can be added as <see cref="Script"/>'s to <see cref="Entity"/>'s using the specified key name.<br/>
-/// <b><see cref="Script"/></b>'s can be used if <b>OPP</b> programming approach is preferred
-/// while dealing with a small amount (&lt; 100) of <see cref="Entity"/>'s
+/// Assign a custom tag name used for serialization of the annotated structs implementing <see cref="ITag"/>.<br/>
+/// <br/>
+/// This enables changing the struct name in code without changing the serialization format.  
 /// </summary>
 [AttributeUsage(AttributeTargets.Struct)]
 public sealed class TagAttribute : Attribute {
-    public TagAttribute (string key) { }
+    public TagAttribute (string name) { }
 }
 
 /// <summary>
-/// Annotated structs can be added as component to <see cref="Entity"/>'s using the specified key name.<br/>
-/// <b><c>struct</c></b> components are the preferred when dealing with a large amount (> 1.000) of <see cref="Entity"/>'s. 
+/// Assign a custom component name used for serialization of the annotated structs implementing <see cref="IComponent"/>.<br/>
+/// <br/>
+/// This enables changing the struct name in code without changing the serialization format.  
 /// </summary>
 [AttributeUsage(AttributeTargets.Struct)]
 public sealed class ComponentAttribute : Attribute {
@@ -27,9 +28,9 @@ public sealed class ComponentAttribute : Attribute {
 }
 
 /// <summary>
-/// Annotated classes a can be added as <see cref="Script"/>'s to <see cref="Entity"/>'s using the specified key name.<br/>
-/// <b><see cref="Script"/></b>'s can be used if <b>OPP</b> programming approach is preferred
-/// while dealing with a small amount (&lt; 100) of <see cref="Entity"/>'s
+/// Assign a custom script name used for serialization of the annotated class extending <see cref="Script"/>.<br/>
+/// <br/>
+/// This enables changing the class name in code without changing the serialization format.  
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public sealed class ScriptAttribute : Attribute {
