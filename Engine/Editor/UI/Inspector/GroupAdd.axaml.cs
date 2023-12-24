a94600bc58e0d48b56ad7b78335ab9e7d7503173
Ullrich Praetz <ullrich@praetz.de>
2023-12-24 10:35:19 +0100

    Engine - Editor: remove Unresolved from add component list

--- Engine/Editor/UI/Inspector/GroupAdd.axaml.cs
+++ Engine/Editor/UI/Inspector/GroupAdd.axaml.cs
@@ -20,10 +20,11 @@ public partial class GroupAdd : UserControl
     {
         InitializeComponent();
         List.SelectionChanged += (sender, args) => {
-            var index = List.SelectedIndex;
-            Console.WriteLine($"Select: {index}");
-            Add(index);
-            var button = EditorUtils.FindAncestor<Button>(this);
+            var item    = (ListBoxItem)List.SelectedItem;
+            var key     = item!.Name;
+            Console.WriteLine($"Select: {key}");
+            Add(key);
+            var button  = EditorUtils.FindAncestor<Button>(this);
             button.Flyout?.Hide();
         };
     }
@@ -40,40 +41,42 @@ public partial class GroupAdd : UserControl
                 var tags = schema.Tags;
                 for (int n = 1; n < tags.Length; n++) {
                     var tag = tags[n];
-                    List.Items.Add(new ListBoxItem { Content = tag.tagName });
+                    List.Items.Add(new ListBoxItem { Content = tag.tagName, Name = tag.tagName });
                 }
                 break;
             case "components":
                 var components = schema.Components;
                 for (int n = 1; n < components.Length; n++) {
                     var component = components[n];
-                    List.Items.Add(new ListBoxItem { Content = component.name });
+                    if (component.type == typeof(Unresolved)) {
+                        continue;
+                    }
+                    List.Items.Add(new ListBoxItem { Content = component.name, Name = component.componentKey });
                 }
                 break;
             case "scripts":
                 var scripts = schema.Scripts;
                 for (int n = 1; n < scripts.Length; n++) {
                     var script = scripts[n];
-                    List.Items.Add(new ListBoxItem { Content = script.name });
+                    List.Items.Add(new ListBoxItem { Content = script.name, Name = script.componentKey });
                 }
                 break;
         }
     }
     
-    private void Add(int index)
+    private void Add(string key)
     {
         var schema = EntityStore.GetEntitySchema();
-        index++; // Schema types start with index 1 
         var entity = Entity;
         switch (GroupName)
         {
             case "tags":
-                var tagType = schema.Tags[index];
-                var tag = new Tags(tagType);
+                var tagType = schema.TagTypeByName[key];
+                var tag     = new Tags(tagType);
                 entity.AddTags(tag);
                 break;
             case "components":
-                var componentType = schema.Components[index];
+                var componentType = (ComponentType)schema.SchemaTypeByKey[key];
                 if (componentType.type == typeof(EntityName)) {
                     if (entity.TryGetComponent<EntityName>(out var name)) {
                         entity.AddComponent(name);
@@ -87,7 +90,7 @@ public partial class GroupAdd : UserControl
                 inspector.Observer.FocusComponent(componentType);
                 break;
             case "scripts":
-                var scriptType = schema.Scripts[index];
+                var scriptType = (ScriptType)schema.SchemaTypeByKey[key];
                 EntityUtils.AddNewEntityScript(entity, scriptType);
                 inspector.Observer.FocusComponent(scriptType);
                 break;
