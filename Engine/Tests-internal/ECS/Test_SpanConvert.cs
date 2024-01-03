681caf950497d3efda366d5d33e263394ce638df
Ullrich Praetz <ullrich@praetz.de>
2024-01-03 09:49:20 +0100

    Engine - Tests-internal: add Test_SpanConvert

---
+++ Engine/Tests-internal/ECS/Test_SpanConvert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Friflo.Engine.ECS;
+using NUnit.Framework;
+using Tests.Utils;
+
+namespace Internal.ECS;
+
+// ReSharper disable InconsistentNaming
+public static class Test_SpanConvert
+{
+    [Test]
+    public static void Test_Convert_Position_to_Vectors() {
+        Span<Position> positions = new Position[] { new Position(1,1,1), new Position(2,2,2) };
+        var vectors = MemoryMarshal.Cast<Position, Vector3>(positions);
+        
+        Mem.AreEqual(positions.Length, vectors.Length);
+        Mem.AreEqual(positions[0].value, vectors[0]);
+        Mem.AreEqual(positions[1].value, vectors[1]);
+    }
+}
