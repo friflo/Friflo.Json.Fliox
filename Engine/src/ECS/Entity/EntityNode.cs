136ef187c396ee14d03dfd8a7e2a193b6a5008fc
Ullrich Praetz <ullrich@praetz.de>
2024-01-10 21:15:58 +0100

    Engine - ECS: add EntityNode comments

--- Engine/src/ECS/Entity/EntityNode.cs
+++ Engine/src/ECS/Entity/EntityNode.cs
@@ -67,7 +67,7 @@ public struct EntityNode
     [Browse(Never)] internal            NodeFlags   flags;          //  4 (1)
     /// <remarks> Is set to null only in <see cref="EntityStore.DeleteNode"/>. </remarks>
     [Browse(Never)] internal            Archetype   archetype;      //  8   can be null. Could use int to relieve GC tracing reference types 
-    [Browse(Never)] internal            int         compIndex;      //  4   index within Archetype entityIds & StructHeap<>.components
+    [Browse(Never)] internal            int         compIndex;      //  4   index within Archetype.entityIds & StructHeap<>.components
     [Browse(Never)] internal            int         scriptIndex;    //  4   0 if entity has no scripts
     #endregion
     
