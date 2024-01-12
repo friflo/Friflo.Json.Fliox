de0f8d9d04fcec0bb1c1fa8de9e1969969cbadb1
Ullrich Praetz <ullrich@praetz.de>
2024-01-12 13:59:16 +0100

    Engine - ECS: update experimental entity events

--- Engine/src/ECS/Archetype/EntityStore.Events.cs
+++ Engine/src/ECS/Archetype/EntityStore.Events.cs
@@ -15,22 +15,61 @@ public partial class EntityStoreBase
 #region entity events - experimental
     private void EntityTagsChanged(object sender, TagsChangedArgs args)
     {
-        if (!internBase.entityTagsChanged.TryGetValue(args.entityId, out var handler)) {
+        if (!internBase.entityTagsChanged.TryGetValue(args.entityId, out var handlers)) {
             return;
         }
-        handler(args);
+        foreach (var handler in handlers) {
+            handler(args);
+        }
     }
     
     internal static void AddEntityTagsChangedHandler(EntityStoreBase store, int entityId, Action<TagsChangedArgs> handler)
     {
         var entityHandler = store.internBase.entityTagsChanged;
         if (entityHandler == null) {
-            entityHandler = store.internBase.entityTagsChanged = new Dictionary<int, Action<TagsChangedArgs>>();
+            entityHandler = store.internBase.entityTagsChanged = new Dictionary<int, Action<TagsChangedArgs>[]>();
         }
         if (entityHandler.Count == 0) {
             store.OnTagsChanged += store.EntityTagsChanged;
         }
-        entityHandler.Add(entityId, handler);
+        if (entityHandler.TryGetValue(entityId, out var handlers)) {
+            var newHandlers = new Action<TagsChangedArgs>[handlers.Length + 1];
+            handlers.CopyTo(newHandlers, 0);
+            return;
+        }
+        entityHandler.Add(entityId, [handler]);
+    }
+    
+    internal static void RemoveEntityTagsChangedHandler(EntityStoreBase store, int entityId, Action<TagsChangedArgs> handler)
+    {
+        var entityHandler = store.internBase.entityTagsChanged;
+        if (entityHandler == null) {
+            return;
+        }
+        if (!entityHandler.TryGetValue(entityId, out var handlers)) {
+            return;
+        }
+        int index = Array.FindIndex(handlers, item => item == handler);
+        if (index == -1) {
+            return;
+        }
+        var newLength = handlers.Length - 1;
+        if (newLength > 0) {
+            var newHandler  = new Action<TagsChangedArgs>[newLength];
+            // --- remove handler as index
+            for (int n = 0; n < index; n++) {
+                newHandler[n] = handlers[n];
+            }
+            for (int n = index + 1; n <= newLength; n++) {
+                newHandler[n - 1] = handlers[n];
+            }
+            entityHandler[entityId] = newHandler;
+            return;
+        }
+        entityHandler.Remove(entityId);
+        if (entityHandler.Count == 0) {
+            store.OnTagsChanged -= store.EntityTagsChanged;
+        }
     }
     #endregion
 }
