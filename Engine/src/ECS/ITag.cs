5e7771061171efa8fe97bf8b417ab4ab79ff33c1
Ullrich Praetz <ullrich@praetz.de>
2024-01-18 17:35:14 +0100

    Engine - ECS: add ITags docs

--- Engine/src/ECS/ITag.cs
+++ Engine/src/ECS/ITag.cs
@@ -6,7 +6,12 @@
 namespace Friflo.Engine.ECS;
 
 /// <summary>
-/// Used to create entity <b>Tag</b>'s by declaring a struct without fields or properties extending <see cref="ITag"/>
+/// Used to create entity <b>Tag</b>'s by declaring a struct without fields or properties extending <see cref="ITag"/><br/>
+/// <br/>
+/// <b>Note:</b> Tags should be used to tag a group of multiple entities.<br/>
+/// <br/>
+/// In case you want to find a unique entity add the component <see cref="UniqueEntity"/> to an entity<b/><br/>
+/// and use <see cref="EntityStore.GetUniqueEntity"/> to query for this entity. 
 /// </summary>
 /// <remarks>
 /// Optionally attribute the implementing struct with <see cref="TagNameAttribute"/><br/>
