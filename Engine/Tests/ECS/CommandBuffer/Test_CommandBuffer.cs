b6f4d09fae2772bc49e70295ed1e7167f88c8d80
Ullrich Praetz <ullrich@praetz.de>
2024-01-26 13:56:21 +0100

    Engine - ECS: EntityCommandBuffer wip

--- Engine/Tests/ECS/CommandBuffer/Test_CommandBuffer.cs
+++ Engine/Tests/ECS/CommandBuffer/Test_CommandBuffer.cs
@@ -16,6 +16,7 @@ public static class Test_CommandBuffer
         var entity  = store.CreateEntity(1);
         var ecb     = new EntityCommandBuffer(store);
         
+        // --- structural change: add Position
         var pos1 = new Position(1, 1, 1);
         var pos2 = new Position(2, 2, 2);
         ecb.AddComponent(1, pos1);
@@ -26,11 +27,31 @@ public static class Test_CommandBuffer
         AreEqual(pos2.x,            entity.GetComponent<Position>().x);
         AreSame(entity.Archetype,   store.GetArchetype(ComponentTypes.Get<Position>()));
         
+        // --- handle remove after add
         ecb.AddComponent   <Position>(1);
         ecb.RemoveComponent<Position>(1);
         
         ecb.Playback();
         
         IsFalse(entity.HasComponent<Position>());
+        
+        // --- no structural change
+        ecb.AddComponent   <Position>(1);
+        ecb.RemoveComponent<Position>(1);
+        
+        ecb.Playback();
+        
+        IsFalse(entity.HasComponent<Position>());
+        
+        // --- archetype changes
+        /*
+        entity.AddComponent(new Rotation());
+        
+        ecb.AddComponent(1, pos1);
+        ecb.Playback();
+        
+        AreEqual(2, entity.Components.Count);
+        AreEqual(1, entity.Position.x);
+        */
     }
 }
