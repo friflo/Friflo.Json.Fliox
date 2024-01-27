bca98aa029e5f558fb6d539473e8c2df63bedd71
Ullrich Praetz <ullrich@praetz.de>
2024-01-27 14:01:15 +0100

    Engine - ECS: add Playback comment

--- Engine/src/ECS/CommandBuffer/Playback.cs
+++ Engine/src/ECS/CommandBuffer/Playback.cs
@@ -9,14 +9,15 @@ namespace Friflo.Engine.ECS;
 
 internal struct EntityChange
 {
-    internal ComponentTypes componentTypes;
-    internal Tags           tags;
+    internal            ComponentTypes                  componentTypes; // 32 
+    internal            Tags                            tags;           // 32
 }
 
 internal readonly struct Playback
 {
-    internal readonly   EntityStore                     store;          //  8
+    // possible opportunity for optimization as Dictionary values are using 64 bytes
     internal readonly   Dictionary<int, EntityChange>   entityChanges;  //  8
+    internal readonly   EntityStore                     store;          //  8
     
     internal Playback(EntityStore store) {
         this.store      = store;
