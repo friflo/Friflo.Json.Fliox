d5549a9efd342c281153a0a5d89f19d1226d0e84
Ullrich Praetz <ullrich@praetz.de>
2024-01-27 11:35:23 +0100

    Engine - ECS: moved Playback to separate file

--- Engine/src/ECS/CommandBuffer/ComponentCommands.cs
+++ Engine/src/ECS/CommandBuffer/ComponentCommands.cs
@@ -1,31 +1,11 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-
 using System;
-using System.Collections.Generic;
 using static Friflo.Engine.ECS.ComponentChangedAction;
 
 // ReSharper disable once CheckNamespace
 namespace Friflo.Engine.ECS;
-
-
-internal struct EntityChange
-{
-    internal ComponentTypes componentTypes;
-    internal Tags           tags;
-}
-
-internal readonly struct Playback
-{
-    internal readonly   EntityStore                     store;          //  8
-    internal readonly   Dictionary<int, EntityChange>   entityChanges;  //  8
-    
-    internal Playback(EntityStore store) {
-        this.store      = store;
-        entityChanges   = new Dictionary<int, EntityChange>();
-    }
-}
 
 internal abstract class ComponentCommands
 {
---
+++ Engine/src/ECS/CommandBuffer/Playback.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Ullrich Praetz. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+// ReSharper disable once CheckNamespace
+namespace Friflo.Engine.ECS;
+
+
+internal struct EntityChange
+{
+    internal ComponentTypes componentTypes;
+    internal Tags           tags;
+}
+
+internal readonly struct Playback
+{
+    internal readonly   EntityStore                     store;          //  8
+    internal readonly   Dictionary<int, EntityChange>   entityChanges;  //  8
+    
+    internal Playback(EntityStore store) {
+        this.store      = store;
+        entityChanges   = new Dictionary<int, EntityChange>();
+    }
+}
