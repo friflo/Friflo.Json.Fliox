c1c8981d9dc52600cb68837c1c7f207b9b14d2fc
Ullrich Praetz <ullrich@praetz.de>
2024-01-29 11:24:46 +0100

    Engine - ECS: EventRecorder - wip

--- Engine/src/ECS/Events/EventFilter.cs
+++ Engine/src/ECS/Events/EventFilter.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System;
 using System.Diagnostics.CodeAnalysis;
 
 // ReSharper disable LoopCanBeConvertedToQuery
@@ -12,11 +13,17 @@ namespace Friflo.Engine.ECS;
 [ExcludeFromCodeCoverage]
 internal sealed class EventFilter
 {
-#pragma warning disable CS0649 // Field is never assigned to, and will always have its default value
-    private readonly    int[]           addedComponents;
-    private readonly    int[]           removedComponents;
-    private readonly    int[]           addedTags;
-    private readonly    int[]           removedTags;
+    private             int[]           addedComponents;
+    private             int             addedComponentsCount;
+    //
+    private             int[]           removedComponents;
+    private             int             removedComponentsCount;
+    //
+    private             int[]           addedTags;
+    private             int             addedTagsCount;
+    //
+    private             int[]           removedTags;
+    private             int             removedTagsCount;
     
     private readonly    EntityEvents[]  componentAdded;
     private readonly    EntityEvents[]  componentRemoved;
@@ -32,12 +39,44 @@ internal sealed class EventFilter
         tagRemoved          = recorder.tagRemoved;
     }
     
+    public void ComponentAdded<T>()
+        where T : struct, IComponent
+    {
+        AddFilter(ref addedComponents, ref addedComponentsCount, StructHeap<T>.StructIndex);
+    }
+    
+    public void ComponentRemoved<T>()
+        where T : struct, IComponent
+    {
+        AddFilter(ref removedComponents, ref removedComponentsCount, StructHeap<T>.StructIndex);
+    }
+    
+    public void TagAdded<T>()
+        where T : struct, ITag
+    {
+        AddFilter(ref addedTags, ref addedTagsCount, TagType<T>.TagIndex);
+    }
+    
+    public void TagRemoved<T>()
+        where T : struct, ITag
+    {
+        AddFilter(ref removedTags, ref removedTagsCount, TagType<T>.TagIndex);
+    }
+    
+    private static void AddFilter(ref int[] filter, ref int count, int typeIndex)
+    {
+        if (filter == null || count == filter.Length) {
+            ArrayUtils.Resize(ref filter, Math.Max(4, 2 * count));
+        }
+        filter[count++] = typeIndex;
+    }
+    
     public bool Filter(int entityId)
     {
-        if (Contains(addedComponents,   componentAdded,     entityId))  return true;
-        if (Contains(removedComponents, componentRemoved,   entityId))  return true;
-        if (Contains(addedTags,         tagAdded,           entityId))  return true;
-        if (Contains(removedTags,       tagRemoved,         entityId))  return true;
+        if (addedComponents   != null && Contains(addedComponents,   componentAdded,     entityId))  return true;
+        if (removedComponents != null && Contains(removedComponents, componentRemoved,   entityId))  return true;
+        if (addedTags         != null && Contains(addedTags,         tagAdded,           entityId))  return true;
+        if (removedTags       != null && Contains(removedTags,       tagRemoved,         entityId))  return true;
         return false;
     }
     
