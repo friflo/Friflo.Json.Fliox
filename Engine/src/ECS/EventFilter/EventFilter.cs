2bbdbeef54718808351d0c5837a053ef65783cb8
Ullrich Praetz <ullrich@praetz.de>
2024-01-30 14:41:54 +0100

    Engine - ECS: remove unused import

--- Engine/src/ECS/EventFilter/EventFilter.cs
+++ Engine/src/ECS/EventFilter/EventFilter.cs
@@ -2,7 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using static System.Diagnostics.DebuggerBrowsableState;
