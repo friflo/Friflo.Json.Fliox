c2197e6a036a803433e113f1176005379a22a633
Ullrich Praetz <ullrich@praetz.de>
2024-01-30 10:42:50 +0100

    Engine - Tests: moved Test_EventFilter_filter_ToString() back to internal tests

---
+++ Engine/Tests-internal/ECS/Test_EventFilter.cs
@@ -0,0 +1,51 @@
+﻿using Friflo.Engine.ECS;
+using NUnit.Framework;
+using Tests.ECS;
+using static NUnit.Framework.Assert;
+
+// ReSharper disable UseObjectOrCollectionInitializer
+// ReSharper disable InconsistentNaming
+namespace Internal.ECS;
+
+public static class Test_EventFilter
+{
+    [Test]
+    public static void Test_EventFilter_filter_ToString()
+    {
+        var componentEvents = new EntityEvents();
+        AreEqual("events: 0", componentEvents.ToString());
+        
+        var store           = new EntityStore(PidType.UsePidAsId);
+        var recorder        = store.EventRecorder;
+        
+        var positionAdded   = new EventFilter(recorder);
+        var positionRemoved = new EventFilter(recorder);
+        var tagAdded        = new EventFilter(recorder);
+        var tagRemoved      = new EventFilter(recorder);
+        
+        positionAdded.  ComponentAdded  <Position>();
+        positionRemoved.ComponentRemoved<Position>();
+        tagAdded.       TagAdded        <TestTag>();
+        tagRemoved.     TagRemoved      <TestTag>();
+        
+        AreEqual("added: [Position]  removed: []",  positionAdded.ToString());
+        AreEqual("added: []  removed: [Position]",  positionRemoved.ToString());
+        AreEqual("added: [#TestTag]  removed: []",  tagAdded.ToString());
+        AreEqual("added: []  removed: [#TestTag]",  tagRemoved.ToString());
+        
+        AreEqual("added: [Position]  removed: []",  positionAdded.componentFilters.ToString());
+        AreEqual("added: []  removed: [Position]",  positionRemoved.componentFilters.ToString());
+        AreEqual("added: [#TestTag]  removed: []",  tagAdded.tagFilters.ToString());
+        AreEqual("added: []  removed: [#TestTag]",  tagRemoved.tagFilters.ToString());
+        
+        AreEqual("[]",                              positionAdded.tagFilters.ToString());
+        
+        var filter = new EventFilter(recorder);
+        filter.ComponentAdded   <Position>();
+        filter.ComponentRemoved <Position>();
+        filter.TagAdded         <TestTag>();
+        filter.TagRemoved       <TestTag>();
+        
+        AreEqual("added: [Position, #TestTag]  removed: [Position, #TestTag]", filter.ToString());
+    }
+}
--- Engine/Tests/ECS/EventFilter/Test_EventFilter.cs
+++ Engine/Tests/ECS/EventFilter/Test_EventFilter.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using Friflo.Engine.ECS;
 using NUnit.Framework;
-using Tests.ECS;
 using Tests.Utils;
 using static NUnit.Framework.Assert;
 
@@ -12,7 +11,6 @@ namespace Tests.ECS.Filter;
 // ReSharper disable once InconsistentNaming
 public static class Test_EventFilter
 {
-
     [Test]
     public static void Test_EventFilter_Internal()
     {
@@ -41,46 +39,6 @@ public static class Test_EventFilter
             _ = ev.ToString();
         });
     }
-    /*
-    [Test]
-    public static void Test_EventFilter_filter_ToString()
-    {
-        var componentEvents = new EntityEvents();
-        AreEqual("events: 0", componentEvents.ToString());
-        
-        var store           = new EntityStore(PidType.UsePidAsId);
-        var recorder        = store.EventRecorder;
-        
-        var positionAdded   = new EventFilter(recorder);
-        var positionRemoved = new EventFilter(recorder);
-        var tagAdded        = new EventFilter(recorder);
-        var tagRemoved      = new EventFilter(recorder);
-        
-        positionAdded.  ComponentAdded  <Position>();
-        positionRemoved.ComponentRemoved<Position>();
-        tagAdded.       TagAdded        <TestTag>();
-        tagRemoved.     TagRemoved      <TestTag>();
-        
-        AreEqual("added: [Position]  removed: []",  positionAdded.ToString());
-        AreEqual("added: []  removed: [Position]",  positionRemoved.ToString());
-        AreEqual("added: [#TestTag]  removed: []",  tagAdded.ToString());
-        AreEqual("added: []  removed: [#TestTag]",  tagRemoved.ToString());
-        
-        AreEqual("added: [Position]  removed: []",  positionAdded.componentFilters.ToString());
-        AreEqual("added: []  removed: [Position]",  positionRemoved.componentFilters.ToString());
-        AreEqual("added: [#TestTag]  removed: []",  tagAdded.tagFilters.ToString());
-        AreEqual("added: []  removed: [#TestTag]",  tagRemoved.tagFilters.ToString());
-        
-        AreEqual("[]",                              positionAdded.tagFilters.ToString());
-        
-        var filter = new EventFilter(recorder);
-        filter.ComponentAdded   <Position>();
-        filter.ComponentRemoved <Position>();
-        filter.TagAdded         <TestTag>();
-        filter.TagRemoved       <TestTag>();
-        
-        AreEqual("added: [Position, #TestTag]  removed: [Position, #TestTag]", filter.ToString());
-    } */
     
     [Test]
     public static void Test_EventFilter_create_recorder()
