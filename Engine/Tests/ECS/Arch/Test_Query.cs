d5992a1b617b6556a2e584bb405d65fce1c01c60
friflo <ullrich@praetz.de>
2024-01-31 20:15:56 +0100

    Engine - Tests: adjust allocation size for macOS

--- Engine/Tests/ECS/Arch/Test_Query.cs
+++ Engine/Tests/ECS/Arch/Test_Query.cs
@@ -219,7 +219,7 @@ public static class Test_Query
         
         var sig     = Signature.Get<Position, Rotation>();
         _           = store.Query(sig); // for one time allocation for Mem check
-        var expect  = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? 416 : 416;
+        var expect  = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? 400 : 416;
         var start   = GetAllocatedBytes();
         var query   = store.Query(sig);
         AssertAlloc(start, expect);
