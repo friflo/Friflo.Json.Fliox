9fedfb2e5a9530ce9965f9c58aa64c68a2667b9d
Ullrich Praetz <ullrich@praetz.de>
2024-01-31 09:16:51 +0100

    Engine - Tests: add Test_CommandBuffer_debug_properties() assertions

--- Engine/Tests-internal/ECS/Test_CommandBuffer.cs
+++ Engine/Tests-internal/ECS/Test_CommandBuffer.cs
@@ -30,19 +30,30 @@ public static class Test_CommandBuffer
         var store   = new EntityStore(PidType.UsePidAsId);
         
         var ecb = store.GetCommandBuffer();
+        
+        AreEqual(0, ecb.ComponentCommandsCount);
+        
         ecb.CreateEntity();
         
         ecb.AddTag          <TestTag>(1);
         ecb.AddComponent    <Position>(1);
         ecb.RemoveComponent <Position>(1);
         
+        
+        AreEqual(2,                                 ecb.ComponentCommandsCount);
         var componentCommands = (ComponentCommands<Position>)ecb.ComponentCommands[0];
+        AreEqual(2,                                 componentCommands.commandCount);
         AreEqual("[Position] commands - Count: 2",  componentCommands.ToString());
         AreEqual(2,                                 componentCommands.Commands.Length);
         AreEqual("entity: 1 - Add [Position]",      componentCommands.Commands[0].ToString());
         AreEqual("entity: 1 - Remove [Position]",   componentCommands.Commands[1].ToString());
         
+        AreEqual(1,                                 ecb.TagCommandsCount);
+        AreEqual(1,                                 ecb.TagCommands.Length);
         AreEqual("entity: 1 - Add [#TestTag]",      ecb.TagCommands[0].ToString());
+        
+        AreEqual(1,                                 ecb.EntityCommandsCount);
+        AreEqual(1,                                 ecb.EntityCommands.Length);
         AreEqual("Create entity - id: 1",           ecb.EntityCommands[0].ToString());
     }
 }
