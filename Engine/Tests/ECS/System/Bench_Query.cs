4953c416340c424fae28fcb864a73f9f3be1e18f
Ullrich Praetz <ullrich@praetz.de>
2024-02-11 08:51:22 +0100

    Engine - Test: support Test_Bench() runParallel

--- Engine/Tests/ECS/System/Bench_Query.cs
+++ Engine/Tests/ECS/System/Bench_Query.cs
@@ -11,8 +11,9 @@ namespace Tests.ECS.System;
 // ReSharper disable InconsistentNaming
 public static class Bench_Query
 {
-    private static readonly int entityCount     = 32;   // 32 /   100_000
-    private static readonly int JitLoop         = 10;   // 10 / 5_000_000
+    private static readonly int     entityCount = 32;   // 32 /   100_000
+    private static readonly int     JitLoop     = 10;   // 10 / 5_000_000
+    private static readonly bool    runParallel = false;
     
     [Test]
     public static void Test_BenchRef()
@@ -43,7 +44,8 @@ public static class Bench_Query
     [Test]
     public static void Test_Bench()
     {
-        var store   = new EntityStore(PidType.UsePidAsId);
+        using var runner = new ParallelJobRunner(8);
+        var store   = new EntityStore(PidType.UsePidAsId) { JobRunner = runner };
         var archetype = store.GetArchetype(ComponentTypes.Get<MyComponent1>());
         for (int n = 0; n < 32; n++) {
             archetype.CreateEntity();
@@ -52,8 +54,8 @@ public static class Bench_Query
         // --- enable JIT optimization
         var  query = store.Query<MyComponent1>();
         for (int i = 0; i < JitLoop; i++) {
-            Bench_SIMD(query);
-            bench(query);
+            Bench_SIMD(query, runParallel);
+            bench     (query, runParallel);
         }
         
         for (int n = 0; n < entityCount - 32; n++) {
@@ -63,7 +65,7 @@ public static class Bench_Query
         var stopwatch = new Stopwatch(); stopwatch.Start();
         for (int i = 0; i < 1000; i++) {
             // 1000 ~ #PC: 42,1 ms - component: int,   42,1 ms - component: byte
-            bench(query);
+            bench(query, runParallel);
         }
         Console.WriteLine($"Iterate - Span<MyComponent1>: {TestUtils.StopwatchMillis(stopwatch)} ms");
         
@@ -71,37 +73,56 @@ public static class Bench_Query
         stopwatch = new Stopwatch(); stopwatch.Start();
         for (int i = 0; i < 1000; i++) {
             // 1000 ~ #PC: 14,7 ms - component: int,   2,6 ms - component: byte
-            Bench_SIMD(query);
+            Bench_SIMD(query, runParallel);
         }
         Console.WriteLine($"Iterate - SIMD: {TestUtils.StopwatchMillis(stopwatch)} ms");
     }
     
-    private static void bench(ArchetypeQuery<MyComponent1> query)
+    private static void bench(ArchetypeQuery<MyComponent1> query, bool runParallel)
     {
+        if (runParallel) {
+            var job = query.ForEach((component, _) => {
+                foreach (ref var value in  component.Span) {
+                    ++value.a;
+                }
+            });
+            job.RunParallel();
+            return;
+        }
         foreach (var (component, _) in query.Chunks)
         {
-            var components = component.Span;
-            for (int n = 0; n < components.Length; n++) {
-                ++components[n].a;
+            foreach (ref var value in  component.Span) {
+                ++value.a;
             }
         }
     }
     
-    private static void Bench_SIMD(ArchetypeQuery<MyComponent1> query)
+    private static void Bench_SIMD(ArchetypeQuery<MyComponent1> query, bool runParallel)
+    {
+        if (runParallel) {
+            var job = query.ForEach((component, _) => {
+                SIMD_Add(component);
+            });
+            job.RunParallel();
+            return;
+        }
+        foreach (var (component, _) in query.Chunks) {
+            SIMD_Add(component);
+        }
+    }
+    
+    private static void SIMD_Add(Chunk<MyComponent1> component)
     {
         // Requires .NET 8 to enable SIMD on ARM (Apple Silicon).
         // See: [What's new in .NET 8] https://learn.microsoft.com/en-us/dotnet/core/whats-new/dotnet-8#systemnumerics-and-systemruntimeintrinsics
-        var add = Vector256.Create<int>(1);                 // create byte[32] vector - all values = 1
-        foreach (var (component, _) in query.Chunks)
-        {
-            var bytes   = component.AsSpan256<int>();       // bytes.Length - multiple of 32
-            var step    = component.StepSpan256;            // step = 32
-            for (int n = 0; n < bytes.Length; n += step) {
-                var slice   = bytes.Slice(n, step);
-                var value   = Vector256.Create<int>(slice);
-                var result  = Vector256.Add(value, add);    // execute 32 add instructions at once
-                result.CopyTo(slice);
-            }
+        var add     = Vector256.Create<int>(1);         // create byte[32] vector - all values = 1
+        var bytes   = component.AsSpan256<int>();       // bytes.Length - multiple of 32
+        var step    = component.StepSpan256;            // step = 32
+        for (int n = 0; n < bytes.Length; n += step) {
+            var slice   = bytes.Slice(n, step);
+            var value   = Vector256.Create<int>(slice);
+            var result  = Vector256.Add(value, add);    // execute 32 add instructions at once
+            result.CopyTo(slice);
         }
     }
     
