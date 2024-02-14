6a6942ca13c0ba535f4d27aa241793c598bccd38
Ullrich Praetz <ullrich@praetz.de>
2024-02-14 19:26:19 +0100

    Engine - ECS: EntityBatch - wip

--- Engine/Tests-internal/ECS/Test_Batch.cs
+++ Engine/Tests-internal/ECS/Test_Batch.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics;
 using Friflo.Engine.ECS;
 using NUnit.Framework;
 using Tests.ECS;
@@ -12,9 +14,9 @@ public static class Test_Batch
     public static void Test_Batch_Entity()
     {
         var store = new EntityStore();
-        store.OnComponentAdded      += _ => { };
-        store.OnComponentRemoved    += _ => { };
-        store.OnTagsChanged         += _ => { }; 
+        // store.OnComponentAdded      += _ => { };
+        // store.OnComponentRemoved    += _ => { };
+        // store.OnTagsChanged         += _ => { }; 
         var entity = store.CreateEntity();
         
         entity.Batch
@@ -40,6 +42,39 @@ public static class Test_Batch
         
         Assert.AreEqual("id: 1  [Position, #TestTag2]", entity.ToString());
         Assert.AreEqual(new Position(2, 2, 2), entity.Position);
+    }
+    
+    [Test]
+    public static void Test_Batch_Entity_Perf()
+    {
+        long count = 10; // 10_000_000 ~ #PC: 1620 ms
+        var store       = new EntityStore();
+        var entity      = store.CreateEntity();
+        var addTags     = Tags.Get<TestTag2>();
+        var removeTags  = Tags.Get<TestTag>();
+        
+        var sw = new Stopwatch();
+        sw.Start();
+
+        for (int n = 0; n < count; n++)
+        {
+            entity.Batch
+                .AddComponent   (new Position(1, 1, 1))
+                .AddComponent   (new EntityName("test"))
+                .RemoveComponent<Rotation>()
+                .AddTag         <TestTag>()
+                .RemoveTag      <TestTag2>()
+                .Apply();
+        
+            entity.Batch
+                .AddComponent   (new Position(2, 2, 2))
+                .RemoveComponent<EntityName>()
+                .AddTags        (addTags)
+                .RemoveTags     (removeTags)
+                .Apply();
+        }
+        
+        Console.WriteLine($"Entity.Batch - duration: {sw.ElapsedMilliseconds} ms");
     }
 }
 
