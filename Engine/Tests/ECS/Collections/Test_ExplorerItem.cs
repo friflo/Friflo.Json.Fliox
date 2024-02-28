4d41c8fbf6767c4edfeb9664ac3e387a9caa2ed3
Ullrich Praetz <ullrich@praetz.de>
2024-02-28 07:51:24 +0100

    Engine - Tests: removed ReSharper warning

--- Engine/Tests/ECS/Collections/Test_ExplorerItem.cs
+++ Engine/Tests/ECS/Collections/Test_ExplorerItem.cs
@@ -55,9 +55,7 @@ public static class Test_ExplorerItem
         AreEqual(4, root.ChildIds[1]);
         // AreEqual(TreeMembership.treeNode, floating4.TreeMembership); // todo
         var floating4Item = tree.GetItemById(floating4.Id);
-        floating4Item.CollectionChanged += (_, args) => {
-            Fail($"unexpected event: {args}");
-        };
+        floating4Item.CollectionChanged += (_, args) => throw new InvalidOperationException($"unexpected event: {args}");
         floating4.RemoveChild(floating5);   // fires no event as floating5 has no corresponding ExplorerItem
         
         AreEqual(2, rootEvents.seq);
