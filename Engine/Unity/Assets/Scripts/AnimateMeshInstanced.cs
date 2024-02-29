f693a6a799d88953a10a77e8a4cd17d37f23ed45
Ullrich Praetz <ullrich@praetz.de>
2024-02-29 21:22:55 +0100

    Engine - Unity: add HUD for FPS. disavle VSync for standalone builds

--- Engine/Unity/Assets/Scripts/AnimateMeshInstanced.cs
+++ Engine/Unity/Assets/Scripts/AnimateMeshInstanced.cs
@@ -1,38 +1,43 @@
 ﻿using System;
 using Friflo.Engine.ECS;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimateMeshInstanced : MonoBehaviour
 {
+    [SerializeField] private TMP_Text fpsText;
+    [SerializeField] private Toggle   toggle;
+
+    
     private ArchetypeQuery<Position> query;
     private QueryJob<Position> job;
     private ParallelJobRunner runner;
 
     public Material material;
     public Mesh mesh;
-    const int numInstances = 100_000;
+    private const int NumInstances = 100_000;
     float delta;
     RenderParams rp;
     Matrix4x4[] instData;
-
+    
     void Start()
     {
         rp = new RenderParams(material);
-        instData = new Matrix4x4[numInstances];
+        instData = new Matrix4x4[NumInstances];
         runner = new ParallelJobRunner(8);
         var store = new EntityStore() { JobRunner = runner };
         // create entities with a Position component
-        for (int n = 0; n < numInstances; n++) {
+        for (int n = 0; n < NumInstances; n++) {
             store.Batch()
                 .Add(new Position(n, 0, 0))
                 .CreateEntity();    
         }
         query = store.Query<Position>();
-        job = query.ForEach((positions, entities) => {
+        job = query.ForEach((positions, _) => {
             UpdatePositions(positions.Span);
         });
     }
-
 
     private void UpdatePositions(Span<Position> positions) {
         int i = 0;
@@ -41,20 +46,46 @@ public class AnimateMeshInstanced : MonoBehaviour
             instData[i] = Matrix4x4.Translate(new Vector3(-4.5f + i++, position.y, 0f));
         }
     }
-    
-    public bool runParallel = false;
 
+    private const int FPSSampleCount = 30;
+    private readonly int[] fpsSamples = new int[FPSSampleCount];
+    private int sampleIndex;
+    public bool runParallel;
+
+    private void UpdateFps()
+    {
+        var sum = 0;
+        for (var i = 0; i < FPSSampleCount; i++)
+        {
+            sum += fpsSamples[i];
+        }
+
+        fpsText.text = $"FPS: {sum / FPSSampleCount}";
+    }
+    
     void Update()
     {
+        fpsSamples[sampleIndex++] = (int)(1.0f / Time.deltaTime);
+        if (sampleIndex >= FPSSampleCount) sampleIndex = 0;
+        
+        UpdateFps();
+        
+        
         delta = Time.deltaTime / 4;
         if (runParallel) {
             job.RunParallel();
         } else {
-            foreach (var (positions, entities) in query.Chunks) {
+            foreach (var (positions, _) in query.Chunks) {
                 UpdatePositions(positions.Span);
             }
         }
         Graphics.RenderMeshInstanced(rp, mesh, 0, instData);
+        
+        toggle.onValueChanged .AddListener((dd) => {
+            
+            runParallel = dd;
+            // runParallel = _toggle.isOn;
+        });
     }
 
     private void OnDestroy() {
