29f7cff64b64ceaa531d75fe334ba02471743eb4
Ullrich Praetz <ullrich@praetz.de>
2024-03-12 14:27:04 +0100

    Tests - add test for struct with non CLS compliant field types

--- Json.Tests/Common/UnitTest/Fliox/Mapper/TestReadWriteNonCls.cs
+++ Json.Tests/Common/UnitTest/Fliox/Mapper/TestReadWriteNonCls.cs
@@ -27,6 +27,22 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Mapper
         public ushort   srtPrp { get; set; }
         public sbyte    bytPrp { get; set; }
     }
+    
+    public struct TestNonClsStruct
+    {
+        public ulong[]      ulongArray;
+        public List<ulong>  ulongList;
+        //
+        public ulong    lngFld;
+        public uint     intFld;
+        public ushort   srtFld;
+        public sbyte    bytFld;
+        //
+        // public ulong    lngPrp { get; set; }
+        // public uint     intPrp { get; set; }
+        // public ushort   srtPrp { get; set; }
+        // public sbyte    bytPrp { get; set; }
+    }
 
     public static class TestReadWriteNonCls
     {
@@ -34,7 +50,7 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Mapper
         [Test]
         public static void TestReadWriteNonCls_Mapper()
         {
-            string testClassJson = $@"
+            string testJson = $@"
 {{
     ""ulongArray"": [0, 18446744073709551615],
     ""ulongList"":  [1, 18446744073709551615],
@@ -53,16 +69,27 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Mapper
             var typeStore    = new TypeStore();
             var reader       = new ObjectReader(typeStore);
             var writer       = new ObjectWriter(typeStore);
-            
-            var obj = reader.Read<TestNonClsClass>(testClassJson);
-            AssertMembers(obj);
-            
-            var json = writer.Write(obj);
-            obj = reader.Read<TestNonClsClass>(json);
-            AssertMembers(obj);
+            // --- class
+            {            
+                var testClass = reader.Read<TestNonClsClass>(testJson);
+                AssertClassMembers(testClass);
+                
+                var json = writer.Write(testClass);
+                testClass = reader.Read<TestNonClsClass>(json);
+                AssertClassMembers(testClass);
+            }
+            // --- struct
+            {            
+                var testStruct = reader.Read<TestNonClsStruct>(testJson);
+                AssertStructMembers(testStruct);
+                
+                var json = writer.Write(testStruct);
+                var obj = reader.Read<TestNonClsStruct>(json);
+                AssertStructMembers(obj);
+            }
         }
         
-        private static void AssertMembers(TestNonClsClass obj)
+        private static void AssertClassMembers(TestNonClsClass obj)
         {
             AreEqual(new ulong[]    {0, 18446744073709551615}, obj.ulongArray);
             AreEqual(new List<ulong>{1, 18446744073709551615}, obj.ulongList);
@@ -71,6 +98,17 @@ namespace Friflo.Json.Tests.Common.UnitTest.Fliox.Mapper
             AreEqual(101, obj.intPrp);
             AreEqual(102, obj.srtPrp);
             AreEqual(103, obj.bytPrp);
+            
+            AreEqual(202, obj.lngFld);
+            AreEqual(203, obj.intFld);
+            AreEqual(204, obj.srtFld);
+            AreEqual( 25, obj.bytFld);   
+        }
+        
+        private static void AssertStructMembers(TestNonClsStruct obj)
+        {
+            AreEqual(new ulong[]    {0, 18446744073709551615}, obj.ulongArray);
+            AreEqual(new List<ulong>{1, 18446744073709551615}, obj.ulongList);
             
             AreEqual(202, obj.lngFld);
             AreEqual(203, obj.intFld);
