4ea6dbbadcad389995bceeb2f009460ef83ee89f
Ullrich Praetz <ullrich@praetz.de>
2024-03-15 11:01:10 +0100

    Engine - removed ReSharper warnings

--- Engine/src/ECS/Base/NamespaceDoc.cs
+++ Engine/src/ECS/Base/NamespaceDoc.cs
@@ -1,28 +1,30 @@
-﻿// ReSharper disable once CheckNamespace
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable RedundantTypeDeclarationBody
+
 namespace Friflo.Engine.ECS {
     /// <summary>
     /// Contains types and methods to query, add, remove or change <see cref="Entity"/>'s in an <see cref="EntityStore"/>.<br/>
     /// </summary>
-    internal class NamespaceDoc { }
+    internal static class NamespaceDoc { }
 }
 
 namespace Friflo.Engine.ECS.Serialize {
     /// <summary>
     /// Contains type and methods to serialize / deserialize <see cref="Entity"/>'s as JSON.
     /// </summary>
-    internal class NamespaceDoc { }
+    internal static class NamespaceDoc { }
 }
 
 namespace Friflo.Engine.ECS.Utils {
     /// <summary>
     /// Utility types and methods typically used by generic libraries.
     /// </summary>
-    internal class NamespaceDoc { }
+    internal static class NamespaceDoc { }
 }
 
 namespace Friflo.Engine.ECS.Collections {
     /// <summary>
     /// Contains types to enable data binding for common .NET applications.
     /// </summary>
-    internal class NamespaceDoc { }
+    internal static class NamespaceDoc { }
 }
