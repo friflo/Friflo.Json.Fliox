6e45a69a61ceca16d003c2cded32a3170f1dc7aa
Ullrich Praetz <ullrich@praetz.de>
2024-03-15 10:56:52 +0100

    Engine - add namespace docs

---
+++ Engine/src/ECS/Base/NamespaceDoc.cs
@@ -0,0 +1,28 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Friflo.Engine.ECS {
+    /// <summary>
+    /// Contains types and methods to query, add, remove or change <see cref="Entity"/>'s in an <see cref="EntityStore"/>.<br/>
+    /// </summary>
+    internal class NamespaceDoc { }
+}
+
+namespace Friflo.Engine.ECS.Serialize {
+    /// <summary>
+    /// Contains type and methods to serialize / deserialize <see cref="Entity"/>'s as JSON.
+    /// </summary>
+    internal class NamespaceDoc { }
+}
+
+namespace Friflo.Engine.ECS.Utils {
+    /// <summary>
+    /// Utility types and methods typically used by generic libraries.
+    /// </summary>
+    internal class NamespaceDoc { }
+}
+
+namespace Friflo.Engine.ECS.Collections {
+    /// <summary>
+    /// Contains types to enable data binding for common .NET applications.
+    /// </summary>
+    internal class NamespaceDoc { }
+}
