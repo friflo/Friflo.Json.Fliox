ea92e23d99570f91e6099103ac3ab05bfa114c0b
Ullrich Praetz <ullrich@praetz.de>
2024-04-02 18:10:52 +0200

    Engine - ECS: moved EntityStoreExtensions.CreateEntity() overload to Extension folder

--- Engine/src/ECS/Entity/Store/Entities.cs
+++ Engine/src/ECS/Entity/Store/Entities.cs
@@ -6,6 +6,8 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using Friflo.Engine.ECS.Serialize;
 using static Friflo.Engine.ECS.NodeFlags;
+using static Friflo.Engine.ECS.StoreOwnership;
+using static Friflo.Engine.ECS.TreeMembership;
 
 // ReSharper disable UseNullPropagation
 // ReSharper disable ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator
@@ -22,6 +24,38 @@ public partial class EntityStore
     /// <see cref="IComponent"/>, <see cref="ITag"/> and <see cref="Script"/> types.
     /// </summary>
     public static     EntitySchema         GetEntitySchema()=> Static.EntitySchema;
+    
+    /// <summary>
+    /// Create and return a new <see cref="Entity"/> in the entity store.<br/>
+    /// See <a href="https://github.com/friflo/Friflo.Json.Fliox/blob/main/Engine/README.md#entity">Example.</a>
+    /// </summary>
+    /// <returns>An <see cref="attached"/> and <see cref="floating"/> entity</returns>
+    public Entity CreateEntity()
+    {
+        var id = NewId();
+        CreateEntityInternal(defaultArchetype, id);
+        var entity = new Entity(this, id);
+        
+        // Send event. See: SEND_EVENT notes
+        CreateEntityEvent(entity);
+        return entity;
+    }
+    
+    /// <summary>
+    /// Create and return a new <see cref="Entity"/> with the passed <paramref name="id"/> in the entity store.
+    /// </summary>
+    /// <returns>an <see cref="attached"/> and <see cref="floating"/> entity</returns>
+    public Entity CreateEntity(int id)
+    {
+        CheckEntityId(id);
+        CreateEntityInternal(defaultArchetype, id);
+        var entity = new Entity(this, id); 
+        
+        // Send event. See: SEND_EVENT notes
+        CreateEntityEvent(entity);
+        return entity;
+    }
+    
     
     internal void CheckEntityId(int id)
     {
--- Engine/src/ECS/Entity/Store/Extensions/CreateEntity.cs
+++ Engine/src/ECS/Entity/Store/Extensions/CreateEntity.cs
@@ -2,46 +2,10 @@
 // See LICENSE file in the project root for full license information.
 
 
-using static Friflo.Engine.ECS.StoreOwnership;
-using static Friflo.Engine.ECS.TreeMembership;
-
 // ReSharper disable UseNullPropagation
 // ReSharper disable once CheckNamespace
 namespace Friflo.Engine.ECS;
 
-public partial class EntityStore
-{
-    /// <summary>
-    /// Create and return a new <see cref="Entity"/> in the entity store.<br/>
-    /// See <a href="https://github.com/friflo/Friflo.Json.Fliox/blob/main/Engine/README.md#entity">Example.</a>
-    /// </summary>
-    /// <returns>An <see cref="attached"/> and <see cref="floating"/> entity</returns>
-    public Entity CreateEntity()
-    {
-        var id = NewId();
-        CreateEntityInternal(defaultArchetype, id);
-        var entity = new Entity(this, id);
-        
-        // Send event. See: SEND_EVENT notes
-        CreateEntityEvent(entity);
-        return entity;
-    }
-    
-    /// <summary>
-    /// Create and return a new <see cref="Entity"/> with the passed <paramref name="id"/> in the entity store.
-    /// </summary>
-    /// <returns>an <see cref="attached"/> and <see cref="floating"/> entity</returns>
-    public Entity CreateEntity(int id)
-    {
-        CheckEntityId(id);
-        CreateEntityInternal(defaultArchetype, id);
-        var entity = new Entity(this, id); 
-        
-        // Send event. See: SEND_EVENT notes
-        CreateEntityEvent(entity);
-        return entity;
-    }
-}
 
 /// <summary>
 /// Provide generic <c>CreateEntity()</c> overloads to create entities with passed components without any structural change.
