610ce646a007a82c5c4d856403ae39be9e972a80
Ullrich Praetz <ullrich@praetz.de>
2024-05-06 10:50:46 +0200

    Engine - Systems: moved SystemPerf to separate file

---
+++ Engine/src/ECS/Systems/BaseSystem.Perf.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using Friflo.Json.Fliox;
+using static Friflo.Engine.ECS.Systems.SystemExtensions;
+using static System.Diagnostics.DebuggerBrowsableState;
+using Browse = System.Diagnostics.DebuggerBrowsableAttribute;
+// Hard Rule! file must not have any dependency a to a specific game engine. E.g. Unity, Godot, Monogame, ...
+
+// ReSharper disable once CheckNamespace
+namespace Friflo.Engine.ECS.Systems
+{
+    public struct SystemPerf
+    {
+        /// <remarks>Can be 0 in case execution time was below <see cref="Stopwatch.Frequency"/> precision.</remarks>
+        public          int     UpdateCount => updateCount;
+        public          double  LastMs      => lastTicks >= 0 ? lastTicks * StopwatchPeriodMs : -1;
+        public          double  SumMs       => sumTicks * StopwatchPeriodMs;
+
+        public override string  ToString() => $"updates: {UpdateCount} last: {LastMs:0.###} sum: {SumMs:0.###}";
+
+        [Ignore]    [Browse(Never)] internal            int     updateCount;
+        [Ignore]    [Browse(Never)] internal            long    lastTicks;
+        [Ignore]    [Browse(Never)] internal readonly   long[]  history;
+        [Ignore]    [Browse(Never)] internal            long    sumTicks;
+        
+        internal SystemPerf(long[] history) {
+            this.history = history;
+        }
+    }
+    
+    internal sealed class View
+    {
+        public  Tick                    Tick        => system.Tick;
+        public  int                     Id          => system.Id;
+        public  bool                    Enabled     => system.Enabled;
+        public  string                  Name        => system.Name;
+        public  SystemRoot              SystemRoot  => system.SystemRoot;
+        public  SystemGroup             ParentGroup => system.ParentGroup;
+        public  SystemPerf              Perf        => system.perf;
+
+        public override string          ToString()  => $"Enabled: {Enabled}  Id: {Id}";
+
+        [Browse(Never)] private readonly BaseSystem   system;
+        
+        internal View(BaseSystem system) {
+            this.system = system;
+        }
+    }
+}
--- Engine/src/ECS/Systems/BaseSystem.cs
+++ Engine/src/ECS/Systems/BaseSystem.cs
@@ -2,9 +2,7 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
-using System.Diagnostics;
 using Friflo.Json.Fliox;
-using static Friflo.Engine.ECS.Systems.SystemExtensions;
 using static System.Diagnostics.DebuggerBrowsableState;
 using Browse = System.Diagnostics.DebuggerBrowsableAttribute;
 // Hard Rule! file must not have any dependency a to a specific game engine. E.g. Unity, Godot, Monogame, ...
@@ -24,8 +22,7 @@ namespace Friflo.Engine.ECS.Systems
         [Browse(Never)][Ignore] public              bool            Enabled     { get => enabled; set => enabled = value; }
         [Browse(Never)]         public              int             Id          => id;
         [Browse(Never)]         public ref readonly SystemPerf      Perf        => ref perf;
-
-                                internal        View            System      => view ??= new View(this);
+                                internal            View            System      => view ??= new View(this);
         #endregion
             
     #region fields
@@ -218,43 +215,5 @@ namespace Friflo.Engine.ECS.Systems
             }
         }
         #endregion
-    }
-    
-    public struct SystemPerf
-    {
-        /// <remarks>Can be 0 in case execution time was below <see cref="Stopwatch.Frequency"/> precision.</remarks>
-        public          int     UpdateCount => updateCount;
-        public          double  LastMs      => lastTicks >= 0 ? lastTicks * StopwatchPeriodMs : -1;
-        public          double  SumMs       => sumTicks * StopwatchPeriodMs;
-
-        public override string  ToString() => $"updates: {UpdateCount} last: {LastMs:0.###} sum: {SumMs:0.###}";
-
-        [Ignore]    [Browse(Never)] internal            int     updateCount;
-        [Ignore]    [Browse(Never)] internal            long    lastTicks;
-        [Ignore]    [Browse(Never)] internal readonly   long[]  history;
-        [Ignore]    [Browse(Never)] internal            long    sumTicks;
-        
-        internal SystemPerf(long[] history) {
-            this.history = history;
-        }
-    }
-    
-    internal sealed class View
-    {
-        public  Tick                    Tick        => system.Tick;
-        public  int                     Id          => system.Id;
-        public  bool                    Enabled     => system.Enabled;
-        public  string                  Name        => system.Name;
-        public  SystemRoot              SystemRoot  => system.SystemRoot;
-        public  SystemGroup             ParentGroup => system.ParentGroup;
-        public  SystemPerf              Perf        => system.perf;
-
-        public override string          ToString()  => $"Enabled: {Enabled}  Id: {Id}";
-
-        [Browse(Never)] private readonly BaseSystem   system;
-        
-        internal View(BaseSystem system) {
-            this.system = system;
-        }
     }
 }
