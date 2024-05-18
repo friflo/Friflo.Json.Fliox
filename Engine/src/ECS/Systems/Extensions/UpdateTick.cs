5ec114196e39457f5c25ace6d9d0111b68e0c376
Ullrich Praetz <ullrich@praetz.de>
2024-05-18 13:25:23 +0200

    Engine - ECS: add UpdateTick docs

--- Engine/src/ECS/Systems/Extensions/UpdateTick.cs
+++ Engine/src/ECS/Systems/Extensions/UpdateTick.cs
@@ -3,18 +3,35 @@
 
 // Hard Rule! file must not have any dependency a to a specific game engine. E.g. Unity, Godot, Monogame, ...
 
+
+using Friflo.Engine.ECS.Systems;
+
 // ReSharper disable once CheckNamespace
 // ReSharper disable ConvertToPrimaryConstructor
 namespace Friflo.Engine.ECS;
 
-
+/// <summary>
+/// Specify <see cref="deltaTime"/> and <see cref="time"/> for system execution in <see cref="SystemGroup.Update"/>.
+/// </summary>
+/// <remarks>
+/// In case of Unity:<br/>
+/// <c>MonoBehaviour.Update()</c>:      <c>Time.deltaTime</c>,      <c>Time.time</c><br/> 
+/// <c>MonoBehaviour.LateUpdate()</c>:  <c>Time.deltaTime</c>,      <c>Time.time</c><br/> 
+/// <c>MonoBehaviour.FixedUpdate()</c>: <c>Time.fixedDeltaTime</c>, <c>Time.fixedTime</c><br/> 
+/// </remarks>
 public readonly struct UpdateTick
 {
+    /// <summary> The elapsed time since previous <see cref="SystemGroup.Update"/> execution. </summary>
     public readonly float deltaTime;
+    
+    /// <summary> The time at the beginning of the current frame. </summary>
     public readonly float time;
 
     public override string ToString() => $"deltaTime: {deltaTime}";
     
+    /// <summary>
+    /// Create a <see cref="UpdateTick"/> with the given <paramref name="deltaTime"/> and <paramref name="time"/>.
+    /// </summary>
     public UpdateTick(float deltaTime, float time) {
         this.deltaTime  = deltaTime;
         this.time       = time;
