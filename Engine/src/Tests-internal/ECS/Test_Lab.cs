f9f9d935fd63357f7ac7e31aaa0f3e205350a29d
Ullrich Praetz <ullrich@praetz.de>
2024-06-15 10:50:14 +0200

    Engine - ECS: IIndexedComponent<> PoC - wip

--- Engine/src/Tests-internal/ECS/Test_Lab.cs
+++ Engine/src/Tests-internal/ECS/Test_Lab.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Friflo.Engine.ECS;
 using NUnit.Framework;
 using static NUnit.Framework.Assert;
@@ -85,6 +87,38 @@ public static class Test_Lab
             AreEqual(target, attack.target);
         });
     }
+    
+    [Test]
+    public static void Test_AvoidBoxing()
+    {
+        var indexedName = new IndexedInt { value = 123 };
+        
+        var getter = CreateGetValue<IndexedInt,int>();
+        var value = getter(indexedName);
+        AreEqual(123, value);
+        
+        var start = GC.GetAllocatedBytesForCurrentThread();
+        getter(indexedName);
+        var diff = GC.GetAllocatedBytesForCurrentThread() - start;
+        Console.WriteLine($"diff: {diff}");
+    }
+    
+    // https://stackoverflow.com/questions/61185678/c-sharp-casting-t-where-t-struct-to-an-interface-without-boxing
+    private static GetIndexedValue<T,V> CreateGetValue<T,V>() where T : struct, IIndexedComponent<V>
+    {
+        const BindingFlags flags    = BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.InvokeMethod;
+        var method          = typeof(Test_Lab).GetMethod(nameof(GetIndexedComponentValue), flags);
+        var genericMethod   = method!.MakeGenericMethod(typeof(T), typeof(V));
+        
+        var genericDelegate = Delegate.CreateDelegate(typeof(GetIndexedValue<T,V>), genericMethod);
+        return (GetIndexedValue<T,V>)genericDelegate;
+    }
+    
+    internal static V GetIndexedComponentValue<T,V>(in T component) where T : struct, IIndexedComponent<V> {
+        return component.GetValue();
+    }
+    
+    internal delegate V GetIndexedValue<T, out V>(in T component) where T : struct, IIndexedComponent<V>;
 }
 
 }
