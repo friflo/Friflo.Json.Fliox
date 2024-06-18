083f91ae34b2b46e7015ac592c2bdacd9dca8f48
Ullrich Praetz <ullrich@praetz.de>
2024-06-18 16:21:52 +0200

    Engine - ECS: IIndexedComponent<> PoC - wip

--- Engine/src/Tests-internal/ECS/Test_Lab.cs
+++ Engine/src/Tests-internal/ECS/Test_Lab.cs
@@ -191,24 +191,24 @@ public static class Test_Lab
         var entity2 = world.CreateEntity(2);
         var entity3 = world.CreateEntity(3);
         
-        var entity4 = world.CreateEntity(4);
-        var entity5 = world.CreateEntity(5);
+        var target4 = world.CreateEntity(4);
+        var target5 = world.CreateEntity(5);
         
-        entity1.AddComponent(new IndexedEntity { entity = entity4 });
-        entity2.AddComponent(new IndexedEntity { entity = entity5 });
-        entity3.AddComponent(new IndexedEntity { entity = entity5 });
+        entity1.AddComponent(new IndexedEntity { entity = target4 });
+        entity2.AddComponent(new IndexedEntity { entity = target5 });
+        entity3.AddComponent(new IndexedEntity { entity = target5 });
         
-        var query1  = world.Query().HasValue<IndexedEntity,   Entity>(entity4);
-        var query2  = world.Query().HasValue<IndexedEntity,   Entity>(entity4).
-                                    HasValue<IndexedEntity,   Entity>(entity5);
+        var query1  = world.Query().HasValue<IndexedEntity,   Entity>(target4);
+        var query2  = world.Query().HasValue<IndexedEntity,   Entity>(target4).
+                                    HasValue<IndexedEntity,   Entity>(target5);
         
         AreEqual(new int [] { 1       },    query1.Entities.ToIds());
         AreEqual(new int [] { 1, 2, 3 },    query2.Entities.ToIds());
         
-        var references4 = entity4.GetForeignEntities<IndexedEntity>();
+        var references4 = target4.GetForeignEntities<IndexedEntity>();
         AreEqual(new int [] { 1 },          references4.Ids.ToArray());
         
-        var references5 = entity5.GetForeignEntities<IndexedEntity>();
+        var references5 = target5.GetForeignEntities<IndexedEntity>();
         AreEqual(new int [] { 2, 3 },       references5.Ids.ToArray());
     }
     
