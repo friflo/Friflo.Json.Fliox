f592417ad54a1027397d72b98565d539af69b980
Ullrich Praetz <ullrich@praetz.de>
2024-06-21 11:15:47 +0200

    Engine - Index: moved StoreIndex & ComponentIndexAttribute to individual files

---
+++ Engine/src/ECS/Lab/Component/ComponentIndexAttribute.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Friflo.Engine.ECS;
+
+[AttributeUsage(AttributeTargets.Struct)]
+internal sealed class ComponentIndexAttribute : Attribute
+{
+    // ReSharper disable once UnusedParameter.Local
+    public ComponentIndexAttribute(Type type) { }
+    
+    internal static Type GetComponentIndex(Type type)
+    {
+        foreach (var attr in type.CustomAttributes) {
+            if (attr.AttributeType != typeof(ComponentIndexAttribute)) {
+                continue;
+            }
+            var arg = attr.ConstructorArguments;
+            return (Type) arg[0].Value;
+        }
+        return null;
+    }
+}
+
---
+++ Engine/src/ECS/Lab/StoreIndex.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+// ReSharper disable once CheckNamespace
+namespace Friflo.Engine.ECS.Index;
+
+internal struct StoreIndex
+{
+    /// <summary> component index created on demand. </summary>
+    private             ComponentIndex          index;  //  8
+    internal readonly   IndexedComponentType    type;   // 24
+
+    public override string ToString() {
+        if (type.componentType == null) {
+            return null;
+        }
+        var name = type.componentType.Name;
+        if (index == null) {
+            return name;
+        }
+        return $"{name} - {index.GetType().Name} count: {index.Count}";
+    }
+
+    internal StoreIndex(IndexedComponentType type) {
+        this.type   = type;
+    }
+    
+    internal static ComponentIndex GetIndex(EntityStore store, int structIndex)
+    {
+        var index = store.extension.indexes[structIndex].index;
+        if (index != null) {
+            return index;
+        }
+        ref var storeIndex      = ref store.extension.indexes[structIndex];
+        return storeIndex.index = storeIndex.type.CreateComponentIndex(store);
+    }
+}
