d7583ef025b836a078f4499ee67174813f1fb6f6
Ullrich Praetz <ullrich@praetz.de>
2024-06-25 15:57:45 +0200

    typo

--- Engine/src/ECS/Index/IIndexedComponent.cs
+++ Engine/src/ECS/Index/IIndexedComponent.cs
@@ -6,7 +6,7 @@ namespace Friflo.Engine.ECS.Index;
 
 /// <summary>
 /// Is used to define a component type having a single indexed field / property.<br/>
-/// Components values can be queried with <c>HasValue()</c> in a <c>Query()</c> or with <c>ValueInRange()</c>
+/// Component values can be queried with <c>HasValue()</c> in a <c>Query()</c> or with <c>ValueInRange().</c>
 /// </summary>
 /// <remarks>
 /// This component type enables:
