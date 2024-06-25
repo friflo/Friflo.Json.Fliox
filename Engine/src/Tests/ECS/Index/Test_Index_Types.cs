d46334aebfa331470b8c86c062df156a2325075f
Ullrich Praetz <ullrich@praetz.de>
2024-06-25 13:44:48 +0200

    Engine - Tests: add Test_Index_Types assertions

--- Engine/src/Tests/ECS/Index/Test_Index_Types.cs
+++ Engine/src/Tests/ECS/Index/Test_Index_Types.cs
@@ -15,7 +15,8 @@ public static class Test_Index_Types
         var guid1 = Guid.Parse("00000000-0000-0000-0000-000000000000");
         var guid2 = Guid.Parse("10000000-0000-0000-0000-000000000000");
         
-        var store = new EntityStore();
+        var store  = new EntityStore();
+        var values = store.GetIndexedComponentValues<GuidComponent, Guid>();
         
         var query0 = store.Query().HasValue    <GuidComponent, Guid>(guid1);
         var query1 = store.Query().ValueInRange<GuidComponent, Guid>(guid1, guid1);
@@ -26,9 +27,11 @@ public static class Test_Index_Types
         entity1.AddComponent(new GuidComponent { guid = guid1 });
         AreEqual(1, query0.Count);
         AreEqual(1, query1.Count);
+        AreEqual(1, values.Count);
 
         entity2.AddComponent(new GuidComponent { guid = guid2 });
         AreEqual(1, query0.Count);
+        AreEqual(2, values.Count);
     }
     
     [Test]
@@ -37,6 +40,7 @@ public static class Test_Index_Types
         var date1 = new DateTime(2024, 6, 25);
         var date2 = new DateTime(2024, 6, 26);
         var store = new EntityStore();
+        var values = store.GetIndexedComponentValues<DateTimeComponent, DateTime>();
         
         var query1 = store.Query().HasValue    <DateTimeComponent, DateTime>(date1);
         var query2 = store.Query().ValueInRange<DateTimeComponent, DateTime>(date1, date1);
@@ -47,16 +51,19 @@ public static class Test_Index_Types
         entity1.AddComponent(new DateTimeComponent { dateTime = date1 });
         AreEqual(1, query1.Count);
         AreEqual(1, query2.Count);
+        AreEqual(1, values.Count);
 
         entity2.AddComponent(new DateTimeComponent { dateTime = date2 });
         AreEqual(1, query1.Count);
         AreEqual(1, query2.Count);
+        AreEqual(2, values.Count);
     }
     
     [Test]
     public static void Test_Index_Types_enum()
     {
         var store = new EntityStore();
+        var values = store.GetIndexedComponentValues<EnumComponent, MyEnum>();
 
         var query1 = store.Query().HasValue    <EnumComponent, MyEnum>(MyEnum.E1);
         
@@ -66,12 +73,15 @@ public static class Test_Index_Types
         
         entity1.AddComponent(new EnumComponent { value = MyEnum.E0 });
         AreEqual(0, query1.Count);
+        AreEqual(1, values.Count);
 
         entity2.AddComponent(new EnumComponent { value = MyEnum.E1 });
         AreEqual(1, query1.Count);
+        AreEqual(2, values.Count);
         
         entity3.AddComponent(new EnumComponent { value = MyEnum.E2 });
         AreEqual(1, query1.Count);
+        AreEqual(3, values.Count);
     }
 }
 
