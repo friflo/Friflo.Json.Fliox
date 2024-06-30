0afc35b657b0c129eddab7093c41a272b4955e36
Ullrich Praetz <ullrich@praetz.de>
2024-06-30 21:27:56 +0200

    Engine - Relation: removed unused import

--- Engine/src/ECS/Relations/Internal/EntityRelations.generic.cs
+++ Engine/src/ECS/Relations/Internal/EntityRelations.generic.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-
-using System;
 using System.Collections.Generic;
 using Friflo.Engine.ECS.Index;
 
