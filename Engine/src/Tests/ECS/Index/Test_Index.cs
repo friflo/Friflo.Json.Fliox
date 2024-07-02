3e584112b73fc5560973d28f408f9e8acbe917b0
Ullrich Praetz <ullrich@praetz.de>
2024-07-02 08:16:59 +0200

    Engine - Tests: separate Test_Index_Boxing

--- Engine/src/Tests/ECS/Index/Test_Index.cs
+++ Engine/src/Tests/ECS/Index/Test_Index.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Reflection;
 using Friflo.Engine.ECS;
 using NUnit.Framework;
 using Tests.Examples;
@@ -33,8 +31,6 @@ internal class IndexContext
     internal IReadOnlyCollection<int>       intValues;
 }
 
-[SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments")]
-[SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations")]
 public static partial class Test_Index
 {
     private static IndexContext Query_Setup()
@@ -407,38 +403,6 @@ public static partial class Test_Index
         Mem.AssertNoAlloc(start);
         Console.WriteLine($"Test_Index_Perf_Reference - count: {count} duration: {sw.ElapsedMilliseconds} ms");
     }
-
-    [Test]
-    public static void Test_AvoidBoxing()
-    {
-        var indexedName = new IndexedInt { value = 123 };
-        
-        var getter = CreateGetValue<IndexedInt,int>();
-        var value = getter(indexedName);
-        AreEqual(123, value);
-        
-        var start = GC.GetAllocatedBytesForCurrentThread();
-        getter(indexedName);
-        var diff = GC.GetAllocatedBytesForCurrentThread() - start;
-        Console.WriteLine($"diff: {diff}");
-    }
-    
-    // https://stackoverflow.com/questions/61185678/c-sharp-casting-t-where-t-struct-to-an-interface-without-boxing
-    private static GetIndexedValue<T,V> CreateGetValue<T,V>() where T : struct, IComponent
-    {
-        const BindingFlags flags    = BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.InvokeMethod;
-        var method          = typeof(Test_Index).GetMethod(nameof(GetIndexedComponentValue), flags);
-        var genericMethod   = method!.MakeGenericMethod(typeof(T), typeof(V));
-        
-        var genericDelegate = Delegate.CreateDelegate(typeof(GetIndexedValue<T,V>), genericMethod);
-        return (GetIndexedValue<T,V>)genericDelegate;
-    }
-    
-    internal static V GetIndexedComponentValue<T,V>(in T component) where T : struct, IIndexedComponent<V> {
-        return component.GetIndexedValue();
-    }
-    
-    private delegate V GetIndexedValue<T, out V>(in T component) where T : struct, IComponent;
 }
 
 }
---
+++ Engine/src/Tests/ECS/Index/Test_Index_Boxing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Friflo.Engine.ECS;
+using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+// ReSharper disable RedundantExplicitArrayCreation
+// ReSharper disable InconsistentNaming
+namespace Tests.ECS.Index {
+
+
+[SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments")]
+[SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations")]
+public static class Test_Index_Boxing
+{
+    [Test]
+    public static void Test_Index_Boxing_AvoidBoxing()
+    {
+        var indexedName = new IndexedInt { value = 123 };
+        
+        var getter = CreateGetValue<IndexedInt,int>();
+        var value = getter(indexedName);
+        AreEqual(123, value);
+        
+        var start = GC.GetAllocatedBytesForCurrentThread();
+        getter(indexedName);
+        var diff = GC.GetAllocatedBytesForCurrentThread() - start;
+        Console.WriteLine($"diff: {diff}");
+    }
+    
+    // https://stackoverflow.com/questions/61185678/c-sharp-casting-t-where-t-struct-to-an-interface-without-boxing
+    private static GetIndexedValue<T,V> CreateGetValue<T,V>() where T : struct, IComponent
+    {
+        const BindingFlags flags    = BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.InvokeMethod;
+        var method          = typeof(Test_Index_Boxing).GetMethod(nameof(GetIndexedComponentValue), flags);
+        var genericMethod   = method!.MakeGenericMethod(typeof(T), typeof(V));
+        
+        var genericDelegate = Delegate.CreateDelegate(typeof(GetIndexedValue<T,V>), genericMethod);
+        return (GetIndexedValue<T,V>)genericDelegate;
+    }
+    
+    internal static V GetIndexedComponentValue<T,V>(in T component) where T : struct, IIndexedComponent<V> {
+        return component.GetIndexedValue();
+    }
+    
+    private delegate V GetIndexedValue<T, out V>(in T component) where T : struct, IComponent;
+}
+
+}
