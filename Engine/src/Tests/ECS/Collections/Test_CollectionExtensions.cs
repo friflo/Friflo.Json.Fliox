ebd2c4591aa5d729f1f3e7f0818c14c61941339d
Ullrich Praetz <ullrich@praetz.de>
2024-07-06 00:14:32 +0200

    Engine - Tests: add Test_CollectionExtensions

---
+++ Engine/src/Tests/ECS/Collections/Test_CollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Friflo.Engine.ECS;
+using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+// ReSharper disable InconsistentNaming
+namespace Tests.ECS.Collections {
+
+public static class Test_CollectionExtensions
+{
+    [Test]
+    public static void Test_CollectionExtensions_Chunk(){
+        var chunk = new Chunk<Position>();
+        AreEqual("{ }", chunk.Debug());
+    }
+}
+
+}
