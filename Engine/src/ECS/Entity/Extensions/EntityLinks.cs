a847ada67098eeb8ad07e355c1a0dc80285752e3
Ullrich Praetz <ullrich@praetz.de>
2024-07-06 01:14:54 +0200

    Engine - ECS: CountAllOutgoingLinks() cleanup

--- Engine/src/ECS/Entity/Extensions/EntityLinks.cs
+++ Engine/src/ECS/Entity/Extensions/EntityLinks.cs
@@ -85,17 +85,16 @@ public static partial class EntityExtensions
 #region outgoing links
     public static int CountAllOutgoingLinks(this Entity entity)
     {
-        var schema = EntityStoreBase.Static.EntitySchema;
-        var linkComponents = new ComponentTypes();
-        linkComponents.bitSet = BitSet.Intersect(entity.archetype.componentTypes.bitSet, schema.linkComponentTypes.bitSet);
-        int count = linkComponents.Count;
-        
-        var store = entity.store;
-        var relationComponents = new ComponentTypes();
-        relationComponents.bitSet.l0 = store.nodes[entity.Id].isOwner & schema.linkRelationTypes.bitSet.l0;
-        foreach (var componentType in relationComponents) {
-            var relations = store.extension.relationsMap[componentType.StructIndex];
-            count += relations.GetRelationCount(entity);
+        var schema  = EntityStoreBase.Static.EntitySchema;
+        var types   = new ComponentTypes {
+            bitSet      = BitSet.Intersect(entity.archetype.componentTypes.bitSet, schema.linkComponentTypes.bitSet)
+        };
+        int count       = types.Count;
+        var store       = entity.store;
+        types.bitSet.l0 = store.nodes[entity.Id].isOwner & schema.linkRelationTypes.bitSet.l0;
+        foreach (var componentType in types) {
+            var relations   = store.extension.relationsMap[componentType.StructIndex];
+            count          += relations.GetRelationCount(entity);
         }
         return count;
     }
